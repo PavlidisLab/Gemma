028a8819a91ce594bec6d17c90e06537b483b832
keshav <>
2005-03-04 16:16:49 +0000

    Updated the bioAssayData package in Poseidon

---
+++ src/impl/edu/columbia/gemma/expression/bioAssayData/DesignElementDataVectorServiceImpl.java
@@ -0,0 +1,54 @@
+/*
+ * The Gemma project.
+ * 
+ * Copyright (c) 2005 Columbia University
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+/**
+ * This is only generated once! It will never be overwritten.
+ * You can (and have to!) safely modify it by hand.
+ */
+package edu.columbia.gemma.expression.bioAssayData;
+
+/**
+ * @see edu.columbia.gemma.expression.bioAssayData.DesignElementDataVectorService
+ */
+public class DesignElementDataVectorServiceImpl
+    extends edu.columbia.gemma.expression.bioAssayData.DesignElementDataVectorServiceBase
+{
+
+    /**
+     * @see edu.columbia.gemma.expression.bioAssayData.DesignElementDataVectorService#getDesignElementDataVectors()
+     */
+    protected java.util.List handleGetDesignElementDataVectors()
+        throws java.lang.Exception
+    {
+        //@todo implement protected java.util.List handleGetDesignElementDataVectors()
+        return null;
+    }
+
+    /**
+     * @see edu.columbia.gemma.expression.bioAssayData.DesignElementDataVectorService#saveDesignElementDataVector(edu.columbia.gemma.expression.bioAssayData.DesignElementDataVector)
+     */
+    protected void handleSaveDesignElementDataVector(edu.columbia.gemma.expression.bioAssayData.DesignElementDataVector designElementDataVector)
+        throws java.lang.Exception
+    {
+        //@todo implement protected void handleSaveDesignElementDataVector(edu.columbia.gemma.expression.bioAssayData.DesignElementDataVector designElementDataVector)
+        throw new java.lang.UnsupportedOperationException("edu.columbia.gemma.expression.bioAssayData.DesignElementDataVectorService.handleSaveDesignElementDataVector(edu.columbia.gemma.expression.bioAssayData.DesignElementDataVector designElementDataVector) Not implemented!");
+    }
+
+}
