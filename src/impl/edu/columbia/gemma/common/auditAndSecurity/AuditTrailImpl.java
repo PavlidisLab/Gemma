8f2c7461428a42e7014e05b4e3171c7b9e59e6e8
pavlidis <>
2005-03-08 20:09:08 +0000

    restoring my code

--- src/impl/edu/columbia/gemma/common/auditAndSecurity/AuditTrailImpl.java
+++ src/impl/edu/columbia/gemma/common/auditAndSecurity/AuditTrailImpl.java
@@ -18,79 +18,104 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  */
-/**
- * This is only generated once! It will never be overwritten.
- * You can (and have to!) safely modify it by hand.
- */
 package edu.columbia.gemma.common.auditAndSecurity;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * @see edu.columbia.gemma.common.auditAndSecurity.AuditTrail
+ * @author pavlidis
+ * @version $Id$
  */
-public class AuditTrailImpl
-    extends edu.columbia.gemma.common.auditAndSecurity.AuditTrail
-{
+public class AuditTrailImpl extends edu.columbia.gemma.common.auditAndSecurity.AuditTrail {
+
     /**
      * @see edu.columbia.gemma.common.auditAndSecurity.AuditTrail#addEvent(edu.columbia.gemma.common.auditAndSecurity.AuditEvent)
      */
-    public void addEvent(edu.columbia.gemma.common.auditAndSecurity.AuditEvent event)
-    {
-        //@todo implement public void addEvent(edu.columbia.gemma.common.auditAndSecurity.AuditEvent event)
-        throw new java.lang.UnsupportedOperationException("edu.columbia.gemma.common.auditAndSecurity.AuditTrail.addEvent(edu.columbia.gemma.common.auditAndSecurity.AuditEvent event) Not implemented!");
-    }
-
-    /**
-     * @see edu.columbia.gemma.common.auditAndSecurity.AuditTrail#getLast()
-     */
-    public edu.columbia.gemma.common.auditAndSecurity.AuditEvent getLast()
-    {
-        //@todo implement public edu.columbia.gemma.common.auditAndSecurity.AuditEvent getLast()
-        return null;
+    public void addEvent( AuditEvent event ) {
+        if ( event == null ) throw new IllegalArgumentException( "AuditEvent cannot be null" );
+        assert this.getEvents() != null;
+        this.getEvents().add( event );
     }
 
     /**
      * @see edu.columbia.gemma.common.auditAndSecurity.AuditTrail#getCreationEvent()
      */
-    public edu.columbia.gemma.common.auditAndSecurity.AuditEvent getCreationEvent()
-    {
-        //@todo implement public edu.columbia.gemma.common.auditAndSecurity.AuditEvent getCreationEvent()
-        return null;
+    public AuditEvent getCreationEvent() {
+        assert this.getEvents() != null;
+        return ( AuditEvent ) this.getEvents().get( 0 );
+    }
+
+    /**
+     * @see edu.columbia.gemma.common.auditAndSecurity.AuditTrail#getLast()
+     */
+    public AuditEvent getLast() {
+        assert this.getEvents() != null;
+        return ( AuditEvent ) this.getEvents().get( this.getEvents().size() - 1 );
     }
 
     /**
      * @see edu.columbia.gemma.common.auditAndSecurity.AuditTrail#start()
      */
-    public void start()
-    {
-        //@todo implement public void start()
-        throw new java.lang.UnsupportedOperationException("edu.columbia.gemma.common.auditAndSecurity.AuditTrail.start() Not implemented!");
+    public void start() {
+        this.start( null );
     }
 
     /**
      * @see edu.columbia.gemma.common.auditAndSecurity.AuditTrail#start(java.lang.String)
      */
-    public void start(java.lang.String note)
-    {
-        //@todo implement public void start(java.lang.String note)
-        throw new java.lang.UnsupportedOperationException("edu.columbia.gemma.common.auditAndSecurity.AuditTrail.start(java.lang.String note) Not implemented!");
+    public void start( String note ) {
+        this.initialize(); // ensure that this is the first event.
+        AuditEvent newEvent = AuditEvent.Factory.newInstance();
+        newEvent.setAction( AuditAction.CREATE );
+        newEvent.setDate( new Date() );
+        if ( note != null ) newEvent.setNote( note );
+
+        this.addEvent( newEvent );
     }
 
     /**
      * @see edu.columbia.gemma.common.auditAndSecurity.AuditTrail#update()
      */
-    public void update()
-    {
-        //@todo implement public void update()
-        throw new java.lang.UnsupportedOperationException("edu.columbia.gemma.common.auditAndSecurity.AuditTrail.update() Not implemented!");
+    public void update() {
+        this.update( null );
     }
 
     /**
      * @see edu.columbia.gemma.common.auditAndSecurity.AuditTrail#update(java.lang.String)
      */
-    public void update(java.lang.String note)
-    {
-        //@todo implement public void update(java.lang.String note)
-        throw new java.lang.UnsupportedOperationException("edu.columbia.gemma.common.auditAndSecurity.AuditTrail.update(java.lang.String note) Not implemented!");
+    public void update( String note ) {
+        assert this.getEvents() != null;
+        assert this.getEvents().size() > 0; // can't have update as the first
+        // event.
+
+        AuditEvent newEvent = AuditEvent.Factory.newInstance();
+        newEvent.setAction( AuditAction.UPDATE );
+        newEvent.setDate( new Date() );
+        if ( note != null ) newEvent.setNote( note );
+
+        this.addEvent( newEvent );
+    }
+
+    /**
+     * If this AuditTrail's list is empty or null, initialize it. Otherwise, clear any events.
+     */
+    private void initialize() {
+        if ( trailIsNull() ) {
+            this.setEvents( new ArrayList() );
+        } else {
+            this.getEvents().clear();
+        }
+    }
+
+    /**
+     * Check whether the list of events doesn't exist yet.
+     * 
+     * @return
+     */
+    private boolean trailIsNull() {
+        return this.getEvents() == null;
     }
 
 }
