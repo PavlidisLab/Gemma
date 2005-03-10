3c0c8434116ba9d18e22e499b007749b79ecbcd7
pavlidis <>
2005-03-10 16:46:12 +0000

    change due to difference in DatabaseEntry.

--- test/edu/columbia/gemma/common/description/BibliographicReferenceDaoImplTest.java
+++ test/edu/columbia/gemma/common/description/BibliographicReferenceDaoImplTest.java
@@ -18,48 +18,48 @@ import edu.columbia.gemma.BaseDAOTestCase;
  */
 public class BibliographicReferenceDaoImplTest extends BaseDAOTestCase {
 
-   private BibliographicReferenceDao dao = null;
-   private DatabaseEntryDao dedao = null;
+    private BibliographicReferenceDao dao = null;
+    private DatabaseEntryDao dedao = null;
 
-   protected void setUp() throws Exception {
-      Log log = LogFactory.getLog( BibliographicReferenceDaoImplTest.class );
-      dao = ( BibliographicReferenceDao ) ctx.getBean( "bibliographicReferenceDao" );
-      dedao = ( DatabaseEntryDao ) ctx.getBean( "databaseEntryDao" );
-   }
+    protected void setUp() throws Exception {
+        Log log = LogFactory.getLog( BibliographicReferenceDaoImplTest.class );
+        dao = ( BibliographicReferenceDao ) ctx.getBean( "bibliographicReferenceDao" );
+        dedao = ( DatabaseEntryDao ) ctx.getBean( "databaseEntryDao" );
+    }
 
-   protected void tearDown() throws Exception {
-      dao = null;
-   }
+    protected void tearDown() throws Exception {
+        dao = null;
+    }
 
-   /*
-    * Class under test for Object findByExternalId(int, java.lang.String)
-    */
-   public final void testFindByExternalIdintString() {
+    /*
+     * Class under test for Object findByExternalId(int, java.lang.String)
+     */
+    public final void testFindByExternalIdintString() {
 
-      BibliographicReference f = new BibliographicReferenceImpl();
+        BibliographicReference f = BibliographicReference.Factory.newInstance();
 
-      String random = ( new Date() ).toString();
+        String random = ( new Date() ).toString();
 
-      DatabaseEntry de = ( DatabaseEntry ) dedao.create( "2", "", "", ( new Date() ).toString(), "foo" );
-      DatabaseEntry deb = ( DatabaseEntry ) dedao.create( "3", "", "", ( new Date() ).toString(), "foo" );
+        DatabaseEntry de = dedao.create( "", ( new Date() ).toString(), "foo" );
+        DatabaseEntry deb = dedao.create( "", ( new Date() ).toString(), "foo" );
 
-      f.setPubAccession( de );
-      f.setIdentifier( random );
-      dao.create( f );
+        f.setPubAccession( de );
+        f.setIdentifier( random );
+        dao.create( f );
 
-      f = dao.findByExternalId( de );
+        f = dao.findByExternalId( de );
 
-      assertTrue( f != null );
-      assertTrue( dao.findByExternalId( deb ) == null );
+        assertTrue( f != null );
+        assertTrue( dao.findByExternalId( deb ) == null );
 
-      try {
-         dao.create( f );
-         fail( "Create didn't throw DataIntegrityViolationException" );
-      } catch ( DataIntegrityViolationException e ) {
-         assertNotNull( e );
-         log.debug( "Good, expected exception: " + e.getMessage() );
-      }
+        try {
+            dao.create( f );
+            fail( "Create didn't throw DataIntegrityViolationException" );
+        } catch ( DataIntegrityViolationException e ) {
+            assertNotNull( e );
+            log.debug( "Good, expected exception: " + e.getMessage() );
+        }
 
-   }
+    }
 
 }
