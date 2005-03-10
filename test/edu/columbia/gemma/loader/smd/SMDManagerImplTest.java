c5d784f6760b83cb7e9d2c89c2f89aec703ba965
pavlidis <>
2005-03-10 00:51:46 +0000

    smd tests

---
+++ test/edu/columbia/gemma/loader/smd/SMDManagerImplTest.java
@@ -0,0 +1,36 @@
+package edu.columbia.gemma.loader.smd;
+
+import junit.framework.TestCase;
+
+/**
+ * 
+ *
+ * <hr>
+ * <p>Copyright (c) 2004 Columbia University
+ * @author pavlidis
+ * @version $Id$
+ */
+public class SMDManagerImplTest extends TestCase {
+
+   /*
+    * @see TestCase#setUp()
+    */
+   protected void setUp() throws Exception {
+      super.setUp();
+   }
+
+   /*
+    * @see TestCase#tearDown()
+    */
+   protected void tearDown() throws Exception {
+      super.tearDown();
+   }
+
+   /*
+    * Class under test for Set fetchSMDPublications()
+    */
+   public void testFetchSMDPublications() {
+      //TODO Implement fetchSMDPublications().
+   }
+
+}
