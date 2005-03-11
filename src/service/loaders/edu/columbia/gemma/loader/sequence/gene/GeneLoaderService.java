9efda12d8bd9ef24d3b4695101f2da058d8cdfc6
keshav <>
2005-03-11 15:12:54 +0000

    A Service.  These are hand coded (for now)

--- src/service/loaders/edu/columbia/gemma/loader/sequence/gene/GeneLoaderService.java
+++ src/service/loaders/edu/columbia/gemma/loader/sequence/gene/GeneLoaderService.java
@@ -1,8 +1,7 @@
 /*
- * TODO use the findByName() to check if an entry already exists.
- * TODO factor out potential common logic (ie. parseFile)
- * TODO remove hard-coded path of file.  This was done just to get gene data in db.
- * TODO add assertions (see coding standards email RE: Paul Pavlidis Sun 3/6/2005 10:55 PM
+ * TODO use the findByName() to check if an entry already exists. TODO factor out potential common logic (ie. parseFile)
+ * TODO remove hard-coded path of file. This was done just to get gene data in db. TODO add assertions (see coding
+ * standards email RE: Paul Pavlidis Sun 3/6/2005 10:55 PM
  */
 
 package edu.columbia.gemma.loader.sequence.gene;
@@ -10,15 +9,31 @@ package edu.columbia.gemma.loader.sequence.gene;
 //Parser packages.
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.time.StopWatch;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
+import net.sf.hibernate.HibernateException;
+import net.sf.hibernate.MappingException;
+import net.sf.hibernate.SessionFactory;
+import net.sf.hibernate.cfg.Configuration;
+import net.sf.hibernate.cfg.Settings;
+import net.sf.hibernate.impl.SessionFactoryImpl;
+import org.springframework.orm.hibernate.HibernateTemplate;
+
+import util.SmdUtil;
+import edu.columbia.gemma.sequence.gene.Gene;
 import edu.columbia.gemma.sequence.gene.GeneDao;
+import edu.columbia.gemma.sequence.gene.GeneDaoImpl;
 import edu.columbia.gemma.sequence.gene.GeneImpl;
+
+
 
 /**
  * <hr>
@@ -29,93 +44,79 @@ import edu.columbia.gemma.sequence.gene.GeneImpl;
  * @version $Id$
  */
 public class GeneLoaderService {
-   private static int count = 0;
-   private BufferedReader br;
+ //  private String filename = "C:\\Documents and Settings\\keshav\\My Documents\\Gemma\\Database Files\\gene.txt";
+   protected static final Log log = LogFactory.getLog( GeneLoaderService.class );
    private GeneDao geneDao;
-   private BufferedReader in;
+
+   public GeneLoaderService(GeneDao gd) {
+      this.geneDao = gd;
+   }
+   
+   /**
+    * 
+    */
+   public GeneLoaderService() {
+      
+      // TODO Auto-generated constructor stub
+   }
 
    protected GeneDao getGeneDao() {
       return this.geneDao;
    }
 
    /**
-    * 
-    * 
+    * @throws IOException
     */
-   public void loadDatabase() {
-      parseFile();
+   public void loadDatabase( String inputFilename ) throws IOException {
+      //  parseFile();
+      saveGenes( openFileAsStream( inputFilename ) );
    }
+   
 
    /**
-    *
-    *
+    * @param fileName TODO
+    * @return TODO
+    * @throws IOException
     */
-   public void parseFile() {
+   public InputStream openFileAsStream( String fileName ) throws IOException {
       //Initialization
-      boolean fileExists = false;
-      String filename = "C:\\Documents and Settings\\keshav\\My Documents\\Gemma\\Database Files\\gene.txt";
-      in = new BufferedReader( new InputStreamReader( System.in ) );
-      try {
-         File file = new File( filename );
-         br = new BufferedReader( new FileReader( file ) );
-         fileExists = true;
-      } catch ( Exception e ) {
-         System.out.println( "Cannot find file." );
-         fileExists = false;
-      }
+ 
+      File file = new File( fileName );
+      if ( !file.canRead() )
+            throw new IOException( "Can't read from file " + fileName );
 
-      System.out.println( "\nThe filename is " + filename );
-      StopWatch stopWatch = new StopWatch();
-      stopWatch.start();
-      try {
-         //         Resource resource = new ClassPathResource( "tools/remotingclient.xml" );
-         //         BeanFactory bf = new XmlBeanFactory( resource );
-         //         GeneManagerInterface gmi = ( GeneManagerInterface ) bf
-         //               .getBean( "geneManagerProxy-hessian" );
+      return new FileInputStream( file );
 
-         System.out.println( " Reading file ... " );
-         saveGene();
-         stopWatch.stop();
-         in.close();
-         br.close();
-         System.out.println( " Time taken = " + stopWatch.getTime() + " ms" );
-      } catch ( IOException ioe ) {
-         ioe.printStackTrace();
-      } catch ( Exception e ) {
-         // TODO Auto-generated catch block
-         e.printStackTrace();
-      }
    }
 
    /**
-    * 
-    *
+    * @param is TODO
+    * @throws IOException
     */
-   public void saveGene() {
-      String[] sArray = null;
+   public void saveGenes( InputStream is ) throws IOException {
+
+      BufferedReader br = new BufferedReader( new InputStreamReader( is ) );
+
+      int maxColIndex = 4;
+
       String line = null;
-      long totalLinesProcessed = 0l;
-      GeneImpl g = new GeneImpl();
-      int maxCol = 8;
-      try {
-         while ( ( line = br.readLine() ) != null ) {
-            totalLinesProcessed++;
-            sArray = StringUtils.split( line, "\t" );
-            //System.err.println("sArray Length: " + sArray.length);
-            g.setIdentifier( "gene::" + count + "::" + sArray[0] );
-            g.setSymbol( sArray[0] );
-            g.setOfficialName( sArray[1] );
-            g.setPhysicalMapLocation( sArray[4] );
-            if ( sArray.length > maxCol && sArray[7] == "LocusLink" ) {
-               g.setNcbiId( Integer.parseInt( sArray[8] ) );
-            }
-            getGeneDao().create( g );
-            count++;
+      int count = 0;
+      while ( ( line = br.readLine() ) != null ) {
+         String[] sArray = line.split( "\t", 8 );//StringUtils.split( line, "\t" );
+         Gene g = Gene.Factory.newInstance();
+         g.setIdentifier( "gene::" + count + "::" + sArray[0] );
+         g.setSymbol( sArray[1] );
+         g.setOfficialName( sArray[2] );
+         g.setPhysicalMapLocation( Integer.parseInt(sArray[5]) );
+         if ( sArray.length > maxColIndex && sArray[7] == "LocusLink" ) {
+            g.setNcbiId( Integer.parseInt(sArray[8]) );
          }
-         System.out.println( " Objects persisted successfully ... " );
-      } catch ( IOException e ) {
-         e.printStackTrace();
+         getGeneDao().create( g );
+         count++;
       }
+      log.info( " Objects persisted successfully ... " );
+
+      br.close();
    }
 
    /**
@@ -124,5 +125,33 @@ public class GeneLoaderService {
    public void setGeneDao( GeneDao geneDao ) {
       this.geneDao = geneDao;
    }
+   
+   public static void main(String[] args) {
+      
+      try {
+         GeneDaoImpl f =  new GeneDaoImpl();
+         
+         Configuration cfg = new Configuration();
+         cfg.addInputStream(GeneLoaderService.class.getResourceAsStream("/hibernate.cfg.xml"));
+         Settings stg = new Settings();
+         SessionFactory sf = new SessionFactoryImpl(cfg, stg);
+         HibernateTemplate h = new HibernateTemplate();
+         h.setSessionFactory(sf);
+         f.setHibernateTemplate(h);
+         GeneLoaderService gls = new GeneLoaderService();
+         gls.setGeneDao(f);
+         gls.loadDatabase("C:\\Documents and Settings\\keshav\\My Documents\\Gemma\\Database Files\\gene.txt");
+      } catch ( IOException e ) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      } catch ( MappingException e ) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      } catch ( HibernateException e ) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+   }
+   
 }
 
