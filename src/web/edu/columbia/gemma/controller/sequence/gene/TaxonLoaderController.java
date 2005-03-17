f7eec0c6f6e4aee3e67287ff580095bfbb8601e3
keshav <>
2005-03-17 15:39:28 +0000

    Updated TaxonLoaderController.  It works, but still some TODO's to make it usable.

--- src/web/edu/columbia/gemma/controller/sequence/gene/TaxonLoaderController.java
+++ src/web/edu/columbia/gemma/controller/sequence/gene/TaxonLoaderController.java
@@ -5,13 +5,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
 
-import edu.columbia.gemma.loader.sequence.gene.GeneLoaderService;
+import edu.columbia.gemma.common.auditAndSecurity.Person;
+import edu.columbia.gemma.common.auditAndSecurity.PersonImpl;
+import edu.columbia.gemma.loader.sequence.gene.FileName;
 import edu.columbia.gemma.loader.sequence.gene.TaxonLoaderService;
 
 /**
@@ -20,36 +24,43 @@ import edu.columbia.gemma.loader.sequence.gene.TaxonLoaderService;
  * Copyright (c) 2004 Columbia University
  * 
  * @author keshav
- * @version $Id keshav $
+ * @version $Id$
  */
 public class TaxonLoaderController extends SimpleFormController {
    private TaxonLoaderService taxonLoaderService;
    /** Logger for this class and subclasses */
    protected final Log logger = LogFactory.getLog( getClass() );
-
-   /**
-    * @return
-    */
-   public TaxonLoaderService getTaxonLoaderService() {
-      return taxonLoaderService;
-   }
-
-   /**
-    * @param
-    * @return
-    */
-   public ModelAndView onSubmit( Object command ) throws IOException, ServletException {
-      String now = ( new java.util.Date() ).toString();
-      logger.info( "returning hello view with " + now );
-      Map myModel = new HashMap();
-      myModel.put( "now", now );
-      getTaxonLoaderService().loadDatabase();
-      return new ModelAndView( "taxon", "model", myModel );
-   }
+   
    /**
     * @param
     */
    public void setTaxonLoaderService( TaxonLoaderService tls ) {
       this.taxonLoaderService = tls;
    }
+   
+   /**
+    * @return
+    */
+   public TaxonLoaderService getTaxonLoaderService() {
+      return taxonLoaderService;
+   }
+   
+   /**
+    * @param
+    * @return
+    */
+   public ModelAndView onSubmit(Object command) throws IOException, ServletException {
+      //String filename = null; 
+      String filename = ( ( FileName ) command ).getFileName();
+      Map myModel = new HashMap();
+      getTaxonLoaderService().bulkCreate(filename,true);
+      return new ModelAndView( "taxon", "model", myModel );
+   }
+   
+   protected Object formBackingObject( HttpServletRequest request ) throws Exception {
+        FileName fileName = new FileName();
+        fileName.setFileName("Taxon.txt");
+        return fileName;
+    }
+   
 }
