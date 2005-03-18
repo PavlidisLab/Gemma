32e2f4cee41c2ea12779a32f1386cbddaebaaf3f
keshav <>
2005-03-18 14:18:46 +0000

    Added filename to  be used by the controllers.

---
+++ src/impl/edu/columbia/gemma/loader/sequence/gene/FileName.java
@@ -0,0 +1,27 @@
+package edu.columbia.gemma.loader.sequence.gene;
+
+/**
+ * 
+ *
+ * <hr>
+ * <p>Copyright (c) 2004 - 2005 Columbia University
+ * @author keshav
+ * @version $Id$
+ */
+public class FileName {
+  private String fileName;
+  
+  
+/**
+ * @return Returns the fileName.
+ */
+public String getFileName() {
+    return fileName;
+}
+/**
+ * @param fileName The fileName to set.
+ */
+public void setFileName( String fileName ) {
+    this.fileName = fileName;
+}
+}
