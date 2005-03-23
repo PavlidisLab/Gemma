1f8591eca66457da3ce504a342cbe1947395bcb4
pavlidis <>
2005-03-23 00:39:57 +0000

    test for IdentifierCreator

---
+++ test/edu/columbia/gemma/loader/loaderutils/IdentifierCreatorTest.java
@@ -0,0 +1,46 @@
+package edu.columbia.gemma.loader.loaderutils;
+
+import edu.columbia.gemma.BaseDAOTestCase;
+import edu.columbia.gemma.common.Identifiable;
+import edu.columbia.gemma.expression.arrayDesign.ArrayDesign;
+import junit.framework.TestCase;
+
+/**
+ * <hr>
+ * <p>
+ * Copyright (c) 2004-2005 Columbia University
+ * 
+ * @author pavlidis
+ * @version $Id$
+ */
+public class IdentifierCreatorTest extends BaseDAOTestCase {
+
+    Identifiable a;
+    Identifiable c;
+    Identifiable d;
+    Identifiable e;
+    Identifiable f;
+
+    /*
+     * @see TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+        a = ArrayDesign.Factory.newInstance();
+        a.setName( "Foo" );
+    }
+
+    /*
+     * @see TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public final void testCreate() {
+        String actualReturn = IdentifierCreator.create( a, ctx );
+        String expectedReturn = "ArrayDesign:Foo";
+        assertEquals( expectedReturn, actualReturn );
+    }
+
+}
