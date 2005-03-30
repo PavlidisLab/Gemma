ecb5021f7058ddaf8ff88b9099cc7b72088d2440
keshav <>
2005-03-30 15:35:44 +0000

    added

---
+++ src/impl/edu/columbia/gemma/externalDb/ExternalDatabaseDaoHg17.java
@@ -0,0 +1,21 @@
+package edu.columbia.gemma.externalDb;
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+import net.sf.hibernate.HibernateException;
+
+/**
+ * 
+ *
+ * <hr>
+ * <p>Copyright (c) 2004 - 2005 Columbia University
+ * @author keshav
+ * @version $Id$
+ */
+public interface ExternalDatabaseDaoHg17 {
+    
+    public boolean connectToDatabase() throws HibernateException, SQLException;
+    
+    public Collection retreiveFromDatabase() throws HibernateException, SQLException;
+}
