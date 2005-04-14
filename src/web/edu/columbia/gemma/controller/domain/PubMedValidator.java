beae4d12a4f25ae3bf2aad4d4ac119baaf91d07a
keshav <>
2005-04-14 16:16:24 +0000

    Added Pavlidis style

--- src/web/edu/columbia/gemma/controller/domain/PubMedValidator.java
+++ src/web/edu/columbia/gemma/controller/domain/PubMedValidator.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2002-2004 the original author or authors.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 package edu.columbia.gemma.controller.domain;
 
 import org.springframework.validation.Errors;
@@ -20,13 +5,22 @@ import org.springframework.validation.Validator;
 
 import edu.columbia.gemma.common.description.BibliographicReference;
 import edu.columbia.gemma.common.description.BibliographicReferenceImpl;
-
+/**
+ * Validating object for BibliographicReference
+ * 
+ *
+ * <hr>
+ * <p>Copyright (c) 2004 - 2005 Columbia University
+ * @author keshav
+ * @version $Id$
+ */
 public class PubMedValidator implements Validator {
 
 	public boolean supports(Class clazz) {
 		return clazz.equals(BibliographicReferenceImpl.class);
 	}
-
+//TODO either replace this with a BibliographicReferenceServiceQueryValidator 
+//(yes, it should be "...Service...") or add the pubMedId to the POJO BibliographicReference.	
 	public void validate(Object obj, Errors errors) {
 	    System.err.println("Object obj is: "+ obj.toString());
 	    BibliographicReference query = (BibliographicReference)obj;
