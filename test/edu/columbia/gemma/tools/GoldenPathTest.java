fb04938146913349593b0bd015d992d8bd9e2b23
pavlidis <>
2005-04-21 01:15:17 +0000

    moved; fixed test expected value problems.

---
+++ test/edu/columbia/gemma/tools/GoldenPathTest.java
@@ -0,0 +1,147 @@
+package edu.columbia.gemma.tools;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import edu.columbia.gemma.genome.Gene;
+import edu.columbia.gemma.tools.GoldenPath;
+import edu.columbia.gemma.tools.GoldenPath.ThreePrimeData;
+
+import junit.framework.TestCase;
+
+/**
+ * <hr>
+ * <p>
+ * Copyright (c) 2004-2005 Columbia University
+ * 
+ * @author pavlidis
+ * @version $Id$
+ */
+public class GoldenPathTest extends TestCase {
+    protected static final Log log = LogFactory.getLog( GoldenPathTest.class );
+    GoldenPath gp;
+
+    /*
+     * @see TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+        gp = new GoldenPath( 3306, "hg17", "localhost", "pavlidis", "toast" );
+    }
+
+    /*
+     * @see TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        gp = null;
+    }
+
+    public final void testFindRefGenesByLocation() {
+        List actualResult = gp.findRefGenesByLocation( "11", 100000, 300000, null );
+        Collections.sort( actualResult, new Comparator() {
+            public int compare( Object a, Object b ) {
+                Gene ga = ( Gene ) a;
+                Gene gb = ( Gene ) b;
+                return ga.getOfficialSymbol().compareTo( gb.getOfficialSymbol() );
+            };
+        } );
+        for ( Iterator iter = actualResult.iterator(); iter.hasNext(); ) {
+            Gene gene = ( Gene ) iter.next();
+            assertEquals( "BET1L", gene.getOfficialSymbol() );
+            break;
+        }
+
+    }
+
+    // TODO: put hard-coded values in an external file. These are based on the may 2004 hg17.
+    // These locations on chromosome 11 were just chosen at random.
+    // http://genome.ucsc.edu/cgi-bin/hgTracks?hgsid=41239384&hgt.out1=1.5x&position=chr11%3A206144-206244
+    // http://genome.ucsc.edu/cgi-bin/hgTracks?hgsid=41239384&hgt.out3=10x&position=chr11%3A1439902-1439903
+    public final void testGetThreePrimeDistanceA() {
+
+        // gene >>>>, location contained within the gene.
+        int location = 1439902 - 100;
+        int actualResult = ( ( ThreePrimeData ) gp.getThreePrimeDistances( "11", location, location + 2, null, null,
+                null ).get( 0 ) ).getDistance();
+        int expectedResult = 100 - 2;
+        assertEquals( expectedResult, actualResult );
+
+    }
+
+    public final void testGetThreePrimeDistanceB() {
+        // gene >>>>, location overhangs
+        int location = 1439902 - 100;
+        int actualResult = ( ( ThreePrimeData ) gp.getThreePrimeDistances( "11", location, location + 200, null, null,
+                "+" ).get( 0 ) ).getDistance();
+        int expectedResult = 0;
+        assertEquals( expectedResult, actualResult );
+    }
+
+    public final void testGetThreePrimeDistanceC() {
+        // gene >>>>, location does not overlap
+        int location = 1439902 - 100;
+
+        List actualResultL = gp.getThreePrimeDistances( "11", location + 101, location + 200, null, null, "+" );
+        List expectedResultL = null;
+        assertEquals( expectedResultL, actualResultL );
+    }
+
+    public final void testGetThreePrimeDistanceD() {
+        // gene >>>>, location does not overlap but barely missed (test for off-by-one)
+        int location = 1439902 - 100;
+        List actualResultL = gp.getThreePrimeDistances( "11", location + 100, location + 200, null, null, "+" );
+        List expectedResultL = null;
+        assertEquals( expectedResultL, actualResultL );
+    }
+
+    public final void testGetThreePrimeDistanceE() {
+        // Gene in <<<<<< direction.
+        int location = 206144 + 100;
+        int actualResult = ( ( ThreePrimeData ) gp.getThreePrimeDistances( "11", location + 100, location + 200, null,
+                null, "-" ).get( 0 ) ).getDistance();
+        int expectedResult = 206; // this is a funny case, as it turns out.
+        assertEquals( expectedResult, actualResult );
+    }
+
+    public final void testGetThreePrimeDistanceF() {
+        // gene <<<<<, location overhangs on left.
+        int location = 206144 + 100;
+        int actualResult = ( ( ThreePrimeData ) gp.getThreePrimeDistances( "11", location - 200, location + 200, null,
+                null, "-" ).get( 0 ) ).getDistance();
+        int expectedResult = 0;
+        assertEquals( expectedResult, actualResult );
+    }
+
+    public final void testGetThreePrimeDistanceG() {
+        // gene <<<<<, no overlap
+        int location = 206144 + 100;
+        List actualResultL = gp.getThreePrimeDistances( "11", location - 200, location - 199, null, null, "+" );
+        List expectedResultL = null;
+        assertEquals( expectedResultL, actualResultL );
+    }
+
+    public final void testGetThreePrimeDistanceH() {
+        // gene <<<<<, region contains gene entirely
+        int location = 206144; // start of the gene.
+        int actualResult = ( ( ThreePrimeData ) gp.getThreePrimeDistances( "11", location - 200, location + 10000,
+                null, null, "-" ).get( 0 ) ).getDistance();
+        int expectedResult = 0;
+        assertEquals( expectedResult, actualResult );
+    }
+
+    public final void testGetThreePrimeDistanceI() {
+        // gene <<<<<, region contains more than one gene. We allow this.
+        int location = 206144; // start of the gene.
+        int actualResult = gp.getThreePrimeDistances( "11", location - 200, location + 100000, null, null, "-" ).size();
+        int expectedResult = 3; // number of genes detected
+        assertEquals( expectedResult, actualResult );
+
+    }
+}
