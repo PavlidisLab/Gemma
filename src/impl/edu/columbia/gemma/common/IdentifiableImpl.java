3a64f0c28b43f5010f6f0607186d36b98293b0a4
pavlidis <>
2005-04-24 14:44:34 +0000

    removing functionality

--- src/impl/edu/columbia/gemma/common/IdentifiableImpl.java
+++ src/impl/edu/columbia/gemma/common/IdentifiableImpl.java
@@ -24,58 +24,10 @@
  */
 package edu.columbia.gemma.common;
 
-import org.apache.commons.lang.builder.HashCodeBuilder;
-import org.apache.commons.lang.builder.ToStringBuilder;
 
 /**
  * @see edu.columbia.gemma.common.Identifiable
  */
 public class IdentifiableImpl extends edu.columbia.gemma.common.Identifiable {
-
-    /**
-     * @see edu.columbia.gemma.common.Identifiable#compareTo(java.lang.Object)
-     */
-    public int compareTo( java.lang.Object object ) {
-        if ( !( object instanceof Identifiable ) ) return -1;
-        Identifiable myClass = ( Identifiable ) object;
-        return myClass.getIdentifier().compareTo( this.getIdentifier() );
-
-    }
-
-    /**
-     * @see edu.columbia.gemma.common.Identifiable#equals(java.lang.Object)
-     */
-    public boolean equals( java.lang.Object object ) {
-        if ( !( object instanceof Identifiable ) ) {
-            return false;
-        }
-        Identifiable rhs = ( Identifiable ) object;
-        return rhs.getIdentifier().equals( this.getIdentifier() );
-
-    }
-
-    /* (non-Javadoc)
-     * @see edu.columbia.gemma.common.Identifiable#findByIdentifier(java.lang.String)
-     */
-    public Identifiable findByIdentifier( String identifier ) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    /**
-     * @see edu.columbia.gemma.common.Identifiable#hashCode()
-     */
-    public int hashCode() {
-
-        return new HashCodeBuilder( -1867551415, -168940413 ).append( this.getIdentifier().hashCode() ).toHashCode();
-    }
-    /**
-     * @see edu.columbia.gemma.common.Identifiable#toString()
-     */
-    public java.lang.String toString() {
-        return new ToStringBuilder( this ).append( "name", this.getName() ).append( "identifier", this.getIdentifier() )
-                .toString();
-
-    }
 
 }
