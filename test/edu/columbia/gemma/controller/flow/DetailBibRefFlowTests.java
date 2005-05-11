ccfdb56596104f5fc81c570c487b63bbf05ea23a
keshav <>
2005-05-11 15:36:43 +0000

    extends BaseFlowTestCase

--- test/edu/columbia/gemma/controller/flow/DetailBibRefFlowTests.java
+++ test/edu/columbia/gemma/controller/flow/DetailBibRefFlowTests.java
@@ -1,19 +1,9 @@
 package edu.columbia.gemma.controller.flow;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.ResourceBundle;
-
-import org.easymock.MockControl;
-import org.springframework.mock.web.flow.MockRequestContext;
-import org.springframework.test.web.flow.AbstractFlowExecutionTests;
-import org.springframework.web.flow.Event;
 import org.springframework.web.flow.SimpleEvent;
 import org.springframework.web.flow.ViewDescriptor;
 
-import edu.columbia.gemma.common.description.BibliographicReferenceImpl;
-import edu.columbia.gemma.common.description.BibliographicReferenceService;
-import edu.columbia.gemma.web.controller.flow.action.GetPubMedAction;
+import edu.columbia.gemma.BaseFlowTestCase;
 
 /**
  * Tests the pubMedSearch-flow.
@@ -24,7 +14,7 @@ import edu.columbia.gemma.web.controller.flow.action.GetPubMedAction;
  * @author keshav
  * @version $Id$
  */
-public class DetailBibRefFlowTests extends AbstractFlowExecutionTests {
+public class DetailBibRefFlowTests extends BaseFlowTestCase {
     /**
      * 
      *
@@ -36,12 +26,12 @@ public class DetailBibRefFlowTests extends AbstractFlowExecutionTests {
     /**
      * Test criteria.view with an invalid argument. Expect to return to the criteria.view.
      */
-    //TODO add validator object. Fail on validation should take me back to the criteria.view.
-    //    public void testCriteriaView_Submit_Error() {
-    //        startFlow();
-    //        ViewDescriptor view = signalEvent( new SimpleEvent( this, "submit", null ) );
-    //        assertCurrentStateEquals( "criteria.view" );
-    //    }
+    // TODO add validator object. Fail on validation should take me back to the criteria.view.
+    // public void testCriteriaView_Submit_Error() {
+    // startFlow();
+    // ViewDescriptor view = signalEvent( new SimpleEvent( this, "submit", null ) );
+    // assertCurrentStateEquals( "criteria.view" );
+    // }
     /**
      * Test criteria.view with a valid argument. Expect the results.view to be entered. Expect 1 bibliographic reference
      * to be returned from 1 pubMed Id.
@@ -49,11 +39,11 @@ public class DetailBibRefFlowTests extends AbstractFlowExecutionTests {
     public void testCriteriaView_Submit_getBibRef_Success() {
         startFlow();
         ViewDescriptor view = signalEvent( new SimpleEvent( this, "getBibRef" ) );
-        
+
         assertCurrentStateEquals( "bibRef.results.view" );
-        //asserts().assertCollectionAttributeSize( view, "bibliographicReferences", 7 );
+        // asserts().assertCollectionAttributeSize( view, "bibliographicReferences", 7 );
     }
-    
+
     public void testStartFlow() {
         startFlow();
         assertCurrentStateEquals( "criteria.view" );
@@ -62,17 +52,4 @@ public class DetailBibRefFlowTests extends AbstractFlowExecutionTests {
     protected String flowId() {
         return "pubMed.Search";
     }
-
-    protected String[] getConfigLocations() {
-
-        ResourceBundle db = ResourceBundle.getBundle( "testResources" );
-        String daoType = db.getString( "dao.type" );
-        String servletContext = db.getString( "servlet.name.0" );
-        // Make sure you have the /web on the junit classpath.
-        String[] paths = { "applicationContext-dataSource.xml", "applicationContext-" + daoType + ".xml",
-                servletContext + "-servlet.xml" };
-
-        return paths;
-    }
-
 }
