2c3821f9575a287aa0bc7bb56b8103216af7ae0c
keshav <>
2005-05-11 15:28:40 +0000

    new testcase to be used by Flows

---
+++ test/edu/columbia/gemma/BaseFlowTestCase.java
@@ -0,0 +1,25 @@
+package edu.columbia.gemma;
+
+import java.util.ResourceBundle;
+
+import org.springframework.test.web.flow.AbstractFlowExecutionTests;
+
+public class BaseFlowTestCase extends AbstractFlowExecutionTests{
+    
+    protected String flowId() {
+        return null;
+    }
+    
+    protected String[] getConfigLocations() {
+
+        ResourceBundle db = ResourceBundle.getBundle( "test" );
+        String daoType = db.getString( "dao.type" );
+        String servletContext = db.getString( "servlet.name.0" );
+        // Make sure you have the /web on the junit classpath.
+        String[] paths = { "applicationContext-dataSource.xml", "applicationContext-" + daoType + ".xml",
+                servletContext + "-servlet.xml" };
+
+        return paths;
+    }
+
+}
