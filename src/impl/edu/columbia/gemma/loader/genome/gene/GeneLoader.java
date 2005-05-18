f85a77c064626cb629a56c7d8c90c54961469358
keshav <>
2005-05-18 13:53:22 +0000

    Gene Loader

---
+++ src/impl/edu/columbia/gemma/loader/genome/gene/GeneLoader.java
@@ -0,0 +1,21 @@
+package edu.columbia.gemma.loader.genome.gene;
+
+import java.util.Collection;
+
+import edu.columbia.gemma.genome.Gene;
+
+/**
+ * <hr>
+ * <p>
+ * Copyright (c) 2004 - 2005 Columbia University
+ * 
+ * @author keshav
+ * @version $Id$
+ */
+public interface GeneLoader {
+
+    public void create( Gene gene );
+
+    public void create( Collection col );
+
+}
