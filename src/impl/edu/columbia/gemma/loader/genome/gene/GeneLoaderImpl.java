e0f3d1173ad0e4bfb9c41572183b8e5c1650bbf9
keshav <>
2005-05-18 13:53:01 +0000

    Gene Loader Impl

---
+++ src/impl/edu/columbia/gemma/loader/genome/gene/GeneLoaderImpl.java
@@ -0,0 +1,78 @@
+package edu.columbia.gemma.loader.genome.gene;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.ResourceBundle;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import edu.columbia.gemma.genome.Gene;
+import edu.columbia.gemma.genome.GeneDao;
+
+/**
+ * <hr>
+ * <p>
+ * Copyright (c) 2004 - 2005 Columbia University
+ * 
+ * @author keshav
+ * @version $Id$
+ * @spring.bean id="geneLoader"
+ * @spring.property name="geneDao" ref="geneDao"
+ */
+public class GeneLoaderImpl implements GeneLoader {
+    protected static final Log log = LogFactory.getLog( GeneParser.class );
+
+    private static BeanFactory ctx;
+    private GeneDao geneDao;
+
+    /**
+     * @return Returns the geneDao.
+     */
+    public GeneDao getGeneDao() {
+        return geneDao;
+    }
+
+    /**
+     * @param geneDao The geneDao to set.
+     */
+    public void setGeneDao( GeneDao geneDao ) {
+        this.geneDao = geneDao;
+    }
+
+    static {
+        ResourceBundle db = ResourceBundle.getBundle( "test" );
+        String daoType = db.getString( "dao.type" );
+        String servletContext = db.getString( "servlet.name.0" );
+
+        // CAREFUL, these paths are dependent on the classpath for the test.
+        String[] paths = { "applicationContext-dataSource.xml", "applicationContext-" + daoType + ".xml",
+                servletContext + "-servlet.xml" };
+        ctx = new ClassPathXmlApplicationContext( paths );
+    }
+
+    /**
+     * Persist gene.
+     * 
+     * @param gene
+     */
+    public void create( Gene gene ) {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     * Persist genes in collection.
+     * 
+     * @param col
+     */
+    public void create( Collection col ) {
+        // create( col );
+        Iterator iter = col.iterator();
+        while ( iter.hasNext() ) {
+            Gene g = ( Gene ) iter.next();
+            ( ( GeneDao ) ctx.getBean( "geneDao" ) ).create( g );
+        }
+    }
+}
