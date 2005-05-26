451c1498860f5fb71882985fcc2b9d2347e0ba43
keshav <>
2005-05-26 18:25:01 +0000

    Utilities class to be used by loaders and parsers

---
+++ src/impl/edu/columbia/gemma/loader/loaderutils/Utilities.java
@@ -0,0 +1,35 @@
+package edu.columbia.gemma.loader.loaderutils;
+
+import org.apache.commons.lang.time.StopWatch;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import edu.columbia.gemma.loader.genome.gene.GeneParser;
+
+/**
+ * Utilities to be used by parsers and loaders.
+ * <hr>
+ * <p>
+ * Copyright (c) 2004 - 2005 Columbia University
+ * 
+ * @author keshav
+ * @version $Id$
+ */
+public class Utilities {
+    protected static final Log log = LogFactory.getLog( GeneParser.class );
+
+    /**
+     * Display time to be used with org.apache.commons.lang.time.StopWatch
+     * 
+     * @param stopwatch
+     * @see org.apache.commons.lang.time.StopWatch
+     */
+    public static final void displayTime( StopWatch stopwatch ) {
+        long time = stopwatch.getTime();
+        if ( time < 1000 )
+            log.info( "Time taken: " + time + " ms." );
+        else {
+            log.info( "Time taken: " + time / 1000 + " s." );
+        }
+    }
+}
