3002d17502c9a78e0b5a68fe3cca17ebff62035b
keshav <>
2005-05-27 22:53:33 +0000

    added methods to interface

--- src/impl/edu/columbia/gemma/loader/genome/gene/GeneParser.java
+++ src/impl/edu/columbia/gemma/loader/genome/gene/GeneParser.java
@@ -1,9 +1,15 @@
 package edu.columbia.gemma.loader.genome.gene;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
 import java.util.Map;
 
 public interface GeneParser {
+
+    public abstract Method findParseLineMethod( String string ) throws NoSuchMethodException;
+
+    public abstract Map parse( InputStream is, Method m ) throws IOException;
 
     public abstract Map parseFile( String filename ) throws IOException;
 
