575db44234829e4066f0087a127ea1cde2de9178
keshav <>
2005-06-03 15:15:24 +0000

    added taxa

--- metadata/conf/init-script.sql
+++ metadata/conf/init-script.sql
@@ -1,2 +1,6 @@
-insert into contact values ("", "PersonImpl", NULL,  NULL , NULL, NULL, NULL, NULL, NULL, NULL,  "nobody", NULL, NULL, NULL, NULL, NULL, NULL, NULL); 
+insert into contact values ("", "PersonImpl", NULL,  NULL , NULL, NULL, NULL, NULL, NULL, NULL,  "nobody", "nobody", "nobody", NULL, NULL, NULL, NULL, NULL); 
 insert into contact values ("", "UserImpl", NULL,  NULL , NULL, NULL, NULL, NULL, NULL, NULL,  NULL, NULL, NULL, "admin", "dc76e9f0c0006e8f919e0c515c66dbba3982f785", NULL, NULL, NULL); 
+
+insert into taxon (SCIENTIFIC_NAME,COMMON_NAME,NCBI_ID) values ("homo sapien","human","9606");
+insert into taxon (SCIENTIFIC_NAME,COMMON_NAME,NCBI_ID) values ("mus musculus","mouse","10090"); 
+insert into taxon (SCIENTIFIC_NAME,COMMON_NAME,NCBI_ID) values ("rattus","rat","10114");
