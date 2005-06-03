fc2488ff82e961d02b1afecc725197ed4c97c203
pavlidis <>
2005-06-03 13:39:05 +0000

    initialization script for database

---
+++ metadata/conf/init-script.sql
@@ -0,0 +1,2 @@
+insert into contact values ("", "PersonImpl", NULL,  NULL , NULL, NULL, NULL, NULL, NULL, NULL,  "nobody", NULL, NULL, NULL, NULL, NULL, NULL, NULL); 
+insert into contact values ("", "UserImpl", NULL,  NULL , NULL, NULL, NULL, NULL, NULL, NULL,  NULL, NULL, NULL, "admin", "root", NULL, NULL, NULL); 
