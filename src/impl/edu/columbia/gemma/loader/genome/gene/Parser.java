32977870c91232bbc4eb83470895353b6d2ef347
keshav <>
2005-06-06 03:39:17 +0000

    parser interface

---
+++ src/impl/edu/columbia/gemma/loader/genome/gene/Parser.java
@@ -0,0 +1,25 @@
+package edu.columbia.gemma.loader.genome.gene;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+/**
+ * 
+ * 
+ *
+ * <hr>
+ * <p>Copyright (c) 2004 - 2005 Columbia University
+ * @author keshav
+ * @version $Id$
+ */
+public interface Parser {
+
+    public abstract Method findParseLineMethod( String string ) throws NoSuchMethodException;
+
+    public abstract Map parse( InputStream is, Method m ) throws IOException;
+
+    public abstract Map parseFile( String filename ) throws IOException;
+
+}
