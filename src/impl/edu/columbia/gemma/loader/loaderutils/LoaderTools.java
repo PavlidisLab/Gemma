2c1445fdf1cd1658c8728ad942665f7acb10bade
keshav <>
2005-06-10 03:55:05 +0000

    refactored

--- src/impl/edu/columbia/gemma/loader/loaderutils/LoaderTools.java
+++ src/impl/edu/columbia/gemma/loader/loaderutils/LoaderTools.java
@@ -2,7 +2,9 @@ package edu.columbia.gemma.loader.loaderutils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
+import java.util.Collection;
 
 import org.apache.commons.lang.time.StopWatch;
 import org.apache.commons.logging.Log;
@@ -23,11 +25,39 @@ public class LoaderTools {
     /**
      * Display time to be used with org.apache.commons.lang.time.StopWatch
      * 
-     * @param stopwatch
-     * @see org.apache.commons.lang.time.StopWatch
+     * @param loader
+     * @param col
      */
-    public static final void displayTime( StopWatch stopwatch ) {
-        long time = stopwatch.getTime();
+    public static final void loadDatabase( Object loader, Collection col ) {
+        assert ( loader != null );
+        assert ( col != null );
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        try {
+            loader.getClass().getMethod( "create", new Class[] { Collection.class } ).invoke( loader,
+                    new Object[] { col } );
+        } catch ( IllegalArgumentException e ) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch ( SecurityException e ) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch ( IllegalAccessException e ) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch ( InvocationTargetException e ) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch ( NoSuchMethodException e ) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        stopWatch.stop();
+
+        long time = stopWatch.getTime();
         if ( time < 1000 )
             log.info( "Time taken: " + time + " ms." );
         else {
@@ -35,6 +65,11 @@ public class LoaderTools {
         }
     }
 
+    /**
+     * @param count
+     * @param modulus
+     * @param objectName
+     */
     public static void objectsPersistedUpdate( int count, int modulus, String objectName ) {
         if ( count % modulus == 0 ) log.info( count + " " + objectName + " persisted" );
 
