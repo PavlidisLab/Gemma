8a934c13d243986815b158f0bee3fb5a0b25cda8
keshav <>
2005-06-16 14:50:06 +0000

    assigned username a role username is now:  administrator password is now:  admintoast

--- metadata/conf/init-script.sql
+++ metadata/conf/init-script.sql
@@ -1,5 +1,7 @@
-insert into contact values ("", "PersonImpl", NULL,  NULL , NULL, NULL, NULL, NULL, NULL, NULL,  "nobody", "nobody", "nobody", NULL, NULL, NULL, NULL, NULL); 
-insert into contact values ("", "UserImpl", NULL,  NULL , NULL, NULL, NULL, NULL, NULL, NULL,  NULL, NULL, NULL, "admin", "dc76e9f0c0006e8f919e0c515c66dbba3982f785", NULL, NULL, NULL); 
+insert into contact (CLASS, LAST_NAME, FIRST_NAME, MIDDLE_NAME) values ("PersonImpl", "nobody", "nobody", "nobody");
+insert into contact (CLASS, EMAIL) values ("ContactImpl", "admin@gemma.org");    
+insert into contact (CLASS, USER_NAME, PASSWORD, CONFIRM_PASSWORD) values ("UserImpl", "administrator", "cec6877e42b179b133c2a6c1285488755aefc071", "cec6877e42b179b133c2a6c1285488755aefc071");
+insert into user_role (NAME, USER_NAME) values ("admin", "administrator");
 
 insert into taxon (SCIENTIFIC_NAME,COMMON_NAME,NCBI_ID) values ("homo sapien","human","9606");
 insert into taxon (SCIENTIFIC_NAME,COMMON_NAME,NCBI_ID) values ("mus musculus","mouse","10090"); 
