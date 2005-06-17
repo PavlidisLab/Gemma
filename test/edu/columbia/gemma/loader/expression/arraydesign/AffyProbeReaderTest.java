242b5f2ec70fe6b54344b82e0a84efcc99038843
keshav <>
2005-06-17 15:31:58 +0000

    move here from loader/arraydesign to keep it consistent with the source

---
+++ test/edu/columbia/gemma/loader/expression/arraydesign/AffyProbeReaderTest.java
@@ -0,0 +1,77 @@
+package edu.columbia.gemma.loader.expression.arraydesign;
+
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import edu.columbia.gemma.expression.designElement.CompositeSequence;
+import edu.columbia.gemma.expression.designElement.Reporter;
+import edu.columbia.gemma.loader.expression.arraydesign.AffyProbeReader;
+
+import junit.framework.TestCase;
+
+/**
+ * <hr>
+ * <p>
+ * Copyright (c) 2004-2005 Columbia University
+ * 
+ * @author pavlidis
+ * @version $Id$
+ */
+public class AffyProbeReaderTest extends TestCase {
+    protected static final Log log = LogFactory.getLog( AffyProbeReaderTest.class );
+    AffyProbeReader apr;
+    InputStream is;
+
+    /*
+     * @see TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+        apr = new AffyProbeReader();
+        apr.setSequenceField( 5 );
+        is = AffyProbeReaderTest.class.getResourceAsStream( "/data/loader/affymetrix-probes-test.txt" );
+        
+    }
+
+    /*
+     * @see TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        apr = null;
+        is.close();
+    }
+
+    /*
+     * Class under test for Map read(InputStream)
+     */
+    public final void testReadInputStream() throws Exception {
+        
+        assertTrue("InputStream was null", is != null );
+        
+        apr.parse( is );
+
+        String expectedValue = "TCACGGCAGGACAACGAGAAAGCCC"; // 10
+        CompositeSequence cs = ( ( CompositeSequence ) apr.get( "1004_at" ) );
+
+        assertTrue( "CompositeSequence was null", cs != null );
+
+        boolean foundIt = false;
+        for ( Iterator iter = cs.getReporters().iterator(); iter.hasNext(); ) {
+            Reporter element = ( Reporter ) iter.next();
+            if ( element.getName().equals( "1004_at:265:573" ) ) {
+                String actualValue = element.getImmobilizedCharacteristic().getSequence();
+
+                assertEquals( expectedValue, actualValue );
+                foundIt = true;
+                break;
+            }
+        }
+        assertTrue( "Didn't find the probe ", foundIt );
+    }
+
+}
