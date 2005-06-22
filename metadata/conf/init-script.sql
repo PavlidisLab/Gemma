1b79101437b337844b92cb566726172073a2f9b0
keshav <>
2005-06-22 15:58:44 +0000

    removed the invalid insert statement.  When using table-per-class-hierarchy, the value used in the discriminator column must be a <subclass> element in hbm.xml file, not the parent class itself.

--- metadata/conf/init-script.sql
+++ metadata/conf/init-script.sql
@@ -1,6 +1,5 @@
-insert into contact (CLASS, LAST_NAME, FIRST_NAME, MIDDLE_NAME) values ("PersonImpl", "nobody", "nobody", "nobody");
-insert into contact (CLASS, EMAIL) values ("ContactImpl", "admin@gemma.org");    
-insert into contact (CLASS, USER_NAME, PASSWORD, CONFIRM_PASSWORD) values ("UserImpl", "administrator", "cec6877e42b179b133c2a6c1285488755aefc071", "cec6877e42b179b133c2a6c1285488755aefc071");
+insert into contact (CLASS, NAME, LAST_NAME, FIRST_NAME, MIDDLE_NAME) values ("PersonImpl", "person", "nobody", "nobody", "nobody");
+insert into contact (CLASS, NAME, USER_NAME, PASSWORD, CONFIRM_PASSWORD) values ("UserImpl", "user", "administrator", "cec6877e42b179b133c2a6c1285488755aefc071", "cec6877e42b179b133c2a6c1285488755aefc071");
 insert into user_role (NAME, USER_NAME) values ("admin", "administrator");
 
 insert into taxon (SCIENTIFIC_NAME,COMMON_NAME,NCBI_ID) values ("homo sapien","human","9606");
