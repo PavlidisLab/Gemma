8e772a5c09d5452849aa76ec27ce0c23fcb79e9c
keshav <>
2005-06-22 20:06:03 +0000

    updated

--- metadata/conf/init-script.sql
+++ metadata/conf/init-script.sql
@@ -1,5 +1,6 @@
 insert into contact (CLASS, NAME, LAST_NAME, FIRST_NAME, MIDDLE_NAME) values ("PersonImpl", "person", "nobody", "nobody", "nobody");
 insert into contact (CLASS, NAME, USER_NAME, PASSWORD, CONFIRM_PASSWORD) values ("UserImpl", "user", "administrator", "cec6877e42b179b133c2a6c1285488755aefc071", "cec6877e42b179b133c2a6c1285488755aefc071");
+insert into contact (CLASS, NAME, EMAIL) values ("edu.columbia.gemma.common.auditAndSecurity.ContactImpl", "contact", "admin@gemma.org");
 insert into user_role (NAME, USER_NAME) values ("admin", "administrator");
 
 insert into taxon (SCIENTIFIC_NAME,COMMON_NAME,NCBI_ID) values ("homo sapien","human","9606");
