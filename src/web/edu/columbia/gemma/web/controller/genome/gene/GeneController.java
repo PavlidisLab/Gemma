0f9668e18651a385b14c11e4cfdc95b49084b30b
daq2101 <>
2005-06-27 22:03:21 +0000

    Finished basic BibRef handling

--- src/web/edu/columbia/gemma/web/controller/genome/gene/GeneController.java
+++ src/web/edu/columbia/gemma/web/controller/genome/gene/GeneController.java
@@ -3,6 +3,8 @@ package edu.columbia.gemma.web.controller.genome.gene;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Collection;
+import java.util.Iterator;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -76,7 +78,10 @@ public class GeneController extends BaseCommandController {
 
 		if(action.equals("addcitation")){
 			String pubmedID = request.getParameter("pubmedID");
-			BibliographicReference br = this.getBibliographicReferenceService().findByExternalId(pubmedID);
+			BibliographicReference br = this.getBibliographicReferenceService().findByExternalId(pubmedID, "PUBMED");
+	        if( br==null )
+	        	br = this.getBibliographicReferenceService().createBibliographicReferenceByLookup(pubmedID, "PUBMED");
+	        
 			if( br!= null){
 				java.util.Collection cites = g.getCitations();
 				cites.add(br);
@@ -99,6 +104,23 @@ public class GeneController extends BaseCommandController {
 			g.setCitations(cites);
 			g = this.getGeneService().updateGene(g);
 			return new ModelAndView(new RedirectView("candidateGeneListActionComplete.htm?target=geneDetail&geneID=" + g.getId()));
+		}
+		
+		if( action.equals("updatecitation")){
+			long citationID = new Long(request.getParameter("citationID")).longValue();
+			String description = request.getParameter("description");
+			Collection<BibliographicReference> cites = g.getCitations();
+			BibliographicReference br = null;
+			for(Iterator<BibliographicReference> iter=cites.iterator();iter.hasNext();){
+				br = (BibliographicReference) iter.next();
+				if(br.getId().longValue()==citationID){
+					br.setDescription(description);
+				}
+			}
+			g.setCitations(cites);
+			g = this.getGeneService().updateGene(g);
+			return new ModelAndView(new RedirectView("candidateGeneListActionComplete.htm?target=geneDetail&geneID=" + g.getId()));
+			
 		}
 		geneModel.put("gene", g);
 		return new ModelAndView("geneDetail", "model", geneModel);
