871446439911422728963413428dbefab1cf6028
daq2101 <>
2005-07-05 19:26:40 +0000

    Now uses Mock objects

--- test/edu/columbia/gemma/common/description/BibliographicReferenceServiceImplTest.java
+++ test/edu/columbia/gemma/common/description/BibliographicReferenceServiceImplTest.java
@@ -1,7 +1,11 @@
 package edu.columbia.gemma.common.description;
 
-import edu.columbia.gemma.BaseDAOTestCase;
+import org.easymock.MockControl;
 
+import edu.columbia.gemma.BaseServiceTestCase;
+import edu.columbia.gemma.common.description.DatabaseEntry;
+import edu.columbia.gemma.common.description.ExternalDatabase;
+import edu.columbia.gemma.common.description.BibliographicReference;
 /**
  * <hr>
  * <p>
@@ -10,33 +14,59 @@ import edu.columbia.gemma.BaseDAOTestCase;
  * @author pavlidis
  * @version $Id$
  */
-public class BibliographicReferenceServiceImplTest extends BaseDAOTestCase {
+public class BibliographicReferenceServiceImplTest extends BaseServiceTestCase {
 
-    private BibliographicReferenceService svc = null;
+    private BibliographicReferenceServiceImpl svc = null;
     private BibliographicReferenceDao brdao = null;
+    private ExternalDatabaseDao eddao = null;
+    private DatabaseEntry de = null;
+    private ExternalDatabase extDB = null;
+    private MockControl controlBR;
+    private MockControl controlED;
 
+    /*
+     * @see TestCase#setUp()
+     */
     protected void setUp() throws Exception {
         super.setUp();
-        svc = ( BibliographicReferenceService ) ctx.getBean( "bibliographicReferenceService" );
-        brdao = ( BibliographicReferenceDao ) ctx.getBean( "bibliographicReferenceDao" );
         
-
+        svc = new BibliographicReferenceServiceImpl();
+        controlBR = MockControl.createControl( BibliographicReferenceDao.class );
+        controlED = MockControl.createControl( ExternalDatabaseDao.class );
+        
+        brdao = ( BibliographicReferenceDao ) controlBR.getMock();
+        eddao = ( ExternalDatabaseDao ) controlED.getMock();
+        
+        svc.setBibliographicReferenceDao(brdao);
+        svc.setExternalDatabaseDao(eddao);
+        extDB = ExternalDatabase.Factory.newInstance();
+        extDB.setName("PUBMED");
+        
+        de = DatabaseEntry.Factory.newInstance();
+        de.setAccession("12345");
+        de.setExternalDatabase(extDB);
     }
 
+    /*
+     * @see TestCase#tearDown()
+     */
     protected void tearDown() throws Exception {
- 
+    	super.tearDown();
     }
 
     public final void testFindByExternalId() {
-
-		BibliographicReference br = svc.findByExternalId("12345", "PUBMED");
-        if( br==null ){
-        	br = svc.createBibliographicReferenceByLookup("12345", "PUBMED");
-        }
        
-        assertTrue(br.getPubAccession().getAccession().equals("12345"));
-    	assertTrue(br.getPubAccession().getExternalDatabase().getName().equals("PUBMED"));
         
+        BibliographicReference mockBR = BibliographicReference.Factory.newInstance();
+        mockBR.setPubAccession(de);
+        mockBR.setTitle("My Title");
+        brdao.findByExternalId("12345", "PUBMED");
+		controlBR.setReturnValue( mockBR );
+		
+        controlBR.replay();
+        BibliographicReference brRet = svc.findByExternalId("12345", "PUBMED");
+        controlBR.verify();
     }
+    
 
 }
