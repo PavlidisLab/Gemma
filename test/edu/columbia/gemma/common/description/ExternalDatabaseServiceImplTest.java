78e1cc773781596a0b56f53bb79cb1ba0c2c7df7
pavlidis <>
2005-07-05 00:50:14 +0000

    trivial test of ExternalDatabase service

---
+++ test/edu/columbia/gemma/common/description/ExternalDatabaseServiceImplTest.java
@@ -0,0 +1,52 @@
+package edu.columbia.gemma.common.description;
+
+import org.easymock.MockControl;
+
+import edu.columbia.gemma.BaseDAOTestCase;
+import edu.columbia.gemma.BaseServiceTestCase;
+import edu.columbia.gemma.expression.arrayDesign.ArrayDesignDao;
+
+/**
+ * <hr>
+ * <p>
+ * Copyright (c) 2004-2005 Columbia University
+ * 
+ * @author pavlidis
+ * @version $Id$
+ */
+public class ExternalDatabaseServiceImplTest extends BaseServiceTestCase {
+
+    /*
+     * @see TestCase#setUp()
+     */
+    private ExternalDatabaseServiceImpl svc = null;
+    private ExternalDatabaseDao dao = null;
+    private MockControl control;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        svc = new ExternalDatabaseServiceImpl();
+        control = MockControl.createControl( ExternalDatabaseDao.class );
+        dao = ( ExternalDatabaseDao ) control.getMock();
+        svc.setExternalDatabaseDao( dao );
+    }
+
+    /*
+     * @see TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testFind() {
+        ExternalDatabase m = ExternalDatabase.Factory.newInstance();
+        m.setName( "PubMed" );
+        dao.findByName( "PubMed" );
+        control.setReturnValue( m );
+
+        control.replay(); // switch from record mode to replay
+        svc.find( "PubMed" );
+        control.verify();
+    }
+
+}
