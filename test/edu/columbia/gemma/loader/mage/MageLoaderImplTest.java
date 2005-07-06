56835dd5401d5e66437037e5360c40795b6cc50b
pavlidis <>
2005-07-06 00:26:03 +0000

    mage loadertest

--- test/edu/columbia/gemma/loader/mage/MageLoaderImplTest.java
+++ test/edu/columbia/gemma/loader/mage/MageLoaderImplTest.java
@@ -18,16 +18,23 @@
  */
 package edu.columbia.gemma.loader.mage;
 
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.InputStream;
 import java.util.Collection;
 import java.util.zip.ZipInputStream;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import edu.columbia.gemma.BaseDAOTestCase;
+import edu.columbia.gemma.common.auditAndSecurity.PersonDao;
+import edu.columbia.gemma.common.description.OntologyEntryDao;
+import edu.columbia.gemma.expression.arrayDesign.ArrayDesignDao;
+import edu.columbia.gemma.expression.biomaterial.BioMaterialDao;
+import edu.columbia.gemma.expression.experiment.ExpressionExperimentDao;
 import edu.columbia.gemma.loader.expression.mage.MageLoaderImpl;
 import edu.columbia.gemma.loader.expression.mage.MageMLParser;
-
-import junit.framework.TestCase;
 
 /**
  * <hr>
@@ -37,8 +44,30 @@ import junit.framework.TestCase;
  * @author pavlidis
  * @version $Id$
  */
-public class MageLoaderImplTest extends TestCase {
+public class MageLoaderImplTest extends BaseDAOTestCase {
     private static Log log = LogFactory.getLog( MageLoaderImplTest.class.getName() );
+    MageLoaderImpl ml;
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see junit.framework.TestCase#setUp()
+     */
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        ml = new MageLoaderImpl();
+        ml.setBioMaterialDao( ( BioMaterialDao ) ctx.getBean( "bioMaterialDao" ) );
+        ml.setExpressionExperimentDao( ( ExpressionExperimentDao ) ctx.getBean( "expressionExperimentDao" ) );
+        ml.setPersonDao( ( PersonDao ) ctx.getBean( "personDao" ) );
+        ml.setOntologyEntryDao( ( OntologyEntryDao ) ctx.getBean( "ontologyEntryDao" ) );
+        ml.setArrayDesignDao( ( ArrayDesignDao ) ctx.getBean( "arrayDesignDao" ) );
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
 
     /*
      * Class under test for void create(Collection)
@@ -54,15 +83,24 @@ public class MageLoaderImplTest extends TestCase {
         log.info( result.size() + " Objects parsed from the MAGE file." );
         log.info( "Tally:\n" + mlp );
         istMageExamples.close();
-
-        MageLoaderImpl ml = new MageLoaderImpl();
         ml.create( result );
     }
 
-    /*
-     * Class under test for void removeAll()
+    /**
+     * A real example of an experimental package.
+     * 
+     * @throws Exception
      */
-    public void testRemoveAll() {
+    public void testCreateCollectionReal() throws Exception {
+        log.debug( "Parsing MAGE from ArrayExpress" );
+        InputStream istMageExamples = MageMLParserTest.class.getResourceAsStream( "/data/mage/E-WMIT-4.xml" );
+        MageMLParser mlp = new MageMLParser();
+        mlp.parse( istMageExamples );
+        Collection result = mlp.getConvertedData();
+        log.info( result.size() + " Objects parsed from the MAGE file." );
+        log.info( "Tally:\n" + mlp );
+        istMageExamples.close();
+        ml.create( result );
     }
 
 }
