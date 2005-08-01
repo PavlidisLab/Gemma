8e6cc82b3ec749c24d95d0580b37c26db0b9b48d
keshav <>
2005-08-01 00:21:28 +0000

    added the acl_permission and the acl_object_identity tables as well as a 'pavlab' user.

---
+++ metadata/conf/mysql-acegi-acl.sql
@@ -0,0 +1,41 @@
+DROP TABLE IF EXISTS acl_object_identity;
+CREATE TABLE acl_object_identity (
+  id int(11) NOT NULL auto_increment,
+  object_identity varchar(255) NOT NULL,
+  parent_object int(11) default NULL,
+  acl_class varchar(255) NOT NULL,
+  PRIMARY KEY  (id),
+  UNIQUE KEY object_identity (object_identity)
+) TYPE=InnoDB;
+
+DROP TABLE IF EXISTS acl_permission;
+CREATE TABLE acl_permission (
+  id int(11) NOT NULL auto_increment,
+  acl_object_identity int(11) NOT NULL,
+  recipient varchar(100) NOT NULL,
+  mask int(11) NOT NULL,
+  PRIMARY KEY  (id)
+  ---  don't forget the comma after PRIMARY KEY UNIQUE KEY recipient (recipient)
+) TYPE=InnoDB;
+
+--- (id, object identity (of form class:getId()), parent object, acl class)
+INSERT INTO acl_object_identity VALUES (1, 'edu.columbia.gemma.expression.arrayDesign.ArrayDesignImpl:1', null, 'net.sf.acegisecurity.acl.basic.SimpleAclEntry');
+INSERT INTO acl_object_identity VALUES (2, 'edu.columbia.gemma.expression.arrayDesign.ArrayDesignImpl:2', null, 'net.sf.acegisecurity.acl.basic.SimpleAclEntry');
+
+--- (id, acl object identity, recepient (principal username), mask)
+---INSERT INTO acl_permission VALUES (null, 1, 'administrator', 1);
+INSERT INTO acl_permission VALUES (null, 1, 'pavlab', 2);
+INSERT INTO acl_permission VALUES (null, 2, 'pavlab', 2);
+
+--- Add the user pavlab to the database.  Due to the fact that this user is used specifically for testing acegi security (acl), I have
+--- put the insert statements in this acegi specific .sql file as opposed to the init-script.sql.
+insert into contact (CLASS, NAME, LAST_NAME, FIRST_NAME, MIDDLE_NAME) values ("PersonImpl", "aPersonWithUserPriviledges", "lastname", "firstname", "middlename");
+insert into contact (CLASS, NAME, USER_NAME, PASSWORD, CONFIRM_PASSWORD, ENABLED) values ("UserImpl", "aUserWithUserPriviledges", "pavlab", "7478822cc546bb339c7cd198ca96df2b7a50cf5c", "7478822cc546bb339c7cd198ca96df2b7a50cf5c", 1);
+insert into contact (CLASS, NAME, EMAIL) values ("edu.columbia.gemma.common.auditAndSecurity.ContactImpl", "contact", "keshav@cu-genome.org");
+insert into user_role (NAME, USER_NAME) values ("user", "pavlab");
+
+--- Mask integer 0  = no permissions
+--- Mask integer 1  = administer
+--- Mask integer 2  = read
+--- Mask integer 6  = read and write permissions
+--- Mask integer 14 = read and write and create permissions
