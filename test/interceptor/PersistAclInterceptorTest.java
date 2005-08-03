5a7a8db6fab4ea31157ccec0d226084c9845a785
keshav <>
2005-08-03 19:26:08 +0000

    added this to make sure my interceptor was getting called before the create method invocation.

---
+++ test/interceptor/PersistAclInterceptorTest.java
@@ -0,0 +1,53 @@
+package interceptor;
+
+import java.util.Collection;
+import java.util.Random;
+
+import junit.framework.TestCase;
+
+import org.springframework.beans.factory.BeanFactory;
+
+import edu.columbia.gemma.expression.arrayDesign.ArrayDesign;
+import edu.columbia.gemma.expression.arrayDesign.ArrayDesignService;
+import edu.columbia.gemma.util.SpringContextUtil;
+
+/**
+ * <hr>
+ * <p>
+ * Copyright (c) 2004 - 2005 Columbia University
+ * 
+ * @author keshav
+ * @version $Id$
+ */
+public class PersistAclInterceptorTest extends TestCase {
+    ArrayDesign ad = null;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        
+        ad = ArrayDesign.Factory.newInstance();
+        
+        ad.setName((new Random()).toString());
+    }
+
+    /*
+     * @see TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Calling the method getArrayDesigns, which should have the PersistAclInterceptor.invoke called on it before the
+     * actual method invocation.
+     * 
+     * @throws Exception
+     */
+    public void testGetArrayDesignsWithDao() throws Exception {
+        BeanFactory ctx = SpringContextUtil.getApplicationContext();
+             
+        ( ( ArrayDesignService ) ctx.getBean( "arrayDesignService" ) ).saveArrayDesign(ad);
+
+    }
+
+}
