e85e2d951cf12ab4693727ee70c1c655b6129b2f
keshav <>
2005-08-04 16:14:37 +0000

    added test to confirm that not all methods are being intercepted.  this will be fleshed out so the methods to be intercepted can be specified in the xml file (similar to the securityInterceptor and transactionInterceptor).

--- test/edu/columbia/gemma/interceptor/PersistAclInterceptorTest.java
+++ test/edu/columbia/gemma/interceptor/PersistAclInterceptorTest.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import junit.framework.TestCase;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.BeanFactory;
 
 import edu.columbia.gemma.expression.arrayDesign.ArrayDesign;
@@ -19,11 +21,13 @@ import edu.columbia.gemma.util.SpringContextUtil;
  * @version $Id$
  */
 public class PersistAclInterceptorTest extends TestCase {
+    private static Log log = LogFactory.getLog( PersistAclInterceptorTest.class.getName() );
     ArrayDesign ad = null;
+    BeanFactory ctx = null;
 
     protected void setUp() throws Exception {
         super.setUp();
-
+        ctx = SpringContextUtil.getApplicationContext();
         ad = ArrayDesign.Factory.newInstance();
         ad.setName( ( new Date() ).toString() );
     }
@@ -36,16 +40,21 @@ public class PersistAclInterceptorTest extends TestCase {
     }
 
     /**
-     * Calling the method saveArrayDesign, which should have the PersistAclInterceptor.invoke called on it before the
+     * Calling the method saveArrayDesign, which should have the PersistAclInterceptor.invoke called on it after the
      * actual method invocation.
      * 
      * @throws Exception
      */
-    public void testGetArrayDesignsWithDao() throws Exception {
-        BeanFactory ctx = SpringContextUtil.getApplicationContext();
+    public void testSaveArrayDesign() throws Exception {
+        log.info( "Testing saveArrayDesign(ArrayDesign ad)" );
 
         ( ( ArrayDesignService ) ctx.getBean( "arrayDesignService" ) ).saveArrayDesign( ad );
+    }
 
+    public void testInvalidMethodToIntercept() throws Exception {
+        log.info( "Testing an invalid method to intercept" );
+
+        ( ( ArrayDesignService ) ctx.getBean( "arrayDesignService" ) ).getAllArrayDesigns();
     }
 
 }
