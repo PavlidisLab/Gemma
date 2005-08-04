c4ef5ee8065dabdab8c00581d7e6f26d68d1958e
keshav <>
2005-08-04 16:16:02 +0000

    this is persisting properly.  i'll make this implement an interface so I can add another implementation that uses acegi's dao.

--- src/impl/edu/columbia/gemma/interceptor/PersistAclInterceptor.java
+++ src/impl/edu/columbia/gemma/interceptor/PersistAclInterceptor.java
@@ -2,6 +2,7 @@ package edu.columbia.gemma.interceptor;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Date;
@@ -31,39 +32,84 @@ public class PersistAclInterceptor implements MethodInterceptor {
      * @throws Throwable
      */
     public Object invoke( MethodInvocation invocation ) throws Throwable {
-        Object object = null;
-        log.info( "Before: invocation=[" + invocation + "]" );
+        if ( isValidMethodToIntercept( invocation ) ) {
+            Object object = null;
+            log.info( "Before: invocation=[" + invocation + "]" );
 
-        log.info( "The method is: " + invocation.getMethod() );
+            log.info( "The method is: " + invocation.getMethod() );
 
-        Object[] arguments = invocation.getArguments();
-        for ( Object obj : arguments ) {
-            object = obj;
+            Object[] arguments = invocation.getArguments();
+            for ( Object obj : arguments ) {
+                object = obj;
+            }
+
+            String fullyQualifiedName = object.getClass().getName();
+            log.info( "The object is: " + fullyQualifiedName );
+
+            log.info( "Connecting to database ... " );
+            Connection c = makeDatabaseConnection();
+
+            invocation.proceed();
+
+            Statement s1 = c.createStatement();
+            String last = null;
+            try {
+                ResultSet rs = s1.executeQuery( "SELECT * FROM array_design" );
+                int index = 0;
+
+                while ( rs.next() ) {
+                    last = rs.getString( 1 );
+                    log.info( "ID: " + last );
+                }
+                s1.close();
+            } catch ( SQLException se ) {
+                se.printStackTrace();
+                System.exit( 1 );
+            }
+
+            Statement s2 = c.createStatement();
+            try {
+                s2.executeUpdate( "INSERT INTO acl_object_identity (object_identity, acl_class)" + "VALUES ('"
+                        + fullyQualifiedName + ":" + last + "','net.sf.acegisecurity.acl.basic.SimpleAclEntry')" );
+            } catch ( SQLException se ) {
+                se.printStackTrace();
+                System.exit( 1 );
+            }
+            s2.close();
+
+            Statement s3 = c.createStatement();
+
+            try {
+
+                s3.executeUpdate( "INSERT INTO acl_permission (acl_object_identity, recipient, mask)"
+                        + "VALUES (LAST_INSERT_ID(),'pavlab',2)" );
+            } catch ( SQLException se ) {
+                se.printStackTrace();
+                System.exit( 1 );
+            }
+            s3.close();
+
+            log.info( "Acl persisted successfully." );
+
+            c.close();
+
+            return null;
         }
-
-        String fullyQualifiedName = object.getClass().toString();
-        log.info( "The object is: " + fullyQualifiedName );
-
-        invocation.proceed();
-        // TODO I have shown here via jdbc that you can get the fully qualifed object name of the object you are
-        // trying to create. Now, I just need the id and I should be able to run the arrayDesignLoaderTest and
-        // insert the correct permission in the acl table.
-        log.info( "Connecting to database ... " );
-        Connection c = makeDatabaseConnection();
-
-        Statement s = c.createStatement();
-        String dummyId = ( new Date() ).toString();
-        try {
-            s.executeUpdate( "INSERT INTO acl_object_identity (object_identity, acl_class)" + "VALUES ('"
-                    + fullyQualifiedName + ":" + dummyId + "','net.sf.acegisecurity.acl.basic.SimpleAclEntry')" );
-        } catch ( SQLException se ) {
-            se.printStackTrace();
-            System.exit( 1 );
-        }
-
-        log.info( "Object persisted successfully." );
-
         return null;
+    }
+
+    /**
+     * Test to see if this is a valid method to intercept. This will be fleshed out to add the actual methods intercept
+     * to the xml configuration file.
+     * 
+     * @param invocation
+     * @return
+     */
+    private boolean isValidMethodToIntercept( MethodInvocation invocation ) {
+        if ( invocation.getMethod().getName().contains( "save" ) ) return true;
+
+        return false;
+
     }
 
     /**
