bd4ce68393468bd3b562a03418029f8b7f7295cf
pavlidis <>
2005-08-10 02:04:11 +0000

    integration test

---
+++ test/edu/columbia/gemma/loader/expression/geo/GeoDatasetServiceTest.java
@@ -0,0 +1,39 @@
+package edu.columbia.gemma.loader.expression.geo;
+
+import edu.columbia.gemma.BaseDAOTestCase;
+import edu.columbia.gemma.common.auditAndSecurity.PersonDao;
+import edu.columbia.gemma.common.description.ExternalDatabaseDao;
+import edu.columbia.gemma.common.description.OntologyEntryDao;
+import edu.columbia.gemma.expression.arrayDesign.ArrayDesignDao;
+import edu.columbia.gemma.expression.biomaterial.BioMaterialDao;
+import edu.columbia.gemma.expression.designElement.DesignElementDao;
+import edu.columbia.gemma.expression.experiment.ExpressionExperimentDao;
+import edu.columbia.gemma.loader.expression.ExpressionLoaderImpl;
+
+/**
+ * <hr>
+ * <p>
+ * Copyright (c) 2004-2005 Columbia University
+ * 
+ * @author pavlidis
+ * @version $Id$
+ */
+public class GeoDatasetServiceTest extends BaseDAOTestCase {
+
+    /** This is an integration test */
+    public void testFetchAndLoad() throws Exception {
+        GeoDatasetService gds = new GeoDatasetService();
+        ExpressionLoaderImpl ml = new ExpressionLoaderImpl();
+        ml.setBioMaterialDao( ( BioMaterialDao ) ctx.getBean( "bioMaterialDao" ) );
+        ml.setExpressionExperimentDao( ( ExpressionExperimentDao ) ctx.getBean( "expressionExperimentDao" ) );
+        ml.setPersonDao( ( PersonDao ) ctx.getBean( "personDao" ) );
+        ml.setOntologyEntryDao( ( OntologyEntryDao ) ctx.getBean( "ontologyEntryDao" ) );
+        ml.setArrayDesignDao( ( ArrayDesignDao ) ctx.getBean( "arrayDesignDao" ) );
+        // ml.setTreatmentDao( ( TreatmentDao ) ctx.getBean( "treatmentDao" ) );
+        ml.setExternalDatabaseDao( ( ExternalDatabaseDao ) ctx.getBean( "externalDatabaseDao" ) );
+        ml.setDesignElementDao( ( DesignElementDao ) ctx.getBean( "designElementDao" ) );
+        gds.setExpressionLoader( ml );
+        gds.fetchAndLoad( "GDS100" );
+    }
+
+}
