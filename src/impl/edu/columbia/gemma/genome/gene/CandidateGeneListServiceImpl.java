17eb6d75b7b581b67eb5d04a39b63edacad7480b
keshav <>
2005-08-12 18:43:17 +0000

    updated method names. added security

--- src/impl/edu/columbia/gemma/genome/gene/CandidateGeneListServiceImpl.java
+++ src/impl/edu/columbia/gemma/genome/gene/CandidateGeneListServiceImpl.java
@@ -20,75 +20,84 @@
  */
 
 package edu.columbia.gemma.genome.gene;
+
 import java.util.Collection;
 import edu.columbia.gemma.common.auditAndSecurity.Person;
 import edu.columbia.gemma.common.auditAndSecurity.AuditTrail;
 import edu.columbia.gemma.common.auditAndSecurity.AuditEvent;
 import edu.columbia.gemma.common.auditAndSecurity.AuditAction;
 import java.util.Date;
+
 /**
- *
  * <hr>
- * <p>Copyright (c) 2004, 2005 Columbia University
+ * <p>
+ * Copyright (c) 2004, 2005 Columbia University
+ * 
  * @author daq2101
  * @version $Id$
  */
-public class CandidateGeneListServiceImpl
-    extends edu.columbia.gemma.genome.gene.CandidateGeneListServiceBase
-{
+public class CandidateGeneListServiceImpl extends edu.columbia.gemma.genome.gene.CandidateGeneListServiceBase {
     // CandidateGeneList manipulation
-	Person actor = null;
-	
-	protected void handleSetActor(Person actor){
-		this.actor=actor;
-	}
-	
-    protected CandidateGeneList handleCreateCandidateGeneList(String newName ) throws java.lang.Exception{
-        assert(this.actor!=null);
-        
-    	CandidateGeneList cgl = CandidateGeneList.Factory.newInstance();
-    	AuditTrail auditTrail = AuditTrail.Factory.newInstance();
-    	AuditEvent auditEvent = AuditEvent.Factory.newInstance();
-    	auditEvent.setAction(AuditAction.CREATE);
-    	auditEvent.setDate(new Date());
-    	auditEvent.setNote("CandidateGeneList Created");
-    	auditEvent.setPerformer(actor);
-    	auditTrail.addEvent(auditEvent);
-    	cgl.setAuditTrail(auditTrail);
-    	cgl.setName(newName);
-        cgl.setOwner(actor);
-    	this.getCandidateGeneListDao().create(cgl);
-    	return cgl;
+    Person actor = null;
+
+    protected void handleSetActor( Person actor ) {
+        this.actor = actor;
     }
-    
-    protected void  handleRemoveCandidateGeneList(CandidateGeneList candidateGeneList) throws java.lang.Exception{
-    	this.getCandidateGeneListDao().remove(candidateGeneList);
+
+    protected CandidateGeneList handleCreateByName( String newName ) throws java.lang.Exception {
+        assert ( this.actor != null );
+
+        CandidateGeneList cgl = CandidateGeneList.Factory.newInstance();
+        AuditTrail auditTrail = AuditTrail.Factory.newInstance();
+        AuditEvent auditEvent = AuditEvent.Factory.newInstance();
+        auditEvent.setAction( AuditAction.CREATE );
+        auditEvent.setDate( new Date() );
+        auditEvent.setNote( "CandidateGeneList Created" );
+        auditEvent.setPerformer( actor );
+        auditTrail.addEvent( auditEvent );
+        cgl.setAuditTrail( auditTrail );
+        cgl.setName( newName );
+        cgl.setOwner( actor );
+        //this.getCandidateGeneListDao().create( cgl );
+        this.saveCandidateGeneList( cgl );
+        return cgl;
     }
-    
-    protected void handleSaveCandidateGeneList(CandidateGeneList candidateGeneList)throws java.lang.Exception{
-    	assert(actor!=null);
-    	candidateGeneList.getAuditTrail().update("CandidateGeneList Saved", actor);
-    	this.getCandidateGeneListDao().update(candidateGeneList);   
+
+    protected CandidateGeneList handleSaveCandidateGeneList( CandidateGeneList candidateGeneList ) throws Exception {
+        this.getCandidateGeneListDao().create( candidateGeneList );
+        return candidateGeneList;
     }
-    
+
+    protected void handleRemoveCandidateGeneList( CandidateGeneList candidateGeneList ) throws java.lang.Exception {
+        this.getCandidateGeneListDao().remove( candidateGeneList );
+    }
+
+    protected void handleUpdateCandidateGeneList( CandidateGeneList candidateGeneList ) throws java.lang.Exception {
+        assert ( actor != null );
+        candidateGeneList.getAuditTrail().update( "CandidateGeneList Saved", actor );
+        this.getCandidateGeneListDao().update( candidateGeneList );
+    }
+
     // Finder methods
-    
-    public Collection handleFindByGeneOfficialName(String officialName){
-       return this.getCandidateGeneListDao().findByGeneOfficialName(officialName);
+
+    public Collection handleFindByGeneOfficialName( String officialName ) {
+        return this.getCandidateGeneListDao().findByGeneOfficialName( officialName );
     }
-    
-    public Collection handleFindByContributer(Person person){
-        return this.getCandidateGeneListDao().findByContributer(person);
+
+    public Collection handleFindByContributer( Person person ) {
+        return this.getCandidateGeneListDao().findByContributer( person );
     }
-   
-    public Collection handleGetAll(){
+
+    public Collection handleGetAll() {
         return this.getCandidateGeneListDao().findAll();
     }
-    public CandidateGeneList handleFindByID(long id){
-    	return this.getCandidateGeneListDao().findByID(id);
+
+    public CandidateGeneList handleFindByID( long id ) {
+        return this.getCandidateGeneListDao().findByID( id );
     }
-    
-    public Collection handleFindByListOwner(Person owner){
-    	return this.getCandidateGeneListDao().findByListOwner(owner);
+
+    public Collection handleFindByListOwner( Person owner ) {
+        return this.getCandidateGeneListDao().findByListOwner( owner );
     }
+
 }
