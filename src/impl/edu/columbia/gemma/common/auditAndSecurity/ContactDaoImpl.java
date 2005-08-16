cf68b7618a12eefd351e1b048a2d8bab14a943e6
pavlidis <>
2005-08-16 19:47:55 +0000

    findOrCreate and find for person and contact

---
+++ src/impl/edu/columbia/gemma/common/auditAndSecurity/ContactDaoImpl.java
@@ -0,0 +1,68 @@
+/*
+ * The Gemma project.
+ * 
+ * Copyright (c) 2005 Columbia University
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package edu.columbia.gemma.common.auditAndSecurity;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+
+/**
+ * <hr>
+ * <p>
+ * Copyright (c) 2004-2005 Columbia University
+ * 
+ * @author pavlidis
+ * @version $Id$
+ * @see edu.columbia.gemma.common.auditAndSecurity.Contact
+ */
+public class ContactDaoImpl extends edu.columbia.gemma.common.auditAndSecurity.ContactDaoBase {
+
+    @Override
+    public Contact findOrCreate( Contact contact ) {
+        try {
+            Criteria queryObject = super.getSession( false ).createCriteria( Contact.class );
+            queryObject.add( Restrictions.eq( "address", contact.getAddress() ) ).add(
+                    Restrictions.eq( "email", contact.getEmail() ) )
+                    .add( Restrictions.eq( "phone", contact.getPhone() ) );
+            java.util.List results = queryObject.list();
+            Object result = null;
+            if ( results != null ) {
+                if ( results.size() > 1 ) {
+                    throw new org.springframework.dao.InvalidDataAccessResourceUsageException(
+                            "More than one instance of '"
+                                    + edu.columbia.gemma.common.auditAndSecurity.Contact.class.getName()
+                                    + "' was found when executing query" );
+
+                } else if ( results.size() == 1 ) {
+                    result = ( edu.columbia.gemma.common.auditAndSecurity.Contact ) results.iterator().next();
+                }
+            }
+            return ( Contact ) result;
+        } catch ( org.hibernate.HibernateException ex ) {
+            throw super.convertHibernateAccessException( ex );
+        }
+    }
+
+    @Override
+    public Contact find( Contact contact ) {
+        if ( contact == null || contact.getName() == null ) return null;
+        Contact newContact = find( contact );
+        if ( newContact != null ) return newContact;
+        return ( Contact ) create( contact );
+    }
+}
---
+++ src/impl/edu/columbia/gemma/common/auditAndSecurity/PersonDaoImpl.java
@@ -0,0 +1,75 @@
+/*
+ * The Gemma project.
+ * 
+ * Copyright (c) 2005 Columbia University
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+package edu.columbia.gemma.common.auditAndSecurity;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+
+import edu.columbia.gemma.common.auditAndSecurity.Person;
+
+/**
+ * <hr>
+ * <p>
+ * Copyright (c) 2004-2005 Columbia University
+ * 
+ * @author pavlidis
+ * @version $Id$
+ * @see edu.columbia.gemma.common.auditAndSecurity.Person
+ */
+public class PersonDaoImpl extends edu.columbia.gemma.common.auditAndSecurity.PersonDaoBase {
+
+    @Override
+    public Person findOrCreate( Person person ) {
+        try {
+            Criteria queryObject = super.getSession( false ).createCriteria( Person.class );
+            queryObject.add( Restrictions.eq( "firstName", person.getFirstName() ) ).add(
+                    Restrictions.eq( "lastName", person.getLastName() ) ).add(
+                    Restrictions.eq( "middleName", person.getMiddleName() ) ).add(
+                    Restrictions.eq( "email", person.getEmail() ) ).add( Restrictions.eq( "phone", person.getPhone() ) )
+                    .add( Restrictions.eq( "address", person.getAddress() ) );
+            java.util.List results = queryObject.list();
+            Object result = null;
+            if ( results != null ) {
+                if ( results.size() > 1 ) {
+                    throw new org.springframework.dao.InvalidDataAccessResourceUsageException(
+                            "More than one instance of '"
+                                    + edu.columbia.gemma.common.auditAndSecurity.Person.class.getName()
+                                    + "' was found when executing query" );
+
+                } else if ( results.size() == 1 ) {
+                    result = ( edu.columbia.gemma.common.auditAndSecurity.Person ) results.iterator().next();
+                }
+            }
+            return ( Person ) result;
+        } catch ( org.hibernate.HibernateException ex ) {
+            throw super.convertHibernateAccessException( ex );
+        }
+    }
+
+    @Override
+    public Person find( Person person ) {
+        if ( person == null || person.getName() == null ) return null;
+        Person newPerson = find( person );
+        if ( newPerson != null ) return newPerson;
+        return ( Person ) create( person );
+    }
+
+}
