31e8b82ee2ae89abf512686c13169df6c4175185
keshav <>
2005-08-18 15:23:37 +0000

    moved our security stuff here

---
+++ src/impl/edu/columbia/gemma/security/interceptor/PersistAclInterceptorAfterAdvice.java
@@ -0,0 +1,133 @@
+package edu.columbia.gemma.security.interceptor;
+
+import java.lang.reflect.Method;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.aop.AfterReturningAdvice;
+
+/**
+ * This is an 'AfterAdvice' implementation of the 'AroundAdvice' PersistAclInterceptor
+ * <hr>
+ * <p>
+ * Copyright (c) 2004 - 2005 Columbia University
+ * 
+ * @author keshav
+ * @version $Id$
+ */
+public class PersistAclInterceptorAfterAdvice implements AfterReturningAdvice {
+    private static Log log = LogFactory.getLog( PersistAclInterceptorAfterAdvice.class.getName() );
+
+    /**
+     * Must implement this method if this class is going to be used as an interceptor
+     * 
+     * @param invocation
+     * @return
+     * @throws Throwable
+     */
+    public void afterReturning( Object retValue, Method m, Object[] args, Object target ) throws Throwable {
+
+        if ( isValidMethodToIntercept( m ) ) {
+            Object object = null;
+            log.info( "Before: method=[" + m + "]" );
+
+            log.info( "The method is: " + m.getName() );
+
+            Object[] arguments = args;
+            for ( Object obj : arguments ) {
+                object = obj;
+            }
+
+            String fullyQualifiedName = object.getClass().getName();
+            log.info( "The object is: " + fullyQualifiedName );
+
+            log.info( "Connecting to database ... " );
+            Connection c = makeDatabaseConnection();
+
+            // retVal = invocation.proceed();
+
+            Statement s1 = c.createStatement();
+            String last = null;
+            try {
+                ResultSet rs = s1.executeQuery( "SELECT * FROM array_design" );
+                int index = 0;
+
+                while ( rs.next() ) {
+                    last = rs.getString( 1 );
+                    log.info( "ID: " + last );
+                }
+                s1.close();
+            } catch ( SQLException se ) {
+                se.printStackTrace();
+                System.exit( 1 );
+            }
+
+            Statement s2 = c.createStatement();
+            try {
+                s2.executeUpdate( "INSERT INTO acl_object_identity (object_identity, parent_object, acl_class)"
+                        + "VALUES ('" + fullyQualifiedName + ":" + last
+                        + "',1,'net.sf.acegisecurity.acl.basic.SimpleAclEntry')" );
+            } catch ( SQLException se ) {
+                se.printStackTrace();
+                System.exit( 1 );
+            }
+            s2.close();
+
+            Statement s3 = c.createStatement();
+
+            try {
+
+                s3.executeUpdate( "INSERT INTO acl_permission (acl_object_identity, recipient, mask)"
+                        + "VALUES (LAST_INSERT_ID(),'pavlab',2)" );
+            } catch ( SQLException se ) {
+                se.printStackTrace();
+                System.exit( 1 );
+            }
+            s3.close();
+
+            log.info( "Acl persisted successfully." );
+
+            c.close();
+        } else {
+            log.info( "Invalid method to intercept" );
+        }
+
+    }
+
+    /**
+     * Test to see if this is a valid method to intercept. This will be fleshed out to add the actual methods intercept
+     * to the xml configuration file.
+     * 
+     * @param m
+     * @return
+     */
+    private boolean isValidMethodToIntercept( Method m ) {
+        if ( m.getName().contains( "save" ) ) return true;
+
+        return false;
+
+    }
+
+    /**
+     * Making a jdbc connection.
+     * 
+     * @return Connection
+     */
+    private Connection makeDatabaseConnection() {
+        Connection c = null;
+        try {
+            c = DriverManager.getConnection( "jdbc:mysql://localhost/gemd", "pavlidis", "toast" );
+        } catch ( SQLException se ) {
+            log.info( "Couldn't establish connection.  View stace trace." );
+            se.printStackTrace();
+            System.exit( 1 );
+        }
+        return c;
+    }
+
+}
---
+++ src/impl/edu/columbia/gemma/security/interceptor/PersistAclInterceptorBackend.java
@@ -0,0 +1,180 @@
+package edu.columbia.gemma.security.interceptor;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import net.sf.acegisecurity.Authentication;
+import net.sf.acegisecurity.GrantedAuthority;
+import net.sf.acegisecurity.UserDetails;
+import net.sf.acegisecurity.acl.basic.AclObjectIdentity;
+import net.sf.acegisecurity.acl.basic.BasicAclExtendedDao;
+import net.sf.acegisecurity.acl.basic.NamedEntityObjectIdentity;
+import net.sf.acegisecurity.acl.basic.SimpleAclEntry;
+import net.sf.acegisecurity.context.SecurityContextHolder;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.aop.AfterReturningAdvice;
+import org.springframework.dao.DataAccessException;
+
+/**
+ * An implementation of the 'AroundAdvice' PersistAclInterceptor. Persists acl_permission and acl_object_identity
+ * entries.
+ * <hr>
+ * <p>
+ * Copyright (c) 2004 - 2005 Columbia University
+ * 
+ * @author keshav
+ * @version $Id$
+ */
+public class PersistAclInterceptorBackend implements AfterReturningAdvice {
+    private static Log log = LogFactory.getLog( PersistAclInterceptorBackend.class.getName() );
+
+    private BasicAclExtendedDao basicAclExtendedDao;
+
+    /**
+     * Must implement this method if this class is going to be used as an interceptor
+     * 
+     * @param retValue
+     * @param m
+     * @param args - the argument to the method that is invoked.
+     * @param target
+     * @throws Throwable
+     */
+    public void afterReturning( Object retValue, Method m, Object[] args, Object target ) throws Throwable {
+
+        Object object = null;
+        log.debug( "Before: method=[" + m + "]" );
+        log.debug( "Target: " + target );
+
+        if ( m.getName().contains( "save" ) || m.getName().contains( "remove" ) ) {
+
+            object = args[0];
+
+            String fullyQualifiedName = object.getClass().getName();
+            log.debug( "The object is: " + fullyQualifiedName );
+
+            if ( m.getName().startsWith( "save" ) )
+                addPermission( object, getUsername(), getAuthority() );
+
+            else if ( m.getName().startsWith( "remove" ) ) deletePermission( object, getUsername() );
+
+        }
+
+    }
+
+    /**
+     * Creates the acl_permission object and the acl_object_identity object.
+     * 
+     * @param object - represents the domain object.
+     * @param recipient
+     * @param permission
+     * @throws NoSuchMethodException
+     * @throws IllegalArgumentException
+     * @throws IllegalAccessException
+     * @throws InvocationTargetException
+     */
+    public void addPermission( Object object, String recipient, Integer permission ) throws NoSuchMethodException,
+            IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+        SimpleAclEntry simpleAclEntry = new SimpleAclEntry();
+        simpleAclEntry.setAclObjectIdentity( makeObjectIdentity( object ) );
+        simpleAclEntry.setMask( permission.intValue() );
+        simpleAclEntry.setRecipient( recipient );
+
+        simpleAclEntry.setAclObjectParentIdentity( new NamedEntityObjectIdentity( "dummy", "1" ) );
+
+        basicAclExtendedDao.create( simpleAclEntry );
+
+        if ( log.isDebugEnabled() ) {
+            log.debug( "Added permission " + permission + " for recipient " + recipient + " on " + object );
+        }
+    }
+
+    /**
+     * Delete object and acl permissions
+     * 
+     * @param object
+     * @param recipient
+     * @throws InvocationTargetException
+     * @throws IllegalAccessException
+     * @throws NoSuchMethodException
+     * @throws IllegalArgumentException
+     * @throws DataAccessException
+     */
+    public void deletePermission( Object object, String recipient ) throws DataAccessException,
+            IllegalArgumentException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        // basicAclExtendedDao.delete( makeObjectIdentity( object ), recipient );
+        basicAclExtendedDao.delete( makeObjectIdentity( object ) );
+        if ( log.isDebugEnabled() ) {
+            log.debug( "Deleted object " + object + " ACL permissions for recipient " + recipient );
+        }
+    }
+
+    /**
+     * Forms the object identity to be inserted in acl_object_identity table.
+     * 
+     * @param object
+     * @return
+     * @throws NoSuchMethodException
+     * @throws InvocationTargetException
+     * @throws IllegalAccessException
+     * @throws IllegalArgumentException
+     */
+    private AclObjectIdentity makeObjectIdentity( Object object ) throws NoSuchMethodException,
+            IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+        log.info( object );
+
+        return new NamedEntityObjectIdentity( object );
+    }
+
+    /**
+     * Returns a String username.
+     * 
+     * @return
+     */
+    protected String getUsername() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if ( auth.getPrincipal() instanceof UserDetails ) {
+            return ( ( UserDetails ) auth.getPrincipal() ).getUsername();
+        }
+        return auth.getPrincipal().toString();
+
+    }
+
+    /**
+     * For the current principal, return the permissions mask. If the current principal has role "admin", they are
+     * granted ADMINISTRATION authority. If they are role "user", they are granted READ_WRITE authority.
+     * 
+     * @return
+     */
+    protected Integer getAuthority() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        GrantedAuthority[] ga = auth.getAuthorities();
+        for ( int i = 0; i < ga.length; i++ ) {
+            if ( ga[i].equals( "admin" ) ) {
+                log.debug( "Granting ADMINISTRATION privileges" );
+                return new Integer( SimpleAclEntry.ADMINISTRATION );
+            }
+        }
+        log.debug( "Granting READ_WRITE privileges" );
+        return new Integer( SimpleAclEntry.READ_WRITE );
+
+    }
+
+    /**
+     * @param basicAclExtendedDao
+     */
+    public void setBasicAclExtendedDao( BasicAclExtendedDao basicAclExtendedDao ) {
+        this.basicAclExtendedDao = basicAclExtendedDao;
+    }
+
+    /**
+     * @return
+     */
+    public BasicAclExtendedDao getBasicAclExtendedDao() {
+        return basicAclExtendedDao;
+    }
+
+}
