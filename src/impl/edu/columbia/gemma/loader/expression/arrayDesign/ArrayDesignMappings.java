da52bd75b7b34bc8fd601a88d095f67cebfa5a24
keshav <>
2005-08-19 04:23:33 +0000

    after making arrayDesign and designElement bi-directional, inverse="true" in arrayDesign.hbm.xml forces a call to be made to compositeSequence.setArrayDesign(arrayDesign).

--- src/impl/edu/columbia/gemma/loader/expression/arrayDesign/ArrayDesignMappings.java
+++ src/impl/edu/columbia/gemma/loader/expression/arrayDesign/ArrayDesignMappings.java
@@ -37,11 +37,12 @@ public class ArrayDesignMappings {
     private final int ARRAY_DESCRIPTION = conf.getInt( "array.description" );
     private final int ARRAY_NUM_OF_REPORTERS = conf.getInt( "array.numofreporters" );
 
-    private final int MGU74A_NAME = conf.getInt( "mgu74a.name" );
-    private final int MGU74A_DESCRIPTION = conf.getInt( "mgu74a.description" );
+    private final int MGU74A_COMPOSITESEQUENCE_NAME = conf.getInt( "mgu74a.compositesequence.name" );
+    private final int MGU74A_COMPOSITESEQUENCE_DESCRIPTION = conf.getInt( "mgu74a.compositesequence.description" );
 
     Map<String, ArrayDesign> arrayDesignMap = new HashMap<String, ArrayDesign>();
     Map<String, Contact> designProvidersMap = null;
+    Collection<DesignElement> designElements = new HashSet<DesignElement>();
 
     private ContactDao contactDao = null;
     private ArrayDesignDao arrayDesignDao = null;
@@ -87,14 +88,19 @@ public class ArrayDesignMappings {
         ArrayDesign arrayDesign = checkAndGetExistingArrayDesign( "MG-U74A" );
 
         CompositeSequence cs = CompositeSequence.Factory.newInstance();
-        cs.setName( values[MGU74A_NAME] );
-        cs.setDescription( values[MGU74A_DESCRIPTION] );
+        cs.setName( values[MGU74A_COMPOSITESEQUENCE_NAME] );
+        cs.setDescription( values[MGU74A_COMPOSITESEQUENCE_DESCRIPTION] );
 
-        Collection<DesignElement> designElements = new HashSet<DesignElement>();
+        // NOTE: if you use cs.setArrayDesign(arrayDesign), 
+        // make sure you set inverse="true" in ArrayDesign.hbm.xml under the <set> designElements.  This tells 
+        // hibernate to only propagate changes to the database that are made from the arrayDesign end.  That is,
+        // arrayDesign.setDesignElements(designElements).  If you do not set inverse="true", then you must leave
+        // this first line out (that is: cs.setArrayDesign(arrayDesign)).
+        cs.setArrayDesign( arrayDesign );
         designElements.add( cs );
         arrayDesign.setDesignElements( designElements );
 
-        return null;
+        return arrayDesign;
     }
 
     /**
@@ -104,6 +110,7 @@ public class ArrayDesignMappings {
      * @return ArrayDesign
      */
     private ArrayDesign checkAndGetExistingArrayDesign( String name ) {
+        log.info( "*******" + arrayDesignMap.containsKey( name ) );
         if ( arrayDesignMap.containsKey( name ) ) return arrayDesignMap.get( name );
 
         ArrayDesign ad = ArrayDesign.Factory.newInstance();
