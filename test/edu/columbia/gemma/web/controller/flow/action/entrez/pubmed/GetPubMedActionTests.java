43b15f649a14d2d13760fd266e8700505bd90e97
pavlidis <>
2005-08-28 00:38:00 +0000

    add some error checking, todos.

--- test/edu/columbia/gemma/web/controller/flow/action/entrez/pubmed/GetPubMedActionTests.java
+++ test/edu/columbia/gemma/web/controller/flow/action/entrez/pubmed/GetPubMedActionTests.java
@@ -1,6 +1,22 @@
+/*
+ * The Gemma project
+ * 
+ * Copyright (c) 2005 Columbia University
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package edu.columbia.gemma.web.controller.flow.action.entrez.pubmed;
-
-import junit.framework.TestCase;
 
 import org.easymock.MockControl;
 import org.springframework.webflow.test.AbstractFlowExecutionTests;
@@ -10,6 +26,46 @@ import org.springframework.webflow.Event;
 import edu.columbia.gemma.common.description.BibliographicReferenceImpl;
 import edu.columbia.gemma.common.description.BibliographicReferenceService;
 import edu.columbia.gemma.web.controller.flow.action.entrez.pubmed.GetPubMedAction;
+
+// //// here is an example of what this looks like, from the sample code
+// public class SearchPersonFlowTests extends AbstractFlowExecutionTests {
+//
+// public SearchPersonFlowTests() {
+// setDependencyCheck(false);
+// }
+//
+// protected String flowId() {
+// return "searchFlow";
+// }
+//
+// protected String[] getConfigLocations() {
+// return new String[] { "classpath:org/springframework/webflow/samples/phonebook/deploy/service-layer.xml",
+// "classpath:org/springframework/webflow/samples/phonebook/deploy/web-layer.xml" };
+// }
+//
+// public void testStartFlow() {
+// startFlow();
+// assertCurrentStateEquals("displayCriteria");
+// }
+//    
+// public void testCriteriaView_Submit_Success() {
+// startFlow();
+// Map parameters = new HashMap();
+// parameters.put("firstName", "Keith");
+// parameters.put("lastName", "Donald");
+// ViewDescriptor view = signalEvent(event("search", parameters));
+// assertCurrentStateEquals("displayResults");
+// Assert.collectionAttributeSizeEquals(view, "persons", 1);
+// }
+//    
+// public void testCriteriaView_Submit_Error() {
+// startFlow();
+// // simulate user error by not passing in any params
+// signalEvent(event("search"));
+// assertCurrentStateEquals("displayCriteria");
+// }
+//
+// }
 
 /**
  * Test the PubMedAction used in the flow pubMed.Search
@@ -23,9 +79,9 @@ import edu.columbia.gemma.web.controller.flow.action.entrez.pubmed.GetPubMedActi
 public class GetPubMedActionTests extends AbstractFlowExecutionTests {
 
     public GetPubMedActionTests() {
-        setDependencyCheck(false);
+        setDependencyCheck( false );
     }
-    
+
     public void testDoExecuteActionError() throws Exception {
         // set up mock object
         MockControl control = MockControl.createControl( BibliographicReferenceService.class );
