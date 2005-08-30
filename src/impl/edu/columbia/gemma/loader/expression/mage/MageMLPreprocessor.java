173f23310540f6a0cf831a6148752ba01b9c1a76
keshav <>
2005-08-30 16:36:04 +0000

    added Apache 2.0 License header

--- src/impl/edu/columbia/gemma/loader/expression/mage/MageMLPreprocessor.java
+++ src/impl/edu/columbia/gemma/loader/expression/mage/MageMLPreprocessor.java
@@ -1,5 +1,6 @@
 package edu.columbia.gemma.loader.expression.mage;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
@@ -22,19 +23,16 @@ import edu.columbia.gemma.loader.loaderutils.Preprocessor;
 public class MageMLPreprocessor implements Preprocessor {
 
     /**
-     * Create 2D matrices from raw data and the 3 dimensions - BioAssay, QuantitationType, DesignElement 
-     * Raw files:
-     * Column 1 - int - X 
-     * Column 2 - int - Y 
-     * Column 3 - double - Intensity
-     * Column 4 - double - Standard Deviation
-     * Column 5 - boolean - Pixels
-     * Column 6 - boolean - Outlier
+     * Create 2D matrices from raw data and the 3 dimensions - BioAssay, QuantitationType, DesignElement Raw files:
+     * Column 1 - int - X Column 2 - int - Y Column 3 - double - Intensity Column 4 - double - Standard Deviation Column
+     * 5 - int - Pixels Column 6 - boolean - Outlier Column 7 - boolean - Masked
+     * 
      * @param bioAssays
      * @param quantitationTypes
-     * @param designElements
-     * TODO this is a work in progress - the javadoc column names are for my benefit.  I will clean this up. 
+     * @param designElements TODO this is a work in progress - the javadoc column names are for my benefit. I will clean
+     *        this up.
      */
+    @SuppressWarnings("unchecked")
     public void preprocess( BioAssay bioAssay, List<QuantitationType> quantitationTypes,
             List<DesignElement> designElements ) {
         Log log = LogFactory.getLog( MageMLPreprocessor.class.getName() );
@@ -42,15 +40,25 @@ public class MageMLPreprocessor implements Preprocessor {
         log.info( " preprocessing the data ..." );
 
         log.debug( "There are " + quantitationTypes.size() + " quantitation types for bioassay " + bioAssay );
-        for ( Object qt : quantitationTypes ) {
+
+        List<QuantitationType> arrayList = new ArrayList();// used ArrayList because it is equivalent to Vector
+                                                            // (ordered and more efficient than LinkedList).
+        for ( QuantitationType qt : quantitationTypes ) {
             log.debug( "QuantitationType: " + qt );
+            //TODO send qt to the file as a title
+            //arrayList.add(  );
         }
 
         log.debug( "There are " + designElements.size() + " design elements for bioassay " + bioAssay );
-        for ( Object de : designElements ) {
+        for ( DesignElement de : designElements ) {
             log.debug( "DesignElement: " + de );
         }
 
     }
+
+    // you will want to parse the raw file, where each row is a design element and each column is quantitation type.
+    // the output will be a file where the title is the quantitation type, the x-axis is the design element (is this
+    // each
+    // row in the raw data file?
 
 }
