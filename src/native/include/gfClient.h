cbec90cadc4369985f693767e70317e141a4c605
pavlidis <>
2005-08-30 22:27:33 +0000

    headers

---
+++ src/native/include/gfClient.h
@@ -0,0 +1,10 @@
+#ifndef GFCLIENT_H
+#define GFCLIENT_H
+#include <jni.h>
+
+void gfClient(const char *hostName, const char *portName, const char *tSeqDir, const char *inName,
+             const char *outName, const char *tTypeName, const char *qTypeName);
+
+#endif /* GFCLIENT_H */
+
+
