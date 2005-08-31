d48e62c40f2098323b1ade61cce4292d8e9cd774
pavlidis <>
2005-08-31 02:04:39 +0000

    add unimplemented methods. I interpret "save" to mean "create", not "update"

---
+++ src/impl/edu/columbia/gemma/expression/experiment/ExperimentalDesignServiceImpl.java
@@ -0,0 +1,55 @@
+/*
+ * The Gemma project.
+ * 
+ * Copyright (c) 2005 Columbia University
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+package edu.columbia.gemma.expression.experiment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <hr>
+ * <p>
+ * Copyright (c) 2004-2005 Columbia University
+ * 
+ * @author pavlidis
+ * @version $Id$
+ * @see edu.columbia.gemma.expression.experiment.ExperimentalDesignService
+ */
+public class ExperimentalDesignServiceImpl extends
+        edu.columbia.gemma.expression.experiment.ExperimentalDesignServiceBase {
+
+    /**
+     * @see edu.columbia.gemma.expression.experiment.ExperimentalDesignService#getExperimentalDesigns()
+     */
+    @SuppressWarnings("unchecked")
+    protected java.util.List handleGetExperimentalDesigns() throws java.lang.Exception {
+        List result = new ArrayList();
+        result.addAll( this.getExperimentalDesignDao().loadAll() );
+        return result;
+    }
+
+    /**
+     * @see edu.columbia.gemma.expression.experiment.ExperimentalDesignService#saveExperimentalDesign(edu.columbia.gemma.expression.experiment.ExperimentalDesign)
+     */
+    protected void handleSaveExperimentalDesign(
+            edu.columbia.gemma.expression.experiment.ExperimentalDesign experimentalDesign ) throws java.lang.Exception {
+        this.getExperimentalDesignDao().create( experimentalDesign );
+    }
+}
