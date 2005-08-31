e767515499761af7b3fa1514a3ffa51a5ab8b5a2
pavlidis <>
2005-08-31 10:52:34 +0000

    remove redundant contact

--- metadata/conf/init-script.sql
+++ metadata/conf/init-script.sql
@@ -1,7 +1,7 @@
 insert into CONTACT (CLASS, NAME, LAST_NAME, FIRST_NAME, MIDDLE_NAME) values ("PersonImpl", "person", "nobody", "nobody", "nobody");
 insert into CONTACT (CLASS, NAME, USER_NAME, PASSWORD, CONFIRM_PASSWORD, ENABLED) values ("UserImpl", "user", "administrator", "cec6877e42b179b133c2a6c1285488755aefc071", "cec6877e42b179b133c2a6c1285488755aefc071", 1);
 insert into CONTACT (CLASS, NAME, EMAIL) values ("edu.columbia.gemma.common.auditAndSecurity.ContactImpl", "contact", "admin@gemma.org");
-insert into CONTACT (NAME, USER_NAME) values ("admin", "administrator");
+-- insert into CONTACT (NAME, USER_NAME) values ("admin", "administrator");
 
 insert into TAXON (SCIENTIFIC_NAME,COMMON_NAME,NCBI_ID) values ("homo sapien","human","9606");
 insert into TAXON (SCIENTIFIC_NAME,COMMON_NAME,NCBI_ID) values ("mus musculus","mouse","10090"); 
