9b126cd5953f1a548ba0a3252f5cce94884c91b9
pavlidis <>
2005-08-31 01:25:56 +0000

    Fix capitalization of table names

--- metadata/conf/init-script.sql
+++ metadata/conf/init-script.sql
@@ -1,11 +1,11 @@
-insert into contact (CLASS, NAME, LAST_NAME, FIRST_NAME, MIDDLE_NAME) values ("PersonImpl", "person", "nobody", "nobody", "nobody");
-insert into contact (CLASS, NAME, USER_NAME, PASSWORD, CONFIRM_PASSWORD, ENABLED) values ("UserImpl", "user", "administrator", "cec6877e42b179b133c2a6c1285488755aefc071", "cec6877e42b179b133c2a6c1285488755aefc071", 1);
-insert into contact (CLASS, NAME, EMAIL) values ("edu.columbia.gemma.common.auditAndSecurity.ContactImpl", "contact", "admin@gemma.org");
-insert into user_role (NAME, USER_NAME) values ("admin", "administrator");
+insert into CONTACT (CLASS, NAME, LAST_NAME, FIRST_NAME, MIDDLE_NAME) values ("PersonImpl", "person", "nobody", "nobody", "nobody");
+insert into CONTACT (CLASS, NAME, USER_NAME, PASSWORD, CONFIRM_PASSWORD, ENABLED) values ("UserImpl", "user", "administrator", "cec6877e42b179b133c2a6c1285488755aefc071", "cec6877e42b179b133c2a6c1285488755aefc071", 1);
+insert into CONTACT (CLASS, NAME, EMAIL) values ("edu.columbia.gemma.common.auditAndSecurity.ContactImpl", "contact", "admin@gemma.org");
+insert into CONTACT (NAME, USER_NAME) values ("admin", "administrator");
 
-insert into taxon (SCIENTIFIC_NAME,COMMON_NAME,NCBI_ID) values ("homo sapien","human","9606");
-insert into taxon (SCIENTIFIC_NAME,COMMON_NAME,NCBI_ID) values ("mus musculus","mouse","10090"); 
-insert into taxon (SCIENTIFIC_NAME,COMMON_NAME,NCBI_ID) values ("rattus","rat","10114");
+insert into TAXON (SCIENTIFIC_NAME,COMMON_NAME,NCBI_ID) values ("homo sapien","human","9606");
+insert into TAXON (SCIENTIFIC_NAME,COMMON_NAME,NCBI_ID) values ("mus musculus","mouse","10090"); 
+insert into TAXON (SCIENTIFIC_NAME,COMMON_NAME,NCBI_ID) values ("rattus","rat","10114");
 
-insert into external_database(NAME, DESCRIPTION, WEB_URI, FTP_URI) values ("PubMed", "PubMed database from NCBI", "http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?db=PubMed", "ftp://ftp.ncbi.nlm.nih.gov/pubmed/");
-insert into external_database(NAME, DESCRIPTION,  WEB_URI, FTP_URI) values ("GO", "Gene Ontology database", "http://www.godatabase.org/dev/database/", "http://archive.godatabase.org");
+insert into EXTERNAL_DATABASE (NAME, DESCRIPTION, WEB_URI, FTP_URI) values ("PubMed", "PubMed database from NCBI", "http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?db=PubMed", "ftp://ftp.ncbi.nlm.nih.gov/pubmed/");
+insert into EXTERNAL_DATABASE (NAME, DESCRIPTION,  WEB_URI, FTP_URI) values ("GO", "Gene Ontology database", "http://www.godatabase.org/dev/database/", "http://archive.godatabase.org");
--- metadata/conf/mysql-acegi-acl.sql
+++ metadata/conf/mysql-acegi-acl.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS acl_object_identity;
-CREATE TABLE acl_object_identity (
+DROP TABLE IF EXISTS ACL_OBJECT_IDENTITY;
+CREATE TABLE ACL_OBJECT_IDENTITY (
   id int(11) NOT NULL auto_increment,
   object_identity varchar(255) NOT NULL,
   parent_object int(11) default NULL,
@@ -8,8 +8,8 @@ CREATE TABLE acl_object_identity (
   UNIQUE KEY object_identity (object_identity)
 ) TYPE=InnoDB;
 
-DROP TABLE IF EXISTS acl_permission;
-CREATE TABLE acl_permission (
+DROP TABLE IF EXISTS ACL_PERMISSION;
+CREATE TABLE ACL_PERMISSION (
   id int(11) NOT NULL auto_increment,
   acl_object_identity int(11) NOT NULL,
   recipient varchar(100) NOT NULL,
@@ -18,17 +18,17 @@ CREATE TABLE acl_permission (
 ) TYPE=InnoDB;
 
 --- (id, object identity (of form class:getId()), parent object, acl class)
-INSERT INTO acl_object_identity VALUES (1, 'dummy:1', null, 'net.sf.acegisecurity.acl.basic.SimpleAclEntry');
+INSERT INTO ACL_OBJECT_IDENTITY VALUES (1, 'dummy:1', null, 'net.sf.acegisecurity.acl.basic.SimpleAclEntry');
 
 --- (id, acl object identity, recepient (principal username), mask)
-INSERT INTO acl_permission VALUES (null, 1, 'administrator', 1);
+INSERT INTO ACL_PERMISSION VALUES (null, 1, 'administrator', 1);
 
 --- Add the user pavlab to the database.  Due to the fact that this user is used specifically for testing acegi security (acl), I have
 --- put the insert statements in this acegi specific .sql file as opposed to the init-script.sql.
-insert into contact (CLASS, NAME, LAST_NAME, FIRST_NAME, MIDDLE_NAME) values ("PersonImpl", "aPersonWithUserPriviledges", "lastname", "firstname", "middlename");
-insert into contact (CLASS, NAME, USER_NAME, PASSWORD, CONFIRM_PASSWORD, ENABLED) values ("UserImpl", "aUserWithUserPriviledges", "pavlab", "7478822cc546bb339c7cd198ca96df2b7a50cf5c", "7478822cc546bb339c7cd198ca96df2b7a50cf5c", 1);
-insert into contact (CLASS, NAME, EMAIL) values ("edu.columbia.gemma.common.auditAndSecurity.ContactImpl", "contact", "keshav@cu-genome.org");
-insert into user_role (NAME, USER_NAME) values ("user", "pavlab");
+insert into CONTACT (CLASS, NAME, LAST_NAME, FIRST_NAME, MIDDLE_NAME) values ("PersonImpl", "aPersonWithUserPriviledges", "lastname", "firstname", "middlename");
+insert into CONTACT (CLASS, NAME, USER_NAME, PASSWORD, CONFIRM_PASSWORD, ENABLED) values ("UserImpl", "aUserWithUserPriviledges", "pavlab", "7478822cc546bb339c7cd198ca96df2b7a50cf5c", "7478822cc546bb339c7cd198ca96df2b7a50cf5c", 1);
+insert into CONTACT (CLASS, NAME, EMAIL) values ("edu.columbia.gemma.common.auditAndSecurity.ContactImpl", "contact", "keshav@cu-genome.org");
+insert into USER_ROLE (NAME, USER_NAME) values ("user", "pavlab");
 
 --- Mask integer 0  = no permissions
 --- Mask integer 1  = administrator
