7b0e3964667a7230208e0476c8b6cf1708173272
keshav <>
2005-09-01 18:47:00 +0000

    moved username and password to Gemma.properties.  Added apache license.

--- test/edu/columbia/gemma/BaseServiceTestCase.java
+++ test/edu/columbia/gemma/BaseServiceTestCase.java
@@ -1,3 +1,21 @@
+/*
+ * The Gemma project
+ * 
+ * Copyright (c) 2005 Columbia University
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package edu.columbia.gemma;
 
 import java.util.Map;
@@ -7,9 +25,16 @@ import java.util.ResourceBundle;
 import junit.framework.TestCase;
 
 import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.BeanFactory;
+
+import edu.columbia.gemma.security.ui.ManualAuthenticationProcessing;
 import edu.columbia.gemma.util.ConvertUtil;
+import edu.columbia.gemma.util.SpringContextUtil;
 
 /**
  * A service is a class that uses daos. To test the service without testing the daos (and using the database), we use
@@ -29,10 +54,30 @@ public class BaseServiceTestCase extends TestCase {
 
    protected final Log log = LogFactory.getLog( getClass() );
    protected ResourceBundle rb;
+   
+   protected BeanFactory ctx = SpringContextUtil.getApplicationContext();
+
+   /* authentication */
+   ManualAuthenticationProcessing manAuthentication = ( ManualAuthenticationProcessing ) ctx
+           .getBean( "manualAuthenticationProcessing" );
 
    //~ Constructors ===========================================================
-
+   
    public BaseServiceTestCase() {
+       
+       /* set user */
+       String username = null;
+       String password = null;
+           try {
+            Configuration conf = new PropertiesConfiguration( "Gemma.properties" );
+            username = conf.getString("acegi.authorization.username");
+            password = conf.getString("acegi.authorization.password");
+        } catch ( ConfigurationException e ) {
+            e.printStackTrace();
+        }
+       
+       manAuthentication.validateRequest(username, password);
+       
       // Since a ResourceBundle is not required for each class, just
       // do a simple check to see if one exists
       String className = this.getClass().getName();
