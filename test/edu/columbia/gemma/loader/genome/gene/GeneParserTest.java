01a7165d56c9e16726365519ed1a516f7455d275
pavlidis <>
2005-09-09 22:00:29 +0000

    add .txt extension to file names

--- test/edu/columbia/gemma/loader/genome/gene/GeneParserTest.java
+++ test/edu/columbia/gemma/loader/genome/gene/GeneParserTest.java
@@ -1,3 +1,21 @@
+/*
+ * The Gemma project
+ * 
+ * Copyright (c) 2005 Columbia University
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package edu.columbia.gemma.loader.genome.gene;
 
 import java.io.InputStream;
@@ -40,11 +58,11 @@ public class GeneParserTest extends BaseServiceTestCase {
      * @throws Exception
      */
     public void testParseAndLoad() throws Exception {
-        InputStream is = this.getClass().getResourceAsStream( "/data/loader/genome/gene/geneinfo" );
+        InputStream is = this.getClass().getResourceAsStream( "/data/loader/genome/gene/geneinfo.txt" );
         Method m = ParserAndLoaderTools.findParseLineMethod( geneParser.getGeneMappings(), "geneinfo" );
         geneParser.parse( is, m );
 
-        InputStream is2 = this.getClass().getResourceAsStream( "/data/loader/genome/gene/gene2accession" );
+        InputStream is2 = this.getClass().getResourceAsStream( "/data/loader/genome/gene/gene2accession.txt" );
         Method m2 = ParserAndLoaderTools.findParseLineMethod( geneParser.getGeneMappings(), "gene2accession" );
         map = geneParser.parse( is2, m2 );
 
