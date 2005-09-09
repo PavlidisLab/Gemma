3f8f60d08eff801d54eed2b14839a3e9391578fd
pavlidis <>
2005-09-09 17:43:41 +0000

    minor

--- test/edu/columbia/gemma/BaseDAOTestCase.java
+++ test/edu/columbia/gemma/BaseDAOTestCase.java
@@ -1,3 +1,21 @@
+/*
+ * The Gemma project
+ * 
+ * Copyright (c) 2005 Columbia University
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package edu.columbia.gemma;
 
 import java.util.Enumeration;
@@ -27,8 +45,6 @@ public class BaseDAOTestCase extends TestCase {
     protected final static BeanFactory ctx = SpringContextUtil.getApplicationContext();
     protected ResourceBundle rb;
 
-    
-    
     public BaseDAOTestCase() {
         // Since a ResourceBundle is not required for each class, just
         // do a simple check to see if one exists
