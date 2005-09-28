10901dbe3c5855bba0198ee51ee593ea516557a9
pavlidis <>
2005-09-28 19:23:31 +0000

    stub

---
+++ src/web/edu/columbia/gemma/web/controller/expression/experiment/ExperimentListController.java
@@ -0,0 +1,27 @@
+package edu.columbia.gemma.web.controller.expression.experiment;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+/**
+ * 
+ *
+ * <hr>
+ * <p>Copyright (c) 2004-2005 Columbia University
+ * @author pavlidis
+ * @version $Id$
+ */
+public class ExperimentListController implements Controller {
+
+    /* (non-Javadoc)
+     * @see org.springframework.web.servlet.mvc.Controller#handleRequest(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     */
+    public ModelAndView handleRequest( HttpServletRequest request, HttpServletResponse response ) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
