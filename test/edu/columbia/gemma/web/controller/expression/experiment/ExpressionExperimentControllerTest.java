0405200f0f203cee763cf6ee13d25f50c0462307
keshav <>
2005-10-11 08:02:10 +0000

    packed attribute in mock req

--- test/edu/columbia/gemma/web/controller/expression/experiment/ExpressionExperimentControllerTest.java
+++ test/edu/columbia/gemma/web/controller/expression/experiment/ExpressionExperimentControllerTest.java
@@ -88,6 +88,7 @@ public class ExpressionExperimentControllerTest extends BaseControllerTestCase {
                 .getBean( "expressionExperimentController" );
 
         MockHttpServletRequest req = new MockHttpServletRequest( "GET", "Gemma/expressionExperiments.htm" );
+        req.setRequestURI("/Gemma/expressionExperiments.htm");
 
         ModelAndView mav = c.handleRequest( req, ( HttpServletResponse ) null );
 
