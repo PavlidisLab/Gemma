b4f0263799e7ec209a75e818f4fa08a145d38a1e
keshav <>
2005-10-11 08:04:58 +0000

    ee controller to handle multiple actions

--- src/web/edu/columbia/gemma/web/controller/expression/experiment/ExpressionExperimentController.java
+++ src/web/edu/columbia/gemma/web/controller/expression/experiment/ExpressionExperimentController.java
@@ -1,35 +1,59 @@
+/*
+ * The Gemma project
+ * 
+ * Copyright (c) 2005 Columbia University
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package edu.columbia.gemma.web.controller.expression.experiment;
+
+import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
+import edu.columbia.gemma.expression.experiment.ExpressionExperiment;
 import edu.columbia.gemma.expression.experiment.ExpressionExperimentService;
 import edu.columbia.gemma.web.controller.common.description.BibliographicReferenceController;
 
 /**
- * 
- * 
- *
  * <hr>
- * <p>Copyright (c) 2004 - 2005 Columbia University
+ * <p>
+ * Copyright (c) 2004 - 2005 Columbia University
+ * 
  * @author keshav
  * @author daq2101
  * @version $Id$
- * 
- * @spring.bean id="expressionExperimentController" name="/expressionExperiments.htm"
+ * @spring.bean id="expressionExperimentController" name="/expressionExperiments.htm /experimentalDesigns.htm
+ *              /expressionExperimentDetails.htm"
  * @spring.property name = "expressionExperimentService" ref="expressionExperimentService"
+ * @spring.property name = "messageSource" ref="messageSource"
  */
-public class ExpressionExperimentController implements Controller{
-    
+public class ExpressionExperimentController implements Controller {
+
     private static Log log = LogFactory.getLog( BibliographicReferenceController.class.getName() );
 
     private ExpressionExperimentService expressionExperimentService = null;
-    
+
+    private ResourceBundleMessageSource messageSource = null;
+
     /**
      * @param request
      * @param response
@@ -40,10 +64,49 @@ public class ExpressionExperimentController implements Controller{
         if ( log.isDebugEnabled() )
             log.debug( "entered 'handleRequest'" + " HttpServletRequest: " + request + " HttpServletResponse: "
                     + response );
+
+        Locale locale = request.getLocale();
+
+        String uri = request.getRequestURI();
+
+        log.info( uri );
         
-        log.info(expressionExperimentService.getAllExpressionExperiments().size());
-        return new ModelAndView("expressionExperiment.GetAll.results.view", "expressionExperiments",
-                expressionExperimentService.getAllExpressionExperiments());
+        /* handle "get all" case. */
+        if ( uri.equals( "/Gemma/expressionExperiments.htm" ) )
+            return new ModelAndView( "expressionExperiment.GetAll.results.view", "expressionExperiments",
+                    expressionExperimentService.getAllExpressionExperiments() );
+        
+        /* handle details or delete, depending on whether _eventId=delete. */
+        else if ( uri.equals( "/Gemma/expressionExperimentDetails.htm" ) ) {
+            
+            /* passed from jsp, and must be packed again to view in the next jsp. */
+            request.setAttribute( "name", request.getParameter( "name" ) );
+            log.debug( "request parameter: " + request.getAttribute( "name" ) );
+
+            ExpressionExperiment ee = expressionExperimentService.findByName( request.getParameter( "name" ) );
+
+            String event = request.getParameter( "_eventId" );
+            if ( event != null && event.equals( "delete" ) ) {
+                expressionExperimentService.removeExpressionExperiment( ee );
+                log.info( "Expression experiment with name: " + ee.getName() + " deleted" );
+                request.getSession().setAttribute(
+                        "messages",
+                        messageSource
+                                .getMessage( "expressionExperiment.deleted", new Object[] { ee.getName() }, locale ) );
+                /* delete */
+                return new ModelAndView( "expressionExperiment.GetAll.results.view", "expressionExperiments",
+                        expressionExperimentService.getAllExpressionExperiments() );
+            }
+            
+            /* details */
+            return new ModelAndView( "expressionExperiment.Detail.view", "expressionExperiment",
+                    expressionExperimentService.findByName( request.getParameter( "name" ) ) );
+        }
+        // else if ( uri.equals( "/Gemma/experimentalDesigns.htm" ) )
+        // return new ModelAndView( "experimentalDesign.GetAll.results.view", "experimentalDesigns",
+        // expressionExperimentService.getAllExpressionExperiments() );
+        else
+            throw new RuntimeException( "There is no view to match the url" );
     }
 
     /**
@@ -51,6 +114,13 @@ public class ExpressionExperimentController implements Controller{
      */
     public void setExpressionExperimentService( ExpressionExperimentService expressionExperimentService ) {
         this.expressionExperimentService = expressionExperimentService;
+    }
+
+    /**
+     * @param messageSource The messageSource to set.
+     */
+    public void setMessageSource( ResourceBundleMessageSource messageSource ) {
+        this.messageSource = messageSource;
     }
 
 }
