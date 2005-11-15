a6313a03669519d805829c6ca33bf593cf514d6e
as2628 <>
2005-11-15 23:35:27 +0000

    edited to acount for gene aliases (was crashing gene loader)

--- src/impl/edu/columbia/gemma/loader/genome/gene/ncbi/NcbiGeneConverter.java
+++ src/impl/edu/columbia/gemma/loader/genome/gene/ncbi/NcbiGeneConverter.java
@@ -58,15 +58,13 @@ public class NcbiGeneConverter implements Converter {
      * @see edu.columbia.gemma.loader.loaderutils.Converter#convert(java.lang.Object)
      */
     @SuppressWarnings("unchecked")
-    public Object convert( Object sourceDomainObject ) {
-        assert sourceDomainObject instanceof NCBIGene2Accession;
-        NCBIGene2Accession ncbiGene = ( NCBIGene2Accession ) sourceDomainObject;
-        NCBIGeneInfo info = ncbiGene.getInfo();
+    public Object convert( NCBIGeneInfo info ) {
         Gene gene = Gene.Factory.newInstance();
 
-        gene.setNcbiId( ncbiGene.getGeneId() );
+        gene.setNcbiId( info.getGeneId() );
         gene.setOfficialSymbol( info.getDefaultSymbol() );
-        gene.setOfficialName( info.getDescription() );
+        gene.setOfficialName( info.getDefaultSymbol() );
+        gene.setDescription(info.getDescription());
 
         Taxon t = Taxon.Factory.newInstance();
         t.setNcbiId( new Integer( info.getTaxId() ) );
@@ -78,15 +76,33 @@ public class NcbiGeneConverter implements Converter {
         // gene.setProducts();
         //        gene.setAccessions();
 
+        //System.out.println("synonyms: "+info.getSynonyms());
+        //System.out.println("aliases: "+gene.getGeneAliasses());
+        //System.out.println("aliases null: "+(gene.getGeneAliasses()==null));
         Collection<GeneAlias> aliases = gene.getGeneAliasses();
         for ( String alias : info.getSynonyms() ) {
             GeneAlias newAlias = GeneAlias.Factory.newInstance();
             newAlias.setGene( gene );
             newAlias.setSymbol( alias );
+            newAlias.setAlias(alias);  //added by AS - non-nullable, won't work w/o it!
             aliases.add( newAlias );
         }
-
+        
+        System.out.println("added aliases");
         return gene;
+
+    }
+    
+    /*
+     * (non-Javadoc)
+     * 
+     * @see edu.columbia.gemma.loader.loaderutils.Converter#convert(java.lang.Object)
+     */
+    @SuppressWarnings("unchecked")
+    public Object convert( Object sourceDomainObject ) {
+        assert sourceDomainObject instanceof NCBIGene2Accession;
+        NCBIGene2Accession ncbiGene = ( NCBIGene2Accession ) sourceDomainObject;
+        return convert(ncbiGene.getInfo());
 
     }
 }
