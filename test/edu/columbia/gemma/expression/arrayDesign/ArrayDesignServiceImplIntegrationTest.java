bedb1a99d5a808c85c2720084688d221b5804bb9
keshav <>
2005-11-29 03:27:38 +0000

    updated authorization and authentication tests.

--- test/edu/columbia/gemma/expression/arrayDesign/ArrayDesignServiceImplIntegrationTest.java
+++ test/edu/columbia/gemma/expression/arrayDesign/ArrayDesignServiceImplIntegrationTest.java
@@ -20,6 +20,7 @@ package edu.columbia.gemma.expression.arrayDesign;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.ResourceBundle;
 
 import org.springframework.beans.factory.BeanFactory;
@@ -30,7 +31,6 @@ import edu.columbia.gemma.expression.designElement.CompositeSequence;
 import edu.columbia.gemma.expression.designElement.CompositeSequenceService;
 import edu.columbia.gemma.expression.designElement.DesignElement;
 import edu.columbia.gemma.security.ui.ManualAuthenticationProcessing;
-import edu.columbia.gemma.util.SpringContextUtil;
 
 /**
  * Use this to test the acegi functionality. Namely, this is a good test to illustrate how getting This test class
@@ -61,86 +61,94 @@ public class ArrayDesignServiceImplIntegrationTest extends BaseServiceTestCase {
 
     /**
      * Test removing an arrayDesign without having the correct authorization privileges. You should get an
-     * unsuccessfulAuthentication. Does not use mock objects because I need the data from the database.
+     * unsuccessfulAuthentication. Does not use mock objects because I need the data from the database. This test does
+     * not use SpringContextUtil because I wanted to test the acegi implementation and SpringContextUtil grants
+     * authorization for all users.
      * 
      * @throws Exception
      */
-    // public void testRemoveArrayDesignWithoutAuthorizationWithoutMock() throws Exception {
-    // BeanFactory ctx = SpringContextUtil.getApplicationContext();
-    //
-    // ManualAuthenticationProcessing manAuthentication = ( ManualAuthenticationProcessing ) ctx
-    // .getBean( "manualAuthenticationProcessing" );
-    //
-    // manAuthentication.validateRequest( "KiranKeshav", "7Champin" );
-    //
-    // ArrayDesignService ads = ( ArrayDesignService ) ctx.getBean( "arrayDesignService" );
-    //
-    // Collection<ArrayDesign> col = ads.getAllArrayDesigns();
-    // if ( col.size() == 0 )
-    // log.info( "There are no arrayDesigns in database" );
-    //
-    // else {
-    // Iterator iter = col.iterator();
-    // ArrayDesign ad = ( ArrayDesign ) iter.next();
-    //
-    // ads.removeArrayDesign( ad );
-    // }
-    //
-    // 
-    // assertNull( null, null );
-    // }
+    @SuppressWarnings("unchecked")
+    public void testRemoveArrayDesignWithoutAuthorizationWithoutMock() throws Exception {
+
+        ResourceBundle db = ResourceBundle.getBundle( "Gemma" );
+        String daoType = db.getString( "dao.type" );
+        String servletContext = db.getString( "servlet.name.0" );
+        String[] paths = { "applicationContext-localDataSource.xml", "applicationContext-" + daoType + ".xml",
+                "applicationContext-security.xml", servletContext + "-servlet.xml", "applicationContext-validation.xml" };
+
+        BeanFactory cpCtx = new ClassPathXmlApplicationContext( paths );
+
+        /* Manual Authentication */
+        ManualAuthenticationProcessing manAuthentication = ( ManualAuthenticationProcessing ) cpCtx
+                .getBean( "manualAuthenticationProcessing" );
+
+        manAuthentication.validateRequest( "keshav", "pavlab" );
+
+        ArrayDesignService ads = ( ArrayDesignService ) ctx.getBean( "arrayDesignService" );
+
+        Collection<ArrayDesign> col = ads.getAllArrayDesigns();
+        if ( col.size() == 0 )
+            log.info( "There are no arrayDesigns in database" );
+
+        else {
+            Iterator iter = col.iterator();
+            ArrayDesign ad = ( ArrayDesign ) iter.next();
+
+            ads.remove( ad );
+        }
+
+        assertNull( null, null );
+    }
+
     /**
      * Test removing an arrayDesign with correct authorization. The security interceptor should be called on this
      * method, as should the AddOrRemoveFromACLInterceptor. Does not use mock objects because I need to remove an
-     * element from a live database.
+     * element from a live database. This test does not use SpringContextUtil because I wanted to test the acegi
+     * implementation and SpringContextUtil grants authorization for all users.
      * 
      * @throws Exception
      */
-//    public void testRemoveArrayDesignWithoutMock() throws Exception {
-//        /*
-//         * Use this or your ManualAuthenticationProcessing solution. The authentication from a non-http client here
-//         * comes from Fish du jour - See SpringContextUtil BeanFactory ctx = SpringContextUtil.getApplicationContext(
-//         * false );
-//         */
-//
-//        /*
-//         * Context Setup - I did not use SpringContextUtil because this creates a principal provides both admin and user
-//         * rights. This test is used to go deeper and actually test out the security so I don't want to use it.
-//         */
-//        ResourceBundle db = ResourceBundle.getBundle( "Gemma" );
-//        String daoType = db.getString( "dao.type" );
-//        String servletContext = db.getString( "servlet.name.0" );
-//        String[] paths = { "applicationContext-localDataSource.xml", "applicationContext-" + daoType + ".xml",
-//                "applicationContext-security.xml", servletContext + "-servlet.xml", "applicationContext-validation.xml" };
-//
-//        BeanFactory cpCtx = new ClassPathXmlApplicationContext( paths );
-//
-//        /* Manual Authentication */
-//        ManualAuthenticationProcessing manAuthentication = ( ManualAuthenticationProcessing ) cpCtx
-//                .getBean( "manualAuthenticationProcessing" );
-//
-//        manAuthentication.validateRequest( "pavlab", "pavlab" );
-//
-//        /* get bean and invoke methods. */
-//        ArrayDesignService ads = ( ArrayDesignService ) cpCtx.getBean( "arrayDesignService" );
-//
-//        ArrayDesign ad = ads.findArrayDesignByName( "AD Foo" );
-//        if ( ad == null )
-//            log.info( "ArrayDesign does not exist" );
-//
-//        else{
-//            ads.removeArrayDesign( ad );
-//            log.info("*** REMOVED ***");
-//        }
-//    }
+    public void testRemoveArrayDesignWithoutMock() throws Exception {
+        /*
+         * Use this or your ManualAuthenticationProcessing solution. The authentication from a non-http client here
+         * comes from Fish du jour - See SpringContextUtil BeanFactory ctx = SpringContextUtil.getApplicationContext(
+         * false );
+         */
+        ResourceBundle db = ResourceBundle.getBundle( "Gemma" );
+        String daoType = db.getString( "dao.type" );
+        String servletContext = db.getString( "servlet.name.0" );
+        String[] paths = { "applicationContext-localDataSource.xml", "applicationContext-" + daoType + ".xml",
+                "applicationContext-security.xml", servletContext + "-servlet.xml", "applicationContext-validation.xml" };
+
+        BeanFactory cpCtx = new ClassPathXmlApplicationContext( paths );
+
+        /* Manual Authentication */
+        ManualAuthenticationProcessing manAuthentication = ( ManualAuthenticationProcessing ) cpCtx
+                .getBean( "manualAuthenticationProcessing" );
+
+        manAuthentication.validateRequest( "keshav", "pavlab" );
+
+        /* get bean and invoke methods. */
+        ArrayDesignService ads = ( ArrayDesignService ) cpCtx.getBean( "arrayDesignService" );
+
+        ArrayDesign ad = ads.findArrayDesignByName( "AD Foo" );
+        if ( ad == null )
+            log.info( "ArrayDesign does not exist" );
+
+        else {
+            ads.remove( ad );
+            log.info( "*** REMOVED ***" );
+        }
+    }
 
     /**
      * Save an array design
      * 
      * @throws Exception
      */
+    @SuppressWarnings("unchecked")
     public void testSaveArrayDesignWithoutMock() {
-        
+
         ArrayDesignService ads = ( ArrayDesignService ) ctx.getBean( "arrayDesignService" );
 
         ArrayDesign arrayDesign = ArrayDesign.Factory.newInstance();
@@ -164,29 +172,28 @@ public class ArrayDesignServiceImplIntegrationTest extends BaseServiceTestCase {
         cs2.setArrayDesign( arrayDesign );
         arrayDesign.setDesignElements( col );
 
-      
-            ArrayDesign ad = ads.findOrCreate( arrayDesign );
-            log.info( "*** SAVED ***" );
+        ads.findOrCreate( arrayDesign );
 
-        
+        log.info( "*** SAVED ***" );
 
     }
+
     /**
-     * Will test getting all design elements given authorization on an array design (ie. tests getting the 'owned
-     * objects' given the authorization on an owner). This test was used to test the Acegi Security functionality. Mock
-     * objects not used because I need the objects from the database.
+     * Tests getting all design elements given authorization on an array design (ie. tests getting the 'owned objects'
+     * given the authorization on the owner). This test was used to test the Acegi Security functionality. Mock objects
+     * not used because I need the objects from the database.
      * 
      * @throws Exception
      */
-    // @SuppressWarnings("unchecked")
-    // public void testGetAllDesignElementsFromArrayDesignsWithoutMock() {
-    // CompositeSequenceService css = ( CompositeSequenceService ) ctx.getBean( "compositeSequenceService" );
-    //    
-    // Collection<CompositeSequence> col = css.getAllCompositeSequences();
-    // for ( CompositeSequence cs : col ) {
-    // log.debug( cs );
-    // }
-    //    
-    // if ( col.size() == 0 ) fail( "User not authorized for to access at least one of the objects in the graph" );
-    // }
+    @SuppressWarnings("unchecked")
+    public void testGetAllDesignElementsFromArrayDesignsWithoutMock() {
+        CompositeSequenceService css = ( CompositeSequenceService ) ctx.getBean( "compositeSequenceService" );
+
+        Collection<CompositeSequence> col = css.getAllCompositeSequences();
+        for ( CompositeSequence cs : col ) {
+            log.debug( cs );
+        }
+
+        if ( col.size() == 0 ) fail( "User not authorized for to access at least one of the objects in the graph" );
+    }
 }
