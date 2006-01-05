caee976d7aaced0fe43414b345a8617420d656bc
as2628 <>
2006-01-05 18:40:38 +0000

    BIND data loader files

---
+++ src/impl/edu/columbia/gemma/loader/association/PPIFileParser.java
@@ -0,0 +1,113 @@
+package edu.columbia.gemma.loader.association;
+
+
+import java.util.Collection;
+
+import baseCode.util.StringUtil;
+
+import edu.columbia.gemma.association.ProteinProteinInteractionImpl;
+import edu.columbia.gemma.genome.gene.GeneProduct;
+import edu.columbia.gemma.genome.gene.GeneProductDao;
+import edu.columbia.gemma.common.description.ExternalDatabase;
+import edu.columbia.gemma.common.description.ExternalDatabaseDao;
+import edu.columbia.gemma.association.ProteinProteinInteractionDao;
+import edu.columbia.gemma.loader.loaderutils.BasicLineMapParser;
+
+
+/**
+ * Class to parse a file of protein-protein interactions (retrieved from BIND). 
+ * 
+ * Format: (read whole row)
+ * pl_ncbiid\t  p2_ncbiid\t  external_db\t  db_id\t numMentions\t   action\t
+ * <hr>
+ * <p>
+ * Copyright (c) 2004-2005 Columbia University
+ * 
+ * @author anshu
+ * @version $Id$
+ */
+
+public class PPIFileParser extends BasicLineMapParser /*implements Persister*/ {
+    
+    public static final int PPI_FIELDS_PER_ROW=6;
+    public static final int PERSIST_CONCURRENTLY=1;
+    public static final int DO_NOT_PERSIST_CONCURRENTLY=0;
+    public static final int PERSIST_DEFAULT=0;
+    
+    private int mPersist = PERSIST_DEFAULT;
+    private GeneProductDao gpDao;
+    private ProteinProteinInteractionDao ppiDao;
+    private ExternalDatabaseDao dbDao;
+    
+    public PPIFileParser(int persistType, GeneProductDao gdao, ProteinProteinInteractionDao ldao, ExternalDatabaseDao dDao){
+        this.mPersist=persistType;
+        this.gpDao=gdao;
+        this.ppiDao=ldao;        
+        this.dbDao=dDao;
+    }
+    
+    
+    /*
+     * (non-Javadoc)
+     * 
+     * @see edu.columbia.gemma.loader.loaderutils.LineParser#parseOneLine(java.lang.String)
+     */
+    public Object parseOneLine( String line ) {
+        System.out.println(line);
+        String[] fields = StringUtil.splitPreserveAllTokens( line, '\t' );
+
+        if ( fields.length != PPI_FIELDS_PER_ROW ) {
+            throw new IllegalArgumentException( "Line is not in the right format: has " + fields.length
+                    + " fields, expected " + PPI_FIELDS_PER_ROW );
+        }
+        
+        Collection<GeneProduct> c;
+        ProteinProteinInteractionImpl assoc = new ProteinProteinInteractionImpl();
+        GeneProduct g1=null;
+        GeneProduct g2=null;
+        Integer id=null;
+        ExternalDatabase db;
+        try {
+            id=new Integer(fields[1]);
+            c =  gpDao.findByNcbiId(id);
+            if ((c!=null) && (c.size()==1)) {
+                g1 = (c.iterator()).next();
+            }else throw new Exception("gene product "+id+" not found. Entry skipped.");
+ 
+            id=new Integer(fields[2]);
+            c = gpDao.findByNcbiId(id);
+            if ((c!=null) && (c.size()==1)) {
+                g2 = (c.iterator()).next();
+            }else throw new Exception("gene "+id+" not found. Entry skipped.");
+            
+            assoc.setFirstProduct(g1);
+            assoc.setSecondProduct(g2);
+            db=ExternalDatabase.Factory.newInstance();
+            db.setName(fields[3]);
+            db=dbDao.findOrCreate(db);
+            //db=dbDao.findByName(fields[8]); //calls fior external db to be pre-loaded
+            assoc.setSource(db); 
+            
+            if (mPersist==PERSIST_CONCURRENTLY) {
+                ppiDao.create(assoc);
+            }
+        }catch(Exception e){
+            System.out.println(e.toString());
+        }
+        return null;
+    }
+
+    /**
+     * 
+     */
+    public void removeAll() {
+        Collection col = ppiDao.loadAll();
+        ppiDao.remove( col );
+    }
+    
+    public Object getKey(Object obj) {
+        return null;
+    }
+    
+
+}
---
+++ src/impl/edu/columbia/gemma/loader/association/ProteinProteinInteractionLoaderCLI.java
@@ -0,0 +1,184 @@
+package edu.columbia.gemma.loader.association;
+
+
+import java.io.IOException;
+
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.BeanFactory;
+
+
+import edu.columbia.gemma.security.ui.ManualAuthenticationProcessing;
+import edu.columbia.gemma.util.SpringContextUtil;
+import edu.columbia.gemma.genome.GeneDao;
+import edu.columbia.gemma.genome.gene.GeneProductDao;
+import edu.columbia.gemma.association.ProteinProteinInteractionDao;
+import edu.columbia.gemma.common.description.ExternalDatabaseDao;
+
+
+
+/**
+ * Command line interface to retrieve and load literature associations
+ * <hr>
+ * <p>
+ * Copyright (c) 2004 - 2005 Columbia University
+ * 
+ * @author anshu 
+ * @version $Id$
+ */
+public class ProteinProteinInteractionLoaderCLI {
+    protected static final Log log = LogFactory.getLog( ProteinProteinInteractionLoaderCLI.class );
+    protected static BeanFactory ctx = null;
+    protected static ManualAuthenticationProcessing manAuthentication = null;
+
+    private static final String USAGE = "[-h] [-u <username>] [-p <password>]  [-t <true|false>] [-l <file>] [-r] ";
+    private static final String HEADER = "The Gemma project, Copyright (c) 2005 Columbia University";
+    private static final String FOOTER = "For more information, see our website at http://www.neurogemma.org";
+
+    //private PersisterHelper mPersister;
+    private GeneDao geneDao;
+    private GeneProductDao gpDao;
+    private ProteinProteinInteractionDao ppiDao;
+    private ExternalDatabaseDao dbDao;
+    private static String username = null;
+    private static String password = null;    
+
+    /**
+     * Command line interface to run the gene parser/loader
+     * 
+     * @param args
+     * @throws ConfigurationException
+     * @throws IOException
+     */
+    public static void main( String args[] ) throws IOException {
+       
+        ProteinProteinInteractionLoaderCLI cli = null;
+
+        // options stage
+        /*help*/
+        OptionBuilder.withDescription( "Print help for this application" );
+        Option helpOpt = OptionBuilder.create( 'h' );
+        
+        /* username */
+        OptionBuilder.hasArgs();
+        OptionBuilder.withDescription( "Username" );
+        Option usernameOpt = OptionBuilder.create( 'u' );
+
+        /* password */
+        OptionBuilder.hasArgs();
+        OptionBuilder.withDescription( "Password" );
+        Option passwordOpt = OptionBuilder.create( 'p' );
+
+        /* environment (test or prod) */
+        OptionBuilder.hasArgs();
+        OptionBuilder.withDescription( "Set use of test or production environment" );
+        Option testOpt = OptionBuilder.create( 't' );
+        
+        /*load*/
+        OptionBuilder.hasArg();
+        OptionBuilder.withDescription( "Specify file (requires file arg) and load database" );
+        Option loadOpt = OptionBuilder.create( 'l' );
+
+        /*remove*/
+        OptionBuilder.withDescription( "Remove protein-protein interactions from database" );
+        Option removeOpt = OptionBuilder.create( 'r' );
+
+        Options opt = new Options();
+        opt.addOption( helpOpt );
+        opt.addOption( usernameOpt );
+        opt.addOption( passwordOpt );
+        opt.addOption( testOpt );        
+        opt.addOption( loadOpt );
+        opt.addOption( removeOpt );
+
+        
+        try {
+            // parser stage
+            BasicParser parser = new BasicParser();
+            CommandLine cl = parser.parse( opt, args );
+
+
+            /* check if using test or production context */
+            if ( cl.hasOption( 't' ) ) {
+                boolean isTest = Boolean.parseBoolean( cl.getOptionValue( 't' ) );
+                if ( isTest )
+                    ctx = SpringContextUtil.getApplicationContext( true );
+                else
+                    ctx = SpringContextUtil.getApplicationContext( false );
+
+                cli = new ProteinProteinInteractionLoaderCLI();
+            }
+            // if no ctx is set, default to test environment.
+            else {
+                ctx = SpringContextUtil.getApplicationContext( true );
+                cli = new ProteinProteinInteractionLoaderCLI();
+            }
+
+            /* check username and password. */
+            if ( cl.hasOption( 'u' ) ) {
+                if ( cl.hasOption( 'p' ) ) {
+                    username = cl.getOptionValue( 'u' );
+                    password = cl.getOptionValue( 'p' );
+                    manAuthentication = ( ManualAuthenticationProcessing ) ctx
+                            .getBean( "manualAuthenticationProcessing" );
+                    manAuthentication.validateRequest( username, password );
+                }
+            } else {
+                log.debug( "Not authenticated.  Make sure you entered a valid username and/or password" );
+                // TODO inform user of this (print to System.out).
+                System.exit( 0 );
+            }
+            PPIFileParser assocParser = new PPIFileParser(PPIFileParser.PERSIST_CONCURRENTLY,cli.gpDao,cli.ppiDao, cli.dbDao);
+            
+            // interrogation stage
+            if ( cl.hasOption( 'l' ) ) {
+
+                String filename = cl.getOptionValue( 'l' );
+                System.out.println("option l: "+filename);
+
+                assocParser.parse(filename);
+
+            } else if ( cl.hasOption( 'r' ) ) {
+                System.out.println("option r ");
+                assocParser.removeAll();
+            } else {
+                printHelp( opt );
+            }
+            
+        } catch ( ParseException e ) {
+            printIncorrectUsage(opt,e.toString());
+        }
+        
+    }
+
+    public ProteinProteinInteractionLoaderCLI() {
+        BeanFactory ctx = SpringContextUtil.getApplicationContext( false );
+        //mPersister = new PersisterHelper();
+        //geneDao = ( GeneDao ) ctx.getBean( "geneDao" ) ;
+        gpDao = ( GeneProductDao ) ctx.getBean( "geneProductDao" ) ;
+        ppiDao = ( ProteinProteinInteractionDao ) ctx.getBean( "ProteinProteinInteractionDao" ) ;
+        dbDao = ( ExternalDatabaseDao ) ctx.getBean( "externalDatabaseDao" ) ;
+    }
+
+    /**
+     * @param opt
+     */
+    private static void printHelp( Options opt ) {
+        HelpFormatter h = new HelpFormatter();
+        //h.printHelp( "Options Tip", opt );
+        h.printHelp( USAGE, HEADER, opt, FOOTER );
+    }
+
+    private static void printIncorrectUsage( Options opt, String errorString ) {
+        HelpFormatter h = new HelpFormatter();
+        h.printHelp( "Incorrect Usage: "+errorString, opt );
+    }
+
+}
