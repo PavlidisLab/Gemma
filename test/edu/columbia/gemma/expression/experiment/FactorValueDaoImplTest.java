edbfb304bf24dc1cdc52c23f28c4a5060984265f
pavlidis <>
2006-01-15 21:23:50 +0000

    implementation of FactorValue methods. Tests not implemented yet.

---
+++ test/edu/columbia/gemma/expression/experiment/FactorValueDaoImplTest.java
@@ -0,0 +1,59 @@
+/*
+ * The Gemma project
+ * 
+ * Copyright (c) 2006 University of British Columbia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package edu.columbia.gemma.expression.experiment;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests of methods for FactorValueDao
+ * 
+ * @author pavlidis
+ * @version $Id$
+ */
+public class FactorValueDaoImplTest extends TestCase {
+
+    /*
+     * @see TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    /*
+     * @see TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /*
+     * Test method for 'edu.columbia.gemma.expression.experiment.FactorValueDaoImpl.find(FactorValue)'
+     */
+    public void testFindFactorValue() {
+        // TODO implement test
+    }
+
+    /*
+     * Test method for 'edu.columbia.gemma.expression.experiment.FactorValueDaoImpl.findOrCreate(FactorValue)'
+     */
+    public void testFindOrCreateFactorValue() {
+        // TODO implement test
+    }
+
+}
