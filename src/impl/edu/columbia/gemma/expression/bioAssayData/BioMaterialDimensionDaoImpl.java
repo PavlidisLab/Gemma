f287c8da44b4db88041a570587761956ed5996fd
pavlidis <>
2006-02-09 21:19:07 +0000

    initial version

---
+++ src/impl/edu/columbia/gemma/expression/bioAssayData/BioMaterialDimensionDaoImpl.java
@@ -0,0 +1,49 @@
+/*
+ * The Gemma project.
+ * 
+ * Copyright (c) 2006 University of British Columbia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package edu.columbia.gemma.expression.bioAssayData;
+
+/**
+ * @author pavlidis
+ * @version $Id$
+ * @see edu.columbia.gemma.expression.bioAssayData.BioMaterialDimension
+ */
+public class BioMaterialDimensionDaoImpl extends edu.columbia.gemma.expression.bioAssayData.BioMaterialDimensionDaoBase {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see edu.columbia.gemma.expression.bioAssayData.BioMaterialDimensionDaoBase#find(edu.columbia.gemma.expression.bioAssayData.BioMaterialDimension)
+     */
+    @Override
+    public BioMaterialDimension find( BioMaterialDimension bioMaterialDimension ) {
+        // TODO Auto-generated method stub
+        return super.find( bioMaterialDimension );
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see edu.columbia.gemma.expression.bioAssayData.BioMaterialDimensionDaoBase#findOrCreate(edu.columbia.gemma.expression.bioAssayData.BioMaterialDimension)
+     */
+    @Override
+    public BioMaterialDimension findOrCreate( BioMaterialDimension bioMaterialDimension ) {
+        // TODO Auto-generated method stub
+        return super.findOrCreate( bioMaterialDimension );
+    }
+}
