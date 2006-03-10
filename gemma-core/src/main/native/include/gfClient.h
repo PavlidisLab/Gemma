7dd5c7198662b1264138c59f42083f3ee7e85389
pavlidis <>
2006-03-10 23:36:22 +0000

    blat interfaces

---
+++ gemma-core/src/main/native/include/gfClient.h
@@ -0,0 +1,9 @@
+#ifndef GFCLIENT_H
+#define GFCLIENT_H
+#include <jni.h>
+
+void gfClientMinimal(const char *hostName, const char *portName, const char *tseqDir, const char *inName, const char *outName);
+
+#endif /* GFCLIENT_H */
+
+
