e633ef2612cd53cc1c2b15ead1779584ad77b215
pavlidis <>
2006-03-10 19:30:59 +0000

    reorganize

--- src/impl/edu/columbia/gemma/loader/loaderutils/FtpFetcher.java
+++
@@ -1,9 +0,0 @@
-package edu.columbia.gemma.loader.loaderutils;
-
-import org.apache.commons.net.ftp.FTPClient;
-
-public abstract class FtpFetcher extends AbstractFetcher {
-
-    protected FTPClient f;
-
-}
--- src/native/include/gfClient.h
+++
@@ -1,9 +0,0 @@
-#ifndef GFCLIENT_H
-#define GFCLIENT_H
-#include <jni.h>
-
-void gfClientMinimal(const char *hostName, const char *portName, const char *tseqDir, const char *inName, const char *outName);
-
-#endif /* GFCLIENT_H */
-
-
