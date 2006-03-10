f39f867de49838da1fb7b08c03c7d44fe0fdf985
pavlidis <>
2006-03-10 19:26:02 +0000

    gemma-util - initial version

---
+++ gemma-util/src/main/java/ubic/gemma/util/SpringContextUtil.java
@@ -0,0 +1,85 @@
+/*
+ * The Gemma project
+ * 
+ * Copyright (c) 2006 University of British Columbia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package ubic.gemma.util;
+
+import java.util.ResourceBundle;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Methods to create Spring contexts. For tests see ubic.gemma.testing.
+ * 
+ * @author pavlidis
+ * @version $Id$
+ */
+public class SpringContextUtil {
+    private static Log log = LogFactory.getLog( SpringContextUtil.class.getName() );
+    private static BeanFactory ctx = null;
+
+    /**
+     * @param testing If true, it will get a test configured-BeanFactory
+     * @return BeanFactory
+     */
+    public static BeanFactory getApplicationContext( boolean testing ) {
+        if ( ctx == null ) {
+            String[] paths = getConfigLocations( testing );
+            ctx = new ClassPathXmlApplicationContext( paths );
+            if ( ctx != null ) {
+                log.info( "Got context" );
+            } else {
+                log.error( "Failed to load context" );
+            }
+        }
+        return ctx;
+    }
+
+    /**
+     * Find the configuration file locations. The files must be in your class path for this to work.
+     * 
+     * @return
+     */
+    public static String[] getConfigLocations() {
+        return getConfigLocations( false );
+    }
+
+    /**
+     * Find the configuration file locations. The files must be in your class path for this to work.
+     * 
+     * @param testing - if true, it will use the test configuration.
+     * @return
+     */
+    public static String[] getConfigLocations( boolean testing ) {
+        ResourceBundle db = ResourceBundle.getBundle( "Gemma" );
+        String daoType = db.getString( "dao.type" );
+        String servletContext = db.getString( "servlet.name.0" );
+        if ( testing ) {
+            log.warn( "************** Using test configuration ***************" );
+            return new String[] { "localTestDataSource.xml", "applicationContext-" + daoType + ".xml",
+                    "applicationContext-security.xml", servletContext + "-servlet.xml",
+                    "applicationContext-validation.xml" };
+        }
+        return new String[] { "applicationContext-localDataSource.xml", "applicationContext-" + daoType + ".xml",
+                "applicationContext-security.xml", servletContext + "-servlet.xml", "applicationContext-validation.xml" };
+
+    }
+
+}
---
+++ gemma-util/src/test/java/ubic/gemma/util/BeanPropertyCompleterTest.java
@@ -0,0 +1,110 @@
+/*
+ * The Gemma project
+ * 
+ * Copyright (c) 2006 University of British Columbia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package ubic.gemma.util;
+
+import junit.framework.TestCase;
+//import ubic.gemma.model.common.description.ExternalDatabase;
+//import ubic.gemma.model.common.description.OntologyEntry;
+
+/**
+ * @author pavlidis
+ * @version $Id$
+ */
+public class BeanPropertyCompleterTest extends TestCase {
+
+    /*
+     * @see TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public final void testComplete() throws Exception {
+
+//        OntologyEntry a = OntologyEntry.Factory.newInstance();
+//        OntologyEntry b = OntologyEntry.Factory.newInstance();
+//
+//        a.setCategory( "foo" );
+//        a.setValue( "bar" );
+//
+//        ExternalDatabase d = ExternalDatabase.Factory.newInstance();
+//        d.setName( "dbfoo" );
+//
+//        b.setCategory( "foo" );
+//        b.setValue( "bar" );
+//        b.setExternalDatabase( d );
+//
+//        BeanPropertyCompleter.complete( a, b, false );
+//
+//        assertTrue( a.getExternalDatabase() == d );
+
+    }
+
+    public final void testCompleteUpdate() throws Exception {
+
+//        OntologyEntry a = OntologyEntry.Factory.newInstance();
+//        OntologyEntry b = OntologyEntry.Factory.newInstance();
+//
+//        a.setCategory( "foo" );
+//        a.setValue( "bar" );
+//
+//        ExternalDatabase d = ExternalDatabase.Factory.newInstance();
+//        d.setName( "dbfoo" );
+//
+//        b.setCategory( "foo" );
+//        b.setValue( "barbie" );
+//        b.setExternalDatabase( d );
+//
+//        BeanPropertyCompleter.complete( a, b, true );
+//
+//        assertTrue( a.getValue().equals( "barbie" ) );
+
+    }
+
+    public final void testCompleteNullVals() throws Exception {
+
+//        OntologyEntry a = OntologyEntry.Factory.newInstance();
+//        OntologyEntry b = OntologyEntry.Factory.newInstance();
+//
+//        a.setCategory( "foo" );
+//        a.setValue( "bar" );
+//
+//        ExternalDatabase d = ExternalDatabase.Factory.newInstance();
+//        d.setName( "dbfoo" );
+//
+//        b.setCategory( null );
+//        b.setValue( "barbie" );
+//        b.setExternalDatabase( d );
+//
+//        BeanPropertyCompleter.complete( a, b, true );
+//
+//        assertTrue( a.getCategory().equals( "foo" ) );
+
+    }
+
+    public final void testCompleteUnsameType() throws Exception {
+        try {
+            BeanPropertyCompleter.complete( new String(), new Double( 1.0 ), false );
+            fail( "Should have thrown an exception" );
+        } catch ( IllegalArgumentException e ) {
+
+        }
+    }
+
+}
