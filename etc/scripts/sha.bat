e5a4e4e48dcfa4a3ea5d029e3ade002cb174442f
pavlidis <>
2006-03-10 19:35:44 +0000

    reorgnize/delete files

--- etc/scripts/sha.bat
+++
@@ -1,4 +0,0 @@
-set CLASSPATH=%CATALINA_HOME%\server\lib\catalina.jar
-set CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\common\lib\jmx.r
-set CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\bin\commons-logging-api.jar
-java -classpath %CLASSPATH% org.apache.catalina.realm.RealmBase -a SHA %1%
--- etc/scripts/sha.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-CLASSPATH="$CATALINA_HOME"/server/lib/catalina.jar
-CLASSPATH="$CLASSPATH"\;"$CATALINA_HOME"/bin/jmx.jar
-CLASSPATH="$CLASSPATH"\;"$CATALINA_HOME"/bin/commons-logging-api.jar
-echo $CLASSPATH
-java -classpath $CLASSPATH org.apache.catalina.realm.RealmBase -a SHA $1
-
-
