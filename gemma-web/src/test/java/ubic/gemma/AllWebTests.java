87c9863ecfb892d1b246736196c85fad76f64b9f
ppavlidis <paul@chibi.ubc.ca>
2006-08-14 06:55:40 +0000

    update to include all tests

--- gemma-web/src/test/java/ubic/gemma/AllWebTests.java
+++ gemma-web/src/test/java/ubic/gemma/AllWebTests.java
@@ -24,7 +24,12 @@ import ubic.gemma.web.controller.common.auditAndSecurity.SignupControllerTest;
 import ubic.gemma.web.controller.common.description.BibRefControllerTest;
 import ubic.gemma.web.controller.expression.arrayDesign.ArrayDesignControllerTest;
 import ubic.gemma.web.controller.expression.arrayDesign.ArrayDesignFormControllerTest;
+import ubic.gemma.web.controller.expression.bioAssay.BioAssayFormControllerTest;
+import ubic.gemma.web.controller.expression.experiment.ExperimentalDesignControllerTest;
 import ubic.gemma.web.controller.expression.experiment.ExpressionExperimentControllerTest;
+import ubic.gemma.web.controller.expression.experiment.ExpressionExperimentSearchControllerIntegrationTest;
+import ubic.gemma.web.util.progress.ProgressDataTest;
+import ubic.gemma.web.util.progress.ProgressIntegrationTest;
 
 /**
  * Tests for gemma-web.
@@ -37,9 +42,16 @@ public class AllWebTests extends TestSuite {
         TestSuite suite = new TestSuite( "Tests for gemma-web" );
         suite.addTestSuite( SignupControllerTest.class );
         suite.addTestSuite( BibRefControllerTest.class );
+
         suite.addTestSuite( ArrayDesignControllerTest.class );
         suite.addTestSuite( ArrayDesignFormControllerTest.class );
+
+        suite.addTestSuite( BioAssayFormControllerTest.class );
+        suite.addTestSuite( ExperimentalDesignControllerTest.class );
         suite.addTestSuite( ExpressionExperimentControllerTest.class );
+        suite.addTestSuite( ExpressionExperimentSearchControllerIntegrationTest.class );
+        suite.addTestSuite( ProgressDataTest.class );
+        suite.addTestSuite( ProgressIntegrationTest.class );
         return suite;
     }
 }
