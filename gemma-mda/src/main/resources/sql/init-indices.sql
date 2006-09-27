1720801fed493c772006649a7a7928be5bfe90af
ppavlidis <paul@chibi.ubc.ca>
2006-09-27 03:47:15 +0000

    capitalize

--- gemma-mda/src/main/resources/sql/init-indices.sql
+++ gemma-mda/src/main/resources/sql/init-indices.sql
@@ -1,5 +1,5 @@
-alter table bio_sequence add index name (name);
-alter table expression_experiment add index name (name);
-alter table database_entry add index acc_ex (accession, external_database_fk);
-alter table chromosome_feature add index symbol_tax (official_symbol, taxon_fk);
-alter table chromosome_feature add index ncbiid (ncbi_id);
+alter table BIO_SEQUENCE add index name (NAME);
+alter table EXPRESSION_EXPERIMENT add index name (NAME);
+alter table DATABASE_ENTRY add index acc_ex (ACCESSION, EXTERNAL_DATABASE_FK);
+alter table CHROMOSOME_FEATURE add index symbol_tax (OFFICIAL_SYMBOL, TAXON_FK);
+alter table CHROMOSOME_FEATURE add index ncbiid (NCBI_ID);
