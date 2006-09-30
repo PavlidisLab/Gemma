7fac1b6f6b757697c81956947fbe1c01b3bdcc34
ppavlidis <paul@chibi.ubc.ca>
2006-09-30 15:41:48 +0000

    fix header

--- gemma-core/src/main/java/ubic/gemma/loader/genome/gene/ncbi/model/NCBIGeneInfo.java
+++ gemma-core/src/main/java/ubic/gemma/loader/genome/gene/ncbi/model/NCBIGeneInfo.java
@@ -11,72 +11,68 @@ import java.util.Map;
  * </p>
  * 
  * <pre>
- *                        ===========================================================================
- *                         gene_info
- *                         ---------------------------------------------------------------------------
- *                         This file can be considered as the logical equivalent of
- *                        
- *                         ftp://ftp.ncbi.nih.gov/refseq/LocusLink/LL.out
- *                        
- *                         tab-delimited
- *                         one line per GeneID
- *                         ---------------------------------------------------------------------------
- *                        
- *                         tax_id:
- *                         the unique identifier provided by NCBI Taxonomy
- *                         for the species or strain/isolate
- *                        
- *                         GeneID:
- *                         the unique identifier for a gene
- *                         ASN1:  geneid
- *                         --note:  for genomes previously available from LocusLink,
- *                         the identifiers are equivalent
- *                        
- *                         Symbol:
- *                         the default symbol for the gene
- *                         ASN1:  gene-&gt;locus
- *                        
- *                         LocusTag:
- *                         the LocusTag value
- *                         ASN1:  gene-&gt;locus-tag
- *                        
- *                         Synonyms:
- *                         bar-delimited set of unoffical symbols for the gene
- *                        
- *                         dbXrefs:
- *                         bar-delimited set of identifiers in other databases
- *                         for this gene.  The unit of the set is database:value.
- *                        
- *                         chromosome:
- *                         the chromosome on which this gene is placed
- *                        
- *                         map location:
- *                         the map location for this gene
- *                        
- *                         description
- *                         a descriptive name for this gene
- *                        
- *                         type of gene:
- *                         the type assigned to the gene according to the list of options
- *                         provided in http://www.ncbi.nlm.nih.gov/IEB/ToolBox/CPP_DOC/lxr/source/src/objects/entrezgene/entrezgene.asn
- *                        
- *                        
- *                         Symbol from nomenclature authority:
- *                         when not '-', indicates that this symbol is from a
- *                         a nomenclature authority
- *                        
- *                         Full name from nomenclature authority:
- *                         when not '-', indicates that this full name is from a
- *                         a nomenclature authority
- *                        
- *                         Nomenclature status
- *                         when not '-', indicates the status of the name from the 
- *                         nomenclature authority (O for official, I for interim)
+ *   ===========================================================================
+ *   gene_info
+ *   ---------------------------------------------------------------------------
+ *   This file can be considered as the logical equivalent of
+ *                          
+ *   ftp://ftp.ncbi.nih.gov/refseq/LocusLink/LL.out
+ *                          
+ *   tab-delimited
+ *   one line per GeneID
+ *   ---------------------------------------------------------------------------
+ *                          
+ *   tax_id:
+ *   the unique identifier provided by NCBI Taxonomy
+ *   for the species or strain/isolate
+ *                          
+ *   GeneID:
+ *   the unique identifier for a gene
+ *   ASN1:  geneid
+ *   --note:  for genomes previously available from LocusLink,
+ *   the identifiers are equivalent
+ *                          
+ *   Symbol:
+ *   the default symbol for the gene
+ *   ASN1:  gene-&gt;locus
+ *                          
+ *   LocusTag:
+ *   the LocusTag value
+ *   ASN1:  gene-&gt;locus-tag
+ *                          
+ *   Synonyms:
+ *   bar-delimited set of unoffical symbols for the gene
+ *                          
+ *   dbXrefs:
+ *   bar-delimited set of identifiers in other databases
+ *   for this gene.  The unit of the set is database:value.
+ *                          
+ *   chromosome:
+ *   the chromosome on which this gene is placed
+ *                          
+ *   map location:
+ *   the map location for this gene
+ *                          
+ *   description
+ *   a descriptive name for this gene
+ *                          
+ *   type of gene:
+ *   the type assigned to the gene according to the list of options
+ *   provided in http://www.ncbi.nlm.nih.gov/IEB/ToolBox/CPP_DOC/lxr/source/src/objects/entrezgene/entrezgene.asn
+ *                          
+ *                          
+ *   Symbol from nomenclature authority:
+ *   when not '-', indicates that this symbol is from a
+ *   a nomenclature authority
+ *                          
+ *   Full name from nomenclature authority:
+ *   when not '-', indicates that this full name is from a
+ *   a nomenclature authority
+ *                          
+ *   Nomenclature status
+ *   when not '-', indicates the status of the name from the 
+ *   nomenclature authority (O for official, I for interim)
  * </pre>
- * 
- * <hr>
- * <p>
- * Copyright (c) 2004-2006 University of British Columbia
  * 
  * @author pavlidis
  * @version $Id$
