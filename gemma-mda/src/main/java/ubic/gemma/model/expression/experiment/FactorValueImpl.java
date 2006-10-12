efea3a2868c8ab72724280fdb3c8fe077d785e9a
ppavlidis <paul@chibi.ubc.ca>
2006-10-12 16:28:05 +0000

    - fix pom and buildpath issues - factorvalue toString implementation

---
+++ gemma-mda/src/main/java/ubic/gemma/model/expression/experiment/FactorValueImpl.java
@@ -0,0 +1,58 @@
+/*
+ * The Gemma project.
+ * 
+ * Copyright (c) 2006 University of British Columbia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+/**
+ * This is only generated once! It will never be overwritten.
+ * You can (and have to!) safely modify it by hand.
+ */
+package ubic.gemma.model.expression.experiment;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * @author pavlidis
+ * @version $Id$
+ */
+public class FactorValueImpl extends ubic.gemma.model.expression.experiment.FactorValue {
+    /**
+     * The serial version UID of this class. Needed for serialization.
+     */
+    private static final long serialVersionUID = -5395878022298281346L;
+
+    /**
+     * @see ubic.gemma.model.expression.experiment.FactorValue#toString()
+     */
+    public java.lang.String toString() {
+        StringBuilder buf = new StringBuilder();
+        buf.append( this.getClass().getSimpleName() );
+        if ( this.getId() != null ) {
+            buf.append( "Id:" + this.getId() );
+        }
+        if ( this.getOntologyEntry() != null ) {
+            buf.append( " " + this.getOntologyEntry() );
+        }
+        if ( this.getMeasurement() != null ) {
+            buf.append( " " + this.getMeasurement() );
+        }
+        if ( StringUtils.isNotBlank( this.getValue() ) ) {
+            buf.append( "Value: " + this.getValue() );
+        }
+        return buf.toString();
+    }
+
+}
