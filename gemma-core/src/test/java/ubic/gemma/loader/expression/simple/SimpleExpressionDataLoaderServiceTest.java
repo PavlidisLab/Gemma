db45dc984694cee5522f89b6108ac6bf7765fe6c
keshav <>
2006-10-24 21:23:39 +0000

    added test for the anova dataset

--- gemma-core/src/test/java/ubic/gemma/loader/expression/simple/SimpleExpressionDataLoaderServiceTest.java
+++ gemma-core/src/test/java/ubic/gemma/loader/expression/simple/SimpleExpressionDataLoaderServiceTest.java
@@ -71,4 +71,41 @@ public class SimpleExpressionDataLoaderServiceTest extends BaseSpringContextTest
         assertEquals( 12, ee.getBioAssays().size() );
     }
 
+    /**
+     * @throws Exception
+     *         {@link ubic.gemma.loader.expression.simple.SimpleExpressionDataLoaderService#load(ubic.gemma.loader.expression.simple.model.ExpressionExperimentMetaData, java.io.InputStream)}.
+     */
+    public final void testLoadB() throws Exception {
+        SimpleExpressionDataLoaderService service = ( SimpleExpressionDataLoaderService ) this
+                .getBean( "simpleExpressionDataLoaderService" );
+
+        SimpleExpressionExperimentMetaData metaData = new SimpleExpressionExperimentMetaData();
+        ArrayDesign ad = ArrayDesign.Factory.newInstance();
+        ad.setName( "new ad" );
+        metaData.setArrayDesign( ad );
+
+        Taxon taxon = Taxon.Factory.newInstance();
+        taxon.setCommonName( "mouse" );
+        metaData.setTaxon( taxon );
+        metaData.setName( "ee" );
+        metaData.setQuantitationTypeName( "testing" );
+        metaData.setGeneralType( GeneralType.QUANTITATIVE );
+        metaData.setScale( ScaleType.LOG2 );
+        metaData.setType( StandardQuantitationType.RATIO );
+
+        InputStream data = this.getClass().getResourceAsStream(
+                "/data/loader/aov.results-2-monocyte-data-bytime.bypat.data.sort" );
+
+        ExpressionExperiment ee = service.load( metaData, data );
+
+        ExpressionExperimentService eeService = ( ExpressionExperimentService ) this
+                .getBean( "expressionExperimentService" );
+        eeService.thaw( ee );
+
+        assertNotNull( ee );
+        assertEquals( 200, ee.getDesignElementDataVectors().size() );
+        assertEquals( 59, ee.getBioAssays().size() );
+        setComplete();
+    }
+
 }
