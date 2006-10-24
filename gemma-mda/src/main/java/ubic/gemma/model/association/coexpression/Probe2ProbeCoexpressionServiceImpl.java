e935a15153dd9f9ad8274abbb9104d62d2c0cf33
kelsey <>
2006-10-24 01:54:49 +0000

    added create(collection)

--- gemma-mda/src/main/java/ubic/gemma/model/association/coexpression/Probe2ProbeCoexpressionServiceImpl.java
+++ gemma-mda/src/main/java/ubic/gemma/model/association/coexpression/Probe2ProbeCoexpressionServiceImpl.java
@@ -22,7 +22,6 @@
  */
 package ubic.gemma.model.association.coexpression;
 
-
 /**
  * @see ubic.gemma.model.association.coexpression.Probe2ProbeCoexpressionService
  */
@@ -38,16 +37,58 @@ public class Probe2ProbeCoexpressionServiceImpl extends
             throws java.lang.Exception {
 
         if ( p2pCoexpression instanceof RatProbeCoExpression )
-            return (RatProbeCoExpression) this.getRatProbeCoExpressionDao().create( ( RatProbeCoExpression ) p2pCoexpression );
+            return ( RatProbeCoExpression ) this.getRatProbeCoExpressionDao().create(
+                    ( RatProbeCoExpression ) p2pCoexpression );
         else if ( p2pCoexpression instanceof MouseProbeCoExpression )
-            return ( MouseProbeCoExpression ) this.getMouseProbeCoExpressionDao().create( ( MouseProbeCoExpression ) p2pCoexpression );
+            return ( MouseProbeCoExpression ) this.getMouseProbeCoExpressionDao().create(
+                    ( MouseProbeCoExpression ) p2pCoexpression );
         else if ( p2pCoexpression instanceof HumanProbeCoExpression )
-            return ( HumanProbeCoExpression ) this.getHumanProbeCoExpressionDao().create( ( HumanProbeCoExpression ) p2pCoexpression );
+            return ( HumanProbeCoExpression ) this.getHumanProbeCoExpressionDao().create(
+                    ( HumanProbeCoExpression ) p2pCoexpression );
         else if ( p2pCoexpression instanceof OtherProbeCoExpression )
-            return ( OtherProbeCoExpression ) this.getOtherProbeCoExpressionDao().create( ( OtherProbeCoExpression ) p2pCoexpression );
+            return ( OtherProbeCoExpression ) this.getOtherProbeCoExpressionDao().create(
+                    ( OtherProbeCoExpression ) p2pCoexpression );
         else
             throw new java.lang.Exception( "p2pCoexpression isn't of a known type. don't know how to create."
                     + p2pCoexpression );
+
+    }
+
+    /**
+     * @see ubic.gemma.model.association.coexpression.Probe2ProbeCoexpressionServiceBase#handleCreate(java.util.Collection)
+     */
+    @Override
+    protected java.util.Collection handleCreate( java.util.Collection p2pExpressions ) throws java.lang.Exception {
+
+        // sanity check.
+        if ( ( p2pExpressions == null ) || ( p2pExpressions.size() == 0 ) ) return null;
+
+        // Make sure that the collections passed in is all of the same Class
+
+        Object last = p2pExpressions.iterator().next();
+
+        for ( Object next : p2pExpressions ) {
+
+            if ( last.getClass() != next.getClass() ) {
+                throw new IllegalArgumentException(
+                        "Given collection doesn't contain objects of uniform type. Contains an object of type "
+                                + last.getClass() + " and another of type " + next.getClass() );
+            }
+
+            last = next;
+        }
+
+        if ( last instanceof RatProbeCoExpression )
+            return this.getRatProbeCoExpressionDao().create( p2pExpressions );
+        else if ( last instanceof MouseProbeCoExpression )
+            return this.getMouseProbeCoExpressionDao().create( p2pExpressions );
+        else if ( last instanceof HumanProbeCoExpression )
+            return this.getHumanProbeCoExpressionDao().create( p2pExpressions );
+        else if ( last instanceof OtherProbeCoExpression )
+            return this.getOtherProbeCoExpressionDao().create( p2pExpressions );
+        else
+            throw new IllegalArgumentException( "Collection contains objects that it can't persist:" + last.getClass()
+                    + " no service method for persisting." );
 
     }
 
