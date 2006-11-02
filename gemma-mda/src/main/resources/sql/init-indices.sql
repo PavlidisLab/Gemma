583eb89245df5f3639d7afd292b96000b6da64c9
ppavlidis <paul@chibi.ubc.ca>
2006-11-02 19:38:26 +0000

    add index for DESIGN_ELEMENT name

--- gemma-mda/src/main/resources/sql/init-indices.sql
+++ gemma-mda/src/main/resources/sql/init-indices.sql
@@ -5,3 +5,4 @@ alter table CHROMOSOME_FEATURE add index symbol_tax (OFFICIAL_SYMBOL, TAXON_FK);
 alter table CHROMOSOME_FEATURE add index ncbiid (NCBI_ID);
 alter table CHROMOSOME_FEATURE add index name (NAME);
 alter table GENE_ALIAS add index `alias` (`ALIAS`);
+alter table DESIGN_ELEMENT add index name (NAME);
