38950f58dd57c3550b60bce870dc6661413bc818
xwan <>
2006-12-12 19:50:36 +0000

    Fixed the bug for merging the nodes.

--- gemma-core/src/main/java/ubic/gemma/analysis/linkAnalysis/FrequentLinkSetFinder.java
+++ gemma-core/src/main/java/ubic/gemma/analysis/linkAnalysis/FrequentLinkSetFinder.java
@@ -3,70 +3,251 @@ package ubic.gemma.analysis.linkAnalysis;
 
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Set;
+import java.util.Vector;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+
+import ubic.basecode.dataStructure.Visitable;
 import ubic.basecode.dataStructure.matrix.CompressedNamedBitMatrix;
-import ubic.basecode.dataStructure.tree.TreeNode;
+import ubic.gemma.model.expression.experiment.ExpressionExperiment;
+import ubic.gemma.model.expression.experiment.ExpressionExperimentService;
+import ubic.gemma.model.genome.Gene;
+import ubic.gemma.model.genome.gene.GeneService;
 
 public class FrequentLinkSetFinder {
+	private class TreeNode {
+	    public Object element = null;
+	    public TreeNode firstChild = null;
+	    public TreeNode nextSibling = null;
+	    public TreeNode parent = null;
+	    public TreeNode prevSibling = null;
+	    public int level = 0;
+	    public long mask[];
+	    public TreeNode(Object element){
+	    	this.element = element;
+	    }
+	}
+	private HashMap<TreeNode,Set> sameSiblings = null;
+    private GeneService geneService = null;
+    private ExpressionExperimentService eeService = null;
     private CompressedNamedBitMatrix linkCount = null;
+    private Vector allEE = null;
     private int Threshold = 3;
     private TreeNode root = null;
-    int shift = 50000;
-    public FrequentLinkSetFinder( CompressedNamedBitMatrix linkCount, int threshold ) {
+    private int shift = 100000;
+    private int minNumInSet = 8;
+    public static int nodeNum = 0; 
+    private int pruned = 0;
+    private int merged = 0;
+    private Vector allLeafNodes = null; //a linked list
+    public FrequentLinkSetFinder( CompressedNamedBitMatrix linkCount, int threshold, int minNumInSet) {
         super();
         this.linkCount = linkCount;
         Threshold = threshold;
         root = new TreeNode(new Long(0xFFFFFFFFFFFFFFFFL));
-        shift = this.linkCount.rows() > this.linkCount.columns()?this.linkCount.rows():this.linkCount.columns();
+        root.level = 1;
+        //shift = this.linkCount.rows() > this.linkCount.columns()?this.linkCount.rows():this.linkCount.columns();
+        this.minNumInSet = minNumInSet;
+        sameSiblings = new HashMap<TreeNode, Set>();
+        allLeafNodes = new Vector();
     }
-    private void expand(TreeNode rootNode, HashSet<Long> candidates){
-        TreeNode cur = rootNode.getFirstChild();
-        while( cur != null){
-            long id = ((Long)cur.getElement()).longValue();
-            int curRow = (int)(id/shift);
-            int curCol = (int)(id%shift);
-            candidates.remove( cur.getElement() );
-            HashSet curCandidates = new HashSet<Long>();
-            Object[] idArray = candidates.toArray();
-            TreeNode curNode = null;
-            for(int i = 0; i < idArray.length; i++){
-                long oneId = ((Long)idArray[i]).longValue();
-                int row = (int)(oneId/shift);
-                int col = (int)(oneId%shift);
-                if(this.linkCount.overlap( curRow, curCol, row, col) >= this.Threshold){
-                    curCandidates.add( (Long)idArray[i] );
-                    TreeNode oneNode = new TreeNode(idArray[i]);
-                    if(curNode == null){
-                        curNode = oneNode;
-                        cur.setFirstChild( curNode );
-                    }else{
-                        curNode.setNextSibling( oneNode );
-                        curNode = oneNode;
-                    }
+
+    private void expand(TreeNode rootNode){
+        TreeNode subBranchRootNode = rootNode.firstChild;
+        
+        while( subBranchRootNode != null){
+        	if(((Long)subBranchRootNode.element).longValue() == 462718468 && ((Long)rootNode.element).longValue() == 904818468 && rootNode.level == 2)
+            	this.pruned = this.pruned;
+            TreeNode iter = subBranchRootNode.nextSibling;
+            while(iter != null){
+                long[] childMask = new long[iter.mask.length]; 
+                for(int i = 0; i < childMask.length; i++) childMask[i] = iter.mask[i];
+                boolean notCreatedNewOne = true;
+                for(int i = 0; i < childMask.length; i++){
+                	if(childMask[i] != (childMask[i] & subBranchRootNode.mask[i])){
+                		childMask[i] = childMask[i] & subBranchRootNode.mask[i];
+                		notCreatedNewOne = false;
+                	}
+                }
+                TreeNode nextOne = iter.nextSibling;
+                if(countBits(childMask) >= this.Threshold){
+                	TreeNode newFacedNode = null;
+                	if(notCreatedNewOne){
+                		iter.prevSibling.nextSibling = iter.nextSibling;
+                		if(iter.nextSibling != null)
+                			iter.nextSibling.prevSibling = iter.prevSibling;
+                		newFacedNode = iter;
+                		newFacedNode.prevSibling = newFacedNode.nextSibling = null;
+                		merged = merged + 1;
+                	}else{
+                        newFacedNode = new TreeNode(iter.element);
+                        newFacedNode.mask = childMask;
+                        nodeNum++;
+                        if(nodeNum%10000 == 0)
+                        	System.err.println(nodeNum + " " +  merged);
+                	}
+                	this.addTreeNodeInOrder(subBranchRootNode, newFacedNode,false);
+                }
+                iter = nextOne;//Use nextOne to save in case the merging broke the links
+            }
+            TreeNode tmpRecord = subBranchRootNode;
+            if(subBranchRootNode.firstChild != null){
+            	this.expand( subBranchRootNode);
+            }
+            subBranchRootNode = subBranchRootNode.nextSibling;
+            if(tmpRecord.firstChild == null && tmpRecord.level > this.minNumInSet){
+            	insertLeafNode(tmpRecord);
+            	//if(this.pruned < 30){outputPath(tmpRecord);this.pruned = this.pruned+1;}
+            }
+            
+            if(tmpRecord.firstChild == null && tmpRecord.level <= this.minNumInSet){ //check the level and prune the nodes
+            	if(subBranchRootNode != null) subBranchRootNode.prevSibling = tmpRecord.prevSibling;
+            	if( tmpRecord.parent.firstChild == tmpRecord){
+        				tmpRecord.parent.firstChild = subBranchRootNode;
+            	}else{
+            		tmpRecord.prevSibling.nextSibling = subBranchRootNode;
+            	}
+            	tmpRecord = tmpRecord.prevSibling = tmpRecord.nextSibling = null;
+            	nodeNum = nodeNum - 1;
+            	pruned = pruned + 1;
+            	//if(pruned %10000 == 0) System.err.println("Pruned " + pruned);
+            }
+        }
+        if(root.firstChild == null) System.err.println("Logic Error");
+    }
+    private void addTreeNodeInOrder(TreeNode parent, TreeNode childNode, boolean merged){
+    	TreeNode iter = parent.firstChild;
+    	childNode.parent = parent;
+    	childNode.level = parent.level + 1;
+    	if(iter == null){
+    		parent.firstChild = childNode;
+    		return;
+    	}
+    	int childNodeBits = countBits(childNode.mask);
+    	while(iter != null){
+    		int iterNodeBits = countBits(iter.mask);
+    		if(iterNodeBits == childNodeBits){
+    			if(merged && compare(childNode.mask, iter.mask)){
+    				Set siblingSet = this.sameSiblings.get(iter);
+    				if(siblingSet == null){
+    					siblingSet = new HashSet<TreeNode>();
+    					this.sameSiblings.put(iter,siblingSet);
+    				}
+    				siblingSet.add(childNode);
+    				return;
+    			}
+    		}
+    		if(iterNodeBits < childNodeBits){
+    			childNode.nextSibling = iter;
+    			if(iter.prevSibling != null){
+    				iter.prevSibling.nextSibling = childNode;
+    				childNode.prevSibling = iter.prevSibling;
+    			}else{
+    				parent.firstChild = childNode;
+    			}
+    			iter.prevSibling = childNode;
+    			return;
+    		}		
+    		if(iter.nextSibling == null) break;
+    		else iter = iter.nextSibling;
+    	}
+    	iter.nextSibling = childNode;
+    	childNode.prevSibling = iter;
+    	return;
+    }
+    private void insertLeafNode(TreeNode newLeafNode){
+    	int bits = countBits(newLeafNode.mask);
+    	int i;
+    	for(i = 0; i < allLeafNodes.size(); i++){
+    		TreeNode ele = (TreeNode)allLeafNodes.elementAt(i);
+    		if(ele.level < newLeafNode.level || (ele.level == newLeafNode.level && countBits(ele.mask) < bits)){
+    			allLeafNodes.insertElementAt(newLeafNode, i);
+    			break;
+    		}
+    	}
+    	if(i == allLeafNodes.size()) allLeafNodes.add(newLeafNode);
+    }
+    public void outputPath(TreeNode leafNode){
+    	TreeNode iter = leafNode;
+    	while(iter.parent != null){
+    		long oneId = ((Long)iter.element).longValue();
+            int row = (int)(oneId/shift);
+            int col = (int)(oneId%shift);
+        	Object geneId = this.linkCount.getRowName(row);
+            String geneName1 = this.geneService.load(((Long)geneId).longValue()).getName();
+        	geneId = this.linkCount.getColName(col);
+            String geneName2 = this.geneService.load(((Long)geneId).longValue()).getName();
+            //System.err.print(" ("+geneName1 + " " + geneName2+") ");
+    		System.err.print(oneId + " ");
+            iter = iter.parent;
+    	}
+    	for(int i = 0; i < leafNode.mask.length; i++){
+    		for(int j = 0; j < CompressedNamedBitMatrix.DOUBLE_LENGTH; j++)
+    			if((leafNode.mask[i]&(CompressedNamedBitMatrix.BIT1<<j)) != 0){
+    		    	Object eeId = this.allEE.elementAt(j+i*CompressedNamedBitMatrix.DOUBLE_LENGTH);
+    		    	ExpressionExperiment ee = this.eeService.findById((Long)eeId);
+    		    	System.err.print(ee.getShortName() + " ");
+    			}
+    	}
+    	System.err.println("");
+    }
+    public static int countBits(long[] mask){
+    	int bits = 0;
+    	for(int i = 0; i < mask.length; i++)
+    		bits = bits + CompressedNamedBitMatrix.countBits(mask[i]);
+    	return bits;
+    }
+    private boolean compare(long[] mask1, long mask2[]){
+    	for(int i = 0; i < mask1.length; i++)
+    		if(mask1[i] != mask2[i]) return false;
+    	return true;
+    }
+
+    public void find(){
+        for(int i = 0; i < this.linkCount.rows(); i++)
+            for(int j = i+1; j < this.linkCount.columns(); j++){
+            	if(!compare(this.linkCount.getAllBits( i, j ), this.linkCount.getAllBits( j,i )))
+            		System.err.println(i + "  " + j);
+                if(this.linkCount.bitCount( i, j ) >= this.Threshold){
+                    TreeNode oneNode = new TreeNode(new Long((long)i*(long)shift+ j) );
+                    oneNode.mask = this.linkCount.getAllBits(i, j);
+                    nodeNum++;
+                    this.addTreeNodeInOrder(root, oneNode,false);
                 }
             }
-            this.expand( cur, curCandidates );
-            cur = cur.getNextSibling();
+        System.err.println(this.sameSiblings.size());
+        this.expand( root);
+        System.err.println(this.allLeafNodes.size()+ " " + ((TreeNode)allLeafNodes.elementAt(0)).level);
+        this.outputPath((TreeNode)allLeafNodes.elementAt(0));
+        int mostBits = 0;
+        TreeNode nodeWithMostBits = null;
+        for(int i = 0; i < allLeafNodes.size(); i++){
+        	TreeNode node = (TreeNode)allLeafNodes.elementAt(i);
+        	if(countBits(node.mask) > mostBits){mostBits = countBits(node.mask);nodeWithMostBits = node;}
         }
+        for(int i = 0; i < allLeafNodes.size(); i++){
+        	TreeNode node = (TreeNode)allLeafNodes.elementAt(i);
+        	if(mostBits == countBits(node.mask)){System.err.println(node.level + " ");this.outputPath(node);}
+        }
+
+		this.outputPath(nodeWithMostBits);
     }
-    public void find(){
-        HashSet <Long> startSet = new HashSet<Long>();
-        
-        TreeNode cur = null;
-        for(int i = 0; i < this.linkCount.rows(); i++)
-            for(int j = 0; j < this.linkCount.columns(); j++)
-                if(this.linkCount.bitCount( i, j ) >= this.Threshold){
-                    startSet.add( new Long((long)i*(long)shift+ j) );
-                    TreeNode oneNode = new TreeNode(new Long((long)i*(long)shift+ j) );
-                    if(cur == null){
-                        root.setFirstChild( oneNode );
-                        cur = oneNode;
-                    }else{
-                        cur.setNextSibling( oneNode );
-                        cur = oneNode;
-                    }
-                }
-        this.expand( root, startSet );
+    public void output(int num){
+    	
+    }
+    private void browse(TreeNode rootNode){
+    	
+    }
+    public void setGeneService(GeneService geneService){
+    	this.geneService = geneService;
+    }
+    public void setEEService(ExpressionExperimentService eeService){
+    	this.eeService = eeService;
+    }
+    public void setEEIndex(java.util.Vector vector){
+    	this.allEE = vector;
     }
 }
