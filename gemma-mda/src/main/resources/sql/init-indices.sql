e0ab0651a22721dd54b657d5b971f9ecb83ad546
joseph <>
2007-01-16 19:24:46 +0000

    added Type index to CHROMOSOME_FEATURE

--- gemma-mda/src/main/resources/sql/init-indices.sql
+++ gemma-mda/src/main/resources/sql/init-indices.sql
@@ -6,5 +6,6 @@ alter table CHROMOSOME_FEATURE add index symbol_tax (OFFICIAL_SYMBOL, TAXON_FK);
 alter table CHROMOSOME_FEATURE add index ncbiid (NCBI_ID);
 alter table CHROMOSOME_FEATURE add index name (NAME);
 alter table CHROMOSOME_FEATURE add index class (class);
+alter table CHROMOSOME_FEATURE add index type (TYPE);
 alter table GENE_ALIAS add index `alias` (`ALIAS`);
 alter table DESIGN_ELEMENT add index name (NAME);
