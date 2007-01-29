a529a12c64febf667415c0aa09edc18f115a649c
keshav <>
2007-01-29 04:05:43 +0000

    added a command object to hold the security form preferances

---
+++ gemma-web/src/main/java/ubic/gemma/web/controller/security/SecurityCommand.java
@@ -0,0 +1,91 @@
+/*
+ * The Gemma project
+ * 
+ * Copyright (c) 2006 Columbia University
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package ubic.gemma.web.controller.security;
+
+import java.io.Serializable;
+
+/**
+ * Security command object that wraps security preferences.
+ * 
+ * @author keshav
+ * @version $Id$
+ */
+public class SecurityCommand implements Serializable {
+
+    private static final long serialVersionUID = 2166768356457316142L;
+
+    private String securableType = null;
+
+    private Long securableId = null;
+
+    private String mask = null;
+
+    // private String name = null;
+    //
+    // private Long expressionExperimentId = null;
+    //
+    // private String searchString = null;
+    //
+    // private boolean viewSampling;
+    //
+    // private QuantitationType quantitationType = null;
+
+    /**
+     * @return the securableType
+     */
+    public String getSecurableType() {
+        return securableType;
+    }
+
+    /**
+     * @param securableType the securableType to set
+     */
+    public void setSecurableType( String securableType ) {
+        this.securableType = securableType;
+    }
+
+    /**
+     * @return the securableId
+     */
+    public Long getSecurableId() {
+        return securableId;
+    }
+
+    /**
+     * @param securableId the securableId to set
+     */
+    public void setSecurableId( Long securableId ) {
+        this.securableId = securableId;
+    }
+
+    /**
+     * @return the mask
+     */
+    public String getMask() {
+        return mask;
+    }
+
+    /**
+     * @param mask the mask to set
+     */
+    public void setMask( String mask ) {
+        this.mask = mask;
+    }
+
+}
