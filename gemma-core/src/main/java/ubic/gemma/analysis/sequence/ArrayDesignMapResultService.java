216f8ab47f17838c0ee00f49c874a19f5359e016
joseph <>
2007-01-30 22:22:10 +0000

    removed extraneous code

--- gemma-core/src/main/java/ubic/gemma/analysis/sequence/ArrayDesignMapResultService.java
+++ gemma-core/src/main/java/ubic/gemma/analysis/sequence/ArrayDesignMapResultService.java
@@ -19,7 +19,6 @@
 package ubic.gemma.analysis.sequence;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -32,10 +31,8 @@ import ubic.gemma.model.expression.arrayDesign.ArrayDesign;
 import ubic.gemma.model.expression.arrayDesign.ArrayDesignService;
 import ubic.gemma.model.expression.designElement.CompositeSequence;
 import ubic.gemma.model.expression.designElement.CompositeSequenceService;
-import ubic.gemma.model.genome.Gene;
 import ubic.gemma.model.genome.biosequence.BioSequence;
 import ubic.gemma.model.genome.biosequence.BioSequenceService;
-import ubic.gemma.model.genome.gene.GeneProduct;
 import ubic.gemma.model.genome.gene.GeneProductService;
 import ubic.gemma.model.genome.gene.GeneProductValueObject;
 import ubic.gemma.model.genome.gene.GeneService;
@@ -52,10 +49,7 @@ import ubic.gemma.model.genome.sequenceAnalysis.BlatResultService;
  * @spring.property name="blatAssociationService" ref="blatAssociationService"
  * @spring.property name="arrayDesignService" ref ="arrayDesignService"
  * @spring.property name="blatResultService" ref="blatResultService"
- * @spring.property name="geneProductService" ref="geneProductService"
- * @spring.property name="geneService" ref="geneService"
- * @spring.property name="bioSequenceService" ref="bioSequenceService"
- * @spring.property name="compositeSequenceService" ref="compositeSequenceService"
+ * 
  * @author Paul
  * @version $Id$
  */
@@ -66,10 +60,7 @@ public class ArrayDesignMapResultService {
     private BlatResultService blatResultService;
     private BlatAssociationService blatAssociationService;
     private ArrayDesignService arrayDesignService;
-    private GeneProductService geneProductService;
-    private GeneService geneService;
-    private BioSequenceService bioSequenceService;
-    private CompositeSequenceService compositeSequenceService;
+
 
     public void setBlatResultService( BlatResultService blatResultService ) {
         this.blatResultService = blatResultService;
@@ -90,86 +81,6 @@ public class ArrayDesignMapResultService {
     }
 
     /**
-     * FIXME is this used anywhere?
-     * 
-     * @param arrayDesign
-     * @return
-     */
-    @SuppressWarnings("unchecked")
-    public Collection<CompositeSequenceMapSummary> getSummaryMapResults( ArrayDesign arrayDesign ) {
-        Collection sequenceData = arrayDesignService.getRawCompositeSequenceSummary( arrayDesign );
-        return getSummaryMapResults( sequenceData );
-    }
-
-    /**
-     * @param sequenceData
-     * @return
-     */
-    @SuppressWarnings("unchecked")
-    private Collection<CompositeSequenceMapSummary> getSummaryMapResults( Collection<CompositeSequence> sequenceData ) {
-        Collection<CompositeSequenceMapSummary> compositeSequenceSummaries;
-        HashMap<Long, Long> bioSequenceIds = new HashMap<Long, Long>();
-        HashMap<Long, HashSet<Long>> blatResultIds = new HashMap<Long, HashSet<Long>>();
-        HashMap<Long, HashSet<Long>> geneProductIds = new HashMap<Long, HashSet<Long>>();
-        HashMap<Long, HashSet<Long>> geneIds = new HashMap<Long, HashSet<Long>>();
-        HashSet<Long> compositeSequenceIds = new HashSet<Long>();
-
-        for ( Object o : sequenceData ) {
-            String[] row = ( String[] ) o;
-            Long csId = Long.parseLong( row[0] );
-
-            Long bioSequenceId = null;
-            if ( row[1] != null ) {
-                Long.parseLong( row[1] );
-            }
-            Long blatId = null;
-            if ( row[3] != null ) {
-                Long.parseLong( row[3] );
-            }
-            Long geneProductId = null;
-            if ( row[4] != null ) {
-                Long.parseLong( row[4] );
-            }
-            Long geneId = null;
-            if ( row[5] != null ) {
-                Long.parseLong( row[5] );
-            }
-
-            // store compositeSequenceId
-            compositeSequenceIds.add( csId );
-            // fill in bioSequence if not known
-            if ( bioSequenceId != null && !bioSequenceIds.containsKey( csId ) ) {
-                bioSequenceIds.put( csId, bioSequenceId );
-            }
-            // add in blatResult (if it exists)
-            addToResultSet( blatResultIds, csId, blatId );
-            // add to geneProducts (if it exists)
-            addToResultSet( geneProductIds, csId, geneProductId );
-            // add to genes (if it exists)
-            addToResultSet( geneIds, csId, geneId );
-        }
-
-        sequenceData = null;
-
-        // all ids are in memory now. Mass-load compositeSequences, bioSequences, blatResults, geneProducts, and genes.
-        Collection<CompositeSequence> compositeSequences = compositeSequenceService.load( compositeSequenceIds );
-        // Collection<BioSequence> bioSequences = bioSequenceService.load( bioSequenceIds.values() );
-        Collection<BlatResult> blatResults = blatResultService.load( blatResultIds.values() );
-        // Collection<GeneProduct> geneProducts = geneProductService.load( geneProductIds.values() );
-        // Collection<Gene> genes = geneService.load( geneIds.values() );
-
-        // build summary array
-        compositeSequenceSummaries = new ArrayList<CompositeSequenceMapSummary>();
-        for ( CompositeSequence cs : compositeSequences ) {
-            // Long csId = cs.getId();
-            CompositeSequenceMapSummary summary = new CompositeSequenceMapSummary( cs );
-            summary.setBlatResults( blatResults );
-
-        }
-        return compositeSequenceSummaries;
-    }
-
-    /**
      * @param arrayDesign
      * @return
      */
@@ -181,6 +92,9 @@ public class ArrayDesignMapResultService {
     /**
      * FIXME this is only public so we can use it in the DesignElementController; need refactoring (see
      * CompositeSequenceService)
+     * 
+     * Function to get a collection of CompositeSequenceMapValueObjects that contain information about
+     * a composite sequence and related tables. 
      * 
      * @param rawSummaryData
      * @return
@@ -284,23 +198,9 @@ public class ArrayDesignMapResultService {
     }
 
     /**
-     * @param results
-     * @param keyId
-     * @param valueId
-     */
-    private void addToResultSet( HashMap<Long, HashSet<Long>> results, Long keyId, Long valueId ) {
-        if ( valueId != null ) {
-            HashSet<Long> blatList;
-            if ( results.containsKey( keyId ) ) {
-                blatList = ( HashSet<Long> ) results.get( keyId );
-            } else {
-                blatList = new HashSet<Long>();
-            }
-            blatList.add( valueId );
-        }
-    }
-
-    /**
+     * 
+     * Non-HQL version of the composite sequence data summary query. Returns a summary of the composite sequence data and related tables.
+     * 
      * @param compositeSequences
      * @return
      */
@@ -344,32 +244,6 @@ public class ArrayDesignMapResultService {
         this.arrayDesignService = arrayDesignService;
     }
 
-    /**
-     * @param bioSequenceService the bioSequenceService to set
-     */
-    public void setBioSequenceService( BioSequenceService bioSequenceService ) {
-        this.bioSequenceService = bioSequenceService;
-    }
 
-    /**
-     * @param geneProductService the geneProductService to set
-     */
-    public void setGeneProductService( GeneProductService geneProductService ) {
-        this.geneProductService = geneProductService;
-    }
-
-    /**
-     * @param geneService the geneService to set
-     */
-    public void setGeneService( GeneService geneService ) {
-        this.geneService = geneService;
-    }
-
-    /**
-     * @param compositeSequenceService the compositeSequenceService to set
-     */
-    public void setCompositeSequenceService( CompositeSequenceService compositeSequenceService ) {
-        this.compositeSequenceService = compositeSequenceService;
-    }
 
 }
