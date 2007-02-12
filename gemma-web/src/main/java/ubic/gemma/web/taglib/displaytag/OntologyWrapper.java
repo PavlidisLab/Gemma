7a2d3cf64328718486fddfce6910e4e08176049a
ppavlidis <paul@chibi.ubc.ca>
2007-02-12 15:43:55 +0000

    fix way rows map to objects; comment out unneeded methods

--- gemma-web/src/main/java/ubic/gemma/web/taglib/displaytag/OntologyWrapper.java
+++ gemma-web/src/main/java/ubic/gemma/web/taglib/displaytag/OntologyWrapper.java
@@ -1,8 +1,5 @@
 package ubic.gemma.web.taglib.displaytag;
 
-import java.util.Collection;
-import java.util.Map;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.displaytag.decorator.TableDecorator;
@@ -16,44 +13,33 @@ import ubic.gemma.model.common.description.OntologyEntry;
  * for explanation of how this works.
  * 
  * @author klc
- * @version $Id $
+ * @version $Id$
  */
 public class OntologyWrapper extends TableDecorator {
 
     Log log = LogFactory.getLog( this.getClass() );
 
-    public String getValue() {
-        Map.Entry oe = ( Map.Entry ) getCurrentRowObject();
-        return ( ( OntologyEntry ) oe.getKey() ).getValue();
-    }
+    // public String getValue() {
+    // OntologyEntry oe = (OntologyEntry ) getCurrentRowObject();
+    // return oe.getValue();
+    // }
+    //
+    // public String getDescription() {
+    // OntologyEntry oe = (OntologyEntry ) getCurrentRowObject();
+    // return oe.getDescription();
+    // }
+    //
+    // public String getCategory() {
+    // OntologyEntry oe = (OntologyEntry ) getCurrentRowObject();
+    // return oe.getCategory();
+    // }
 
-    public String getDescription() {
-        Map.Entry oe = ( Map.Entry ) getCurrentRowObject();
-        return ( ( OntologyEntry ) oe.getKey() ).getDescription();
-    }
-
-    public String getCategory() {
-        Map.Entry oe = ( Map.Entry ) getCurrentRowObject();
-        return ( ( OntologyEntry ) oe.getKey() ).getCategory();
-    }
-    
     public String getAccession() {
-        OntologyEntry oe = (OntologyEntry ) (( Map.Entry ) getCurrentRowObject()).getKey();
-  
-        return  "<a target='_blank' href='http://www.godatabase.org/cgi-bin/amigo/go.cgi?view=details&query=" + oe.getAccession()  + "'>" + oe.getAccession() + "</a>";
-  
-    }
+        OntologyEntry oe = ( OntologyEntry ) getCurrentRowObject();
 
+        return "<a target='_blank' href='http://www.godatabase.org/cgi-bin/amigo/go.cgi?view=details&query="
+                + oe.getAccession() + "'>" + oe.getAccession() + "</a>";
 
-    public String getParents() {
-        Map.Entry oe = ( Map.Entry ) getCurrentRowObject();
-        
-        String parents = "";
-        for ( Object obj : (Collection) oe.getValue() )             
-              parents += ( ( OntologyEntry ) obj ).getValue() + " &nbsp;";        
-        
-        return parents;
     }
-    
 
 }
