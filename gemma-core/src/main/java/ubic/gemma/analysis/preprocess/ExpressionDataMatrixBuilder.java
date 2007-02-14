775d5a4ff1ab904576ce938f5de37b283d87c992
ppavlidis <paul@chibi.ubc.ca>
2007-02-14 15:33:57 +0000

    minor

--- gemma-core/src/main/java/ubic/gemma/analysis/preprocess/ExpressionDataMatrixBuilder.java
+++ gemma-core/src/main/java/ubic/gemma/analysis/preprocess/ExpressionDataMatrixBuilder.java
@@ -75,7 +75,7 @@ public class ExpressionDataMatrixBuilder {
 
         for ( BioAssayDimension dimension : dimensions ) {
             QuantitationType qType = dat.getBackgroundChannelA( dimension );
-            if(qType != null) qTypes.add( qType );
+            if ( qType != null ) qTypes.add( qType );
         }
 
         return makeMatrix( dimensions, qTypes );
@@ -94,7 +94,7 @@ public class ExpressionDataMatrixBuilder {
 
         for ( BioAssayDimension dimension : dimensions ) {
             QuantitationType qType = dat.getBackgroundChannelB( dimension );
-            if(qType != null) qTypes.add( qType );
+            if ( qType != null ) qTypes.add( qType );
         }
 
         return makeMatrix( dimensions, qTypes );
@@ -125,7 +125,7 @@ public class ExpressionDataMatrixBuilder {
             if ( arrayDesign == null || adUsed.equals( arrayDesign ) ) {
                 assert vector.getBioAssayDimension() != null;
                 dimensions.add( vector.getBioAssayDimension() );
-                //dimMap.put( arrayDesign, vector.getBioAssayDimension() );
+                // dimMap.put( arrayDesign, vector.getBioAssayDimension() );
                 dimMap.put( adUsed, vector.getBioAssayDimension() );
                 continue;
             }
@@ -234,7 +234,7 @@ public class ExpressionDataMatrixBuilder {
 
         for ( BioAssayDimension dimension : dimensions ) {
             QuantitationType qType = dat.getSignalChannelB( dimension );
-            if(qType != null) qTypes.add( qType );
+            if ( qType != null ) qTypes.add( qType );
         }
 
         return makeMatrix( dimensions, qTypes );
@@ -264,7 +264,7 @@ public class ExpressionDataMatrixBuilder {
             if ( channelANeedsReconstruction ) {
                 return getSignalChannelAFancy( arrayDesign );
             }
-            if(signalChannelA != null) qTypes.add( signalChannelA );
+            if ( signalChannelA != null ) qTypes.add( signalChannelA );
         }
 
         return makeMatrix( dimensions, qTypes );
@@ -513,8 +513,7 @@ public class ExpressionDataMatrixBuilder {
                 || name.toLowerCase().matches( "f532[\\s_\\.](mean|median)" ) || name.equals( "SIGNAL_CHANNEL 1MEDIAN" )
                 || name.toLowerCase().matches( "ch1_smtm" ) || name.equals( "G_MEAN" ) || name.equals( "Ch1SigMedian" )
                 || name.equals( "ch1.Intensity" ) || name.equals( "CH1_SIG_MEAN" ) || name.equals( "CH1_ Median" )
-                || name.toUpperCase().matches( "\\w{2}\\d{3}_CY3" )
-        		|| name.toUpperCase().matches( "NORM*CH1" );
+                || name.toUpperCase().matches( "\\w{2}\\d{3}_CY3" ) || name.toUpperCase().matches( "NORM*CH1" );
     }
 
     /**
@@ -528,8 +527,7 @@ public class ExpressionDataMatrixBuilder {
                 || name.toLowerCase().matches( "f635[\\s_\\.](mean|median)" ) || name.equals( "SIGNAL_CHANNEL 2MEDIAN" )
                 || name.toLowerCase().matches( "ch2_smtm" ) || name.equals( "R_MEAN" ) || name.equals( "Ch2SigMedian" )
                 || name.equals( "ch2.Intensity" ) || name.equals( "CH2_SIG_MEAN" ) || name.equals( "CH2_ Median" )
-                || name.toUpperCase().matches( "\\w{2}\\d{3}_CY5" )
-                || name.toUpperCase().matches( "NORM*CH2" );
+                || name.toUpperCase().matches( "\\w{2}\\d{3}_CY5" ) || name.toUpperCase().matches( "NORM*CH2" );
     }
 
     /**
