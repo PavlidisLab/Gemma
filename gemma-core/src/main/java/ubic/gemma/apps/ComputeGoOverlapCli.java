58294fb1b1b8a7d5d902b36d986eca6e6b9e5abb
kelsey <>
2007-03-08 04:08:24 +0000

    just some clean up.

--- gemma-core/src/main/java/ubic/gemma/apps/ComputeGoOverlapCli.java
+++ gemma-core/src/main/java/ubic/gemma/apps/ComputeGoOverlapCli.java
@@ -1,3 +1,23 @@
+/*
+ * The Gemma project
+ * 
+ * Copyright (c) 2007 University of British Columbia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
 package ubic.gemma.apps;
 
 import java.io.File;
@@ -176,6 +196,8 @@ public class ComputeGoOverlapCli extends AbstractSpringAwareCLI {
 
         if ( ( coExpGO == null ) || coExpGO.isEmpty() ) return null;
 
+        if ( ( masterGO == null ) || masterGO.isEmpty() ) return null;
+
         for ( OntologyEntry ontologyEntry : masterGO ) {
             for ( OntologyEntry ontologyEntryC : coExpGO ) {
 
@@ -183,7 +205,6 @@ public class ComputeGoOverlapCli extends AbstractSpringAwareCLI {
                     overlapTerms.add( ontologyEntry );
             }
         }
-        // overlapTerms.retainAll( coExpGO );
 
         return overlapTerms;
     }
@@ -274,11 +295,9 @@ public class ComputeGoOverlapCli extends AbstractSpringAwareCLI {
      */
     private Collection<Gene> getGeneObject( String goID, String commonName ) {
 
-        Taxon taxon;
-        taxon = taxonService.findByCommonName( commonName );
-        Collection<Gene> results = new HashSet<Gene>();
-        results = gene2GOAssociationService.findByGOTerm( goID, taxon );
-        return results;
+        Taxon taxon = taxonService.findByCommonName( commonName );
+        return gene2GOAssociationService.findByGOTerm( goID, taxon );
+        
     }
 
     public static void main( String[] args ) {
