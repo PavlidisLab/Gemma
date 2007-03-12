6ddc7525b07e2bcf3834a21f9c0eb374b84cf0a9
xwan <>
2007-03-12 22:21:56 +0000

    *** empty log message ***

--- gemma-core/src/main/java/ubic/gemma/analysis/linkAnalysis/MetaLinkFinder.java
+++ gemma-core/src/main/java/ubic/gemma/analysis/linkAnalysis/MetaLinkFinder.java
@@ -82,7 +82,7 @@ public class MetaLinkFinder {
     public void find(Collection<Gene> genes){
     	if(genes == null || genes.size() == 0) return;
     	Taxon taxon = genes.iterator().next().getTaxon();
-    	Collection <ExpressionExperiment> ees = eeService.getByTaxon(taxon);
+    	Collection <ExpressionExperiment> ees = eeService.findByTaxon(taxon);
     	if(ees == null || ees.size() == 0) return;
     	this.find(genes, ees);
     }
