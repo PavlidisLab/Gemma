8a34a59801deb7550639b3dd3127f10fa9c4dc84
keshav <>
2007-03-28 16:33:12 +0000

    minor

--- gemma-core/src/main/resources/ubic/gemma/javaspaces/gigaspaces/ExpressionExperimentTaskImpl.java
+++ gemma-core/src/main/resources/ubic/gemma/javaspaces/gigaspaces/ExpressionExperimentTaskImpl.java
@@ -1,13 +1,40 @@
+/*
+ * The Gemma project
+ * 
+ * Copyright (c) 2006 University of British Columbia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package ubic.gemma.javaspaces.gigaspaces;
 
 import ubic.gemma.model.expression.experiment.ExpressionExperiment;
 import ubic.gemma.model.expression.experiment.ExpressionExperimentService;
 
+/**
+ * @author keshav
+ * @version $Id$
+ */
 public class ExpressionExperimentTaskImpl implements ExpressionExperimentTask {
 
     private long counter = 0;
     private ExpressionExperimentService expressionExperimentService = null;
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see ubic.gemma.javaspaces.gigaspaces.ExpressionExperimentTask#execute(ubic.gemma.model.expression.experiment.ExpressionExperiment)
+     */
     public Result execute( ExpressionExperiment expressionExperiment ) {
 
         ExpressionExperiment persistedExpressionExperiment = expressionExperimentService.create( expressionExperiment );
@@ -21,6 +48,9 @@ public class ExpressionExperimentTaskImpl implements ExpressionExperimentTask {
 
     }
 
+    /**
+     * @param expressionExperimentService
+     */
     public void setExpressionExperimentService( ExpressionExperimentService expressionExperimentService ) {
         this.expressionExperimentService = expressionExperimentService;
     }
