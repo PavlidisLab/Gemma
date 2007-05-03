b407216174f4324cfcb0f6f80d864efba9d7701a
ppavlidis <paul@chibi.ubc.ca>
2007-05-03 00:24:37 +0000

    support getaspect

--- gemma-web/src/main/java/ubic/gemma/web/taglib/displaytag/OntologyWrapper.java
+++ gemma-web/src/main/java/ubic/gemma/web/taglib/displaytag/OntologyWrapper.java
@@ -5,10 +5,11 @@ import org.apache.commons.logging.LogFactory;
 import org.displaytag.decorator.TableDecorator;
 
 import ubic.gemma.model.common.description.Characteristic;
-import ubic.gemma.ontology.GeneOntologyService; 
+import ubic.gemma.model.common.description.VocabCharacteristic;
+import ubic.gemma.ontology.GeneOntologyService;
 
 /**
- * used for proper decoration of the ontologyEntry
+ * This is for Gene Ontology.
  * <p>
  * See http://displaytag.sourceforge.net/10/tut_decorators.html and http://displaytag.sourceforge.net/10/tut_links.html
  * for explanation of how this works.
@@ -41,6 +42,11 @@ public class OntologyWrapper extends TableDecorator {
         return "<a target='_blank' href='http://amigo.geneontology.org/cgi-bin/amigo/go.cgi?view=details&query="
                 + GeneOntologyService.asRegularGoId( oe ) + "'>" + GeneOntologyService.asRegularGoId( oe ) + "</a>";
 
+    }
+
+    public String getAspect() {
+        VocabCharacteristic oe = ( VocabCharacteristic ) getCurrentRowObject();
+        return GeneOntologyService.getTermAspect( oe );
     }
 
 }
