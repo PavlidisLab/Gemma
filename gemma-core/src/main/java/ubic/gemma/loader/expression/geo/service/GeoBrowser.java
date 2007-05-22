4abcd442f39684a230d0095585c5fe64d93ac2f8
ppavlidis <paul@chibi.ubc.ca>
2007-05-22 20:04:36 +0000

    mionr

--- gemma-core/src/main/java/ubic/gemma/loader/expression/geo/service/GeoBrowser.java
+++ gemma-core/src/main/java/ubic/gemma/loader/expression/geo/service/GeoBrowser.java
@@ -25,7 +25,6 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
