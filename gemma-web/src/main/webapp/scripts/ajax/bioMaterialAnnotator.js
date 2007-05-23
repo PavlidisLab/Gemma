12725de57e74b3eaf0a7407bf452f78efa846da2
kelsey <>
2007-05-23 20:10:10 +0000

    uses simple dwr call

--- gemma-web/src/main/webapp/scripts/ajax/bioMaterialAnnotator.js
+++ gemma-web/src/main/webapp/scripts/ajax/bioMaterialAnnotator.js
@@ -4,6 +4,7 @@
 //expose the main layout panels for use later. 
  var northPanel, southPanel, eastPanel, westPanel, centerPanel;
  
+//The simple function will be called when the document is finished loading.  See event at bottem of file 
 Simple = function() {
 
    return {
@@ -16,7 +17,7 @@ Simple = function() {
                    split: false, initialSize: 400
                },
                east: {
-                   split: false, initialSize: 300
+                   split: true, initialSize: 300
                },
                west: {
                    split: false, initialSize: 0
@@ -32,13 +33,13 @@ Simple = function() {
                fitToFrame: true, closable: false
            }));
            mainLayout.add('east', eastPanel = new Ext.ContentPanel('east-div', {
-               fitToFrame: true, closable: false
+               fitToFrame: true, closable: false, autoScroll: true
            }));
            mainLayout.add('west', westPanel = new Ext.ContentPanel('west-div', {
                fitToFrame: true, closable: false
            }));
            mainLayout.add('center', centerPanel = new Ext.ContentPanel('center-div', {
-               fitToFrame: true
+               fitToFrame: true , autoScroll: true
            }));
            mainLayout.endUpdate();
                        mainLayout.getRegion('west').hide();
@@ -95,30 +96,9 @@ var initTree = function(div){
    });
 
 
- 	var read = function( data, attr ){
-    	var node = attr[0];
-        this.transId = false;
-        displayRestrictionsPanel(node);
-        //this.processResponse(data, node, "foo" ); // no callback.
-        //this.fireEvent("load", this, node, data);
-        //node.fireEvent("load", this, data);
-       // return node; // need to complete the Reader interface but this isn't really used?
-    }
     
-   tree.on('click', function(node){
-                       //MgedOntologyService.getTerm{params:[node.id]});
-                       
-                        var args = [];
-            			args.push(node.id);
-            			var cb = {
-                				success: this.read,
-				                failure: this.handleFailure,
-                				scope: this,
-        						argument: {callback: read, node: node}
-            				 };
-                       var proxy = new Ext.data.DWRProxy( MgedOntologyService.getTerm, cb);
-           			   this.transId = proxy.load(args, this, "foo", this, args );
-           			   
+   tree.on('click', function(node){                       
+                   MgedOntologyService.getTerm(node.id,displayRestrictionsPanel);                                  			   
           });
 
 
@@ -131,7 +111,6 @@ var initTree = function(div){
    });
    tree.setRootNode(root);
 
-	var read = 
 
    // render the tree
    tree.render();
@@ -140,63 +119,16 @@ var initTree = function(div){
 
 };
 
-var showDetails = function(node) {
-       dds.load({params:[{id: node}]});
-};
-
 
 var displayRestrictionsPanel = function(node) {
 
 		var dh = Ext.DomHelper;
-		dh.overwrite("east-div", {tag : 'h2', html : "Details for: " + node.id + " on " });
+		dh.overwrite("east-div", {tag : 'h2', html : "Details for: " + node.uri });
 		//dh.append("east-div", {tag : 'ul', children : [
 		//	{tag : 'li' , html: "Sequence: " + seqName}
 		//]});
 
 
-
-};
-
-
-
-var individualDS;
-
-var initIndividualsGrid = function(div) {
-       var     recordIndividualType = Ext.data.Record.create([
-                       {name:"id", type:"int"},
-                       {name:"restrictedTo" },
-                       {name:"restrictionOn"}
-               ]);
-
-
-
-       individualDS = new Ext.data.Store(
-               {
-                       proxy:new Ext.data.DWRProxy(MgedOntologyService.getTermIndividuals),
-                       reader:new Ext.data.ListRangeReader({id:"id"}, recordIndividualType),
-                       remoteSort:false
-                       //sortInfo:{field:'uri'}
-               });
-
-       var individualCM = new Ext.grid.ColumnModel([
-                       {header: "Label", width: 100, dataIndex:"label"},
-                       {header: "Restricted To",  width: 130, dataIndex:"restrictedTo", renderer: restrictedToRenderer},
-                       {header: "Restriction On",  width: 130, dataIndex:"restrictionOn", renderer: restritionOn}
-
-                       ]);
-
-       var restrictedToRenderer =  function(arg){
-               return "im here";
-       };
-
-       var restritionOn =  function(arg){
-               return arg.uri;
-       };
-
-       //individualCM.defaultSortable = true;
-
-       individualGrid = new Ext.grid.Grid("west-div", {autoSizeColumns: true, ds:individualDS, cm:individualCM, loadMask: true });
-       individualGrid.render();
 
 };
 
