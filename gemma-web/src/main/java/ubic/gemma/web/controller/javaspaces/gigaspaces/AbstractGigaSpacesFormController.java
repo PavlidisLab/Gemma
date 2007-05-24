1245c667d3f740cfba6be49ad211ea8025d92038
keshav <>
2007-05-24 20:45:01 +0000

    now implements RemoteEventListener to receive notifications from the compute server

--- gemma-web/src/main/java/ubic/gemma/web/controller/javaspaces/gigaspaces/AbstractGigaSpacesFormController.java
+++ gemma-web/src/main/java/ubic/gemma/web/controller/javaspaces/gigaspaces/AbstractGigaSpacesFormController.java
@@ -18,10 +18,15 @@
  */
 package ubic.gemma.web.controller.javaspaces.gigaspaces;
 
+import java.rmi.RemoteException;
 import java.util.concurrent.FutureTask;
 
 import javax.servlet.http.HttpServletRequest;
 
+import net.jini.core.event.RemoteEvent;
+import net.jini.core.event.RemoteEventListener;
+import net.jini.core.event.UnknownEventException;
+import net.jini.core.lease.Lease;
 import net.jini.space.JavaSpace;
 
 import org.acegisecurity.context.SecurityContext;
@@ -29,13 +34,20 @@ import org.acegisecurity.context.SecurityContextHolder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
+import org.springmodules.javaspaces.gigaspaces.GigaSpacesTemplate;
 
 import ubic.gemma.util.javaspaces.gigaspaces.GemmaSpacesEnum;
 import ubic.gemma.util.javaspaces.gigaspaces.GigaSpacesUtil;
+import ubic.gemma.util.progress.GigaspacesProgressJobImpl;
+import ubic.gemma.util.progress.ProgressData;
 import ubic.gemma.web.controller.BackgroundControllerJob;
 import ubic.gemma.web.controller.BackgroundProcessingFormController;
 import ubic.gemma.web.controller.TaskRunningService;
 import ubic.gemma.web.util.MessageUtil;
+
+import com.j_spaces.core.client.EntryArrivedRemoteEvent;
+import com.j_spaces.core.client.ExternalEntry;
+import com.j_spaces.core.client.NotifyModifiers;
 
 /**
  * Controllers requiring the capability to submit jobs to a compute server should extend this controller.
@@ -43,7 +55,8 @@ import ubic.gemma.web.util.MessageUtil;
  * @author keshav
  * @version $Id$
  */
-public abstract class AbstractGigaSpacesFormController extends BackgroundProcessingFormController {
+public abstract class AbstractGigaSpacesFormController extends BackgroundProcessingFormController implements
+        RemoteEventListener {
 
     private GigaSpacesUtil gigaSpacesUtil = null;
 
@@ -111,7 +124,14 @@ public abstract class AbstractGigaSpacesFormController extends BackgroundProcess
                 this.saveMessage( request, "Cannot service task " + taskName + " on the compute server at this time." );
                 return new ModelAndView( new RedirectView( "/Gemma/loadExpressionExperiment.html" ) );
             }
+            /* register this "spaces client" to receive notifications */
+            GigaSpacesTemplate template = ( GigaSpacesTemplate ) updatedContext.getBean( "gigaspacesTemplate" );
+            template.addNotifyDelegatorListener( this, new GigaspacesProgressJobImpl(), null, true, Lease.FOREVER,
+                    NotifyModifiers.NOTIFY_ALL );
 
+            // TODO Verify if you actually need a new space runner. I don't think I do since the gigaspaces
+            // progress job updating will happen on the compute server end, then I'll receive the notification
+            // here at which time I'll just do a log.info, which should get caught by the ProgressAppender.
             job = getSpaceRunner( taskId, context, request, command, this.getMessageUtil() );
         } else {
             job = getRunner( taskId, context, request, command, this.getMessageUtil() );
@@ -132,5 +152,36 @@ public abstract class AbstractGigaSpacesFormController extends BackgroundProcess
      */
     protected void injectGigaspacesUtil( GigaSpacesUtil gigaspacesUtil ) {
         this.gigaSpacesUtil = gigaspacesUtil;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see net.jini.core.event.RemoteEventListener#notify(net.jini.core.event.RemoteEvent)
+     */
+    public void notify( RemoteEvent remoteEvent ) throws UnknownEventException, RemoteException {
+
+        log.debug( "Notification received from space ..." );
+
+        try {
+            EntryArrivedRemoteEvent arrivedRemoteEvent = ( EntryArrivedRemoteEvent ) remoteEvent;
+            log.debug( "event: " + arrivedRemoteEvent );
+
+            ExternalEntry entry = ( ExternalEntry ) arrivedRemoteEvent.getEntry( true );
+            log.debug( "entry: " + entry );
+            log.debug( "id: " + arrivedRemoteEvent.getID() );
+            log.debug( "sequence number: " + arrivedRemoteEvent.getSequenceNumber() );
+            log.debug( "notify type: " + arrivedRemoteEvent.getNotifyType() );
+
+            /*
+             * Since the logging level is INFO, this logging event should be subscribed by the ProgressAppender and the
+             * ProgressGigaspacesJobImpl updated.
+             */
+            log.info( "message: " + ( ( ProgressData ) entry.getFieldValue( "pData" ) ).getDescription() );
+
+        } catch ( Exception e ) {
+            throw new RuntimeException( e );
+        }
+
     }
 }
