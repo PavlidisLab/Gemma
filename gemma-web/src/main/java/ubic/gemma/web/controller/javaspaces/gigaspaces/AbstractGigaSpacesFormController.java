8fa8202ecdb423af52facb3236dccdf7dc7baf14
keshav <>
2007-05-29 13:58:30 +0000

    No longer implements the RemoteEventListener.  Now, this instantiates a JavaSpacesJobObserver.

--- gemma-web/src/main/java/ubic/gemma/web/controller/javaspaces/gigaspaces/AbstractGigaSpacesFormController.java
+++ gemma-web/src/main/java/ubic/gemma/web/controller/javaspaces/gigaspaces/AbstractGigaSpacesFormController.java
@@ -18,14 +18,10 @@
  */
 package ubic.gemma.web.controller.javaspaces.gigaspaces;
 
-import java.rmi.RemoteException;
 import java.util.concurrent.FutureTask;
 
 import javax.servlet.http.HttpServletRequest;
 
-import net.jini.core.event.RemoteEvent;
-import net.jini.core.event.RemoteEventListener;
-import net.jini.core.event.UnknownEventException;
 import net.jini.core.lease.Lease;
 import net.jini.space.JavaSpace;
 
@@ -36,18 +32,15 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 import org.springmodules.javaspaces.gigaspaces.GigaSpacesTemplate;
 
-import ubic.basecode.thread.ThreadUtil;
+import ubic.gemma.util.javaspaces.GemmaSpacesLoggingEntry;
+import ubic.gemma.util.javaspaces.JavaSpacesJobObserver;
 import ubic.gemma.util.javaspaces.gigaspaces.GemmaSpacesEnum;
 import ubic.gemma.util.javaspaces.gigaspaces.GigaSpacesUtil;
-import ubic.gemma.util.progress.GigaspacesProgressJobImpl;
-import ubic.gemma.util.progress.ProgressData;
 import ubic.gemma.web.controller.BackgroundControllerJob;
 import ubic.gemma.web.controller.BackgroundProcessingFormController;
 import ubic.gemma.web.controller.TaskRunningService;
 import ubic.gemma.web.util.MessageUtil;
 
-import com.j_spaces.core.client.EntryArrivedRemoteEvent;
-import com.j_spaces.core.client.ExternalEntry;
 import com.j_spaces.core.client.NotifyModifiers;
 
 /**
@@ -56,8 +49,7 @@ import com.j_spaces.core.client.NotifyModifiers;
  * @author keshav
  * @version $Id$
  */
-public abstract class AbstractGigaSpacesFormController extends BackgroundProcessingFormController implements
-        RemoteEventListener {
+public abstract class AbstractGigaSpacesFormController extends BackgroundProcessingFormController {
 
     private GigaSpacesUtil gigaSpacesUtil = null;
 
@@ -122,19 +114,28 @@ public abstract class AbstractGigaSpacesFormController extends BackgroundProcess
                 // .getAuthentication().getPrincipal() );
                 // this.sendEmail( user, "Cannot service task " + taskName + " on the compute server at this time.",
                 // "http://www.bioinformatics.ubc.ca/Gemma/" );
-                this.saveMessage( request, "Cannot service task " + taskName + " on the compute server at this time." );
+                this.saveMessage( request, "Cannot service task " + taskName.getClass().getSimpleName()
+                        + " on the compute server at this time." );
                 return new ModelAndView( new RedirectView( "/Gemma/loadExpressionExperiment.html" ) );
             }
             /* register this "spaces client" to receive notifications */
-            GigaSpacesTemplate template = ( GigaSpacesTemplate ) updatedContext.getBean( "gigaspacesTemplate" );
-            template.addNotifyDelegatorListener( this, new GigaspacesProgressJobImpl(), null, true, Lease.FOREVER,
-                    NotifyModifiers.NOTIFY_ALL );
+            JavaSpacesJobObserver javaSpacesJobObserver = new JavaSpacesJobObserver( taskId );
 
-            // TODO Verify if you actually need a new space runner. I don't think I do since the gigaspaces
-            // progress job updating will happen on the compute server end, then I'll receive the notification
-            // here at which time I'll just do a log.info, which should get caught by the ProgressAppender.
+            GigaSpacesTemplate template = ( GigaSpacesTemplate ) updatedContext.getBean( "gigaspacesTemplate" );
+
+            template.addNotifyDelegatorListener( javaSpacesJobObserver, new GemmaSpacesLoggingEntry(), null, true,
+                    Lease.FOREVER, NotifyModifiers.NOTIFY_ALL );
+
             job = getSpaceRunner( taskId, context, request, command, this.getMessageUtil() );
-        } else {
+        } else if ( !updatedContext.containsBean( "gigaspacesTemplate" ) && taskName != null ) {
+            this
+                    .saveMessage( request,
+                            "This task must be run on the compute server, but the space is not running.  Please try again later." );
+
+            return new ModelAndView( new RedirectView( "/Gemma/loadExpressionExperiment.html" ) );
+        }
+
+        else {
             job = getRunner( taskId, context, request, command, this.getMessageUtil() );
         }
 
@@ -153,38 +154,5 @@ public abstract class AbstractGigaSpacesFormController extends BackgroundProcess
      */
     protected void injectGigaspacesUtil( GigaSpacesUtil gigaspacesUtil ) {
         this.gigaSpacesUtil = gigaspacesUtil;
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see net.jini.core.event.RemoteEventListener#notify(net.jini.core.event.RemoteEvent)
-     */
-    public void notify( RemoteEvent remoteEvent ) throws UnknownEventException, RemoteException {
-
-        log.debug( "Notification received from space ..." );
-
-        try {
-            EntryArrivedRemoteEvent arrivedRemoteEvent = ( EntryArrivedRemoteEvent ) remoteEvent;
-            log.debug( "event: " + arrivedRemoteEvent );
-
-            ExternalEntry entry = ( ExternalEntry ) arrivedRemoteEvent.getEntry( true );
-            log.debug( "entry: " + entry );
-            log.debug( "id: " + arrivedRemoteEvent.getID() );
-            log.debug( "sequence number: " + arrivedRemoteEvent.getSequenceNumber() );
-            log.debug( "notify type: " + arrivedRemoteEvent.getNotifyType() );
-
-            // ThreadUtil.visitAllRunningThreads();
-
-            /*
-             * Since the logging level is INFO, this logging event should be subscribed by the ProgressAppender and the
-             * ProgressGigaspacesJobImpl updated.
-             */
-            log.info( "message: " + ( ( ProgressData ) entry.getFieldValue( "pData" ) ).getDescription() );
-
-        } catch ( Exception e ) {
-            throw new RuntimeException( e );
-        }
-
     }
 }
