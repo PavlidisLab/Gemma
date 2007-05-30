d09b885d201a0fb4ccd7fc35cee17fa226f909ee
xwan <>
2007-05-30 22:19:18 +0000

    *** empty log message ***

--- gemma-core/src/main/java/ubic/gemma/apps/ShuffleLinksCli.java
+++ gemma-core/src/main/java/ubic/gemma/apps/ShuffleLinksCli.java
@@ -6,6 +6,7 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.Format;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -17,6 +18,8 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.time.StopWatch;
+
+import com.ibm.icu.text.NumberFormat;
 
 import ubic.basecode.dataStructure.matrix.CompressedNamedBitMatrix;
 import ubic.gemma.analysis.linkAnalysis.MetaLinkFinder;
@@ -44,13 +47,16 @@ public class ShuffleLinksCli extends AbstractSpringAwareCLI {
 	private Probe2ProbeCoexpressionService p2pService = null;
 	private GeneService geneService = null;
 	private ExpressionExperimentService eeService = null;
-    private HashMap<Long, Integer> eeMap = null;
+    private Map<Long, Integer> eeMap = null;
     private int iteration_num = 100;
     private final static int LINK_MAXIMUM_COUNT = 100;
     private CompressedNamedBitMatrix linkCount = null;
     private CompressedNamedBitMatrix negativeLinkCount = null;
     private int[][] stats = null;
     private int currentIteration = 0;
+    private int linkStringency = 0;
+    private int totalLinks = 0;
+    private Set<Long> geneCoverage = new HashSet<Long>(); 
 	@Override
 	protected void buildOptions() {
 //		 TODO Auto-generated method stub
@@ -60,15 +66,20 @@ public class ShuffleLinksCli extends AbstractSpringAwareCLI {
 //        Option eeId = OptionBuilder.hasArg().withArgName( "Expression Experiment Id" ).withDescription(
 //        "Expression Experiment Id" ).withLongOpt( "eeId" ).create( 'e' );
 //        addOption( eeId );
-        Option eeNameFile = OptionBuilder.hasArg().isRequired().withArgName( "File having Expression Experiment Names" ).withDescription(
+        Option eeNameFile = OptionBuilder.hasArg().withArgName( "File having Expression Experiment Names" ).withDescription(
         "File having Expression Experiment Names" ).withLongOpt( "eeFileName" ).create( 'f' );
         addOption( eeNameFile );
         Option startPreparing = OptionBuilder.withArgName( " Starting preparing " ).withDescription(
         " Starting preparing the temppory tables " ).withLongOpt( "startPreparing" ).create( 's' );
         addOption( startPreparing );
+        
         Option iterationNum = OptionBuilder.hasArg().withArgName( " The number of iteration for shuffling " ).withDescription(
         " The number of iteration for shuffling " ).withLongOpt( "iterationNum" ).create( 'i' );
         addOption( iterationNum );
+        
+        Option linkStringency = OptionBuilder.hasArg().withArgName( " The Link Stringency " ).withDescription(
+        " The link Stringency " ).withLongOpt( "linkStringency" ).create( 'l' );
+        addOption( linkStringency );
 	}
     protected void processOptions() {
         super.processOptions();
@@ -87,6 +98,10 @@ public class ShuffleLinksCli extends AbstractSpringAwareCLI {
         if ( hasOption( 'i' ) ) {
             this.iteration_num = Integer.valueOf(getOptionValue( 'i' ));
         }
+        if ( hasOption( 'l' ) ) {
+            this.linkStringency = Integer.valueOf(getOptionValue( 'l' ));
+        }
+
         p2pService = (Probe2ProbeCoexpressionService) this.getBean ( "probe2ProbeCoexpressionService" );
         geneService = (GeneService) this.getBean( "geneService" );
         eeService = (ExpressionExperimentService) this.getBean( "expressionExperimentService" );
@@ -156,7 +171,7 @@ public class ShuffleLinksCli extends AbstractSpringAwareCLI {
         }
         return linkCount;
     }
-    private void fillingMatrix(CompressedNamedBitMatrix linkCount, Collection<Link> links, ExpressionExperiment ee){
+    private void fillingMatrix(Collection<Link> links, ExpressionExperiment ee){
         Set<Long> csIds = new HashSet<Long>();
         for(Link link:links){
             csIds.add(link.getFirst_design_element_fk());
@@ -171,43 +186,57 @@ public class ShuffleLinksCli extends AbstractSpringAwareCLI {
             	log.info(" Preparation is not correct (get null genes) " + link.getFirst_design_element_fk() + "," + link.getSecond_design_element_fk());
             	continue;
             }
-            if(firstGeneIds.size() != 1 || secondGeneIds.size() != 1){
-            	log.info(" Preparation is not correct (get non-specific genes)" + link.getFirst_design_element_fk() + "," + link.getSecond_design_element_fk());
-            	System.exit(0);
+//            if(firstGeneIds.size() != 1 || secondGeneIds.size() != 1){
+//            	log.info(" Preparation is not correct (get non-specific genes)" + link.getFirst_design_element_fk() + "," + link.getSecond_design_element_fk());
+//            	System.exit(0);
+//            }
+            for(Long firstGeneId:firstGeneIds){
+            	for(Long secondGeneId:secondGeneIds){
+                    firstGeneId = firstGeneIds.iterator().next();
+                    secondGeneId = secondGeneIds.iterator().next();
+               		geneCoverage.add(firstGeneId);
+               		geneCoverage.add(secondGeneId);
+                    try{
+                    	int rowIndex = linkCount.getRowIndexByName(firstGeneId);
+                    	int colIndex = linkCount.getColIndexByName(secondGeneId);
+                        if(link.getScore() > 0){
+                            linkCount.set( rowIndex, colIndex, eeIndex );
+                        }
+                        else{
+                           negativeLinkCount.set( rowIndex, colIndex, eeIndex );
+                        }
+                    }catch(Exception e){
+                    	log.info(" No Gene Definition " + firstGeneId + "," + secondGeneId);
+                    	//Aligned Region and Predicted Gene
+                    	continue;
+                    }
+            	}
             }
-            Long firstGeneId = firstGeneIds.iterator().next();
-            Long secondGeneId = secondGeneIds.iterator().next();
-            try{
-            	int rowIndex = linkCount.getRowIndexByName(firstGeneId);
-            	int colIndex = linkCount.getColIndexByName(secondGeneId);
-                if(link.getScore() > 0)
-                    linkCount.set( rowIndex, colIndex, eeIndex );
-                else
-                    negativeLinkCount.set( rowIndex, colIndex, eeIndex );
-            }catch(Exception e){
-            	log.info(" No Gene Definition " + firstGeneId + "," + secondGeneId);
-            	//Aligned Region and Predicted Gene
-            	continue;
-            }
+            totalLinks++;
         }
     }
     private void counting(){
         int rows = linkCount.rows();
+        int cols = linkCount.columns();
         //The filling process only filled one item. So the matrix is not symetric
         for(int i = 0; i < rows; i++){
-            for(int j = 0; j < rows; j++){
-                int bits = linkCount.bitCount( i, j );
-                int negativeBits = negativeLinkCount.bitCount(i, j);
-                if(bits > 0){
-                    stats[currentIteration][bits]++;
+        	int[] positiveBits = new int[cols];
+        	positiveBits = linkCount.getRowBits(i, positiveBits);
+        	int[] negativeBits = new int[cols];
+        	negativeBits = negativeLinkCount.getRowBits(i, negativeBits);
+        	for(int j = 0; j < cols; j++){
+                int positiveBit = positiveBits[j];
+                int negativeBit = negativeBits[j];
+                if(positiveBit > 0){
+                    stats[currentIteration][positiveBit]++;
                 }
-                if(negativeBits > 0){
-                    stats[currentIteration][negativeBits]++;
+                if(negativeBit > 0){
+                    stats[currentIteration][negativeBit]++;
                 }
-            }
+        	}
         }
-    }
 
+    }
     private void doShuffling(Collection<ExpressionExperiment> ees){
         int total = 0;
         for(ExpressionExperiment ee:ees){
@@ -218,29 +247,55 @@ public class ShuffleLinksCli extends AbstractSpringAwareCLI {
             	total = total + links.size();
             	shuffleLinks(links);
             }
-            fillingMatrix(linkCount, links,ee);
+            fillingMatrix(links,ee);
         }
         counting();
         log.info(" Shuffled " + total + " links");
     }
-    private void saveMatrix(String outFile){
-        try {
-            FileWriter out = new FileWriter( new File( outFile ) );
-            for ( int i = 0; i < linkCount.rows(); i++ )
-                for ( int j = 0; j < linkCount.columns(); j++ ) {
-                    int bits = linkCount.bitCount( i, j ); 
-                    int negativeBits = negativeLinkCount.bitCount(i,j);
-                    if (  bits != 0  || negativeBits != 0) {
-                        out.write( linkCount.getRowName( i ) + "\t" + linkCount.getColName( i ) + "\t"+ bits + "\t" + negativeBits + "\n"); 
+  private void saveMatrix(String outFile, Collection<Gene> genes){
+	Map<Long, String> geneId2Name = new HashMap<Long, String>();
+	for(Gene gene:genes){
+		geneId2Name.put(gene.getId(), gene.getName());
+	}
+    try {
+        FileWriter out = new FileWriter( new File( outFile ) );
+        int rows = linkCount.rows();
+        int cols = linkCount.columns();
+        //The filling process only filled one item. So the matrix is not symetric
+        for(int i = 0; i < rows; i++){
+        	int[] positiveBits = new int[cols];
+        	positiveBits = linkCount.getRowBits(i, positiveBits);
+        	int[] negativeBits = new int[cols];
+        	negativeBits = negativeLinkCount.getRowBits(i, negativeBits);
+        	
+        	for(int j = 0; j < cols; j++){
+                int positiveBit = positiveBits[j];
+                int negativeBit = negativeBits[j];
+                if(this.linkStringency > 0){
+                	if(positiveBit >= this.linkStringency){
+                		out.write( geneId2Name.get(linkCount.getRowName( i )) + "\t" + geneId2Name.get(linkCount.getColName( j )) + "\t"+ positiveBit + "\t" + "+" + "\n");
+                	}
+                	if(negativeBit >= this.linkStringency){
+                		out.write( geneId2Name.get(linkCount.getRowName( i )) + "\t" + geneId2Name.get(linkCount.getColName( j )) + "\t"+ negativeBit + "\t" + "-" + "\n");
+                	}
+                }else{
+                    if (  positiveBit != 0  || negativeBit != 0) {
+                        out.write( geneId2Name.get(linkCount.getRowName( i )) + "\t" + geneId2Name.get(linkCount.getColName( j )) + "\t"+ positiveBit + "\t" + negativeBit + "\n"); 
                     }
                 }
-            out.close();
-        } catch ( Exception e ) {
-            e.printStackTrace();
+        	}
         }
+        out.close();
+    } catch ( Exception e ) {
+        e.printStackTrace();
     }
+}
+
     private void saveStats(String outFile){
     	try{
+    		NumberFormat nf = NumberFormat.getInstance();
+    		nf.setMaximumFractionDigits(3);
+    		
     		FileWriter out = new FileWriter(new File(outFile));
     		for(int i = 0; i < iteration_num + 1; i++){
     			for(int j = LINK_MAXIMUM_COUNT - 2; j >= 0; j--){
@@ -254,10 +309,31 @@ public class ShuffleLinksCli extends AbstractSpringAwareCLI {
     				break;
     			}
     		}
+    		for(int j = 1; j <= maxBits; j++)
+    			out.write("Link"+j+"\t");
+    		out.write("\n");
+    		out.write("Link Distribution:\n");
+    		for(int j = 1; j <= maxBits; j++){
+    			out.write(stats[0][j]+"\t");
+    		}
+    		out.write("\n");
+    		out.write("Average False Positive Rate\n");
+    		double[] falsePositiveRates = new double[maxBits+1];
+    		for(int i = 1; i < iteration_num + 1; i++){
+    			for(int j = 1; j <= maxBits; j++){
+    				if(stats[0][j] != 0)
+    				falsePositiveRates[j] = falsePositiveRates[j] + (double)stats[i][j]/(double)stats[0][j];
+    			}
+    		}
+    		for(int j = 1; j <maxBits; j++){
+    			out.write(nf.format(falsePositiveRates[j]/iteration_num)+"\t");
+    		}
+    		out.write("\n");
+    		out.write("All shuffled false positive rates:\n");
     		for(int i = 1; i < iteration_num + 1; i++){
     			for(int j = 1; j <= maxBits; j++){
     				if(stats[0][j] == 0) out.write("\t");
-    				else out.write((double)stats[i][j]/(double)stats[0][j] + "\t");
+    				else out.write(nf.format((double)stats[i][j]/(double)stats[0][j]) + "\t");
     			}
     			out.write("\n");
     		}
@@ -294,15 +370,27 @@ public class ShuffleLinksCli extends AbstractSpringAwareCLI {
             eeMap.put(eeIter.getId(), new Integer(index));
             index++;
         }
-        //The first iteration doesn't do the shuffling and only read the real data and do the counting
-        for(currentIteration = 0; currentIteration < iteration_num + 1; currentIteration++){
-            linkCount = getMatrix(ees,genes);
-            negativeLinkCount = getMatrix(ees,genes);
-            System.gc();
-            doShuffling(candidates);
-            saveMatrix( "matrix"+currentIteration+".txt" );
+        if(linkStringency != 0){
+        	totalLinks = 0;
+    		linkCount = getMatrix(ees,genes);
+    		negativeLinkCount = getMatrix(ees,genes);
+    		System.gc();
+    		doShuffling(candidates);
+    		saveMatrix( "matrix_"+linkStringency+".txt", genes);
+    		log.info("Total Links " + totalLinks);
+    		log.info("Covered Gene " + geneCoverage.size());
+        }else{
+        	//The first iteration doesn't do the shuffling and only read the real data and do the counting
+        	for(currentIteration = 0; currentIteration < iteration_num + 1; currentIteration++){
+        		totalLinks = 0;
+        		linkCount = getMatrix(ees,genes);
+        		negativeLinkCount = getMatrix(ees,genes);
+        		System.gc();
+        		doShuffling(candidates);
+        		saveMatrix( "matrix"+currentIteration+".txt", genes );
+        	}
+        	saveStats("stats.txt");
         }
-        saveStats("stats.txt");
 //        if(this.eeId > 0){
 //        	ExpressionExperiment ee = ExpressionExperiment.Factory.newInstance();
 //        	ee.setId(this.eeId);
