8c393e2d6beb4d22837a1d91cfea31722e7c2c87
kelsey <>
2007-06-13 23:59:52 +0000

    Statment is successfully being generated on the client side. Now for persisting on the server.

--- gemma-web/src/main/webapp/scripts/ajax/bioMaterialAnnotator.js
+++ gemma-web/src/main/webapp/scripts/ajax/bioMaterialAnnotator.js
@@ -186,25 +186,17 @@ var createRestrictionGui = function(node, vc, indent, parentDivId) {
 					    });
 					    
 					    var handler = createPrimitiveTypeHandler(restrictedOn, vc);
-//					     var handler = function(field) {
-//					     	console.log(dwr.util.toDescriptiveString(vc,10));
-//					     	var newVc = {termUri : primitiveRestrictedOn.uri, data : field.getValue(), type : primitiveRestrictedTo};
-//					     	
-//					     	if (vc.properties === undefined){
-//    	                	 	vc.properties = [];    	                	 	
-//    	                	}
-//					     	
-//					   		vc.properties.push(newVc); 
-//					   };
+
 					    var valueField = new Ext.form.TextField({
 					            fieldLabel: restrictedOn.label ,
 					            name: 'hasValue',
 					            width:175,
 					            allowBlank:false,					           
 					        });
-					     
-					  //valueField.on.createDelegate(this,['valid', handler],true);					       
-					  valueField.on('valid', handler);
+					   
+					   valueField.vocabId = divId;
+					   							       
+					   valueField.on('valid', handler);
 					        
 					   simple.add(valueField);
                        dh.append(divId, {tag: 'h3', html : 'Value:' });
@@ -218,17 +210,19 @@ var createRestrictionGui = function(node, vc, indent, parentDivId) {
                     if (restrictedTo.restrictions === undefined || restrictedTo.restrictions === null || restrictedTo.restrictions.size() === 0){	// ie) we are at a leaf node so display gui
                         dh.append(divId, {tag: 'h3', html : "Create an instance of: " +restrictedTo.term });
  					 
-	                    var simple = createForm(restrictedOn,vc);	
+	                    var simple = createForm(restrictedOn,vc, divId);	
 					                       
     	                if (restrictedTo.individuals !== undefined && restrictedTo.individuals !== null && restrictedTo.individuals.size() > 0){  //are there examples?
-    	                		var combo = createComboBox(restrictedOn, restrictedTo.individuals, vc);    	                
+    	                		var combo = createComboBox(restrictedOn, restrictedTo.individuals, vc, divId);    	                
         		            	simple.column({width:285},combo);                                 	                    
                 	    }
                     
 	                    simple.render(divId);
                     } else{        //Not a leaf node. recurse down another level
+                    	//do i need to use propertiesPush here? ie do i want a field id associated with this?
                         var vcChild = { termUri : restrictedOn.uri, object : { termUri : restrictedTo.uri }, properties : []};
-                        vc.properties.push(vcChild);
+                        propertiesPush(vc, divId,vcChild);
+                        //vc.properties.push(vcChild);
     	            	createRestrictionGui( restrictedTo, vcChild.object, indent + 3, divId );
                    }                
                     
@@ -250,17 +244,19 @@ var createRestrictionGui = function(node, vc, indent, parentDivId) {
         	var divId = (Math.random() * 100000).toFixed();   
            	dh.append(parentDivId, {tag: 'div', id: divId, style : "border-width:thin;border-style:dotted;padding:5px;margin:5px;"});
            	dh.append(divId, {tag: 'h3', html :  "Create an instance of: " + node.term });      
-           	var simple = createForm(restrictedOn,vc);	
+           	var simple = createForm(restrictedOn,vc, divId);	
            	
            	//If there already exisit individuals display them in a drop down box so the user can select one.
             if ((node.individuals !== undefined) && (node.individuals !== null) && (node.individuals.size() > 0))
-            	simple.column({width:285},createComboBox(restrictedOn, node.individuals)); 	   	    
+            	simple.column({width:285},createComboBox(restrictedOn, node.individuals, divId)); 	   	    
     		
         	simple.render(divId);
         }
 	};
 
-var createComboBox = function(subject, individuals, vc){
+
+//subject
+var createComboBox = function(subject, individuals, vc, divId){
 	     	var recordType = Ext.data.Record.create([
 							{name:"label", type : "string" }, 
 							{name:"uri", type: "string" } 
@@ -284,16 +280,17 @@ var createComboBox = function(subject, individuals, vc){
 					        triggerAction: 'all',
 					        emptyText:'Select an individual',
 					        selectOnFocus:true
-					    });
+					    });	
+					    
+					    combo.vocabId = divId;				    
 					    
 					    var comboHandler = function(field,record,index){
-					    	console.log(dwr.util.toDescriptiveString(vc,10));
-					    	var vcChild = { termUri : subject.uri, object : record.data};
+					    	var vcChild = { termUri : subject.uri, object : {termUri : record.data.uri}};
 					    	
 					    	if (vc.properties === undefined){
     	                	 	vc.properties = [];    	                	 	
     	                	}
-    	                	vc.properties.push(vcChild); 
+    	                	propertiesPush(vc, field.vocabId, vcChild);    	                
                         
     	                };
     	                		
@@ -304,27 +301,68 @@ var createComboBox = function(subject, individuals, vc){
 	
 };
 
-var createForm = function(subject, vc){
+//the vc is the VocabCharacteristic we would like to add to.
+//The fieldId = the div ID of the component that contains the gui component
+//Will check to make sure that the fieldId doesn't already exisit in the properties and if it does update it
+//if not create a new one.  This is to resolve the problem of duplicates and enforces that the VC that is created is 
+//well formed. 
+
+var propertiesPush = function(vc, fieldId, toAdd){
+	
+	//Make sure that the vc we've been given has a properties field
+	if ((vc.properties === undefined) || (vc.properties === null) || (vc.properties.length === 0)){
+		vc.properties = [];
+		toAdd.fieldId = fieldId;
+		vc.properties.push(toAdd);
+		return;		
+	}
+	
+	//Check if the field id already has a property set
+	for(var i=0; i < vc.properties.length; i++){
+	
+		if (vc.properties[i].fieldId === undefined){
+			console.log("The fieldId should be defined: " + vc.properties[i]);
+			continue;
+		}
+		
+		if (vc.properties[i].fieldId === fieldId) {
+			toAdd.fieldId = fieldId;			
+			vc.properties[i] = toAdd;
+			return;					
+		}						
+	}
+	
+	//If we made it this far then the property hasn't been set already. 
+	toAdd.fieldId = fieldId;
+	vc.properties.push(toAdd);
+	
+}
+
+//creates the Lookup textbox and the custom textbox.  The form is just used for alignment purposes. 
+//The subject = the OntologyTerm that we are trying to create the vocabulary characteristic for
+//the vc = the vocabulary charactersit we are trying to create. 
+var createForm = function(subject, vc, divId){
 
 	var lookUp = 	new Ext.form.TextField({
             fieldLabel: 'Lookup',
             name: 'lookup',
             width:150,
             allowBlank:true	});
+    lookUp.vocabId = divId;
 	
 	var custom =   new Ext.form.TextField({
             fieldLabel: 'custom',
             name: 'custom',
             width:150 });
+    
+    custom.vocabId = divId;
 
-	var customHandler = function(field){
-							console.log(dwr.util.toDescriptiveString(vc,10));
-							var newVC = {termUri : subject, object : {value: field.getValue()}};
+	var customHandler = function(field){							
+							var newVC = {termUri : subject.uri, object : {value: field.getValue()}};
     	                	if (vc.properties === undefined){
     	                	 	vc.properties = [];    	                	 	
     	                	}
-    	                	//todo:  need to do something about clicking back and forth and creating more objects than necessary
-    	                	vc.properties.push(newVC); 
+    	                	propertiesPush(vc, field.vocabId, newVC);             
     	                };
     	                		
     	       custom.on('valid', customHandler);
@@ -342,11 +380,7 @@ var createForm = function(subject, vc){
 		{width:250},
 		custom								
     );
-    
-    
-
-    //simple.addButton('Save');
-    //simple.addButton('Cancel');		
+    	
    
    return simple;		
 }
@@ -363,8 +397,7 @@ var createPrimitiveTypeHandler = function(restrictedOn, vc){
     	                	 	vc.properties = [];    	                	 	
     	                	}
 					     	
-					   		vc.properties.push(newVc); 
-					   		console.log(dwr.util.toDescriptiveString(vc,10));
+					     	propertiesPush(vc, field.vocabId,newVc);
 			}
 };
 
