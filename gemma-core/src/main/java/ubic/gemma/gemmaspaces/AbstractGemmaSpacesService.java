63fdae0ea73b74772d86390c9eebea0ce8c837a3
keshav <>
2007-06-23 13:41:22 +0000

    Updated the AbstractGemmaSpacesService.  The main idea is to take existing services and wrap them as tasks if possible, then create another service which extends this that is smart enough to either run the task in the space or the task locally (ie. just as the service)

--- gemma-core/src/main/java/ubic/gemma/gemmaspaces/AbstractGemmaSpacesService.java
+++ gemma-core/src/main/java/ubic/gemma/gemmaspaces/AbstractGemmaSpacesService.java
@@ -18,10 +18,23 @@
  */
 package ubic.gemma.gemmaspaces;
 
-import org.springframework.context.ApplicationContext;
+import net.jini.core.lease.Lease;
 
+import org.acegisecurity.context.SecurityContext;
+import org.acegisecurity.context.SecurityContextHolder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.servlet.ModelAndView;
+import org.springmodules.javaspaces.gigaspaces.GigaSpacesTemplate;
+
+import ubic.gemma.gemmaspaces.expression.experiment.ExpressionExperimentReportTaskImpl;
 import ubic.gemma.util.gemmaspaces.GemmaSpacesEnum;
+import ubic.gemma.util.gemmaspaces.GemmaSpacesJobObserver;
 import ubic.gemma.util.gemmaspaces.GemmaSpacesUtil;
+import ubic.gemma.util.gemmaspaces.entry.GemmaSpacesProgressEntry;
+import ubic.gemma.util.progress.TaskRunningService;
+import ubic.gemma.web.controller.BackgroundControllerJob;
+
+import com.j_spaces.core.client.NotifyModifiers;
 
 /**
  * @author keshav
@@ -36,11 +49,64 @@ public abstract class AbstractGemmaSpacesService {
     /**
      * @return ApplicationContext
      */
-    public ApplicationContext addGigaspacesToApplicationContext() {
+    public ApplicationContext addGemmaSpacesToApplicationContext() {
         if ( gemmaSpacesUtil == null ) gemmaSpacesUtil = new GemmaSpacesUtil();
 
         return gemmaSpacesUtil.addGemmaSpacesToApplicationContext( GemmaSpacesEnum.DEFAULT_SPACE.getSpaceUrl() );
     }
+
+    protected void startJob( String spaceUrl, String taskName, boolean runInLocalContext ) {
+        run( spaceUrl, taskName, runInLocalContext );
+    }
+
+    protected String run( String spaceUrl, String taskName, boolean runInLocalContext ) {
+        /*
+         * all new threads need this to acccess protected resources (like services)
+         */
+        SecurityContext context = SecurityContextHolder.getContext();
+
+        String taskId = null;
+
+        updatedContext = addGemmaSpacesToApplicationContext();
+        BackgroundControllerJob<ModelAndView> job = null;
+        if ( updatedContext.containsBean( "gigaspacesTemplate" ) ) {
+
+            taskId = ( String ) ( ( ExpressionExperimentReportTaskImpl ) updatedContext.getBean( "taskBean" ) )
+                    .getTaskId();
+
+            if ( !gemmaSpacesUtil.canServiceTask( taskName, spaceUrl ) ) {
+
+                throw new RuntimeException( "No workers are registered to service task "
+                        + taskName.getClass().getSimpleName() + " on the compute server at this time." );
+            }
+            /* register this "spaces client" to receive notifications */
+            GemmaSpacesJobObserver javaSpacesJobObserver = new GemmaSpacesJobObserver( taskId );
+
+            GigaSpacesTemplate template = ( GigaSpacesTemplate ) updatedContext.getBean( "gigaspacesTemplate" );
+
+            template.addNotifyDelegatorListener( javaSpacesJobObserver, new GemmaSpacesProgressEntry(), null, true,
+                    Lease.FOREVER, NotifyModifiers.NOTIFY_ALL );
+
+            runRemotely( taskId );
+        } else if ( !updatedContext.containsBean( "gigaspacesTemplate" ) && !runInLocalContext ) {
+            throw new RuntimeException(
+                    "This task must be run on the compute server, but the space is not running. Please try again later" );
+        }
+
+        else {
+            taskId = TaskRunningService.generateTaskId();
+            runLocally( taskId );
+        }
+
+        assert taskId != null;
+
+        // taskRunningService.submitTask( taskId, new FutureTask<ModelAndView>( job ) );
+        return taskId;
+    }
+
+    public abstract void runLocally( String taskId );
+
+    public abstract void runRemotely( String taskId );
 
     /**
      * Controllers extending this class must implement this method. The implementation should call
