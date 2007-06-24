eb56b6843a9d770ba9a6de9be7af5656611cba85
ppavlidis <paul@chibi.ubc.ca>
2007-06-24 00:14:11 +0000

    bug 865 : what's new report was bombing

--- gemma-mda/src/main/java/ubic/gemma/model/common/auditAndSecurity/AuditEventDaoImpl.java
+++ gemma-mda/src/main/java/ubic/gemma/model/common/auditAndSecurity/AuditEventDaoImpl.java
@@ -21,9 +21,6 @@ package ubic.gemma.model.common.auditAndSecurity;
 import java.util.Collection;
 import java.util.HashSet;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import ubic.gemma.model.common.Auditable;
 
 /**
@@ -36,76 +33,59 @@ public class AuditEventDaoImpl extends ubic.gemma.model.common.auditAndSecurity.
     /**
      * FIXME this isn't complete.
      */
-    private static String[] AUDITABLES = { 
-//            "ubic.gemma.model.expression.analysis.ExpressionAnalysisImpl",
+    private static String[] AUDITABLES = {
+    // "ubic.gemma.model.expression.analysis.ExpressionAnalysisImpl",
             "ubic.gemma.model.expression.arrayDesign.ArrayDesignImpl",
-//            "ubic.gemma.model.common.description.BibliographicReferenceImpl",
-//            "ubic.gemma.model.common.auditAndSecurity.ContactImpl",
+            // "ubic.gemma.model.common.description.BibliographicReferenceImpl",
+            // "ubic.gemma.model.common.auditAndSecurity.ContactImpl",
             "ubic.gemma.model.expression.experiment.ExpressionExperimentImpl" };
 
-    private static Log log = LogFactory.getLog( AuditEventDaoImpl.class.getName() );
-
     /**
+     * Note that this only returns selected classes of auditables.
+     * 
      * @see ubic.gemma.model.common.auditAndSecurity.AuditEventDao#getUpdatedSinceDate(java.util.Date)
      * @return Collection of Auditables
      */
     @SuppressWarnings("unchecked")
     protected java.util.Collection handleGetUpdatedSinceDate( java.util.Date date ) {
-        // first get the audit trails for the objects. Then we have to get the auditables.
-
-        String queryString = "select atr from AuditTrailImpl as atr inner join fetch atr.events as ae where ae.date > :date and ae.action='U'";
-
-        try {
-            org.hibernate.Query queryObject = super.getSession( false ).createQuery( queryString );
-            queryObject.setParameter( "date", date );
-            Collection<AuditTrail> trails = queryObject.list();
-            return getAuditablesForTrails( trails );
-        } catch ( org.hibernate.HibernateException ex ) {
-            throw super.convertHibernateAccessException( ex );
-        }
-    }
-
-    /**
-     * @param trails
-     * @return
-     */
-    @SuppressWarnings("unchecked")
-    private java.util.Collection getAuditablesForTrails( Collection<AuditTrail> trails ) {
         Collection<Auditable> result = new HashSet<Auditable>();
-        if ( trails == null || trails.size() == 0 ) return result;
-
-        /*
-         * This gives a list of trails for objects...now we have to get the owning auditables.
-         */
         for ( String clazz : AUDITABLES ) {
-            String auditableQuery = "from " + clazz + " as a where a.auditTrail in (:ats)";
-            log.debug( auditableQuery );
-            org.hibernate.Query auditableQueryObj = super.getSession( false ).createQuery( auditableQuery );
-            auditableQueryObj.setParameterList( "ats", trails );
-            Collection<Auditable> auditables = auditableQueryObj.list();
-            if ( auditables != null && auditables.size() > 0 ) {
-                result.addAll( auditables );
+            String queryString = "select distinct adb from "
+                    + clazz
+                    + " adb inner join adb.auditTrail atr inner join atr.events as ae where ae.date > :date and ae.action='U'";
+            try {
+                org.hibernate.Query queryObject = super.getSession( false ).createQuery( queryString );
+                queryObject.setParameter( "date", date );
+                result.addAll( queryObject.list() );
+            } catch ( org.hibernate.HibernateException ex ) {
+                throw super.convertHibernateAccessException( ex );
             }
         }
-
         return result;
     }
 
     /**
+     * Note that this only returns selected classes of auditables.
+     * 
      * @see ubic.gemma.model.common.auditAndSecurity.AuditEventDao#getNewSinceDate(java.util.Date)
      * @return Collection of Auditables
      */
     @SuppressWarnings("unchecked")
-    protected java.util.Collection handleGetNewSinceDate( java.util.Date date ) {
-        String queryString = "select atr from AuditTrailImpl as atr inner join fetch atr.events as ae where ae.date > :date and ae.action='C'";
-        try {
-            org.hibernate.Query queryObject = super.getSession( false ).createQuery( queryString );
-            queryObject.setParameter( "date", date );
-            Collection<AuditTrail> trails = queryObject.list();
-            return getAuditablesForTrails( trails );
-        } catch ( org.hibernate.HibernateException ex ) {
-            throw super.convertHibernateAccessException( ex );
+    protected java.util.Collection<Auditable> handleGetNewSinceDate( java.util.Date date ) {
+        Collection<Auditable> result = new HashSet<Auditable>();
+        for ( String clazz : AUDITABLES ) {
+            String queryString = "select distinct adb from "
+                    + clazz
+                    + " adb inner join adb.auditTrail atr inner join atr.events as ae where ae.date > :date and ae.action='C'";
+            try {
+                org.hibernate.Query queryObject = super.getSession( false ).createQuery( queryString );
+                queryObject.setParameter( "date", date );
+                result.addAll( queryObject.list() );
+            } catch ( org.hibernate.HibernateException ex ) {
+                throw super.convertHibernateAccessException( ex );
+            }
         }
+        return result;
     }
 
 }
