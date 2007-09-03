fa2a8b8024d4fedb0417c555c5d7a4da73b71655
keshav <>
2007-09-03 18:06:57 +0000

    A differential expression analysis tool to determine which analysis to perform based on experimental factor and factor values.

---
+++ gemma-core/src/main/java/ubic/gemma/analysis/diff/DifferentialExpressionAnalysis.java
@@ -0,0 +1,100 @@
+/*
+ * The Gemma project
+ * 
+ * Copyright (c) 2007 University of British Columbia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package ubic.gemma.analysis.diff;
+
+import java.util.Collection;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import ubic.gemma.model.expression.experiment.ExperimentalFactor;
+import ubic.gemma.model.expression.experiment.FactorValue;
+
+/**
+ * @author keshav
+ * @version $Id$
+ */
+public class DifferentialExpressionAnalysis {
+    private Log log = LogFactory.getLog( this.getClass() );
+
+    private int EXPERIMENTAL_FACTOR_ONE = 1;
+    private int EXPERIMENTAL_FACTOR_TWO = 2;
+    private int FACTOR_VALUE_ONE = 1;
+
+    /**
+     * Determines the analysis to execute based on the experimental factors and factor values.
+     * 
+     * @param experimentalFactors
+     */
+    protected void determineAnalysis( Collection<ExperimentalFactor> experimentalFactors ) {
+        // TODO make analyzers implement Analyzer and return the appropriate analyzer from here
+        if ( colIsEmpty( experimentalFactors ) )
+            throw new RuntimeException(
+                    "Collection of experimental factors is either null or 0.  Cannot execute differential expression analysis." );
+
+        if ( experimentalFactors.size() == EXPERIMENTAL_FACTOR_ONE ) {
+            ExperimentalFactor experimentalFactor = experimentalFactors.iterator().next();
+            Collection<FactorValue> factorValues = experimentalFactor.getFactorValues();
+            if ( colIsEmpty( factorValues ) )
+                throw new RuntimeException(
+                        "Collection of factor values is either null or 0. Cannot execute differential expression analysis." );
+            if ( factorValues.size() == FACTOR_VALUE_ONE ) {
+                throw new RuntimeException( experimentalFactors.size() + " experimental factor(s) with "
+                        + factorValues.size() + " factor value(s).  Cannot execute differential expression analysis." );
+            } else {
+                log.debug( experimentalFactors.size() + " experimental factor(s) with " + factorValues.size()
+                        + " factor value(s).  Running one way anova." );
+                // execute one way anova ... this takes care of the t-test as well, since a one-way anova with two
+                // groups is just a t-test
+            }
+
+        }
+
+        else if ( experimentalFactors.size() == EXPERIMENTAL_FACTOR_TWO ) {
+            for ( ExperimentalFactor f : experimentalFactors ) {
+                Collection<FactorValue> factorValues = f.getFactorValues();
+                if ( colIsEmpty( factorValues ) || factorValues.size() < 2 ) {
+                    throw new RuntimeException( experimentalFactors.size() + " experimental factor(s) with "
+                            + factorValues.size()
+                            + " factor value(s).  Cannot execute differential expression analysis." );
+                }
+            }
+            // check for block design and execute two way anova (with or without interactions)
+        }
+
+        else {
+            throw new RuntimeException(
+                    "Differential expression analysis supports a maximum of 2 experimental factors at this time." );
+        }
+
+    }
+
+    /**
+     * @param col
+     * @return
+     */
+    private boolean colIsEmpty( Collection col ) {
+        if ( col == null ) return true;
+
+        if ( col.size() == 0 ) return true;
+
+        return false;
+    }
+
+}
