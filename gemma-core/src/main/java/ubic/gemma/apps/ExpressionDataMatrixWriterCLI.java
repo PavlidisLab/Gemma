d7eb19581da3fdf29e23845fcf9a2998cb04183b
raymond <>
2007-09-04 21:46:52 +0000

    New expression data matrix writer cli class

---
+++ gemma-core/src/main/java/ubic/gemma/apps/ExpressionDataMatrixWriterCLI.java
@@ -0,0 +1,100 @@
+package ubic.gemma.apps;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+import java.util.HashSet;
+
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+
+import ubic.gemma.analysis.preprocess.filter.ExpressionExperimentFilter;
+import ubic.gemma.analysis.preprocess.filter.FilterConfig;
+import ubic.gemma.datastructure.matrix.ExpressionDataDoubleMatrix;
+import ubic.gemma.datastructure.matrix.MatrixWriter;
+import ubic.gemma.model.common.quantitationtype.QuantitationType;
+import ubic.gemma.model.expression.arrayDesign.ArrayDesign;
+import ubic.gemma.model.expression.arrayDesign.ArrayDesignService;
+import ubic.gemma.model.expression.bioAssayData.DesignElementDataVector;
+import ubic.gemma.model.expression.designElement.CompositeSequence;
+import ubic.gemma.model.expression.experiment.ExpressionExperiment;
+
+public class ExpressionDataMatrixWriterCLI extends
+		AbstractGeneExpressionExperimentManipulatingCLI {
+	
+	private ArrayDesignService adService;
+	
+	private String outFileName;
+	protected void buildOptions() {
+		super.buildOptions();
+		Option outputFileOption = OptionBuilder.hasArg().isRequired()
+				.withArgName("outFilePrefix").withDescription(
+						"File prefix for saving the output").withLongOpt(
+						"outFilePrefix").create('o');
+		addOption(outputFileOption);
+	}
+	
+	protected void processOptions() {
+		super.processOptions();
+		
+		outFileName = getOptionValue('o');
+		
+		adService = (ArrayDesignService) getBean("arrayDesignService");
+	}
+	
+	@Override
+	protected Exception doWork(String[] args) {
+		processCommandLine("expressionDataMatrixWriterCLI", args);
+		
+		Collection<ExpressionExperiment> ees;
+		try {
+    		ees = getExpressionExperiments(null);
+		} catch (IOException e) {
+			return e;
+		}
+		FilterConfig filterConfig = new FilterConfig();
+		
+		for (ExpressionExperiment ee : ees) {
+            Collection<ArrayDesign> ads = eeService.getArrayDesignsUsed( ee );
+
+            Collection<CompositeSequence> css = new HashSet<CompositeSequence>();
+            for ( ArrayDesign ad : ads ) {
+                css.addAll( adService.loadCompositeSequences( ad ) );
+            }
+
+            // get quantitation types
+            Collection<QuantitationType> qts;
+            qts = ( Collection<QuantitationType> ) eeService.getPreferredQuantitationType( ee );
+            if ( qts.size() < 1 ) {
+                return null;
+            }
+            QuantitationType qt = qts.iterator().next();
+
+            // get dedvs to build expression data matrix
+            Collection<DesignElementDataVector> dedvs;
+            dedvs = eeService.getDesignElementDataVectors( css, qt );
+            ExpressionExperimentFilter filter = new ExpressionExperimentFilter( ee, eeService.getArrayDesignsUsed( ee ),
+                    filterConfig );
+            ExpressionDataDoubleMatrix dataMatrix = filter.getFilteredMatrix( dedvs );
+            
+            try {
+                MatrixWriter out = new MatrixWriter();
+                PrintWriter writer = new PrintWriter(outFileName);
+                out.write(writer, dataMatrix, true);
+            } catch (IOException e) {
+            	return e;
+            }
+		}
+		
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		ExpressionDataMatrixWriterCLI cli = new ExpressionDataMatrixWriterCLI();
+		Exception exc = cli.doWork(args);
+		if (exc != null) {
+			log.error(exc.getMessage());
+		}
+	}
+
+}
