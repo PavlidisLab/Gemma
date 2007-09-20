7fa7213d8a12e55f88e93f5a08ce50c376424512
keshav <>
2007-09-20 18:47:52 +0000

    two way anova

---
+++ gemma-core/src/main/java/ubic/gemma/analysis/diff/TwoWayAnovaAnalyzer.java
@@ -0,0 +1,54 @@
+/*
+ * The Gemma project
+ * 
+ * Copyright (c) 2006 University of British Columbia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package ubic.gemma.analysis.diff;
+
+import java.util.Collection;
+import java.util.Map;
+
+import ubic.gemma.model.expression.designElement.DesignElement;
+import ubic.gemma.model.expression.experiment.ExperimentalFactor;
+import ubic.gemma.model.expression.experiment.ExpressionExperiment;
+
+/**
+ * @author keshav
+ * @version $Id$
+ */
+public class TwoWayAnovaAnalyzer extends AbstractAnalyzer {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see ubic.gemma.analysis.diff.AbstractAnalyzer#getPValues(ubic.gemma.model.expression.experiment.ExpressionExperiment,
+     *      java.util.Collection)
+     */
+    @Override
+    public Map<DesignElement, Double> getPValues( ExpressionExperiment expressionExperiment,
+            Collection<ExperimentalFactor> experimentalFactors ) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public Map<DesignElement, Double> twoWayAnova() {
+        return null;
+    }
+
+}
