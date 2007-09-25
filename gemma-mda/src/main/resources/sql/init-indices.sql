8d5cf5899e06868b6c9f61383efad6239eda100c
ppavlidis <paul@chibi.ubc.ca>
2007-09-25 23:51:04 +0000

    add index for class in AUDIT_EVENT_TYPE.

--- gemma-mda/src/main/resources/sql/init-indices.sql
+++ gemma-mda/src/main/resources/sql/init-indices.sql
@@ -10,6 +10,6 @@ alter table CHROMOSOME_FEATURE add index type (TYPE);
 alter table GENE_ALIAS add index `alias` (`ALIAS`);
 alter table DESIGN_ELEMENT add index name (NAME);
 alter table PHYSICAL_LOCATION ADD INDEX BIN_KEY (BIN);
-
+alter table AUDIT_EVENT_TYPE ADD INDEX class (class);
 -- Next is not an index, but needed to allow sequences to be long.
 alter table BIO_SEQUENCE modify SEQUENCE LONGTEXT;
