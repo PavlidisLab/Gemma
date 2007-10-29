8da47489838a1043228efd3c6c30f9e12b7d935b
ppavlidis <paul@chibi.ubc.ca>
2007-10-29 21:34:25 +0000

    typo

--- gemma-core/src/test/java/ubic/gemma/analysis/preprocess/DedvRankServiceTest.java
+++ gemma-core/src/test/java/ubic/gemma/analysis/preprocess/DedvRankServiceTest.java
@@ -48,7 +48,7 @@ public class DedvRankServiceTest extends AbstractGeoServiceTest {
         String path = getTestFileBasePath();
         try {
             geoService.setGeoDomainObjectGenerator( new GeoDomainObjectGeneratorLocal( path + GEO_TEST_DATA_ROOT
-                    + "gds999short" ) );
+                    + "gds999Short" ) );
             Collection<ExpressionExperiment> results = ( Collection<ExpressionExperiment> ) geoService.fetchAndLoad(
                     "GDS999", false, true, false );
             ee = results.iterator().next();
