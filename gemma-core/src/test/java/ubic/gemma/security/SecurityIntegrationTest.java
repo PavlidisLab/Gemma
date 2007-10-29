e6b970adb0b244c2fb88c9025ff819fa2f744ced
ppavlidis <paul@chibi.ubc.ca>
2007-10-29 21:39:44 +0000

    method name change

--- gemma-core/src/test/java/ubic/gemma/security/SecurityIntegrationTest.java
+++ gemma-core/src/test/java/ubic/gemma/security/SecurityIntegrationTest.java
@@ -104,7 +104,7 @@ public class SecurityIntegrationTest extends BaseSpringContextTest {
         SecurityService securityService = new SecurityService();
         securityService.setSecurableDao( ( SecurableDao ) this.getBean( "securableDao" ) );
         securityService.setBasicAclExtendedDao( ( BasicAclExtendedDao ) this.getBean( "basicAclExtendedDao" ) );
-        securityService.changePermission( ad, SecurityService.PRIVATE_MASK, new HashSet<Object>() );
+        securityService.setPermissions( ad, SecurityService.PRIVATE_MASK, new HashSet<Object>() );
         /*
          * uncomment so you can see the acl permission has been changed in the database.
          */
