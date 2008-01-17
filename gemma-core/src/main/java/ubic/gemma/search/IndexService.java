7d792e192d52f10800ce08d0b17fb1afc028f459
kelsey <>
2008-01-17 00:49:04 +0000

    bug 919.  IndexService  now allows swapping of indexes on the fly

--- gemma-core/src/main/java/ubic/gemma/search/IndexService.java
+++ gemma-core/src/main/java/ubic/gemma/search/IndexService.java
@@ -18,8 +18,11 @@
  */
 package ubic.gemma.search;
 
+import java.io.IOException;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.compass.core.spi.InternalCompass;
 import org.compass.gps.spi.CompassGpsInterfaceDevice;
 
 import ubic.gemma.util.CompassUtils;
@@ -32,107 +35,204 @@ import ubic.gemma.util.CompassUtils;
  */
 public class IndexService {
 
-    /*
-     * Note: IndexService has not been configured with xdoclet tags because we it needs to reside in
-     * applicationContext-search.xml so we can choose whether or not to load this part of the application context at
-     * spring startup.
-     */
+	/*
+	 * Note: IndexService has not been configured with xdoclet tags because we
+	 * it needs to reside in applicationContext-search.xml so we can choose
+	 * whether or not to load this part of the application context at spring
+	 * startup.
+	 */
 
-    private Log log = LogFactory.getLog( this.getClass() );
+	private Log log = LogFactory.getLog(this.getClass());
 
-    private CompassGpsInterfaceDevice expressionGps;
-    private CompassGpsInterfaceDevice geneGps;
-    private CompassGpsInterfaceDevice arrayGps;
-    private CompassGpsInterfaceDevice probeGps;
-    private CompassGpsInterfaceDevice biosequenceGps;
-    private CompassGpsInterfaceDevice bibliographicGps;
+	private CompassGpsInterfaceDevice expressionGps;
 
-    /**
-     * Indexes expression experiments, genes, array designs, probes and bibliographic references. This is a convenience
-     * method for Quartz to schedule indexing of the entire database.
-     */
-    public void indexAll() {
-        log.debug( "rebuilding compass index" );
-        CompassUtils.rebuildCompassIndex( expressionGps );
-        CompassUtils.rebuildCompassIndex( geneGps );
-        CompassUtils.rebuildCompassIndex( arrayGps );
-        CompassUtils.rebuildCompassIndex( probeGps );
-        CompassUtils.rebuildCompassIndex( biosequenceGps );
-        CompassUtils.rebuildCompassIndex( bibliographicGps );
-    }
+	private CompassGpsInterfaceDevice geneGps;
 
-    /**
-     * Indexes array designs.
-     */
-    public void indexArrayDesigns() {
-        CompassUtils.rebuildCompassIndex( arrayGps );
-    }
+	private CompassGpsInterfaceDevice arrayGps;
 
-    /**
-     * Indexes bibliographic references.
-     */
-    public void indexBibligraphicReferences() {
-        CompassUtils.rebuildCompassIndex( bibliographicGps );
-    }
+	private CompassGpsInterfaceDevice probeGps;
 
-    /**
-     * Indexes probes
-     */
-    public void indexCompositeSequences() {
-        CompassUtils.rebuildCompassIndex( probeGps );
-    }
+	private CompassGpsInterfaceDevice biosequenceGps;
 
-    /**
-     * Indexes expression experiments.
-     */
-    public void indexExpressionExperiments() {
-        CompassUtils.rebuildCompassIndex( expressionGps );
-    }
+	private CompassGpsInterfaceDevice bibliographicGps;
 
-    /**
-     * Indexes genes.
-     */
-    public void indexGenes() {
-        CompassUtils.rebuildCompassIndex( geneGps );
-    }
+	private InternalCompass compassExpression;
 
-    /**
-     * @param arrayGps The arrayGps to set.
-     */
-    public void setArrayGps( CompassGpsInterfaceDevice arrayGps ) {
-        this.arrayGps = arrayGps;
-    }
+	private InternalCompass compassGene;
 
-    public void setBibliographicGps( CompassGpsInterfaceDevice bibliographicGps ) {
-        this.bibliographicGps = bibliographicGps;
-    }
+	private InternalCompass compassArray;
 
-    /**
-     * @param expressionGps The expressionGps to set.
-     */
-    public void setExpressionGps( CompassGpsInterfaceDevice expressionGps ) {
-        this.expressionGps = expressionGps;
-    }
+	private InternalCompass compassProbe;
 
-    /**
-     * @param geneGps The geneGps to set.
-     */
-    public void setGeneGps( CompassGpsInterfaceDevice geneGps ) {
-        this.geneGps = geneGps;
-    }
+	private InternalCompass compassBiosequence;
 
-    /**
-     * @param probeGps
-     */
-    public void setProbeGps( CompassGpsInterfaceDevice probeGps ) {
-        this.probeGps = probeGps;
-    }
+	private InternalCompass compassBibliographic;
 
-    /**
-     * @param biosequenceGps
-     */
-    public void setBiosequenceGps( CompassGpsInterfaceDevice biosequenceGps ) {
-        this.biosequenceGps = biosequenceGps;
-    }
+	/**
+	 * Indexes expression experiments, genes, array designs, probes and
+	 * bibliographic references. This is a convenience method for Quartz to
+	 * schedule indexing of the entire database.
+	 */
+	public void indexAll() {
+		log.debug("rebuilding compass index");
+		CompassUtils.rebuildCompassIndex(expressionGps);
+		CompassUtils.rebuildCompassIndex(geneGps);
+		CompassUtils.rebuildCompassIndex(arrayGps);
+		CompassUtils.rebuildCompassIndex(probeGps);
+		CompassUtils.rebuildCompassIndex(biosequenceGps);
+		CompassUtils.rebuildCompassIndex(bibliographicGps);
+	}
+
+	/**
+	 * Indexes array designs.
+	 */
+	public void indexArrayDesigns() {
+		CompassUtils.rebuildCompassIndex(arrayGps);
+	}
+
+	/**
+	 * Indexes bibliographic references.
+	 */
+	public void indexBibligraphicReferences() {
+		CompassUtils.rebuildCompassIndex(bibliographicGps);
+	}
+
+	/**
+	 * Indexes probes
+	 */
+	public void indexCompositeSequences() {
+		CompassUtils.rebuildCompassIndex(probeGps);
+	}
+
+	/**
+	 * Indexes expression experiments.
+	 */
+	public void indexExpressionExperiments() {
+		CompassUtils.rebuildCompassIndex(expressionGps);
+	}
+
+	/**
+	 * Indexes genes.
+	 */
+	public void indexGenes() {
+		CompassUtils.rebuildCompassIndex(geneGps);
+	}
+
+	/**
+	 * @param pathToNewIndex
+	 * @throws IOException
+	 */
+	public void replaceExperimentIndex(String pathToNewIndex)
+			throws IOException {
+		CompassUtils.swapCompassIndex(compassExpression, pathToNewIndex);
+	}
+
+	/**
+	 * @param pathToNewIndex
+	 * @throws IOException
+	 */
+	public void replaceGeneIndex(String pathToNewIndex) throws IOException {
+		CompassUtils.swapCompassIndex(compassGene, pathToNewIndex);
+	}
+
+	/**
+	 * @param pathToNewIndex
+	 * @throws IOException
+	 */
+	public void replaceProbeIndex(String pathToNewIndex) throws IOException {
+		CompassUtils.swapCompassIndex(compassProbe, pathToNewIndex);
+	}
+
+	/**
+	 * @param pathToNewIndex
+	 * @throws IOException
+	 */
+	public void replaceArrayIndex(String pathToNewIndex) throws IOException {
+		CompassUtils.swapCompassIndex(compassArray, pathToNewIndex);
+	}
+
+	/**
+	 * @param pathToNewIndex
+	 * @throws IOException
+	 */
+	public void replaceBiosequenceIndex(String pathToNewIndex)
+			throws IOException {
+		CompassUtils.swapCompassIndex(compassBiosequence, pathToNewIndex);
+	}
+
+	/**
+	 * @param pathToNewIndex
+	 * @throws IOException
+	 */
+	public void replaceBibliographicIndex(String pathToNewIndex)
+			throws IOException {
+		CompassUtils.swapCompassIndex(compassBibliographic, pathToNewIndex);
+	}
+
+	/**
+	 * @param arrayGps
+	 *            The arrayGps to set.
+	 */
+	public void setArrayGps(CompassGpsInterfaceDevice arrayGps) {
+		this.arrayGps = arrayGps;
+	}
+
+	public void setBibliographicGps(CompassGpsInterfaceDevice bibliographicGps) {
+		this.bibliographicGps = bibliographicGps;
+	}
+
+	/**
+	 * @param expressionGps
+	 *            The expressionGps to set.
+	 */
+	public void setExpressionGps(CompassGpsInterfaceDevice expressionGps) {
+		this.expressionGps = expressionGps;
+	}
+
+	/**
+	 * @param geneGps
+	 *            The geneGps to set.
+	 */
+	public void setGeneGps(CompassGpsInterfaceDevice geneGps) {
+		this.geneGps = geneGps;
+	}
+
+	/**
+	 * @param probeGps
+	 */
+	public void setProbeGps(CompassGpsInterfaceDevice probeGps) {
+		this.probeGps = probeGps;
+	}
+
+	/**
+	 * @param biosequenceGps
+	 */
+	public void setBiosequenceGps(CompassGpsInterfaceDevice biosequenceGps) {
+		this.biosequenceGps = biosequenceGps;
+	}
+
+	public void setCompassArray(InternalCompass compassArray) {
+		this.compassArray = compassArray;
+	}
+
+	public void setCompassBibliographic(InternalCompass compassBibliographic) {
+		this.compassBibliographic = compassBibliographic;
+	}
+
+	public void setCompassBiosequence(InternalCompass compassBiosequence) {
+		this.compassBiosequence = compassBiosequence;
+	}
+
+	public void setCompassExpression(InternalCompass compassExpression) {
+		this.compassExpression = compassExpression;
+	}
+
+	public void setCompassGene(InternalCompass compassGene) {
+		this.compassGene = compassGene;
+	}
+
+	public void setCompassProbe(InternalCompass compassProbe) {
+		this.compassProbe = compassProbe;
+	}
 
 }
--- gemma-util/src/main/java/ubic/gemma/util/CompassUtils.java
+++ gemma-util/src/main/java/ubic/gemma/util/CompassUtils.java
@@ -19,6 +19,7 @@
 package ubic.gemma.util;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 
@@ -27,6 +28,7 @@ import org.apache.commons.io.filefilter.FileFilterUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.lucene.store.FSDirectory;
+import org.compass.core.spi.InternalCompass;
 import org.compass.gps.spi.CompassGpsInterfaceDevice;
 
 /**
@@ -37,116 +39,185 @@ import org.compass.gps.spi.CompassGpsInterfaceDevice;
  */
 public class CompassUtils {
 
-    private static Log log = LogFactory.getLog( CompassUtils.class );
+	private static Log log = LogFactory.getLog(CompassUtils.class);
 
-    /**
-     * Deletes compass lock file(s).
-     * 
-     * @throws IOException
-     */
-    @SuppressWarnings("unchecked")
-    public static void deleteCompassLocks() {
-        /*
-         * FIXME lock directory is now the same as the indexes, by default.
-         */
-        log.debug( "Lucene index lock dir: " + FSDirectory.LOCK_DIR );
+	/**
+	 * Deletes compass lock file(s).
+	 * 
+	 * @throws IOException
+	 */
+	@SuppressWarnings("unchecked")
+	public static void deleteCompassLocks() {
+		/*
+		 * FIXME lock directory is now the same as the indexes, by default.
+		 */
+		log.debug("Lucene index lock dir: " + FSDirectory.LOCK_DIR);
 
-        Collection<File> lockFiles = FileUtils.listFiles( new File( FSDirectory.LOCK_DIR ), FileFilterUtils
-                .suffixFileFilter( "lock" ), null );
+		Collection<File> lockFiles = FileUtils.listFiles(new File(
+				FSDirectory.LOCK_DIR),
+				FileFilterUtils.suffixFileFilter("lock"), null);
 
-        if ( lockFiles.size() == 0 ) {
-            log.debug( "Lucene lock files do not exist." );
-            return;
-        }
+		if (lockFiles.size() == 0) {
+			log.debug("Lucene lock files do not exist.");
+			return;
+		}
 
-        for ( File file : lockFiles ) {
-            log.debug( "Removing Lucene lock file " + file );
-            file.delete();
-        }
-    }
+		for (File file : lockFiles) {
+			log.debug("Removing Lucene lock file " + file);
+			file.delete();
+		}
+	}
 
-    /**
-     * disables the index mirroring operation.
-     * 
-     * @param device
-     */
-    public static void disableIndexMirroring( CompassGpsInterfaceDevice device ) {
-        device.stop();
-    }
+	/**
+	 * disables the index mirroring operation.
+	 * 
+	 * @param device
+	 */
+	public static void disableIndexMirroring(CompassGpsInterfaceDevice device) {
+		device.stop();
+	}
 
-    /**
-     * enables the index mirroring operation.
-     * 
-     * @param device
-     */
-    public static void enableIndexMirroring( CompassGpsInterfaceDevice device ) {
-        device.start();
-    }
+	/**
+	 * enables the index mirroring operation.
+	 * 
+	 * @param device
+	 */
+	public static void enableIndexMirroring(CompassGpsInterfaceDevice device) {
+		device.start();
+	}
 
-    /**
-     * Deletes and re-creates the index.
-     * 
-     * @param gps
-     * @throws IOException
-     * @see IndexService
-     */
-    public static synchronized void rebuildCompassIndex( CompassGpsInterfaceDevice gps ) {
-        boolean wasRunningBefore = gps.isRunning();
+	/**
+	 * Deletes and re-creates the index.
+	 *   See the IndexService
+	 * @param gps
+	 * @throws IOException
+	 */
+	public static synchronized void rebuildCompassIndex(
+			CompassGpsInterfaceDevice gps) {
+		boolean wasRunningBefore = gps.isRunning();
 
-        log.info( "CompassGps was running? " + wasRunningBefore );
+		log.info("CompassGps was running? " + wasRunningBefore);
 
-        /* Check state of device. If not running and you try to index, you will get a device exception. */
-        if ( !wasRunningBefore ) {
-            enableIndexMirroring( gps );
-        }
+		/*
+		 * Check state of device. If not running and you try to index, you will
+		 * get a device exception.
+		 */
+		if (!wasRunningBefore) {
+			enableIndexMirroring(gps);
+		}
 
-        /* We don't need to check if index already exists. If it doesn't, it won't be deleted. */
-        gps.getIndexCompass().getSearchEngineIndexManager().deleteIndex();
-        log.info( "Deleting old index" );
-        gps.getIndexCompass().getSearchEngineIndexManager().createIndex();
-        log.info( "indexing now ... " );
-        gps.index();
+		/*
+		 * We don't need to check if index already exists. If it doesn't, it
+		 * won't be deleted.
+		 */
+		gps.getIndexCompass().getSearchEngineIndexManager().deleteIndex();
+		log.info("Deleting old index");
+		gps.getIndexCompass().getSearchEngineIndexManager().createIndex();
+		log.info("indexing now ... ");
+		gps.index();
 
-        /* Return state of device */
-        if ( !wasRunningBefore ) {
-            disableIndexMirroring( gps );
-        }
+		/* Return state of device */
+		if (!wasRunningBefore) {
+			disableIndexMirroring(gps);
+		}
 
-    }
+	}
 
-    /**
-     * "Turning on" means adding the compass context to our spring context, as well as creating the compass index
-     * directory. This does not turn on index mirroring to automatically update the index while persisting data (to a
-     * database). To do this, call enableIndexMirroring after running this.
-     * 
-     * @param testEnv
-     * @param paths
-     */
-    public static void turnOnCompass( boolean testEnv, List<String> paths ) {
-        deleteCompassLocks();
-        if ( testEnv ) {
-            addCompassTestContext( paths );
-        } else {
-            addCompassContext( paths );
-        }
+	/**
+	 * @param compass eg:    InternalCompass expressionBean = (InternalCompass) this.getBean("compassExpression");  Need this for replacing the indexes and it contains the path to the indexes to replace
+	 * @param pathToIndex  An absolute path to the directory where the new indexes are located. Path should end at index sub dir. 
+	 * @throws IOException
+	 */
+	public static synchronized void swapCompassIndex(
+			InternalCompass compass, String pathToIndex)
+			throws IOException {
+		
+		
+		final File srcDir = new File(pathToIndex);
+		final File targetDir = new File(compass.getSettings().getSetting("compass.engine.connection").replaceFirst("file:", "")+"/index/");
 
-    }
+		//Validate that the new indexes exist and can read from them
+		if (!srcDir.canRead()){
+			log.error("Unable to read from specified directory: " + srcDir.getAbsolutePath());
+			return;
+		}
+		
+		
+		//Validate that we can write where we are copying the file to. 		
+		if (!targetDir.canWrite()){
+			log.error("Unable to read from specified directory: " +targetDir.getAbsolutePath());
+			return;
+		}
 
-    /**
-     * Add the compass contexts to the other spring contexts
-     * 
-     * @param paths
-     */
-    private static void addCompassContext( List<String> paths ) {
-        paths.add( "classpath*:ubic/gemma/applicationContext-search.xml" );
-    }
+		compass.getSearchEngineIndexManager().stop();
+		
+		log.info("Deleting old index....");
+		compass.getSearchEngineIndexManager().deleteIndex();
 
-    /**
-     * Add the compass test contexts to the other spring contexts.
-     * 
-     * @param paths
-     */
-    private static void addCompassTestContext( List<String> paths ) {
-        paths.add( "classpath*:ubic/gemma/applicationContext-search.xml" );
-    }
+		log.info("Clearing Cache.... ");
+		compass.getSearchEngineIndexManager().clearCache();
+		
+		//Oddly this creates some empty segmants that are just cruft.
+		//log.info("Creating index...");
+		//compass.getSearchEngineIndexManager().createIndex();
+		
+		log.info("swapping index.....");		
+		FileUtils.copyDirectory(srcDir, targetDir);
+
+		compass.getSearchEngineIndexManager().start();
+		
+		
+//		gps.getIndexCompass().getSearchEngineIndexManager().replaceIndex(
+//				gps.getIndexCompass().getSearchEngineIndexManager(),
+//				new SearchEngineIndexManager.ReplaceIndexCallback() {
+//					public void buildIndexIfNeeded()
+//							throws SearchEngineException {
+//						try{
+//							//Copy must be put in the call back.  If not put here then the files just get removed. when replace index is called. 
+//							FileUtils.copyDirectory(srcDir, targetDir);
+//						}
+//						catch(IOException ioe){
+//							log.error("Unable to copy" + srcDir.getAbsolutePath() + "  to " + targetDir.getAbsolutePath());
+//						}
+//					}
+//				});
+
+	}
+
+	/**
+	 * "Turning on" means adding the compass context to our spring context, as
+	 * well as creating the compass index directory. This does not turn on index
+	 * mirroring to automatically update the index while persisting data (to a
+	 * database). To do this, call enableIndexMirroring after running this.
+	 * 
+	 * @param testEnv
+	 * @param paths
+	 */
+	public static void turnOnCompass(boolean testEnv, List<String> paths) {
+		deleteCompassLocks();
+		if (testEnv) {
+			addCompassTestContext(paths);
+		} else {
+			addCompassContext(paths);
+		}
+
+	}
+
+	/**
+	 * Add the compass contexts to the other spring contexts
+	 * 
+	 * @param paths
+	 */
+	private static void addCompassContext(List<String> paths) {
+		paths.add("classpath*:ubic/gemma/applicationContext-search.xml");
+	}
+
+	/**
+	 * Add the compass test contexts to the other spring contexts.
+	 * 
+	 * @param paths
+	 */
+	private static void addCompassTestContext(List<String> paths) {
+		paths.add("classpath*:ubic/gemma/applicationContext-search.xml");
+	}
 }
