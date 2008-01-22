66503bf5e373735009c8210ff435716efb8d376d
keshav <>
2008-01-22 21:20:17 +0000

    path changes

--- gemma-web/src/main/java/ubic/gemma/web/jetty/Jetty.java
+++ gemma-web/src/main/java/ubic/gemma/web/jetty/Jetty.java
@@ -1,41 +1,23 @@
 package ubic.gemma.web.jetty;
 
 /*
-
  * The Gemma project
-
  * 
-
  * Copyright (c) 2006 University of British Columbia
-
  * 
-
  * Licensed under the Apache License, Version 2.0 (the "License");
-
  * you may not use this file except in compliance with the License.
-
  * You may obtain a copy of the License at
-
  *
-
  *       http://www.apache.org/licenses/LICENSE-2.0
-
  *
-
  * Unless required by applicable law or agreed to in writing, software
-
  * distributed under the License is distributed on an "AS IS" BASIS,
-
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-
  * See the License for the specific language governing permissions and
-
  * limitations under the License.
-
  *
-
  */
-
 import java.net.URL;
 
 import org.apache.commons.logging.Log;
@@ -69,7 +51,7 @@ public class Jetty {
     public static void main( String[] args ) {
         Server jettyServer = null;
         try {
-            URL jettyConfig = new URL( "file:etc/jetty/jetty.xml" );
+            URL jettyConfig = new URL( "file:gemma-web/etc/conf/jetty.xml" );
 
             log.debug( "jetty config:" + jettyConfig );
 
