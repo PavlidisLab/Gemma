811a64daeb28c06d9621bc6a6cf86e90cb22a899
ppavlidis <paul@chibi.ubc.ca>
2008-02-22 01:44:42 +0000

    fix for build error; cleanup code

--- gemma-web/src/main/java/ubic/gemma/web/services/DEDVfromEEIDGeneIDEndpoint.java
+++ gemma-web/src/main/java/ubic/gemma/web/services/DEDVfromEEIDGeneIDEndpoint.java
@@ -1,5 +1,3 @@
-package ubic.gemma.web.services;
-
 /*
  * The Gemma project
  * 
@@ -18,31 +16,20 @@ package ubic.gemma.web.services;
  * limitations under the License.
  *
  */
+package ubic.gemma.web.services;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
-import java.util.Vector;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.util.Assert;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 import ubic.basecode.io.ByteArrayConverter;
-import ubic.gemma.analysis.preprocess.ExpressionDataMatrixBuilder;
-import ubic.gemma.analysis.service.AnalysisHelperService;
-import ubic.gemma.datastructure.matrix.ExpressionDataDoubleMatrix;
-import ubic.gemma.model.common.quantitationtype.PrimitiveType;
-import ubic.gemma.model.common.quantitationtype.QuantitationType;
 import ubic.gemma.model.expression.bioAssayData.DesignElementDataVector;
 import ubic.gemma.model.expression.bioAssayData.DesignElementDataVectorService;
 import ubic.gemma.model.expression.experiment.ExpressionExperiment;
@@ -51,15 +38,13 @@ import ubic.gemma.model.genome.Gene;
 import ubic.gemma.model.genome.gene.GeneService;
 
 /**
- * Given list Experiment IDs, list gene IDs, return data vectors satisfying both conditions (i.e., string array) 
- *  and the corresponding composite gene sequences. 
- * 
- * Potential problem may be the thawing of design element data vector objects.  
+ * Given list Experiment IDs, list gene IDs, return data vectors satisfying both conditions (i.e., string array) and the
+ * corresponding composite gene sequences. Potential problem may be the thawing of design element data vector objects.
  * For the mini-db, it appears to work.
  * 
  * @author gavin, klc
+ * @versio n$Id$
  */
-
 public class DEDVfromEEIDGeneIDEndpoint extends AbstractGemmaEndpoint {
 
     private static Log log = LogFactory.getLog( ExperimentDEDVEndpoint.class );
@@ -108,7 +93,6 @@ public class DEDVfromEEIDGeneIDEndpoint extends AbstractGemmaEndpoint {
      * @return the response element
      */
     protected Element invokeInternal( Element requestElement, Document document ) throws Exception {
-        
 
         setLocalName( EXPERIMENT_LOCAL_NAME );
         // get ee id's from request
@@ -125,14 +109,14 @@ public class DEDVfromEEIDGeneIDEndpoint extends AbstractGemmaEndpoint {
             geneIDLong.add( Long.parseLong( id ) );
         Collection<Gene> geneResult = geneService.loadMultiple( geneIDLong );
 
-        Map<DesignElementDataVector, Collection<Gene>> dedvMap = designElementDataVectorService.getVectors( eeObjs, geneResult );
+        Map<DesignElementDataVector, Collection<Gene>> dedvMap = designElementDataVectorService.getVectors( eeObjs,
+                geneResult );
 
         // start building the wrapper
-        //xml is built manually here instead of using the buildWrapper method inherited from AbstractGemmaEndpoint
+        // xml is built manually here instead of using the buildWrapper method inherited from AbstractGemmaEndpoint
         String elementName1 = "dedv";
         String elementName2 = "geneIdList";
         String elementName3 = "eeIdList";
-        
 
         Element responseWrapper = document.createElementNS( NAMESPACE_URI, EXPERIMENT_LOCAL_NAME );
         Element responseElement = document.createElementNS( NAMESPACE_URI, EXPERIMENT_LOCAL_NAME + RESPONSE );
@@ -142,11 +126,9 @@ public class DEDVfromEEIDGeneIDEndpoint extends AbstractGemmaEndpoint {
             responseElement.appendChild( document.createTextNode( "No " + elementName1 + " result" ) );
         else {
 
-              
             Set<DesignElementDataVector> keys = dedvMap.keySet();
             designElementDataVectorService.thaw( keys );
 
-            
             ByteArrayConverter converter = new ByteArrayConverter();
             // -build single-row Collections to use for ExpressionDataMatrixBuilder
             // -need to do this so that we can use the .getPrefferedData()
@@ -154,23 +136,20 @@ public class DEDVfromEEIDGeneIDEndpoint extends AbstractGemmaEndpoint {
             // already have a mapping to the genes
             // of the design elements
             for ( DesignElementDataVector dedv : keys ) {
-                
+
                 byte[] dedvData = dedv.getData();
                 double[] convertedDEDV = converter.byteArrayToDoubles( dedvData );
 
-              
-                //data vector string for output
+                // data vector string for output
                 String elementString1 = encode( convertedDEDV );
 
                 Collection<String> geneidCol = gene2ID( dedvMap.get( dedv ) ); //
-                
+
                 // gene ids, space delimited for output
-                String elementString2 = encode( geneidCol.toArray() ); 
-                
+                String elementString2 = encode( ArrayUtils.toPrimitive( geneidCol.toArray( new Double[] {} ) ) );
+
                 String elementString3 = dedv.getExpressionExperiment().getId().toString();
 
-                  
-                
                 Element e1 = document.createElement( elementName1 );
                 e1.appendChild( document.createTextNode( elementString1 ) );
                 responseElement.appendChild( e1 );
@@ -178,15 +157,13 @@ public class DEDVfromEEIDGeneIDEndpoint extends AbstractGemmaEndpoint {
                 Element e2 = document.createElement( elementName2 );
                 e2.appendChild( document.createTextNode( elementString2 ) );
                 responseElement.appendChild( e2 );
-                
-                
-                
+
                 Element e3 = document.createElement( elementName3 );
                 e3.appendChild( document.createTextNode( elementString3 ) );
                 responseElement.appendChild( e3 );
-             
+
             }
-           
+
         }
 
         log.info( "Finished generating matrix. Sending response to client." );
@@ -234,8 +211,6 @@ public class DEDVfromEEIDGeneIDEndpoint extends AbstractGemmaEndpoint {
         }
         return geneIds;
     }
-
-   
 
     /**
      * @param data
