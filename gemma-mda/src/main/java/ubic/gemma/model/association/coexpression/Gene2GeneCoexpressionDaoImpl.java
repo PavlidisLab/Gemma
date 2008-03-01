202aec10ad49dd24b5a74991aef08848b52a302e
ppavlidis <paul@chibi.ubc.ca>
2008-03-01 05:52:40 +0000

    fix collections import

--- gemma-mda/src/main/java/ubic/gemma/model/association/coexpression/Gene2GeneCoexpressionDaoImpl.java
+++ gemma-mda/src/main/java/ubic/gemma/model/association/coexpression/Gene2GeneCoexpressionDaoImpl.java
@@ -20,14 +20,13 @@ package ubic.gemma.model.association.coexpression;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-
-import edu.emory.mathcs.backport.java.util.Collections;
 
 import ubic.gemma.model.analysis.Analysis;
 import ubic.gemma.model.genome.Gene;
