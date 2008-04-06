a34a59ef881ec7ff6a4e9225eaade1b54f19f0e7
keshav <>
2008-04-06 18:13:00 +0000

    removing unused stuff from diff javascript

--- gemma-web/src/main/webapp/scripts/ajax/diff/DiffExpressionSearch.js
+++ gemma-web/src/main/webapp/scripts/ajax/diff/DiffExpressionSearch.js
@@ -33,33 +33,6 @@ Ext.onReady( function() {
 		pageSize : 25
 	} );
 	
-	/*
-	var predictedGeneGrid;
-	var probeAlignedGrid;
-	if ( admin ) {
-		var predictedGeneDatasetGrid = new Ext.Gemma.CoexpressionDatasetGrid( {
-			renderTo : "coexpression-results",
-			adjective : "predicted gene"
-		} );
-		predictedGeneGrid = new Ext.Gemma.CoexpressionGrid( {
-			renderTo : "coexpression-results",
-			title : "Coexpressed predicted genes",
-			pageSize : 25,
-			collapsed : true
-		} );
-		var probeAlignedDatasetGrid = new Ext.Gemma.CoexpressionDatasetGrid( {
-			renderTo : "coexpression-results",
-			adjective : "probe-aligned region"
-		} );
-		probeAlignedGrid = new Ext.Gemma.CoexpressionGrid( {
-			renderTo : "coexpression-results",
-			title : "Coexpressed probe-aligned regions",
-			pageSize : 25,
-			collapsed : true
-		} );
-	}
-	*/
-	
 	searchPanel.on( "aftersearch", function ( panel, result ) {
 	var eeMap = {};
 		if ( result.datasets ) {
@@ -101,93 +74,18 @@ Ext.onReady( function() {
 		knownGeneDatasetGrid.loadData( result.isCannedAnalysis, result.queryGenes.length, result.knownGeneDatasets ) ;
 		knownGeneGrid.loadData( result.isCannedAnalysis, result.queryGenes.length, result.knownGeneResults, result.knownGeneDatasets );
 		
+		if ( admin ) {
+			Ext.Gemma.DiffExpressionDatasetGrid.updateDatasetInfo( result.predictedGeneDatasets, eeMap );
+			predictedGeneDatasetGrid.loadData( result.isCannedAnalysis, result.queryGenes.length, result.predictedGeneDatasets ) ;
+			predictedGeneGrid.loadData( result.isCannedAnalysis, result.queryGenes.length, result.predictedGeneResults, result.predictedGeneDatasets );
+			Ext.Gemma.DiffExpressionDatasetGrid.updateDatasetInfo( result.probeAlignedRegionDatasets, eeMap );
+			probeAlignedDatasetGrid.loadData( result.isCannedAnalysis, result.queryGenes.length, result.probeAlignedRegionDatasets ) ;
+			probeAlignedGrid.loadData( result.isCannedAnalysis, result.queryGenes.length, result.probeAlignedRegionResults, result.probeAlignedRegionDatasets );
+		}
+		
 	} );
 	
 } );
-
-/* Ext.Gemma.DiffExpressionSummaryGrid constructor...
- * 	config is a hash with the following options:
- */
-Ext.Gemma.DiffExpressionSummaryGrid = function ( config ) {
-	
-	var genes = config.genes; delete config.genes;
-	var summary = config.summary; delete config.summary;
-	
-	/* keep a reference to ourselves to avoid convoluted scope issues below...
-	 */
-	var thisGrid = this;
-	
-	/* establish default config options...
-	 */
-	var superConfig = {
-		editable : false,
-		title : 'Search Summary',
-		width : 350,
-		height : 250
-	};
-	
-	var fields = [
-		{ name: 'sort', type: 'int' },
-		{ name: 'group', type: 'string' },
-		{ name: 'key', type: 'string' }
-	];
-	for ( var i=0; i<genes.length; ++i ) {
-		fields.push( { name: genes[i].officialSymbol, type: 'int' } );
-	}
-	superConfig.store = new Ext.data.GroupingStore( {
-		reader: new Ext.data.ArrayReader( {}, fields ),
-		groupField: 'group',
-		data: Ext.Gemma.DiffExpressionSummaryGrid.transformData( genes, summary ),
-		sortInfo: { field: 'sort', direction: 'ASC' }
-	} );
-	
-	var columns = [
-		{ header: 'Group', dataIndex: 'group' },
-		{ id: 'key', header: '', dataIndex: 'key', align: 'right' }
-	];
-	for ( var i=0; i<genes.length; ++i ) {
-		columns.push( { header: genes[i].officialSymbol, dataIndex: genes[i].officialSymbol, align: 'right' } );
-	}
-	superConfig.cm = new Ext.grid.ColumnModel( columns );
-	superConfig.autoExpandColumn = 'key';
-	
-	superConfig.view = new Ext.grid.GroupingView( {
-		enableGroupingMenu : false,
-		enableNoGroups : false,
-		hideGroupedColumn : true,
-		showGroupName : false
-	} );
-	
-	for ( var property in config ) {
-		superConfig[property] = config[property];
-	}
-	Ext.Gemma.DiffExpressionSummaryGrid.superclass.constructor.call( this, superConfig );
-};
-
-/* static methods...
- */
-Ext.Gemma.DiffExpressionSummaryGrid.transformData = function ( genes, summary ) {
-	
-	/*
-	var datasetsAvailable = [ 0, "Datasets", "<span ext:qtip='How many data sets met your criteria'>Available</span>" ];
-	var datasetsTested = [ 1, "Datasets", "<span ext:qtip='How many data sets had the query gene available for analysis'>Query gene testable</span>" ];
-	var linksFound = [ 2, "Links", "<span ext:qtip='Total number of links (may show the number including those not meeting your stringency threshold)'>Found</span>" ];
-	var linksPositive = [ 3, "Links", "<span ext:qtip='How many genes were considered positively correlated with the query'>Met stringency (+)</span>" ];
-	var linksNegative = [ 4, "Links", "<span ext:qtip='How many genes were considered negatively correlated with the query'>Met stringency (-)</span>" ];
-	
-	for ( var i=0; i<genes.length; ++i ) {
-		var thisSummary = summary[ genes[i].officialSymbol ] || {};
-		datasetsAvailable.push( thisSummary.datasetsAvailable );
-		datasetsTested.push( thisSummary.datasetsTested );
-		linksFound.push( thisSummary.linksFound );
-		linksPositive.push( thisSummary.linksMetPositiveStringency );
-		linksNegative.push( thisSummary.linksMetNegativeStringency );
-	}
-
-	return [ datasetsAvailable, datasetsTested, linksFound, linksPositive, linksNegative ];
-	*/
-};
-
 
 /* instance methods...
  */
