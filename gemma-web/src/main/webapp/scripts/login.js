f6421a8091ae350e8d0a94f436568cf1da987a3b
ppavlidis <paul@chibi.ubc.ca>
2008-06-10 00:39:49 +0000

    cleanup; client-side validation works.

--- gemma-web/src/main/webapp/scripts/login.js
+++ gemma-web/src/main/webapp/scripts/login.js
@@ -1,32 +1,42 @@
-<script type="text/javascript">
-    if (getCookie("username") != null) {
-        $("j_username").value = getCookie("username");
-        $("j_password").focus();
-    } else {
-        $("j_username").focus();
-    }
-    
-    function saveUsername(theForm) {
-        var expires = new Date();
-        expires.setTime(expires.getTime() + 24 * 30 * 60 * 60 * 1000); // sets it for approx 30 days.
-        setCookie("username",theForm.j_username.value,expires,"<c:url value="/"/>");
-    }
-    
-    function validateForm(form) { 
-        return validateRequired(form); 
-    } 
-    
-    function passwordHint() {
-        if ($("j_username").value.length == 0) {
-            alert("The <fmt:message key="label.username"/> field must be filled in to get a password hint sent to you.");
-            $("j_username").focus();
-        } else {
-            location.href="<c:url value="/passwordHint.html"/>?username=" + $("j_username").value;     
-        }
-    }
-    
-    function required () { 
-        this.aa = new Array("j_username", "<fmt:message key="errors.required"><fmt:param><fmt:message key="label.username"/></fmt:param></fmt:message>", new Function ("varName", " return this[varName];"));
-        this.ab = new Array("j_password", "<fmt:message key="errors.required"><fmt:param><fmt:message key="label.password"/></fmt:param></fmt:message>", new Function ("varName", " return this[varName];"));
-    } 
-</script>
+/**
+ * FIXME this is non-functional.
+ */
+Ext.onReady(function() {
+	if (getCookie("username") != null) {
+		$("j_username").value = getCookie("username");
+		$("j_password").focus();
+	} else {
+		$("j_username").focus();
+	}
+});
+
+var saveUsername = function(theForm) {
+	var expires = new Date();
+	// sets it for approx 30 days.
+	expires.setTime(expires.getTime() + 24 * 30 * 60 * 60 * 1000);
+	setCookie("username", theForm.j_username.value, expires, "<c:url value="
+			/ "/>");
+}
+
+var  validateForm = function(form) {
+	return validateRequired(form); // defined in global.js
+}
+
+var passwordHint = function() {
+	if ($("j_username").value.length == 0) {
+		alert("The <fmt:message key="
+				+ label.username
+				+ "/> field must be filled in to get a password hint sent to you.");
+		$("j_username").focus();
+	} else {
+		location.href = "Gemma/passwordHint.html?username="
+				+ $("j_username").value;
+	}
+}
+
+var required = function() {
+	this.aa = new Array("j_username", "Required", new Function("varName",
+			" return this[varName];"));
+	this.ab = new Array("j_password", "Required", new Function("varName",
+			" return this[varName];"));
+}
