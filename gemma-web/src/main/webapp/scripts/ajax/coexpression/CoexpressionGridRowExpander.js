8b003e410f208efa42fbf1d6259c6c597799b577
kelsey <>
2008-07-04 22:30:40 +0000

    Fix for ingrid tooltips not working in tabed panel. Bug 1248

--- gemma-web/src/main/webapp/scripts/ajax/coexpression/CoexpressionGridRowExpander.js
+++ gemma-web/src/main/webapp/scripts/ajax/coexpression/CoexpressionGridRowExpander.js
@@ -65,18 +65,19 @@ Gemma.CoexpressionGridRowExpander = Ext.extend(Ext.grid.RowExpander, {
 							}
 						}
 					}
+
 				}]
+
 			});
 
 			// Keep mouse events from propagating to the parent grid. See ExtJS
 			// forums topic "nested grids problem" (242878).
-			dsGrid.getEl().swallowEvent(['mouseover', 'mousedown', 'click',
-					'dblclick']);
-			diffExGrid.getEl().swallowEvent(['mouseover', 'mousedown', 'click',
-					'dblclick']);
+			dsGrid.getEl().swallowEvent(['mousedown', 'click', 'dblclick']);
+			diffExGrid.getEl().swallowEvent(['mousedown', 'click', 'dblclick']);
 
 			return true;
 		}
+
 		return false;
 	},
 
