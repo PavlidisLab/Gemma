87d0dab145860c2658f7555ee65d8150d73b8813
ppavlidis <paul@chibi.ubc.ca>
2008-07-11 05:28:33 +0000

    reorganized to meet new style

--- gemma-web/src/main/webapp/scripts/ajax/coexpression/CoexpressionGrid.js
+++ gemma-web/src/main/webapp/scripts/ajax/coexpression/CoexpressionGrid.js
@@ -1,182 +1,235 @@
 Ext.namespace('Gemma');
 
-/*
- * Gemma.CoexpressionGrid constructor... config is a hash with the following
- * options:
+/**
+ * Grid for showing coexpression results.
+ * 
+ * @class Gemma.CoexpressionGrid
+ * @extends Ext.grid.GridPanel
  */
-Gemma.CoexpressionGrid = function(config) {
+Gemma.CoexpressionGrid = Ext.extend(Ext.grid.GridPanel, {
 
-	this.pageSize = config.pageSize;
-
-	var dataSets; // The data sets that are 'relevant'. Updated during
-	// loaddata.
-
-	/*
-	 * establish default config options...
-	 */
-	var superConfig = {
-		collapsible : true,
-		editable : false,
-		autoHeight : true,
-		style : "margin-bottom: 1em;"
-	};
-
-	if (this.pageSize) {
-		superConfig.store = new Gemma.PagingDataStore({
-			proxy : new Ext.data.MemoryProxy([]),
-			reader : new Ext.data.ListRangeReader({
-				id : "id"
-			}, Gemma.CoexpressionGrid.getRecord()),
-			sortInfo : {
-				field : 'sortKey',
-				direction : 'ASC'
-			},
-			pageSize : this.pageSize
-		});
-		superConfig.bbar = new Gemma.PagingToolbar({
-			pageSize : this.pageSize,
-			store : superConfig.store
-		});
-	} else {
-		superConfig.ds = new Ext.data.Store({
-			proxy : new Ext.data.MemoryProxy([]),
-			reader : new Ext.data.ListRangeReader({
-				id : "id"
-			}, Gemma.CoexpressionGrid.getRecord()),
-			sortInfo : {
-				field : 'sortKey',
-				direction : 'ASC'
-			}
-		});
-	}
-
-	this.rowExpander = new Gemma.CoexpressionGridRowExpander({
-		tpl : ""
-	});
-
-	superConfig.cm = new Ext.grid.ColumnModel([this.rowExpander, {
-		id : 'query',
-		header : "Query Gene",
-		dataIndex : "queryGene",
-		tooltip : "Query Gene"
-	}, {
-		id : 'found',
-		header : "Coexpressed Gene",
-		dataIndex : "foundGene",
-		renderer : Gemma.CoexpressionGrid.getFoundGeneStyler(),
-		tooltip : "Coexpressed Gene"
-	}, {
-		id : 'support',
-		header : "Support",
-		dataIndex : "supportKey",
-		width : 75,
-		renderer : Gemma.CoexpressionGrid.getSupportStyler(),
-		tooltip : "# of Datasets that confirm coexpression"
-	}, {
-		id : 'go',
-		header : "GO Overlap",
-		dataIndex : "goSim",
-		width : 75,
-		renderer : Gemma.CoexpressionGrid.getGoStyler(),
-		tooltip : "GO Similarity Score"
-
-	}, {
-		id : 'datasets',
-		header : "Datasets",
-		dataIndex : "datasetVector",
-		renderer : Gemma.CoexpressionGrid.getBitImageStyler(),
-		tooltip : "Dataset relevence map",
-		sortable : false
-	}]);
-	superConfig.cm.defaultSortable = true;
-	superConfig.plugins = this.rowExpander;
-
-	superConfig.autoExpandColumn = 'found';
-
-	for (property in config) {
-		superConfig[property] = config[property];
-	}
-	Gemma.CoexpressionGrid.superclass.constructor.call(this, superConfig);
-
-	this.originalTitle = this.title;
-
-};
-
-/*
- * static methods
- */
-Gemma.CoexpressionGrid.getRecord = function() {
-	if (Gemma.CoexpressionGrid.record === undefined) {
-		Gemma.CoexpressionGrid.record = Ext.data.Record.create([{
-			name : "queryGene",
-			type : "string",
-			convert : function(g) {
-				return g.officialSymbol;
-			}
-		}, {
-			name : "foundGene",
-			sortType : function(g) {
-				return g.officialSymbol;
-			}
-		}, {
-			name : "sortKey",
-			type : "string"
-		}, {
-			name : "supportKey",
-			type : "int",
-			sortType : Ext.data.SortTypes.asInt,
-			sortDir : "DESC"
-		}, {
-			name : "posLinks",
-			type : "int"
-		}, {
-			name : "negLinks",
-			type : "int"
-		}, {
-			name : "numTestedIn",
-			type : "int"
-		}, {
-			name : "nonSpecPosLinks",
-			type : "int"
-		}, {
-			name : "nonSpecNegLinks",
-			type : "int"
-		}, {
-			name : "hybWQuery",
-			type : "boolean"
-		}, {
-			name : "goSim",
-			type : "int"
-		}, {
-			name : "maxGoSim",
-			type : "int"
-		}, {
-			name : "datasetVector",
-			type : "string"
-		}, {
-			name : "supportingExperiments"
-		}]);
-	}
-	return Gemma.CoexpressionGrid.record;
-};
-
-Gemma.CoexpressionGrid.getRowExpander = function() {
-	if (Gemma.CoexpressionGrid.rowExpander === undefined) {
-		Gemma.CoexpressionGrid.rowExpander = new Gemma.CoexpressionGridRowExpander({
-			tpl : "",
-			grid : this
-		});
-	}
-	return Gemma.CoexpressionGrid.rowExpander;
-};
-
-/*
- * instance methods...
- */
-Ext.extend(Gemma.CoexpressionGrid, Ext.grid.GridPanel, {
+	collapsible : true,
+	editable : false,
+	autoHeight : true,
+	style : "margin-bottom: 1em;",
 
 	viewConfig : {
 		forceFit : true
+	},
+
+	initComponent : function() {
+
+		this.plugins = this.rowExpander;
+		if (this.pageSize) {
+			this.store = new Gemma.PagingDataStore({
+				proxy : new Ext.data.MemoryProxy([]),
+				reader : new Ext.data.ListRangeReader({
+					id : "id"
+				}, this.record),
+				sortInfo : {
+					field : 'sortKey',
+					direction : 'ASC'
+				},
+				pageSize : this.pageSize
+			});
+			this.bbar = new Gemma.PagingToolbar({
+				pageSize : this.pageSize,
+				store : this.store
+			});
+		} else {
+			this.ds = new Ext.data.Store({
+				proxy : new Ext.data.MemoryProxy([]),
+				reader : new Ext.data.ListRangeReader({
+					id : "id"
+				}, this.record),
+				sortInfo : {
+					field : 'sortKey',
+					direction : 'ASC'
+				}
+			});
+		}
+
+		this.rowExpander = new Gemma.CoexpressionGridRowExpander({
+			tpl : "",
+			grid : this
+		});
+
+		Ext.apply(this, {
+			columns : [this.rowExpander, {
+				id : 'query',
+				header : "Query Gene",
+				dataIndex : "queryGene",
+				tooltip : "Query Gene",
+				sortable : true
+			}, {
+				id : 'found',
+				header : "Coexpressed Gene",
+				dataIndex : "foundGene",
+				renderer : this.foundGeneStyler.createDelegate(this),
+				tooltip : "Coexpressed Gene",
+				sortable : true
+			}, {
+				id : 'support',
+				header : "Support",
+				dataIndex : "supportKey",
+				width : 75,
+				renderer : this.supportStyler.createDelegate(this),
+				tooltip : "# of Datasets that confirm coexpression",
+				sortable : true
+			}, {
+				id : 'go',
+				header : "GO Overlap",
+				dataIndex : "goSim",
+				width : 75,
+				renderer : this.goStyler.createDelegate(this),
+				tooltip : "GO Similarity Score",
+				sortable : true
+
+			}, {
+				id : 'datasets',
+				header : "Datasets",
+				dataIndex : "datasetVector",
+				renderer : this.bitImageStyler.createDelegate(this),
+				tooltip : "Dataset relevence map",
+				sortable : false
+			}]
+
+		});
+
+		Ext.apply(this, {
+			plugins : this.rowExpander
+		});
+
+		Gemma.CoexpressionGrid.superclass.initComponent.call(this);
+	},
+
+	record : Ext.data.Record.create([{
+		name : "queryGene",
+		type : "string",
+		convert : function(g) {
+			return g.officialSymbol;
+		}
+	}, {
+		name : "foundGene",
+		sortType : function(g) {
+			return g.officialSymbol;
+		}
+	}, {
+		name : "sortKey",
+		type : "string"
+	}, {
+		name : "supportKey",
+		type : "int",
+		sortType : Ext.data.SortTypes.asInt,
+		sortDir : "DESC"
+	}, {
+		name : "posLinks",
+		type : "int"
+	}, {
+		name : "negLinks",
+		type : "int"
+	}, {
+		name : "numTestedIn",
+		type : "int"
+	}, {
+		name : "nonSpecPosLinks",
+		type : "int"
+	}, {
+		name : "nonSpecNegLinks",
+		type : "int"
+	}, {
+		name : "hybWQuery",
+		type : "boolean"
+	}, {
+		name : "goSim",
+		type : "int"
+	}, {
+		name : "maxGoSim",
+		type : "int"
+	}, {
+		name : "datasetVector",
+		type : "string"
+	}, {
+		name : "supportingExperiments"
+	}]),
+
+	/**
+	 * 
+	 */
+	supportStyler : function(value, metadata, record, row, col, ds) {
+		var d = record.data;
+		if (d.posLinks || d.negLinks) {
+			var s = "";
+			if (d.posLinks) {
+				s = s
+						+ String.format("<span class='positiveLink'>{0}{1}</span> ", d.posLinks, this
+								.getSpecificLinkString(d.posLinks, d.nonSpecPosLinks));
+			}
+			if (d.negLinks) {
+				s = s
+						+ String.format("<span class='negativeLink'>{0}{1}</span> ", d.negLinks, this
+								.getSpecificLinkString(d.negLinks, d.nonSpecNegLinks));
+			}
+			s = s + String.format("{0}/ {1}", d.hybWQuery ? " *" : "", d.numTestedIn);
+			return s;
+		} else {
+			return "-";
+		}
+	},
+
+	/**
+	 * For displaying Gene ontology similarity
+	 * 
+	 */
+	goStyler : function(value, metadata, record, row, col, ds) {
+		var d = record.data;
+		if (d.goSim || d.maxGoSim) {
+			return String.format("{0}/{1}", d.goSim, d.maxGoSim);
+		} else {
+			return "-";
+		}
+	},
+
+	getSpecificLinkString : function(total, nonSpecific) {
+		return nonSpecific ? String.format("<span class='specificLink'> ({0})</span>", total - nonSpecific) : "";
+	},
+
+	/**
+	 * Display the target (found) genes.
+	 * 
+	 */
+	foundGeneStyler : function(value, metadata, record, row, col, ds) {
+		var g = record.data.foundGene;
+		if (g.officialName === null) {
+			g.officialName = "";
+		}
+		return this.foundGeneTemplate.apply(g);
+	},
+
+	bitImageStyler : function(value, metadata, record, row, col, ds) {
+		var bits = record.data.datasetVector;
+		var width = Gemma.CoexpressionGrid.bitImageBarWidth * bits.length;
+		var height = Gemma.CoexpressionGrid.bitImageBarHeight;
+		var s = '<span style="background-color:#DDDDDD;">' + '<img src="/Gemma/spark?type=bar&width=' + width
+				+ '&height=' + height + '&color=black&spacing=0&data=';
+		for (var i = 0; i < bits.length; ++i) {
+			if (i > 0) {
+				s = s + ",";
+			}
+			var state = bits.charAt(i);
+			var b = "";
+			if (state === "0") {
+				b = "0";
+			} else if (state === "1") {
+				b = "4";
+			} else if (state === "2") {
+				b = "20";
+			}
+			s = s + b;
+		}
+		// eeMap is created in CoexpressionSearch.js
+		s = s + '" usemap="#eeMap" /></span>';
+		return s;
 	},
 
 	loadData : function(isCannedAnalysis, numQueryGenes, data, datasets) {
@@ -202,101 +255,16 @@ Ext.extend(Gemma.CoexpressionGrid, Ext.grid.GridPanel, {
 			var cm = this.getColumnModel();
 			var c = cm.getIndexById('datasets');
 			var headerWidth = this.view.getHeaderCell(c).firstChild.scrollWidth;
-			var imageWidth = Gemma.CoexpressionGrid.bitImageBarWidth
-					* first.data.datasetVector.length;
-			cm.setColumnWidth(c, imageWidth < headerWidth
-					? headerWidth
-					: imageWidth);
+			var imageWidth = Gemma.CoexpressionGrid.bitImageBarWidth * first.data.datasetVector.length;
+			cm.setColumnWidth(c, imageWidth < headerWidth ? headerWidth : imageWidth);
 		}
-	}
+	},
+
+	foundGeneTemplate : new Ext.Template(
+			"<img src='/Gemma/images/logo/gemmaTiny.gif' ext:qtip='Make {officialSymbol} the query gene' />",
+			" &nbsp; ", "<a href='/Gemma/gene/showGene.html?id={id}'>{officialSymbol}</a> {officialName}")
 
 });
-
-/*
- * Stylers
- * 
- */
-Gemma.CoexpressionGrid.getFoundGeneStyler = function() {
-	if (Gemma.CoexpressionGrid.foundGeneStyler === undefined) {
-		Gemma.CoexpressionGrid.foundGeneTemplate = new Ext.Template(
-
-				"<img src='/Gemma/images/logo/gemmaTiny.gif' ext:qtip='Make {officialSymbol} the query gene' />",
-
-				" &nbsp; ",
-				"<a href='/Gemma/gene/showGene.html?id={id}'>{officialSymbol}</a> {officialName}");
-		Gemma.CoexpressionGrid.foundGeneStyler = function(value, metadata,
-				record, row, col, ds) {
-			var g = record.data.foundGene;
-			if (g.officialName === null) {
-				g.officialName = "";
-			}
-			return Gemma.CoexpressionGrid.foundGeneTemplate.apply(g);
-		};
-	}
-	return Gemma.CoexpressionGrid.foundGeneStyler;
-};
-
-Gemma.CoexpressionGrid.getSupportStyler = function() {
-	if (Gemma.CoexpressionGrid.supportStyler === undefined) {
-		Gemma.CoexpressionGrid.supportStyler = function(value, metadata,
-				record, row, col, ds) {
-			var d = record.data;
-			if (d.posLinks || d.negLinks) {
-				var s = "";
-				if (d.posLinks) {
-					s = s
-							+ String
-									.format(
-											"<span class='positiveLink'>{0}{1}</span> ",
-											d.posLinks, Gemma.CoexpressionGrid
-													.getSpecificLinkString(
-															d.posLinks,
-															d.nonSpecPosLinks));
-				}
-				if (d.negLinks) {
-					s = s
-							+ String
-									.format(
-											"<span class='negativeLink'>{0}{1}</span> ",
-											d.negLinks, Gemma.CoexpressionGrid
-													.getSpecificLinkString(
-															d.negLinks,
-															d.nonSpecNegLinks));
-				}
-				s = s
-						+ String.format("{0}/ {1}", d.hybWQuery ? " *" : "",
-								d.numTestedIn);
-				return s;
-			} else {
-				return "-";
-			}
-		};
-	}
-	return Gemma.CoexpressionGrid.supportStyler;
-};
-
-Gemma.CoexpressionGrid.getSpecificLinkString = function(total, nonSpecific) {
-	return nonSpecific
-			? String.format("<span class='specificLink'> ({0})</span>", total
-					- nonSpecific)
-			: "";
-};
-
-Gemma.CoexpressionGrid.getGoStyler = function() {
-	if (Gemma.CoexpressionGrid.goStyler === undefined) {
-		Gemma.CoexpressionGrid.goStyler = function(value, metadata, record,
-				row, col, ds) {
-			var d = record.data;
-			if (d.goSim || d.maxGoSim) {
-				return String.format("{0}/{1}", d.goSim, d.maxGoSim);
-			} else {
-				return "-";
-			}
-		};
-	}
-	return Gemma.CoexpressionGrid.goStyler;
-};
-
 Gemma.CoexpressionGrid.bitImageBarWidth = 1;
 Gemma.CoexpressionGrid.bitImageBarHeight = 10;
 
@@ -311,37 +279,4 @@ Gemma.CoexpressionGrid.getBitImageMapTemplate = function() {
 				});
 	}
 	return Gemma.CoexpressionGrid.bitImageMapTemplate;
-};
-
-Gemma.CoexpressionGrid.getBitImageStyler = function() {
-	if (Gemma.CoexpressionGrid.bitImageStyler === undefined) {
-		Gemma.CoexpressionGrid.bitImageStyler = function(value, metadata,
-				record, row, col, ds) {
-			var bits = record.data.datasetVector;
-			var width = Gemma.CoexpressionGrid.bitImageBarWidth * bits.length;
-			var height = Gemma.CoexpressionGrid.bitImageBarHeight;
-			var s = '<span style="background-color:#DDDDDD;">'
-					+ '<img src="/Gemma/spark?type=bar&width=' + width
-					+ '&height=' + height + '&color=black&spacing=0&data=';
-			for (var i = 0; i < bits.length; ++i) {
-				if (i > 0) {
-					s = s + ",";
-				}
-				var state = bits.charAt(i);
-				var b = "";
-				if (state === "0") {
-					b = "0";
-				} else if (state === "1") {
-					b = "4";
-				} else if (state === "2") {
-					b = "20";
-				}
-				s = s + b;
-			}
-			// eeMap is created in CoexpressionSearch.js
-			s = s + '" usemap="#eeMap" /></span>';
-			return s;
-		};
-	}
-	return Gemma.CoexpressionGrid.bitImageStyler;
 };
