662edf064396edd73f547d1f7117eb36dddb849c
keshav <>
2008-10-01 22:53:18 +0000

    removed spring mvc code ... we're using ajax calls and this was overly complicating things.

--- gemma-web/src/main/java/ubic/gemma/web/controller/compass/CustomCompassIndexController.java
+++ gemma-web/src/main/java/ubic/gemma/web/controller/compass/CustomCompassIndexController.java
@@ -19,14 +19,9 @@
 package ubic.gemma.web.controller.compass;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.util.StringUtils;
-import org.springframework.validation.BindException;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 
 import ubic.gemma.grid.javaspaces.index.IndexGemmaTask;
 import ubic.gemma.search.IndexService;
@@ -35,178 +30,77 @@ import ubic.gemma.util.grid.javaspaces.SpacesEnum;
 import ubic.gemma.web.controller.BaseFormController;
 
 /**
- * A general Spring's MVC Controller that perform the index operation of
- * <code>CompassGps</code>. The indexing here rebuilds the database. That is,
- * the index is deleted, then rebuilt (at which time the indicies exist but are
- * empty), and then updated.
+ * A general Spring's MVC Controller that perform the index operation of <code>CompassGps</code>. The indexing here
+ * rebuilds the database. That is, the index is deleted, then rebuilt (at which time the indicies exist but are empty),
+ * and then updated.
  * <p>
- * Will perform the index operation if the
- * {@link org.compass.spring.web.mvc.CompassIndexCommand} <code>doIndex</code>
+ * Will perform the index operation if the {@link org.compass.spring.web.mvc.CompassIndexCommand} <code>doIndex</code>
  * property is set to true.
  * <p>
- * The controller has two views to be set, the <code>indexView</code>, which
- * is the view that holds the screen which the user will initiate the index
- * operation, and the <code>indexResultsView</code>, which will show the
- * results of the index operation.
+ * The controller has two views to be set, the <code>indexView</code>, which is the view that holds the screen which
+ * the user will initiate the index operation, and the <code>indexResultsView</code>, which will show the results of
+ * the index operation.
  * <p>
- * The results of the index operation will be saved under the
- * <code>indexResultsName</code>, which defaults to "indexResults".
+ * The results of the index operation will be saved under the <code>indexResultsName</code>, which defaults to
+ * "indexResults".
  * 
  * @author keshav, klc
- * @version $Id: CustomCompassIndexController.java,v 1.22 2007/12/20 23:11:05
- *          kelsey Exp $
+ * @version $Id$
  * @spring.bean id="indexController"
  * @spring.property name="formView" value="indexer"
  * @spring.property name="successView" value="indexer"
  * @spring.property name="indexService" ref="indexService"
- * 
  */
 public class CustomCompassIndexController extends BaseFormController {
 
-	private Log log = LogFactory.getLog(CustomCompassIndexController.class);
+    private Log log = LogFactory.getLog( CustomCompassIndexController.class );
 
-	private IndexService indexService;
+    private IndexService indexService;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.springframework.web.servlet.mvc.SimpleFormController#onSubmit(javax.servlet.http.HttpServletRequest,
-	 *      javax.servlet.http.HttpServletResponse, java.lang.Object,
-	 *      org.springframework.validation.BindException)
-	 */
-	public ModelAndView onSubmit(HttpServletRequest request,
-			HttpServletResponse response, Object command, BindException errors)
-			throws Exception {
+    /**
+     * Main entry point for AJAX calls.
+     * 
+     * @param IndexGemmaCommand
+     * @return
+     */
+    public String run( IndexGemmaCommand command ) {
+        return indexService.run( command, SpacesEnum.DEFAULT_SPACE.getSpaceUrl(), IndexGemmaTask.class.getName(), true );
 
-		// TODO: Make use of command object (so that indexing can be batched)
-		// Kind of mute case using ajax now
+    }
 
-		IndexGemmaCommand indexCommand = new IndexGemmaCommand();
+    /**
+     * Entry point for quartz
+     * 
+     * @param IndexGemmaCommand
+     * @return
+     */
+    public String indexAllInSpace() {
 
-		if (StringUtils.hasText(request.getParameter("geneIndex"))) {
-			indexCommand.setIndexGene(true);
-		} else if (StringUtils.hasText(request.getParameter("eeIndex"))) {
-			indexCommand.setIndexEE(true);
-		} else if (StringUtils.hasText(request.getParameter("arrayIndex"))) {
-			indexCommand.setIndexArray(true);
-		} else if (StringUtils.hasText(request
-				.getParameter("bibliographicIndex"))) {
-			indexCommand.setIndexBibliographic(true);
-		} else if (StringUtils
-				.hasText(request.getParameter("biosequenceIndex"))) {
-			indexCommand.setIndexBibliographic(true);
-		} else
-			return new ModelAndView(this.getFormView());
+        IndexGemmaCommand command = new IndexGemmaCommand();
+        command.setIndexArray( true );
+        command.setIndexBibliographic( true );
+        command.setIndexBioSequence( true );
+        command.setIndexEE( true );
+        command.setIndexGene( true );
+        command.setIndexProbe( true );
 
-		return indexService.startJob(indexCommand, SpacesEnum.DEFAULT_SPACE
-				.getSpaceUrl(), IndexGemmaTask.class.getName(), true);
+        return indexService.run( command, SpacesEnum.DEFAULT_SPACE.getSpaceUrl(), IndexGemmaTask.class.getName(), true );
+    }
 
-	}
+    /**
+     * This is needed or you will have to specify a commandClass in the DispatcherServlet's context
+     * 
+     * @param request
+     * @return Object
+     * @throws Exception
+     */
+    @Override
+    protected Object formBackingObject( HttpServletRequest request ) throws Exception {
+        return request;
+    }
 
-	/**
-	 * Main entry point for AJAX calls.
-	 * 
-	 * @param IndexGemmaCommand
-	 * @return
-	 */
-	public String run(IndexGemmaCommand command) {
-		return indexService.run(command,
-				SpacesEnum.DEFAULT_SPACE.getSpaceUrl(), IndexGemmaTask.class
-						.getName(), true);
-
-	}
-
-	/**
-	 * Entry point for quartz
-	 * 
-	 * @param IndexGemmaCommand
-	 * @return
-	 */
-	public String indexAllInSpace() {
-
-		IndexGemmaCommand command = new IndexGemmaCommand();
-		command.setIndexArray(true);
-		command.setIndexBibliographic(true);
-		command.setIndexBioSequence(true);
-		command.setIndexEE(true);
-		command.setIndexGene(true);
-		command.setIndexProbe(true);
-
-		return indexService.run(command,
-				SpacesEnum.DEFAULT_SPACE.getSpaceUrl(), IndexGemmaTask.class
-						.getName(), true);
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see ubic.gemma.web.controller.BaseFormController#processFormSubmission(javax.servlet.http.HttpServletRequest,
-	 *      javax.servlet.http.HttpServletResponse, java.lang.Object,
-	 *      org.springframework.validation.BindException)
-	 */
-	public ModelAndView processFormSubmission(HttpServletRequest request,
-			HttpServletResponse response, Object command, BindException errors)
-			throws Exception {
-
-		if (request.getParameter("cancel") != null) {
-			this.saveMessage(request, "Cancelled Index");
-			return new ModelAndView(new RedirectView("mainMenu.html"));
-		}
-
-		return super.processFormSubmission(request, response, command, errors);
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.springframework.web.servlet.mvc.SimpleFormController#showForm(javax.servlet.http.HttpServletRequest,
-	 *      javax.servlet.http.HttpServletResponse,
-	 *      org.springframework.validation.BindException)
-	 */
-	@Override
-	protected ModelAndView showForm(HttpServletRequest request,
-			HttpServletResponse response, BindException errors)
-			throws Exception {
-		if (request.getParameter("geneIndex") != null) {
-			return this.onSubmit(request, response, this
-					.formBackingObject(request), errors);
-		}
-		if (request.getParameter("eeIndex") != null) {
-			return this.onSubmit(request, response, this
-					.formBackingObject(request), errors);
-		}
-		if (request.getParameter("arrayIndex") != null) {
-			return this.onSubmit(request, response, this
-					.formBackingObject(request), errors);
-		}
-		if (request.getParameter("bibliographicIndex") != null) {
-			return this.onSubmit(request, response, this
-					.formBackingObject(request), errors);
-		}
-		if (request.getParameter("probeIndex") != null) {
-			return this.onSubmit(request, response, this
-					.formBackingObject(request), errors);
-		}
-
-		return super.showForm(request, response, errors);
-	}
-
-	/**
-	 * This is needed or you will have to specify a commandClass in the
-	 * DispatcherServlet's context
-	 * 
-	 * @param request
-	 * @return Object
-	 * @throws Exception
-	 */
-	@Override
-	protected Object formBackingObject(HttpServletRequest request)
-			throws Exception {
-		return request;
-	}
-
-	public void setIndexService(IndexService indexService) {
-		this.indexService = indexService;
-	}
+    public void setIndexService( IndexService indexService ) {
+        this.indexService = indexService;
+    }
 
 }
