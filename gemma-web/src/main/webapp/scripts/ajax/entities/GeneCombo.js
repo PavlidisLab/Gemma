16a5b63c86cf4b23e6385ba7f705d1d242873a15
ppavlidis <paul@chibi.ubc.ca>
2008-10-24 02:37:35 +0000

    Tweak behavior after failed search. Way it worked before was no good. It may need further work.

--- gemma-web/src/main/webapp/scripts/ajax/entities/GeneCombo.js
+++ gemma-web/src/main/webapp/scripts/ajax/entities/GeneCombo.js
@@ -12,121 +12,121 @@ Ext.namespace('Gemma');
  */
 Gemma.GeneCombo = Ext.extend(Ext.form.ComboBox, {
 
-	displayField : 'officialSymbol',
-	valueField : 'id',
-	width : 140,// default.
-	listWidth : 450, // ridiculously large so IE displays it properly
-	// (usually)
+			displayField : 'officialSymbol',
+			valueField : 'id',
+			width : 140,// default.
+			listWidth : 450, // ridiculously large so IE displays it properly
+			// (usually)
 
-	loadingText : 'Searching...',
-	emptyText : "Search for a gene",
-	minChars : 1,
-	selectOnFocus : true,
+			loadingText : 'Searching...',
+			emptyText : "Search for a gene",
+			minChars : 1,
+			selectOnFocus : true,
 
-	record : Ext.data.Record.create([{
-		name : "id",
-		type : "int"
-	}, {
-		name : "taxon"
-	}, {
-		name : "officialSymbol",
-		type : "string"
-	}, {
-		name : "officialName",
-		type : "string"
-	}]),
+			record : Ext.data.Record.create([{
+						name : "id",
+						type : "int"
+					}, {
+						name : "taxon"
+					}, {
+						name : "officialSymbol",
+						type : "string"
+					}, {
+						name : "officialName",
+						type : "string"
+					}]),
 
-	initComponent : function() {
+			initComponent : function() {
 
-		var template = new Ext.XTemplate('<tpl for="."><div style="font-size:11px" class="x-combo-list-item" ext:qtip="{officialName} ({[values.taxon.scientificName]})">{officialSymbol} {officialName} ({[values.taxon.scientificName]})</div></tpl>');
+				var template = new Ext.XTemplate('<tpl for="."><div style="font-size:11px" '
+						+ 'class="x-combo-list-item" ext:qtip="{officialName} ({[values.taxon.scientificName]})">'
+						+ '{officialSymbol} {officialName} ({[values.taxon.scientificName]})</div></tpl>');
 
-		Ext.apply(this, {
-			tpl : template,
-			store : new Ext.data.Store({
-				proxy : new Ext.data.DWRProxy(GenePickerController.searchGenes),
-				reader : new Ext.data.ListRangeReader({
-					id : "id"
-				}, this.record),
-				sortInfo : {
-					field : "officialSymbol",
-					dir : "ASC"
+				Ext.apply(this, {
+							tpl : template,
+							store : new Ext.data.Store({
+										proxy : new Ext.data.DWRProxy(GenePickerController.searchGenes),
+										reader : new Ext.data.ListRangeReader({
+													id : "id"
+												}, this.record),
+										sortInfo : {
+											field : "officialSymbol",
+											dir : "ASC"
+										}
+									})
+						});
+
+				Gemma.GeneCombo.superclass.initComponent.call(this);
+
+				this.addEvents('genechanged');
+
+				this.store.on("datachanged", function() {
+							if (this.store.getCount() === 0) {
+								this.fireEvent("invalid", "No matching genes");
+								// this.emptyText = "Nothing found";
+								// this.clearValue();
+							}
+						}, this);
+			},
+
+			onSelect : function(record, index) {
+				Gemma.GeneCombo.superclass.onSelect.call(this, record, index);
+				if (!this.selectedGene || record.data.id != this.selectedGene.id) {
+					this.setGene(record.data);
+					this.fireEvent('select', this, this.selectedGene);
 				}
-			})
-		});
+			},
 
-		Gemma.GeneCombo.superclass.initComponent.call(this);
-
-		this.addEvents('genechanged');
-
-		this.store.on("datachanged", function() {
-			if (this.store.getCount() === 0) {
-				this.fireEvent("invalid", "No matching genes");
-				this.emptyText = "Nothing found";
-				this.clearValue();
-			}
-		}, this);
-	},
-
-	onSelect : function(record, index) {
-		Gemma.GeneCombo.superclass.onSelect.call(this, record, index);
-		if (!this.selectedGene || record.data.id != this.selectedGene.id) {
-			this.setGene(record.data);
-			this.fireEvent('select', this, this.selectedGene);
-		}
-	},
-
-	/**
-	 * Parameters for AJAX call.
-	 * 
-	 * @param {}
-	 *            query
-	 * @return {}
-	 */
-	getParams : function(query) {
-		return [query, this.taxon ? this.taxon.id : -1];
-	},
-
-	getGene : function() {
-		return this.selectedGene;
-	},
-
-	setGene : function(gene) {
-		if (this.tooltip) {
-			this.tooltip.destroy();
-		}
-		if (gene) {
-			this.selectedGene = gene;
-			this.taxon = gene.taxon;
-			this.tooltip = new Ext.ToolTip({
-				target : this.getEl(),
-				html : String.format('{0} ({1})', gene.officialName
-						|| "no description", gene.taxon.scientificName)
-			});
-		}
-	},
-
-	getTaxon : function() {
-		return this.taxon;
-	},
-
-	setTaxon : function(taxon) {
-		if (!this.taxon || this.taxon.id != taxon.id) {
-			//console.log("taxon =" + taxon.id);
-			this.taxon = taxon;
-			delete this.selectedGene;
-			if (this.tooltip) {
-				this.tooltip.destroy();
-			}
-			this.reset();
-
-			/*
-			 * this is to make sure we always search again after a taxon change,
-			 * in case the user searches for the same gene. Otherwise Ext just
-			 * keeps the old results.
+			/**
+			 * Parameters for AJAX call.
+			 * 
+			 * @param {}
+			 *            query
+			 * @return {}
 			 */
-			this.lastQuery = null;
+			getParams : function(query) {
+				return [query, this.taxon ? this.taxon.id : -1];
+			},
 
-		}
-	}
+			getGene : function() {
+				return this.selectedGene;
+			},
 
-});
+			setGene : function(gene) {
+				if (this.tooltip) {
+					this.tooltip.destroy();
+				}
+				if (gene) {
+					this.selectedGene = gene;
+					this.taxon = gene.taxon;
+					this.tooltip = new Ext.ToolTip({
+								target : this.getEl(),
+								html : String.format('{0} ({1})', gene.officialName || "no description",
+										gene.taxon.scientificName)
+							});
+				}
+			},
+
+			getTaxon : function() {
+				return this.taxon;
+			},
+
+			setTaxon : function(taxon) {
+				if (!this.taxon || this.taxon.id != taxon.id) {
+					this.taxon = taxon;
+					delete this.selectedGene;
+					if (this.tooltip) {
+						this.tooltip.destroy();
+					}
+					this.reset();
+
+					/*
+					 * this is to make sure we always search again after a taxon change, in case the user searches for
+					 * the same gene. Otherwise Ext just keeps the old results.
+					 */
+					this.lastQuery = null;
+
+				}
+			}
+
+		});
