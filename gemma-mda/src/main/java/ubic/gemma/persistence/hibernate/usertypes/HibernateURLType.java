b0725de02e5e1b47f909296bbfef3ba71138a535
ppavlidis <paul@chibi.ubc.ca>
2008-12-04 14:46:50 +0000

    add null check

--- gemma-mda/src/main/java/ubic/gemma/persistence/hibernate/usertypes/HibernateURLType.java
+++ gemma-mda/src/main/java/ubic/gemma/persistence/hibernate/usertypes/HibernateURLType.java
@@ -43,17 +43,14 @@ public class HibernateURLType implements UserType {
 
     /*
      * (non-Javadoc)
-     * 
      * @see org.hibernate.usertype.UserType#assemble(java.io.Serializable, java.lang.Object)
      */
-    @SuppressWarnings("unused")
     public Object assemble( Serializable cached, Object owner ) throws HibernateException {
         return this.deepCopy( cached );
     }
 
     /*
      * (non-Javadoc)
-     * 
      * @see org.hibernate.usertype.UserType#deepCopy(java.lang.Object)
      */
     public Object deepCopy( Object value ) throws HibernateException {
@@ -81,7 +78,6 @@ public class HibernateURLType implements UserType {
 
     /*
      * (non-Javadoc)
-     * 
      * @see org.hibernate.usertype.UserType#disassemble(java.lang.Object)
      */
     public Serializable disassemble( Object value ) throws HibernateException {
@@ -90,7 +86,6 @@ public class HibernateURLType implements UserType {
 
     /*
      * (non-Javadoc)
-     * 
      * @see org.hibernate.usertype.UserType#equals(java.lang.Object, java.lang.Object)
      */
     public boolean equals( Object x, Object y ) throws HibernateException {
@@ -107,7 +102,6 @@ public class HibernateURLType implements UserType {
 
     /*
      * (non-Javadoc)
-     * 
      * @see org.hibernate.usertype.UserType#hashCode(java.lang.Object)
      */
     public int hashCode( Object x ) throws HibernateException {
@@ -116,7 +110,6 @@ public class HibernateURLType implements UserType {
 
     /*
      * (non-Javadoc)
-     * 
      * @see org.hibernate.usertype.UserType#isMutable()
      */
     public boolean isMutable() {
@@ -125,11 +118,12 @@ public class HibernateURLType implements UserType {
 
     /*
      * (non-Javadoc)
-     * 
      * @see org.hibernate.usertype.UserType#nullSafeGet(java.sql.ResultSet, java.lang.String[], java.lang.Object)
      */
-    @SuppressWarnings("unused")
     public Object nullSafeGet( ResultSet rs, String[] names, Object owner ) throws HibernateException, SQLException {
+        if ( names == null || names.length == 0 ) {
+            return null;
+        }
         final StringBuffer buffer = new StringBuffer();
         try {
             // First we get the stream
@@ -156,7 +150,6 @@ public class HibernateURLType implements UserType {
 
     /*
      * (non-Javadoc)
-     * 
      * @see org.hibernate.usertype.UserType#nullSafeSet(java.sql.PreparedStatement, java.lang.Object, int)
      */
     public void nullSafeSet( PreparedStatement preparedStatement, Object data, int index ) throws HibernateException,
@@ -175,17 +168,14 @@ public class HibernateURLType implements UserType {
 
     /*
      * (non-Javadoc)
-     * 
      * @see org.hibernate.usertype.UserType#replace(java.lang.Object, java.lang.Object, java.lang.Object)
      */
-    @SuppressWarnings("unused")
     public Object replace( Object original, Object target, Object owner ) throws HibernateException {
         return this.deepCopy( original );
     }
 
     /*
      * (non-Javadoc)
-     * 
      * @see org.hibernate.usertype.UserType#returnedClass()
      */
     public Class returnedClass() {
@@ -194,7 +184,6 @@ public class HibernateURLType implements UserType {
 
     /*
      * (non-Javadoc)
-     * 
      * @see org.hibernate.usertype.UserType#sqlTypes()
      */
     public int[] sqlTypes() {
