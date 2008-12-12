83fbc0d8e0c13f99a01541f330a0abb55f93c399
kelsey <>
2008-12-12 02:20:45 +0000

    took out gigaspace dependency.  Added cache director to command line.

--- gemma-core/etc/scripts/startWorker.sh
+++ gemma-core/etc/scripts/startWorker.sh
@@ -2,7 +2,6 @@
 # $Id$
 
 # This script uses the following environment variables, which you must define here or in your profile.
-# JSHOMEDIR="/proj/GigaSpacesCommunity5.2"
 # GEMMA_LIB="/gemmaData/lib/Gemma"
 
 GEMMA_USER=administrator
@@ -15,12 +14,12 @@ CP=`cat ${GEMMA_LIB}/CLASSPATH`
 
 JAVACMD="${JAVA_HOME}/bin/java"
 
-. ${JSHOMEDIR}/bin/setenv.sh
+#Not necessary.  Also means every worker needs an installation of gigaspaces avaliable for it. 
+#. ${JSHOMEDIR}/bin/setenv.sh
+#JARS="${JSHOMEDIR}${CPS}${JSHOMEDIR}/lib/JSpaces.jar${CPS}${COMMON_JARS}${CPS}$CP"; export JARS
 
-JARS="${JSHOMEDIR}${CPS}${JSHOMEDIR}/lib/JSpaces.jar${CPS}${COMMON_JARS}${CPS}$CP"; export JARS
-
-$JAVACMD $VMARGS -Dcom.gs.home=$JSHOMEDIR \
- -classpath "${CPS}${JARS}" ubic.gemma.grid.javaspaces.worker.$WORKER_CLASS \
+$JAVACMD $VMARGS -Dcom.gs.home=$GEMMA_LIB -Dehcache.disk.store.dir=$HOME/scratch \
+-classpath "${CP}" ubic.gemma.grid.javaspaces.worker.$WORKER_CLASS \
 -u $GEMMA_USER -p $GEMMA_PWD -gigaspacesOn $APPARGS
 
 
