e65030667fd96568be57c3b5ec84cda808b2d1d2
ppavlidis <paul@chibi.ubc.ca>
2009-04-30 04:22:45 +0000

    missed commit.

--- gemma-mda/src/main/java/ubic/gemma/util/ChannelUtils.java
+++ gemma-mda/src/main/java/ubic/gemma/util/ChannelUtils.java
@@ -36,8 +36,9 @@ public class ChannelUtils {
     public static boolean isBackgroundChannelA( String name ) {
         return name.matches( "CH1B(N)?_(MEDIAN|MEAN)" ) || name.equals( "CH1_BKD" )
                 || name.toLowerCase().matches( "b532[\\s_\\.](mean|median)" )
-                || name.equals( "BACKGROUND_CHANNEL 1MEDIAN" ) || name.equals( "G_BG_MEDIAN" )
-                || name.equals( "Ch1BkgMedian" ) || name.equals( "ch1.Background" ) || name.equals( "CH1_BKG_MEAN" )
+                || name.toUpperCase().equals( "BACKGROUND_CHANNEL 1MEDIAN" )
+                || name.toUpperCase().equals( "G_BG_MEDIAN" ) || name.equals( "Ch1BkgMedian" )
+                || name.equals( "ch1.Background" ) || name.toUpperCase().equals( "CH1_BKG_MEAN" )
                 || name.equals( "CH1_BKD_ Median" ) || name.equals( "BKG1Mean" );
     }
 
@@ -50,8 +51,9 @@ public class ChannelUtils {
     public static boolean isBackgroundChannelB( String name ) {
         return name.matches( "CH2B(N)?_(MEDIAN|MEAN)" ) || name.equals( "CH2_BKD" )
                 || name.toLowerCase().matches( "b635[\\s_\\.](mean|median)" )
-                || name.equals( "BACKGROUND_CHANNEL 2MEDIAN" ) || name.equals( "R_BG_MEDIAN" )
-                || name.equals( "Ch2BkgMedian" ) || name.equals( "ch2.Background" ) || name.equals( "CH2_BKG_MEAN" )
+                || name.toUpperCase().equals( "BACKGROUND_CHANNEL 2MEDIAN" )
+                || name.toUpperCase().equals( "R_BG_MEDIAN" ) || name.equals( "Ch2BkgMedian" )
+                || name.equals( "ch2.Background" ) || name.toUpperCase().equals( "CH2_BKG_MEAN" )
                 || name.equals( "CH2_BKD_ Median" ) || name.equals( "BKG2Mean" );
     }
 
@@ -63,14 +65,15 @@ public class ChannelUtils {
      * @return
      */
     public static boolean isSignalChannelA( String name ) {
-        return name.matches( "CH1(I)?(N)?_MEDIAN" ) || name.matches( "CH1(I)?(N)?_MEAN" ) || name.equals( "RAW_DATA" )
-                || name.toLowerCase().matches( "f532[\\s_\\.](mean|median)" ) || name.equals( "SIGNAL_CHANNEL 1MEDIAN" )
-                || name.toLowerCase().matches( "ch1_smtm" ) || name.equals( "G_MEAN" ) || name.equals( "Ch1SigMedian" )
-                || name.equals( "ch1.Intensity" ) || name.matches( "CH1_SIG_(MEAN|MEDIAN)" )
-                || name.equals( "CH1_ Median" ) || name.toUpperCase().matches( "\\w{2}\\d{3}_CY3" )
+        return name.matches( "CH1(I)?(N)?_MEDIAN" ) || name.matches( "CH1(I)?(N)?_MEAN" )
+                || name.toUpperCase().equals( "RAW_DATA" ) || name.toLowerCase().matches( "f532[\\s_\\.](mean|median)" )
+                || name.toUpperCase().equals( "SIGNAL_CHANNEL 1MEDIAN" ) || name.toLowerCase().matches( "ch1_smtm" )
+                || name.equals( "G_MEAN" ) || name.equals( "Ch1SigMedian" ) || name.equals( "ch1.Intensity" )
+                || name.toUpperCase().matches( "CH1_SIG_(MEAN|MEDIAN)" ) || name.equals( "CH1_ Median" )
+                || name.toUpperCase().matches( "\\w{2}\\d{3}_CY3" ) || name.toUpperCase().matches( "CY3.*" )
                 || name.toUpperCase().matches( "NORM(.*)CH1" ) || name.equals( "CH1Mean" )
-                || name.equals( "CH1_SIGNAL" ) || name.equals( "\"log2(532), gN\"" )
-                || name.equals( "gProcessedSignal" ) || name.equals( "CH1_SIG_MEDIAN" );
+                || name.toUpperCase().equals( "CH1_SIGNAL" ) || name.equals( "\"log2(532), gN\"" )
+                || name.equals( "gProcessedSignal" ) || name.toUpperCase().equals( "CH1_SIG_MEDIAN" );
     }
 
     /**
@@ -82,12 +85,14 @@ public class ChannelUtils {
      */
     public static boolean isSignalChannelB( String name ) {
         return name.matches( "CH2(I)?(N)?_MEDIAN" ) || name.matches( "CH2(I)?(N)?_MEAN" )
-                || name.equals( "RAW_CONTROL" ) || name.toLowerCase().matches( "f635[\\s_\\.](mean|median)" )
-                || name.equals( "SIGNAL_CHANNEL 2MEDIAN" ) || name.toLowerCase().matches( "ch2_smtm" )
+                || name.toUpperCase().equals( "RAW_CONTROL" )
+                || name.toLowerCase().matches( "f635[\\s_\\.](mean|median)" )
+                || name.toUpperCase().equals( "SIGNAL_CHANNEL 2MEDIAN" ) || name.toLowerCase().matches( "ch2_smtm" )
                 || name.equals( "R_MEAN" ) || name.equals( "Ch2SigMedian" ) || name.equals( "ch2.Intensity" )
-                || name.matches( "CH2_SIG_(MEAN|MEDIAN)" ) || name.equals( "CH2_ Median" )
-                || name.toUpperCase().matches( "\\w{2}\\d{3}_CY5" ) || name.toUpperCase().matches( "NORM(.*)CH2" )
-                || name.equals( "CH2Mean" ) || name.equals( "CH2_SIGNAL" ) || name.equals( "\"log2(635), gN\"" )
+                || name.toUpperCase().matches( "CH2_SIG_(MEAN|MEDIAN)" ) || name.equals( "CH2_ Median" )
+                || name.toUpperCase().matches( "\\w{2}\\d{3}_CY5" ) || name.toUpperCase().matches( "CY5.*" )
+                || name.toUpperCase().matches( "NORM(.*)CH2" ) || name.equals( "CH2Mean" )
+                || name.toUpperCase().equals( "CH2_SIGNAL" ) || name.equals( "\"log2(635), gN\"" )
                 || name.equals( "rProcessedSignal" );
     }
 
