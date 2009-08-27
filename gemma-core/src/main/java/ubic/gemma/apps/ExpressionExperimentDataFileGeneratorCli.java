81487ea0e2715b4a86748db9056b66f76f8a559f
klc <>
2009-08-27 16:39:19 +0000

    Made multi threaded so runs faster.

--- gemma-core/src/main/java/ubic/gemma/apps/ExpressionExperimentDataFileGeneratorCli.java
+++ gemma-core/src/main/java/ubic/gemma/apps/ExpressionExperimentDataFileGeneratorCli.java
@@ -17,10 +17,11 @@
  *
  */
 
-
 package ubic.gemma.apps;
 
 import java.io.File;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 import ubic.gemma.analysis.service.ExpressionDataFileService;
 import ubic.gemma.model.common.auditAndSecurity.AuditTrailService;
@@ -30,13 +31,13 @@ import ubic.gemma.model.expression.experiment.BioAssaySet;
 import ubic.gemma.model.expression.experiment.ExpressionExperiment;
 
 public class ExpressionExperimentDataFileGeneratorCli extends ExpressionExperimentManipulatingCLI {
-    
+
     private static final boolean FORCE_WRITE = true;
 
     ExpressionDataFileService expressionDataFileService;
 
     private String DESCRIPTION = "Generate Flat data files (diff expression, co-expression) for a given set of experiments";
-    
+
     @Override
     protected Exception doWork( String[] args ) {
 
@@ -45,19 +46,59 @@ public class ExpressionExperimentDataFileGeneratorCli extends ExpressionExperime
             return exp;
         }
 
+        BlockingQueue<BioAssaySet> queue = new ArrayBlockingQueue<BioAssaySet>( expressionExperiments.size() );
+
+        //Add the Experiments to the queue for processing 
         for ( BioAssaySet ee : expressionExperiments ) {
             if ( ee instanceof ExpressionExperiment ) {
-                processExperiment( ( ExpressionExperiment ) ee );
+                try {
+                    queue.put( ( ExpressionExperiment ) ee );
+                } catch ( InterruptedException ie ) {
+                    //FIXME
+                }
             } else {
                 throw new UnsupportedOperationException( "Can't handle non-EE BioAssaySets yet" );
             }
 
         }
-        summarizeProcessing();
+
+        //Inner class for processing the experiments
+        class Worker extends Thread {
+            BlockingQueue<BioAssaySet> q;
+
+            Worker( BlockingQueue<BioAssaySet> q ) {
+                this.q = q;
+            }
+
+            public void run() {
+                try {
+                    while ( true ) {
+                        BioAssaySet ee = q.take();
+                        if ( ee == null ) {
+                            summarizeProcessing();                            
+                            break;
+                        }
+
+                        processExperiment( ( ExpressionExperiment ) ee );
+                     
+                    }
+                } catch ( InterruptedException e ) {
+                    //FIXME
+                }
+            }
+        }
+
+        //
+        int numWorkers = 6;
+        Worker[] workers = new Worker[numWorkers];
+        for ( int i = 0; i < workers.length; i++ ) {
+            workers[i] = new Worker( queue );
+            workers[i].start();
+        }
+        
         return null;
 
     }
-
 
     private void processExperiment( ExpressionExperiment ee ) {
 
@@ -66,15 +107,16 @@ public class ExpressionExperimentDataFileGeneratorCli extends ExpressionExperime
 
             AuditTrailService auditEventService = ( AuditTrailService ) this.getBean( "auditTrailService" );
             AuditEventType type = CommentedEvent.Factory.newInstance();
-            
+
             File coexpressionFile = expressionDataFileService.writeOrLocateCoexpressionDataFile( ee, FORCE_WRITE );
             File diffExpressionFile = expressionDataFileService.writeOrLocateDiffExpressionDataFile( ee, FORCE_WRITE );
-            
-            auditEventService.addUpdateEvent( ee, type, "Generated Flat data files for downloading"  );            
-            super.successObjects.add("Success:  generated data file for " +ee.getShortName() + " ID=" + ee.getId()  );
-        
+
+            auditEventService.addUpdateEvent( ee, type, "Generated Flat data files for downloading" );
+            super.successObjects.add( "Success:  generated data file for " + ee.getShortName() + " ID=" + ee.getId() );
+
         } catch ( Exception e ) {
-            super.errorObjects.add("FAILED: for ee: "  + ee.getShortName() + " ID= "+ ee.getId() + " Error: " + e.getMessage() );
+            super.errorObjects.add( "FAILED: for ee: " + ee.getShortName() + " ID= " + ee.getId() + " Error: "
+                    + e.getMessage() );
         }
     }
 
@@ -93,9 +135,8 @@ public class ExpressionExperimentDataFileGeneratorCli extends ExpressionExperime
     protected void processOptions() {
         super.processOptions();
 
-        expressionDataFileService = (ExpressionDataFileService ) this.getBean( "expressionDataFileService" );
+        expressionDataFileService = ( ExpressionDataFileService ) this.getBean( "expressionDataFileService" );
     }
-
 
     @Override
     public String getShortDesc() {
