a2540a111832e5e01d7ad67633e6feca08903282
klc <>
2009-12-03 21:13:46 +0000

    attempint to turn fifo off

--- gemma-core/src/main/java/ubic/gemma/grid/javaspaces/BaseSpacesTask.java
+++ gemma-core/src/main/java/ubic/gemma/grid/javaspaces/BaseSpacesTask.java
@@ -104,7 +104,7 @@ public abstract class BaseSpacesTask implements SpacesTask {
      */
     public void setGigaSpacesTemplate( GigaSpacesTemplate gigaSpacesTemplate ) {
         this.gigaSpacesTemplate = gigaSpacesTemplate;
-        this.gigaSpacesTemplate.setFifo( true );
+        this.gigaSpacesTemplate.setFifo( false );
     }
 
     /*
