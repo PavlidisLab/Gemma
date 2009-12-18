e656098cf669b832ff6b4c535a86237dca293f9e
ppavlidis <paul@chibi.ubc.ca>
2009-12-18 06:13:12 +0000

    code cleanup

--- gemma-mda/src/main/java/ubic/gemma/model/genome/gene/GeneSetDaoImpl.java
+++ gemma-mda/src/main/java/ubic/gemma/model/genome/gene/GeneSetDaoImpl.java
@@ -19,7 +19,6 @@
 package ubic.gemma.model.genome.gene;
 
 import java.util.Collection;
-import java.util.Iterator;
 
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,36 +31,28 @@ import org.springframework.stereotype.Repository;
  * 
  * @author kelsey
  * @see ubic.gemma.model.genome.gene.GeneSet
- * @version $ID
+ * @version $Id$
  */
 @Repository
 public class GeneSetDaoImpl extends HibernateDaoSupport implements GeneSetDao {
-
 
     @Autowired
     public GeneSetDaoImpl( SessionFactory sessionFactory ) {
         super.setSessionFactory( sessionFactory );
     }
-    
-    @Override
+
     public Collection<? extends GeneSet> create( final Collection<? extends GeneSet> entities ) {
         if ( entities == null ) {
             throw new IllegalArgumentException( "GeneSet.create - 'genesets' can not be null" );
         }
-        this.getHibernateTemplate().executeWithNativeSession(
-                new org.springframework.orm.hibernate3.HibernateCallback<Object>() {
-                    public Object doInHibernate( org.hibernate.Session session )
-                            throws org.hibernate.HibernateException {
-                        for ( java.util.Iterator entityIterator = entities.iterator(); entityIterator.hasNext(); ) {
-                            create( ( GeneSet ) entityIterator.next() );
-                        }
-                        return null;
-                    }
-                } );
+
+        for ( GeneSet geneSet : entities ) {
+            create( geneSet );
+        }
+
         return entities;
     }
 
-    @Override
     public GeneSet create( GeneSet entity ) {
         if ( entity == null ) {
             throw new IllegalArgumentException( "GeneSet.create - 'geneset' can not be null" );
@@ -72,27 +63,28 @@ public class GeneSetDaoImpl extends HibernateDaoSupport implements GeneSetDao {
 
     }
 
-    @Override
+    @SuppressWarnings("unchecked")
     public Collection<? extends GeneSet> load( Collection<Long> ids ) {
         return this.getHibernateTemplate().findByNamedParam( "from GeneSetImpl where id in (:ids)", "ids", ids );
     }
 
-    @Override
     public GeneSet load( Long id ) {
         if ( id == null ) {
             throw new IllegalArgumentException( "CandidateGeneList.load - 'id' can not be null" );
         }
-        return ( GeneSet ) this.getHibernateTemplate().get( GeneSet.class, id );
+        return this.getHibernateTemplate().get( GeneSet.class, id );
 
     }
 
-    @Override
     public Collection<? extends GeneSet> loadAll() {
         final java.util.Collection<GeneSetImpl> results = this.getHibernateTemplate().loadAll( GeneSetImpl.class );
         return results;
     }
 
-    @Override
+    /*
+     * (non-Javadoc)
+     * @see ubic.gemma.persistence.BaseDao#remove(java.util.Collection)
+     */
     public void remove( Collection<? extends GeneSet> entities ) {
         if ( entities == null ) {
             throw new IllegalArgumentException( "GeneSet.remove - 'Collection of geneSet' can not be null" );
@@ -100,7 +92,10 @@ public class GeneSetDaoImpl extends HibernateDaoSupport implements GeneSetDao {
         this.getHibernateTemplate().deleteAll( entities );
     }
 
-    @Override
+    /*
+     * (non-Javadoc)
+     * @see ubic.gemma.persistence.BaseDao#remove(java.lang.Long)
+     */
     public void remove( Long id ) {
         if ( id == null ) {
             throw new IllegalArgumentException( "GeneSet.remove - 'id' can not be null" );
@@ -112,7 +107,10 @@ public class GeneSetDaoImpl extends HibernateDaoSupport implements GeneSetDao {
 
     }
 
-    @Override
+    /*
+     * (non-Javadoc)
+     * @see ubic.gemma.persistence.BaseDao#remove(java.lang.Object)
+     */
     public void remove( GeneSet entity ) {
         if ( entity == null ) {
             throw new IllegalArgumentException( "GeneSet.remove - 'geneset entity' can not be null" );
@@ -121,31 +119,22 @@ public class GeneSetDaoImpl extends HibernateDaoSupport implements GeneSetDao {
 
     }
 
-    @Override
     public void update( final Collection<? extends GeneSet> entities ) {
         if ( entities == null ) {
             throw new IllegalArgumentException( "GeneSet.update - 'Collection of geneSets' can not be null" );
         }
-        this.getHibernateTemplate().executeWithNativeSession(
-                new org.springframework.orm.hibernate3.HibernateCallback<Object>() {
-                    public Object doInHibernate( org.hibernate.Session session )
-                            throws org.hibernate.HibernateException {
-                        for ( Iterator entityIterator = entities.iterator(); entityIterator.hasNext(); ) {
-                            update( ( GeneSet ) entityIterator.next() );
-                        }
-                        return null;
-                    }
-                } );
+
+        for ( GeneSet geneSet : entities ) {
+            update( geneSet );
+        }
 
     }
 
-    @Override
     public void update( GeneSet entity ) {
         if ( entity == null ) {
             throw new IllegalArgumentException( "GeneSet.update - 'geneSet' can not be null" );
         }
         this.getHibernateTemplate().update( entity );
-
     }
 
 }
