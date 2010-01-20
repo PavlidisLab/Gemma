7fa1a61ef4b6f53e04e04a1cbc90ce5a6ba720d6
ppavlidis <paul@chibi.ubc.ca>
2010-01-20 00:08:57 +0000

    prototype shell script for cli

--- gemma-core/etc/scripts/gemmaCli.sh
+++ gemma-core/etc/scripts/gemmaCli.sh
@@ -4,12 +4,13 @@
 # This script uses the following environment variables, which you must define here or in your profile.
 # GEMMA_LIB="/gemmaData/lib/Gemma"
 
-MAIN_CLASS=$3 
-APPARGS=$4
+
+APPARGS=$@
 
 
 JAVACMD="${JAVA_HOME}/bin/java"
 
-$JAVACMD $JAVA_OPTS \
--classpath "${GEMMA_LIB}/*" $MAIN_CLASS \
-$APPARGS
+ 
+CMD="$JAVACMD $JAVA_OPTS -classpath ${GEMMA_LIB}/gemma-core-1.5-SNAPSHOT.jar $APPARGS"
+echo $CMD
+$CMD
