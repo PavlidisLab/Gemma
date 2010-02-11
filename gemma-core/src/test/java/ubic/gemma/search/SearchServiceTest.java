22a3e6bd023862b6865824cb9c5180730febc71d
ppavlidis <paul@chibi.ubc.ca>
2010-02-11 06:32:36 +0000

    cleanup

--- gemma-core/src/test/java/ubic/gemma/search/SearchServiceTest.java
+++ gemma-core/src/test/java/ubic/gemma/search/SearchServiceTest.java
@@ -17,7 +17,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
