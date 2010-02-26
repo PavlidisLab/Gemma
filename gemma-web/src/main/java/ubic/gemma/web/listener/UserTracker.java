bf9b67a162232e0743027931277db8ff92d41e09
ppavlidis <paul@chibi.ubc.ca>
2010-02-26 23:35:54 +0000

    initialize

--- gemma-web/src/main/java/ubic/gemma/web/listener/UserTracker.java
+++ gemma-web/src/main/java/ubic/gemma/web/listener/UserTracker.java
@@ -1,16 +1,34 @@
+/*
+ * The Gemma project
+ * 
+ * Copyright (c) 2010 University of British Columbia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package ubic.gemma.web.listener;
 
 /**
- * TODO Document Me
+ * Basic bean to hold number of authenticated users and sessions.
  * 
  * @author paul
  * @version $Id$
  */
 public class UserTracker {
 
-    private static int activeSessions;
+    private static int activeSessions = 0;
 
-    private static int authenticatedUsers;
+    private static int authenticatedUsers = 0;
 
     public static int decrementSessions() {
         return --activeSessions;
