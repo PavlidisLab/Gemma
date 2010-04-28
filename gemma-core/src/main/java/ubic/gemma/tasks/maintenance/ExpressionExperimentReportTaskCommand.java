d2b4ab126cc7a3dd5fddf42f83f94e949ba0c4d5
ppavlidis <paul@chibi.ubc.ca>
2010-04-28 17:46:23 +0000

    set max runtime

--- gemma-core/src/main/java/ubic/gemma/tasks/maintenance/ExpressionExperimentReportTaskCommand.java
+++ gemma-core/src/main/java/ubic/gemma/tasks/maintenance/ExpressionExperimentReportTaskCommand.java
@@ -1,8 +1,30 @@
+/*
+ * The Gemma project
+ * 
+ * Copyright (c) 2010 University of British Columbia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package ubic.gemma.tasks.maintenance;
 
 import ubic.gemma.job.TaskCommand;
 import ubic.gemma.model.expression.experiment.ExpressionExperiment;
 
+/**
+ * @author paul
+ * @version $Id$
+ */
 public class ExpressionExperimentReportTaskCommand extends TaskCommand {
 
     private static final long serialVersionUID = 1L;
@@ -12,26 +34,13 @@ public class ExpressionExperimentReportTaskCommand extends TaskCommand {
 
     public ExpressionExperimentReportTaskCommand( Boolean all ) {
         super();
+        this.setMaxRuntime( 300 ); /* 5 hours */
         this.all = all;
     }
 
     public ExpressionExperimentReportTaskCommand( ExpressionExperiment expressionExperiment ) {
         super();
-        this.expressionExperiment = expressionExperiment;
-    }
-
-    public ExpressionExperimentReportTaskCommand( String taskId, Boolean all ) {
-        super();
-        this.setTaskId( taskId );
-        this.all = all;
-    }
-
-    /**
-     * @param taskId
-     */
-    public ExpressionExperimentReportTaskCommand( String taskId, ExpressionExperiment expressionExperiment ) {
-        super();
-        this.setTaskId( taskId );
+        this.setMaxRuntime( 5 );
         this.expressionExperiment = expressionExperiment;
     }
 
