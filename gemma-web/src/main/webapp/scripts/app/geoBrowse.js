955cfbd393e0375ea8e3dc96ad0e76ebc6d263d5
ppavlidis <paul@chibi.ubc.ca>
2010-05-08 07:25:07 +0000

    should fix 1934

--- gemma-web/src/main/webapp/scripts/app/geoBrowse.js
+++ gemma-web/src/main/webapp/scripts/app/geoBrowse.js
@@ -4,38 +4,37 @@ function reset(data) {
 
 function handleSuccess(data) {
 	Ext.DomHelper.overwrite("messages", {
-		tag : 'div',
-		html : data
-	});
+				tag : 'div',
+				html : data
+			});
 }
 
 function handleUsabilitySuccess(data, accession) {
 
 	if (data) {
-		Ext.DomHelper.overwrite( accession + "-rating", {
-		tag : 'img',
-		src : '/Gemma/images/icons/thumbsup.png'
-		});
+		Ext.DomHelper.overwrite(accession + "-rating", {
+					tag : 'img',
+					src : '/Gemma/images/icons/thumbsup.png'
+				});
 	} else {
-	Ext.DomHelper.overwrite( accession + "-rating", {
-		tag : 'img',
-		src : '/Gemma/images/icons/thumbsdown-red.png'
-		});
+		Ext.DomHelper.overwrite(accession + "-rating", {
+					tag : 'img',
+					src : '/Gemma/images/icons/thumbsdown-red.png'
+				});
 	}
 
-	
 }
 
 function handleFailure(data, e) {
 	Ext.DomHelper.overwrite("taskId", "");
 	Ext.DomHelper.overwrite("messages", {
-		tag : 'img',
-		src : '/Gemma/images/icons/warning.png'
-	});
+				tag : 'img',
+				src : '/Gemma/images/icons/warning.png'
+			});
 	Ext.DomHelper.append("messages", {
-		tag : 'span',
-		html : "&nbsp;There was an error: " + data
-	});
+				tag : 'span',
+				html : "&nbsp;There was an error: " + data
+			});
 }
 
 function toggleUsability(accession) {
@@ -46,15 +45,15 @@ function toggleUsability(accession) {
 	var errorHandler = handleFailure.createDelegate(this, [], true);
 
 	callParams.push({
-		callback : delegate,
-		errorHandler : errorHandler
-	});
+				callback : delegate,
+				errorHandler : errorHandler
+			});
 
 	GeoBrowserService.toggleUsability.apply(this, callParams);
 	Ext.DomHelper.overwrite(accession + "-rating", {
-		tag : 'img',
-		src : '/Gemma/images/default/tree/loading.gif'
-	});
+				tag : 'img',
+				src : '/Gemma/images/default/tree/loading.gif'
+			});
 }
 
 function showDetails(accession) {
@@ -65,19 +64,19 @@ function showDetails(accession) {
 	var errorHandler = handleFailure.createDelegate(this, [], true);
 
 	callParams.push({
-		callback : delegate,
-		errorHandler : errorHandler
-	});
+				callback : delegate,
+				errorHandler : errorHandler
+			});
 
 	GeoBrowserService.getDetails.apply(this, callParams);
 	Ext.DomHelper.overwrite("messages", {
-		tag : 'img',
-		src : '/Gemma/images/default/tree/loading.gif'
-	});
+				tag : 'img',
+				src : '/Gemma/images/default/tree/loading.gif'
+			});
 	Ext.DomHelper.append("messages", {
-		tag : 'span',
-		html : "&nbsp;Please wait..."
-	});
+				tag : 'span',
+				html : "&nbsp;Please wait..."
+			});
 
 }
 
@@ -89,52 +88,31 @@ function handleLoadSuccess(taskId) {
 					taskId : taskId
 				});
 
-		var w = new Ext.Window({
-					modal : true,
-					closable : false,
-					width : 500,
-					items : [p],
-					buttons : [{
-								id : 'cancel-button',
-								handler : function() {
-									p.cancelJob();
-								},
-								text : 'Cancel'
-							}]
-				});
-
-		p.on('done', function(payload) {
-					// this.onDoneLoading(payload);
-					w.hide('upload-button');
-					w.destroy();
+		p.on('done', function(payload) { 
 					p.destroy();
 					document.location.reload(true); // user will get a warning, but that's okay.
 					Ext.DomHelper.overwrite("messages", "Successfully loaded.");
 				}.createDelegate(this));
 
 		p.on('fail', function(payload) {
-					w.hide('upload-button');
-					w.destroy();
 					p.destroy();
 					handleFailure(payload);
 				});
 
 		p.on('cancel', function() {
-					w.hide();
-					w.destroy();
 					p.destroy();
 					reset();
 				});
 
-		w.show('upload-button');
+		p.show('upload-button');
 
 		p.startProgress();
 	} catch (e) {
 		handleFailure(data, e);
 		return;
 	}
+
 };
- 
 
 function load(accession) {
 
@@ -160,15 +138,15 @@ function load(accession) {
 	var errorHandler = handleFailure.createDelegate(this, [], true);
 
 	callParams.push({
-		callback : delegate,
-		errorHandler : errorHandler
-	});
+				callback : delegate,
+				errorHandler : errorHandler
+			});
 
 	// this should return quickly, with the task id.
 	Ext.DomHelper.overwrite("messages", {
-		tag : 'img',
-		src : '/Gemma/images/default/tree/loading.gif'
-	});
+				tag : 'img',
+				src : '/Gemma/images/default/tree/loading.gif'
+			});
 	Ext.DomHelper.append("messages", "&nbsp;Submitting job...");
 	ExpressionExperimentLoadController.load.apply(this, callParams);
 
