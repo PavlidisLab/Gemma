12a1649f4d2e78d0141ca036a4572bcfe6f81190
ppavlidis <paul@chibi.ubc.ca>
2010-09-02 23:52:22 +0000

    update test file url. old was invalid.

--- gemma-core/src/test/java/ubic/gemma/loader/protein/string/StringProteinFetcherIntegrationTest.java
+++ gemma-core/src/test/java/ubic/gemma/loader/protein/string/StringProteinFetcherIntegrationTest.java
@@ -18,7 +18,6 @@
  */
 package ubic.gemma.loader.protein.string;
 
-
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -30,54 +29,53 @@ import org.junit.Test;
 import ubic.gemma.model.common.description.LocalFile;
 
 /**
- * Test to check that files can be fetched from STRING website. 
- * These files are big and take a few hours to download. So for testing purpose chose 
- * to test downloading small file: species.mappings.v8.2.txt.gz
- * Current version tested with is 8.2 for string.
+ * Test to check that files can be fetched from STRING website. These files are big and take a few hours to download. So
+ * for testing purpose chose to test downloading small file: species.mappings.v8.2.txt.gz Current version tested with is
+ * 8.2 for string.
  * 
  * @author ldonnison
  * @version $Id$
  */
 public class StringProteinFetcherIntegrationTest {
 
-    StringProteinFileFetcher stringProteinLinksDetailedFetcher =null; 
-    String testFileName = "http://string.embl.de/newstring_download/species.mappings.v8.2.txt.gz";
+    StringProteinFileFetcher stringProteinLinksDetailedFetcher = null;
+    // String testFileName = "http://string.embl.de/newstring_download/species.mappings.v8.2.txt.gz";
+    String testFileName = "http://string82.embl.de/newstring_download/species.mappings.v8.2.txt.gz";
     String archiveMethod = "gz";
-    
+
     /**
      * Set up the fetcher passing in the file to test
+     * 
      * @throws java.lang.Exception
      */
     @Before
     public void setUp() throws Exception {
         stringProteinLinksDetailedFetcher = new StringProteinFileFetcher();
-        stringProteinLinksDetailedFetcher.setStringProteinLinksDetailedFileName( testFileName );        
-     
+        stringProteinLinksDetailedFetcher.setStringProteinLinksDetailedFileName( testFileName );
+
     }
-    
-    
+
     /**
      * Test fetching a small file from string in this case a taxon file.
      */
     @Test
-    public void testDownloadProteinAliasFile(){                
-        Collection<LocalFile> localFile = stringProteinLinksDetailedFetcher.fetch(testFileName);
+    public void testDownloadProteinAliasFile() {
+        Collection<LocalFile> localFile = stringProteinLinksDetailedFetcher.fetch( testFileName );
         String outputfilenamegzip = "";
         String outputfilename = "";
-        for(LocalFile file: localFile){
-           outputfilenamegzip = file.getLocalURL().getFile();
-           assertNotNull(outputfilenamegzip);    
-           int lastIndexOf = outputfilenamegzip.lastIndexOf("." + archiveMethod );
-           if(lastIndexOf !=-1){
-               outputfilename =outputfilenamegzip.substring(0, lastIndexOf);
-               //check that the file has been downloaded and unzipped
-              assertTrue(new File(outputfilename).canRead());
-           }else{
-               fail();
-           }         
-        }       
-        
-    }      
-  
-    
+        for ( LocalFile file : localFile ) {
+            outputfilenamegzip = file.getLocalURL().getFile();
+            assertNotNull( outputfilenamegzip );
+            int lastIndexOf = outputfilenamegzip.lastIndexOf( "." + archiveMethod );
+            if ( lastIndexOf != -1 ) {
+                outputfilename = outputfilenamegzip.substring( 0, lastIndexOf );
+                // check that the file has been downloaded and unzipped
+                assertTrue( new File( outputfilename ).canRead() );
+            } else {
+                fail();
+            }
+        }
+
+    }
+
 }
