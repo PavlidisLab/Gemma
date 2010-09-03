441393673dd8c53447e6e1130affde579f0ffefe
ppavlidis <paul@chibi.ubc.ca>
2010-09-03 23:32:08 +0000

    populate the corrected pvalue bin (bug 2013) automatically when the corrected pvalue is set.

---
+++ gemma-mda/src/main/java/ubic/gemma/model/analysis/expression/diff/DifferentialExpressionAnalysisResultImpl.java
@@ -0,0 +1,43 @@
+/*
+ * The Gemma project.
+ * 
+ * Copyright (c) 2006-2010 University of British Columbia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package ubic.gemma.model.analysis.expression.diff;
+
+/**
+ * @see ubic.gemma.model.analysis.expression.diff.DifferentialExpressionAnalysisResult
+ * @version $Id$
+ */
+public abstract class DifferentialExpressionAnalysisResultImpl extends
+        ubic.gemma.model.analysis.expression.diff.DifferentialExpressionAnalysisResult {
+    /**
+     * The serial version UID of this class. Needed for serialization.
+     */
+    private static final long serialVersionUID = 8952834115689524169L;
+
+    @Override
+    public void setCorrectedPvalue( Double correctedPvalue ) {
+        super.setCorrectedPvalue( correctedPvalue );
+
+        /*
+         * See bug 2013. Here we ensure that the bin is always set. The maximum value is 5, representing qvalues better
+         * than 10e-5
+         */
+        this.setCorrectedPValueBin( ( int ) Math.min( 5, -Math.log10( correctedPvalue ) ) );
+    }
+
+}
