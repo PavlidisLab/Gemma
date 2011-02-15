d7cf358bef575a060ae1436e85c96265bcd0bf27
tvrossum <>
2011-02-15 18:04:00 +0000

    made two-colour method actually a two-slice method

--- gemma-web/src/main/webapp/scripts/lib/mini_pie_lib.js
+++ gemma-web/src/main/webapp/scripts/lib/mini_pie_lib.js
@@ -31,36 +31,43 @@ MiniPieLib.drawMiniPie = function(ctx, x, y, size, color, value)
 }
 
 	/**
-	 * Method to draw a two-colour two-piece pie chart in a canvas
+	 * Method to draw a two-colour, two-piece pie chart in a canvas (where sum of pieces can be < total)
 	 * @param {Object} ctx the canvas component to draw in (here, the canvas tag)
 	 * @param {int} x centre of pie on x axis relative to top right of ctx
 	 * @param {int} y centre of pie on y axis relative to top right of ctx
 	 * @param {int} size size of the pie chart
-	 * @param {String} colour one colour for the pie
-	 * @param {String} sliceColour the other colour for the slice, the one associsted with value param 
-	 * @param {int} value corresponds to the size of the slice
-	 * @param {String} outlineColour the colour for the outline of the circle
+	 * @param {String} colourOne colour for slice one of the pie
+	 * @param {int} valueOne size of slice one in degrees
+	 * @param {String} colourTwo colour for slice two of the pie
+	 * @param {int} valueTwo size of slice two in degrees
 	 */
-function drawTwoColourMiniPie(ctx, x, y, size, colour, sliceColour, value, outlineColour){
+function drawTwoColourMiniPie(ctx, x, y, size, colourOne, valueOne, colourTwo, valueTwo){
 	    ctx.save();
-	    ctx.fillStyle = colour;
-	    ctx.strokeStyle = colour;
+	    ctx.fillStyle = '#E0E0E0';
 	    ctx.moveTo(x, y);
 	    // draw circle
 	    ctx.beginPath();
 	    ctx.arc(x,y,size/2, Math.PI*3/2 , Math.PI*3/2+(Math.PI*2),false);
 	    ctx.fill();
-	    // draw slice
-	    ctx.fillStyle = sliceColour;
+	    // draw slice one
+	    ctx.fillStyle = colourOne;
 	    ctx.beginPath();
 	    ctx.moveTo(x, y);        
-	    ctx.arc(x,y,size/2, Math.PI*3/2 , Math.PI*3/2+(Math.PI/180)*value,false);
+	    ctx.arc(x,y,size/2, Math.PI*3/2 , Math.PI*3/2+(Math.PI/180)*valueOne,false);
+	    ctx.lineTo(x,y);
+	    ctx.fill();
+	    // draw slice two
+	    ctx.fillStyle = colourTwo;
+	    ctx.beginPath();
+	    ctx.moveTo(x, y);        
+	    ctx.arc(x,y,size/2, Math.PI*3/2+(Math.PI/180)*valueOne ,
+	    	Math.PI*3/2+(Math.PI/180)*valueOne+(Math.PI/180)*valueTwo,false);
 	    ctx.lineTo(x,y);
 	    ctx.fill();
 		// draw circle outline
 	    ctx.beginPath();
 	    ctx.arc(x,y,size/2, Math.PI*3/2 , Math.PI*3/2+(Math.PI*2),false);
-	    ctx.strokeStyle = outlineColour;
+	    ctx.strokeStyle = 'black';
 	    ctx.stroke();
 	    
 	    ctx.restore();			
