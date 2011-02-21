5c525bae5ad9ec6ce6370308a3c567c50f7b1b34
ppavlidis <paul@chibi.ubc.ca>
2011-02-21 05:44:06 +0000

    style

--- gemma-core/src/main/java/ubic/gemma/loader/protein/biomart/BiomartEnsembleNcbiParser.java
+++ gemma-core/src/main/java/ubic/gemma/loader/protein/biomart/BiomartEnsembleNcbiParser.java
@@ -36,8 +36,7 @@ import ubic.gemma.model.genome.Taxon;
  * LineMapParser which means that after parsing a Map of BioMartEnsembleNcbi value objects are returned keyed on ensembl
  * peptide id.
  * <p>
- * Parsing is triggered by calling super class method parse which then calls child method parse oneline. 
- * 
+ * Parsing is triggered by calling super class method parse which then calls child method parse oneline.
  * 
  * @author ldonnison
  * @version $Id$
@@ -63,7 +62,9 @@ public class BiomartEnsembleNcbiParser extends LineMapParser<String, BioMartEnse
     }
 
     /**
-     * Method to parse one biomart line, note that there is a many to many relationship between ensemble ids and entrez gene ids.
+     * Method to parse one biomart line, note that there is a many to many relationship between ensemble ids and entrez
+     * gene ids.
+     * 
      * @return BioMartEnsembleNcbi Value object representing the line parsed
      */
     @Override
@@ -74,58 +75,59 @@ public class BiomartEnsembleNcbiParser extends LineMapParser<String, BioMartEnse
         if ( line.startsWith( this.bioMartHeaderFields[0] ) || line.isEmpty() ) {
             return null;
         }
-        //split the line into the attributes
+        // split the line into the attributes
         String[] fields = StringUtils.splitPreserveAllTokens( line, FIELD_DELIM );
-        //validate that correct format
+        // validate that correct format
         if ( fields.length != bioMartFieldsPerRow ) {
-            throw new FileFormatException( "Line + " + line + " is not in the right format: has " + fields.length
-                    + " fields, expected " + bioMartFieldsPerRow );
+            throw new FileFormatException( "Line is not in the right format: has " + fields.length
+                    + " fields, expected " + bioMartFieldsPerRow + ": " );
         }
-        //create the object
+        // create the object
         try {
-            return createBioMartEnsembleNcbi(fields);
+            return createBioMartEnsembleNcbi( fields );
 
         } catch ( NumberFormatException e ) {
             throw new FileFormatException( e );
-        }
-        catch ( FileFormatException e ) {
+        } catch ( FileFormatException e ) {
             throw new RuntimeException( e );
-        }        
+        }
 
     }
+
     /**
-     * Given an array of strings representing the line to parse then create a BioMartEnsembleNcbi value object
-     * with some validation. That is if a duplicate record keyed on peptide id is found then that means that it maps to more than one 
-     * entrez gene id. As such check that the duplicate and currently processed record share the same ensemble gene id as a sanity check.
-     * Add the entrez gene to the existing collection of entrez genes.
+     * Given an array of strings representing the line to parse then create a BioMartEnsembleNcbi value object with some
+     * validation. That is if a duplicate record keyed on peptide id is found then that means that it maps to more than
+     * one entrez gene id. As such check that the duplicate and currently processed record share the same ensemble gene
+     * id as a sanity check. Add the entrez gene to the existing collection of entrez genes.
      * 
      * @param fields Parsed line split on delimiter
      * @return BioMartEnsembleNcbi value object
      * @throws NumberFormatException Parsing a number that is not one
-     * @throws FileFormatException Validation than when a duplicate record is found then the peptide id is the same the ensemble gene id should be the same.
+     * @throws FileFormatException Validation than when a duplicate record is found then the peptide id is the same the
+     *         ensemble gene id should be the same.
      */
-    public BioMartEnsembleNcbi createBioMartEnsembleNcbi(String[] fields) throws NumberFormatException,FileFormatException {
+    public BioMartEnsembleNcbi createBioMartEnsembleNcbi( String[] fields ) throws NumberFormatException,
+            FileFormatException {
         BioMartEnsembleNcbi bioMartEnsembleNcbi = new BioMartEnsembleNcbi();
         String entrezGene = fields[2].trim();
         String ensemblProteinId = fields[3].trim();
-        //if there is no entrezgene skip as that is what we want
-        if(entrezGene ==null || entrezGene.isEmpty()){
+        // if there is no entrezgene skip as that is what we want
+        if ( entrezGene == null || entrezGene.isEmpty() ) {
             log.debug( ensemblProteinId + " has no entrez gene mapping" );
-            return bioMartEnsembleNcbi;            
-        }        
-     
+            return bioMartEnsembleNcbi;
+        }
+
         String ensemblGeneID = fields[0].trim();
         bioMartEnsembleNcbi.setSpecies_ncbi_taxon_id( taxon.getNcbiId() );
         bioMartEnsembleNcbi.setEnsembl_gene_id( ensemblGeneID );
         bioMartEnsembleNcbi.setEnsembl_transcript_id( fields[1] );
         bioMartEnsembleNcbi.setEnsembl_peptide_id( ensemblProteinId );
-       
+
         if ( !bioMartHeaderFields[4].isEmpty() && fields[4] != null ) {
             // only humans should have this field
             bioMartEnsembleNcbi.setHgnc_id( fields[4] );
         }
-      
-        
+
         // Ensembl ids can map to multiple entrez genes so we maintain a collection of entrezgenes
         if ( !containsKey( ensemblProteinId ) ) {
             bioMartEnsembleNcbi.getEntrezgenes().add( entrezGene );
@@ -138,16 +140,16 @@ public class BiomartEnsembleNcbiParser extends LineMapParser<String, BioMartEnse
                 this.get( ensemblProteinId ).getEntrezgenes().add( entrezGene );
                 log.debug( ensemblProteinId + "added gene to duplicate  " );
             } else {
-                throw new FileFormatException( "A duplicate ensemblProteinId has been found " + ensemblProteinId + "but it does not match with the exisiting objects gene id " + ensemblGeneID);
+                throw new FileFormatException( "A duplicate ensemblProteinId has been found " + ensemblProteinId
+                        + "but it does not match with the exisiting objects gene id " + ensemblGeneID );
             }
         }
         return bioMartEnsembleNcbi;
     }
-    
 
     /**
      * Based on what attributes were set on the original file then calculate how many columns should be in file.
-     *  
+     * 
      * @return Number of columns in file.
      */
     public int getBioMartFieldsPerRow() {
