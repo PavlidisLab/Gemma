c4896f4aff858d1d98fd14a2f1bf5d06da1eb9a0
cmcdonald <>
2011-03-15 22:41:54 +0000

    Classes for session-based geneset management

---
+++ gemma-web/src/main/java/ubic/gemma/web/session/GeneSetListContainer.java
@@ -0,0 +1,123 @@
+package ubic.gemma.web.session;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import ubic.gemma.web.controller.common.auditAndSecurity.GeneSetValueObject;
+
+public class GeneSetListContainer implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2163440335723257830L;
+
+	static final int MAX_SIZE = 10;
+	
+	Integer largestSessionId=0;
+	
+	ArrayList<GeneSetValueObject> geneSetList;
+	
+	public GeneSetListContainer(){
+		geneSetList = new ArrayList<GeneSetValueObject>();
+		
+	}
+	
+	public Integer incrementAndGetLargestSessionId(){
+		largestSessionId = largestSessionId +1;
+		
+		//unique sessionId for each entry in the user's session(doubt that a user will have over 100000 geneset session entries however
+		// large gaps can occur between sessionIds because of 'fake' sessionIds assigned to db backed sets for the front end store-
+		// eg. page reloads within a user's session lifecycle will create these gaps
+		//still I believe 100000 provides a large enough range to avoid conflicts
+		if (largestSessionId>100000){
+			largestSessionId = 0;
+		}
+		
+		return largestSessionId;
+	}
+	
+	public GeneSetValueObject addGeneSet(GeneSetValueObject gsvo){		
+		
+		
+		boolean setExists=false;
+		
+		
+		//This case shouldn't happen very often (or at all)
+		if (gsvo.getSessionId()!=null){
+			for (int i =0 ; i<geneSetList.size(); i++){
+			
+				Integer sid = geneSetList.get(i).getSessionId();
+			
+				if (sid!=null && sid.equals(gsvo.getSessionId())){
+					geneSetList.remove(i);
+					geneSetList.add(i, gsvo);
+					setExists=true;
+					break;				
+				}
+			
+			}
+		}
+		
+		if (!setExists){			
+			
+			gsvo.setSessionId(incrementAndGetLargestSessionId());
+			gsvo.setSession(true);//this line may be redundant
+			
+			geneSetList.add(gsvo);
+			if (geneSetList.size()>MAX_SIZE){
+				geneSetList.remove(0);
+			}
+		}
+		
+		return gsvo;
+		
+	}
+	
+	public void removeGeneSet(GeneSetValueObject gsvo){		
+		
+		if (gsvo.getSessionId()!=null){
+		
+			for (int i =0 ; i<geneSetList.size(); i++){
+				
+				if (geneSetList.get(i).getSessionId().equals(gsvo.getSessionId())){
+					geneSetList.remove(i);
+					break;
+				}
+				
+			}
+			
+		}
+		
+	}
+	
+	public void updateGeneSet(GeneSetValueObject gsvo){		
+		
+		if (gsvo.getSessionId()!=null){
+		
+			for (int i =0 ; i<geneSetList.size(); i++){
+				
+				if (geneSetList.get(i).getSessionId().equals(gsvo.getSessionId())){
+					geneSetList.remove(i);
+					geneSetList.add(i, gsvo);
+					break;
+				}
+				
+			}
+			
+		}
+		
+	}
+	
+	public Collection<GeneSetValueObject> getRecentGeneSets(){
+		
+		return geneSetList;
+		
+	}
+	
+	
+
+}
---
+++ gemma-web/src/main/java/ubic/gemma/web/session/SessionListManager.java
@@ -0,0 +1,70 @@
+package ubic.gemma.web.session;
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ubic.gemma.web.controller.common.auditAndSecurity.GeneSetValueObject;
+import ubic.gemma.web.controller.expression.experiment.ExpressionExperimentSetValueObject;
+
+
+@Service
+public class SessionListManager{
+	
+	@Autowired
+	GeneSetListContainer geneSetList;
+	
+	@Autowired
+	ExperimentSetListContainer experimentSetList;
+	
+	public Collection<GeneSetValueObject> getRecentGeneSets(){
+		return geneSetList.getRecentGeneSets();
+	}
+	
+	public GeneSetValueObject addGeneSet(GeneSetValueObject gsvo){
+		
+		return geneSetList.addGeneSet(gsvo);
+		
+	}
+	
+	public void removeGeneSet(GeneSetValueObject gsvo){
+		
+		geneSetList.removeGeneSet(gsvo);
+		
+	}
+	
+	public void updateGeneSet(GeneSetValueObject gsvo){
+		
+		geneSetList.updateGeneSet(gsvo);
+		
+	}
+	
+	//this gives result(from the DB) unique Session Ids(used by the front end store)
+	//ugly and hackish
+	public void setUniqueGeneSetStoreIds(Collection<GeneSetValueObject> result, Collection<GeneSetValueObject> sessionResult){		
+        
+        //give db genesets a unique sessionId so that the javascript widget plays nice
+        for (GeneSetValueObject gsvo: result){      	
+        	gsvo.setSessionId(geneSetList.incrementAndGetLargestSessionId());        	
+        	
+        }
+	}
+	
+	public Integer incrementAndGetLargestGeneSetSessionId(){
+		return geneSetList.incrementAndGetLargestSessionId();
+	}
+	
+	
+	public Collection<ExpressionExperimentSetValueObject> getRecentExperimentSets(){
+		return experimentSetList.getRecentExperimentSets();
+	}
+	
+	public void addExperimentSet(ExpressionExperimentSetValueObject eesvo){
+		
+		experimentSetList.addExperimentSet(eesvo);
+		
+	}
+	
+	
+
+}
