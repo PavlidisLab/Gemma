568ef93203910443ca007d01f912361b8a41e11e
cmcdonald <>
2011-03-23 21:18:44 +0000

    Store that is backed by session and db for expression experiment sets

---
+++ gemma-web/src/main/webapp/scripts/ajax/entities/UserSessionDatasetGroupStore.js
@@ -0,0 +1,141 @@
+Ext.namespace('Gemma');
+/**
+ * Holds the list of available db backed and session backed ExpressionExperiment sets from. This is a separate class so it can be more easily shared
+ * between components. It also knows which record is selected, unlike the default store.
+ * 
+ * @class Gemma.UserSessionDatasetGroupStore
+ * @extends Ext.data.Store
+ * @see DatasetGroupCombo
+ */
+Gemma.UserSessionDatasetGroupStore = function(config) {
+
+	/*
+	 * Leave this here so copies of records can be constructed.
+	 */
+	this.record = Ext.data.Record.create([{
+				name : "id",
+				type : "int"
+			},{
+				name : "sessionId",
+				type : "int"
+			},{
+				name : "name",
+				type : "string"
+			}, {
+				name : "description",
+				type : "string"
+			}, {
+				name : "numExperiments",
+				type : "int"
+			}, {
+				name : "expressionExperimentIds"
+			}, {
+				name : "taxonId",
+				type : "int"
+			}, {
+				name : "taxonName"
+			}, {
+				name : "modifiable",
+				type : 'boolean'
+			}, {
+				name : "publik",
+				type : 'boolean'
+			}, {
+				name : "shared",
+				type : 'boolean'
+			}, {
+				name : "currentUserHasWritePermission",
+				type : 'boolean'
+			}, {
+				name : "session",
+				type : 'boolean'
+			}]);
+
+	// todo replace with JsonReader.
+	this.reader = new Ext.data.ListRangeReader({
+				id : "sessionId"
+			}, this.record),
+
+	Gemma.UserSessionDatasetGroupStore.superclass.constructor.call(this, config);
+
+};
+
+/**
+ * 
+ * @class Gemma.DatasetGroupStore
+ * @extends Ext.data.Store
+ */
+Ext.extend(Gemma.UserSessionDatasetGroupStore, Ext.data.Store, {
+
+			autoLoad : true,
+			autoSave : false,
+			selected : null,
+
+			proxy : new Ext.data.DWRProxy({
+						apiActionToHandlerMap : {
+							read : {
+								dwrFunction : ExpressionExperimentSetController.loadAllUserAndSessionGroups
+							},
+							create : {
+								dwrFunction : ExpressionExperimentSetController.addUserAndSessionGroups
+							},
+							update : {
+								dwrFunction : ExpressionExperimentSetController.updateUserAndSessionGroups
+							},
+							destroy : {
+								dwrFunction : ExpressionExperimentSetController.removeUserAndSessionGroups
+							}
+						}
+					}),
+
+			writer : new Ext.data.JsonWriter({
+						writeAllFields : true
+					}),
+
+			getSelected : function() {
+				return this.selected;
+			},
+
+			setSelected : function(rec) {
+				this.previousSelection = this.getSelected();
+				if (rec) {
+					this.selected = rec;
+				}
+			},
+
+			getPreviousSelection : function() {
+				return this.previousSelection;
+			},
+
+			clearSelected : function() {
+				this.selected = null;
+				delete this.selected;
+			},
+
+			listeners : {
+				write : function(store, action, result, res, rs) {
+					Ext.Msg.show({
+								title : "Saved",
+								msg : "Changes were saved",
+								icon : Ext.MessageBox.INFO
+							});
+				},
+				exception : function(proxy, type, action, options, res, arg) {
+					if (type === 'remote') {
+						Ext.Msg.show({
+									title : 'Error',
+									msg : res.message,
+									icon : Ext.MessageBox.ERROR
+								});
+					} else {
+						Ext.Msg.show({
+									title : 'Error',
+									msg : arg.message ? arg.message : arg,
+									icon : Ext.MessageBox.ERROR
+								});
+					}
+				}
+
+			}
+
+		});
