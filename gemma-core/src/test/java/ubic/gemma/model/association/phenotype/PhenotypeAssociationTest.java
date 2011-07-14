b010ec76b51e365861d36bb26573ee26b494632b
nicolas <>
2011-07-14 18:21:30 +0000

    Moved test file to core rather than mda. Since build order is mda, core, web and test file need core.

---
+++ gemma-core/src/test/java/ubic/gemma/model/association/phenotype/PhenotypeAssociationTest.java
@@ -0,0 +1,40 @@
+package ubic.gemma.model.association.phenotype;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ubic.gemma.testing.BaseSpringContextTest;
+
+public class PhenotypeAssociationTest extends BaseSpringContextTest {
+
+    @Autowired
+    private UrlEvidenceDao eviDao;
+
+    @Test
+    public void testUrlEvidence() {
+
+        // create
+        UrlEvidence urlEvidence = new UrlEvidenceImpl();
+        urlEvidence.setDescription( "testDescription" );
+        urlEvidence.setName( "testname" );
+        urlEvidence.setUrl( "www.test.com" );
+        UrlEvidence entityReturn = eviDao.create( urlEvidence );
+        assertNotNull( entityReturn.getId() );
+
+        // update
+        urlEvidence.setUrl( "www.testupdate.com" );
+        eviDao.update( urlEvidence );
+
+        // load
+        UrlEvidence urlEvidenceLoad = eviDao.load( entityReturn.getId() );
+        assertNotNull( urlEvidenceLoad );
+        assertTrue( urlEvidenceLoad.getUrl().equals( "www.testupdate.com" ) );
+
+        // remove
+        eviDao.remove( entityReturn.getId() );
+        assertNull( eviDao.load( entityReturn.getId() ) );
+    }
+
+}
