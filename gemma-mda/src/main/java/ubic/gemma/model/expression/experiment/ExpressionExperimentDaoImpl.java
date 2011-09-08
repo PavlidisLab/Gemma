1977d2be227b74a34f7eeda1a0ffc27dbd0821de
tvrossum <>
2011-09-08 18:29:00 +0000

    bug 2242: temporary move of constants variables to fix build

--- gemma-mda/src/main/java/ubic/gemma/model/expression/experiment/ExpressionExperimentDaoImpl.java
+++ gemma-mda/src/main/java/ubic/gemma/model/expression/experiment/ExpressionExperimentDaoImpl.java
@@ -47,7 +47,6 @@ import org.hibernate.type.LongType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import ubic.gemma.analysis.util.ExperimentalDesignUtils;
 import ubic.gemma.model.common.auditAndSecurity.AuditEvent;
 import ubic.gemma.model.common.auditAndSecurity.eventType.AuditEventType;
 import ubic.gemma.model.common.description.BibliographicReference;
