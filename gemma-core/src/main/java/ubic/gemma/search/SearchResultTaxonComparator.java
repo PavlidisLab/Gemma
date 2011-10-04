a4bcd6f42d6744bf80fedb42f309c6d080aa1867
tvrossum <>
2011-10-04 18:34:16 +0000

    Refactored searchExperimentsAndExperimentGroups method and re-enabled prompting of large sets like "All human". These were removed from prompting because the old the diff ex visualizer couldn't handle them, the new version should be able to.

---
+++ gemma-core/src/main/java/ubic/gemma/search/SearchResultTaxonComparator.java
@@ -0,0 +1,30 @@
+/*
+ * The Gemma project
+ * 
+ * Copyright (c) 2007 University of British Columbia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package ubic.gemma.search;
+
+import java.util.Comparator;
+
+
+public class SearchResultTaxonComparator implements Comparator<SearchResultDisplayObject>{
+    
+    @Override
+    public int compare(SearchResultDisplayObject o1, SearchResultDisplayObject o2) {
+        return (o1.getTaxonId()<o2.getTaxonId() ? -1 : (o1.getTaxonId()==o2.getTaxonId() ? o1.compareTo( o2 ) : 1));
+    }
+}
