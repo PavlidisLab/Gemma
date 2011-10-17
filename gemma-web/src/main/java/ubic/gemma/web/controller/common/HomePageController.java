6b212dd648b43b0caddf0dc9a87684a9ac8925c3
tvrossum <>
2011-10-17 18:04:07 +0000

    Removed code used to support old front-page data summary table

--- gemma-web/src/main/java/ubic/gemma/web/controller/common/HomePageController.java
+++ gemma-web/src/main/java/ubic/gemma/web/controller/common/HomePageController.java
@@ -18,40 +18,22 @@
  */
 package ubic.gemma.web.controller.common;
 
-import java.text.DateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collection;
 import java.util.Comparator;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.Map.Entry;
 
-import net.sf.json.JSONObject;
-
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import ubic.gemma.analysis.report.WhatsNew;
-import ubic.gemma.analysis.report.WhatsNewService;
-import ubic.gemma.model.expression.arrayDesign.ArrayDesign;
-import ubic.gemma.model.expression.arrayDesign.ArrayDesignService;
-import ubic.gemma.model.expression.bioAssay.BioAssayService;
-import ubic.gemma.model.expression.experiment.ExpressionExperiment;
 import ubic.gemma.model.expression.experiment.ExpressionExperimentService;
 import ubic.gemma.model.genome.Taxon;
-import ubic.gemma.model.genome.TaxonService;
 import ubic.gemma.web.controller.WebConstants;
 
 /**
@@ -67,15 +49,6 @@ public class HomePageController {
 
     @Autowired
     private ExpressionExperimentService expressionExperimentService;
-
-    @Autowired
-    private BioAssayService bioAssayService;
-
-    @Autowired
-    private ArrayDesignService arrayDesignService;
-
-    @Autowired
-    private WhatsNewService whatsNewService;
 
     private ModelAndView mav = new ModelAndView();
 
@@ -98,31 +71,15 @@ public class HomePageController {
          * Note that this needs to be fast. The queries involved almost always result in a O(1) cache hit. Don't add new
          * functionality here without considering that.
          */
-        updateCounts();
+        //updateCounts();
+        getCountsForTaxonPieChart();
         return mav;
     }
 
-    /**
-     * FIXME : I wish we could use @Scheduled to update this kind of thing periodically.
-     * 
-     * @param
-     */
-    public void updateCounts() {
-        Map<String, Long> stats = new HashMap<String, Long>();
+    public void getCountsForTaxonPieChart() {
 
-        long bioAssayCount = bioAssayService.countAll(); 
-        long arrayDesignCount = arrayDesignService.countAll();
         Map<Taxon, Long> unsortedEEsPerTaxon = expressionExperimentService.getPerTaxonCount();
-        
-        /*
-         * Sort taxa by name.
-         */
-        TreeMap<Taxon, Long> eesPerTaxon = new TreeMap<Taxon, Long>( new Comparator<Taxon>() {
-            @Override
-            public int compare( Taxon o1, Taxon o2 ) {
-                return o1.getScientificName().compareTo( o2.getScientificName() );
-            }
-        } );
+
         /*
          * Sort taxa by count.
          */
@@ -137,81 +94,8 @@ public class HomePageController {
           }  );
         eesPerTaxonValueSorted.addAll( unsortedEEsPerTaxon.entrySet() );
         
-        eesPerTaxon.putAll( unsortedEEsPerTaxon );
-        
-        long expressionExperimentCount = 0;
-        long otherTaxaEECount = 0;
-        for ( Iterator<Taxon> it = eesPerTaxon.keySet().iterator(); it.hasNext(); ) {
-            Taxon t = it.next();
-            Long c = eesPerTaxon.get( t );
-            // TODO problem with this is we want to make a link to them.
-            if ( c < 10 ) {
-                // temporary, hide 'uncommon' taxa from this table. See bug 2052
-                otherTaxaEECount += c;
-                it.remove();
-            }
-            expressionExperimentCount += c;
-        }
-        if ( otherTaxaEECount > 0 ) {
-            // eesPerTaxon.put( otherTaxa, otherTaxaEECount );
-        }
+        long expressionExperimentCount = expressionExperimentService.countAll();
 
-        WhatsNew wn = whatsNewService.retrieveReport();
-                
-        if(wn == null){
-        	Calendar c = Calendar.getInstance();
-        	Date date = c.getTime();
-        	date = DateUtils.addWeeks( date, -1 );
-        	wn = whatsNewService.getReport( date );
-        }
-        if(wn != null){
-        	 //Get count for new assays
-        	int newAssayCount = wn.getNewAssayCount();
-                
-        	int newExpressionExperimentCount = (wn.getNewExpressionExperiments()!=null)?
-        	                                            wn.getNewExpressionExperiments().size():0;
-        	int updatedExpressionExperimentCount = (wn.getUpdatedExpressionExperiments() != null)?
-        	                                            wn.getUpdatedExpressionExperiments().size():0;
-           
-            /*Store counts for new and updated experiments by taxonId*/
-            /* new front page design doesn't need these structures
-        	Map<Taxon, Long> newEEsPerTaxon = wn.getNewEECountPerTaxon();
-            Map<Taxon, Long> updatedEEsPerTaxon = wn.getUpdatedEECountPerTaxon();
-            mav.addObject( "newPerTaxonCount", newEEsPerTaxon );
-            mav.addObject( "updatedPerTaxonCount", updatedEEsPerTaxon );
-            */
-        	                                            
-        	//Get count for new and updated array designs
-            int newArrayCount = (wn.getNewArrayDesigns()!=null)? wn.getNewArrayDesigns().size():0;
-            int updatedArrayCount = (wn.getUpdatedArrayDesigns()!=null)? wn.getUpdatedArrayDesigns().size():0;
-        	
-        	boolean drawNewColumn = (newExpressionExperimentCount > 0 || newArrayCount > 0 || newAssayCount > 0)? true:false;
-        	boolean drawUpdatedColumn = (updatedExpressionExperimentCount > 0 || updatedArrayCount > 0 )? true:false;
-        	String date = (wn.getDate() != null)?DateFormat.getDateInstance(DateFormat.LONG).format(wn.getDate()): "";
-        	date = date.replace( '-', ' ' );
-
-        	mav.addObject( "updateDate",  date);
-        	mav.addObject( "drawNewColumn", drawNewColumn);
-        	mav.addObject( "drawUpdatedColumn", drawUpdatedColumn);
-        	if(newAssayCount != 0) stats.put( "newBioAssayCount", new Long(newAssayCount) );
-        	if(newArrayCount != 0) stats.put( "newArrayDesignCount", new Long(newArrayCount));
-        	if(updatedArrayCount != 0) stats.put( "updatedArrayDesignCount", new Long(updatedArrayCount));
-            if(newExpressionExperimentCount != 0) mav.addObject( "newExpressionExperimentCount",  newExpressionExperimentCount);
-            if(updatedExpressionExperimentCount != 0) mav.addObject( "updatedExpressionExperimentCount", updatedExpressionExperimentCount);
-
-        }
-               
-        stats.put( "bioAssayCount", bioAssayCount );
-        stats.put( "arrayDesignCount", arrayDesignCount );
-        
-        mav.addObject( "stats", stats );
-        mav.addObject( "taxonCount", eesPerTaxon );
-        mav.addObject( "expressionExperimentCount", expressionExperimentCount );
-        if ( wn != null && wn.getDate() != null ) {
-            mav.addObject( "whatsNew", wn );
-        }
-        
-        
         double groupBelow = 0.1; // if a taxa has less then this percent of total count, group into 'other'
         String googleData = encodeDataForGoogle(eesPerTaxonValueSorted.descendingSet(), expressionExperimentCount, groupBelow);
         List<String> googleLabelsColls = new ArrayList<String>();
@@ -234,6 +118,7 @@ public class HomePageController {
         mav.addObject( "googleLabels", googleLabels );
         
     }
+    
     private String encodeDataForGoogle(Set<Entry<Taxon, Long>> eesPerTaxonValueSorted, long maxValue, double groupBelow){
         char[] simpleEncoding = new String("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789").toCharArray();
         
