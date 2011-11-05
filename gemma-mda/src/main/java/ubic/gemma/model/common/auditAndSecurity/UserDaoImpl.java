d849e29b43c5331ebf58d18a9f163ef13a213ca6
ppavlidis <paul@chibi.ubc.ca>
2011-11-05 06:34:37 +0000

    fix out of date license headers

--- gemma-mda/src/main/java/ubic/gemma/model/common/auditAndSecurity/UserDaoImpl.java
+++ gemma-mda/src/main/java/ubic/gemma/model/common/auditAndSecurity/UserDaoImpl.java
@@ -1,22 +1,16 @@
 /*
- * The Gemma project.
+ * The Gemma project
  * 
- * Copyright (c) 2006 University of British Columbia
+ * Copyright (c) 2011 University of British Columbia
  * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package ubic.gemma.model.common.auditAndSecurity;
 
@@ -43,6 +37,7 @@ public class UserDaoImpl extends ubic.gemma.model.common.auditAndSecurity.UserDa
 
     /*
      * (non-Javadoc)
+     * 
      * @see ubic.gemma.model.common.auditAndSecurity.UserDao#addAuthority(ubic.gemma.model.common.auditAndSecurity.User,
      * java.lang.String)
      */
@@ -53,6 +48,7 @@ public class UserDaoImpl extends ubic.gemma.model.common.auditAndSecurity.UserDa
 
     /*
      * (non-Javadoc)
+     * 
      * @see
      * ubic.gemma.model.common.auditAndSecurity.UserDao#changePassword(ubic.gemma.model.common.auditAndSecurity.User,
      * java.lang.String)
@@ -65,6 +61,7 @@ public class UserDaoImpl extends ubic.gemma.model.common.auditAndSecurity.UserDa
 
     /*
      * (non-Javadoc)
+     * 
      * @see ubic.gemma.model.common.auditAndSecurity.UserDaoBase#find(ubic.gemma.model.common.auditAndSecurity.user)
      */
     @SuppressWarnings("unchecked")
@@ -99,6 +96,7 @@ public class UserDaoImpl extends ubic.gemma.model.common.auditAndSecurity.UserDa
 
     /*
      * (non-Javadoc)
+     * 
      * @see
      * ubic.gemma.model.common.auditAndSecurity.UserDao#loadGroupAuthorities(ubic.gemma.model.common.auditAndSecurity
      * .User)
@@ -107,8 +105,7 @@ public class UserDaoImpl extends ubic.gemma.model.common.auditAndSecurity.UserDa
     public Collection<GroupAuthority> loadGroupAuthorities( User u ) {
 
         return this.getHibernateTemplate().findByNamedParam(
-                "select gr.authorities from UserGroupImpl gr inner join gr.groupMembers m where m = :user ",
-                "user", u );
+                "select gr.authorities from UserGroupImpl gr inner join gr.groupMembers m where m = :user ", "user", u );
 
     }
 
