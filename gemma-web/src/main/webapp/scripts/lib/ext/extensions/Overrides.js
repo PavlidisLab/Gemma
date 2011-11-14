4ed31fa4d37a2f6dd4b83c3f0dc6131ebdc9ba61
tvrossum <>
2011-11-14 18:12:07 +0000

    fix for sliders in IE9

--- gemma-web/src/main/webapp/scripts/lib/ext/extensions/Overrides.js
+++ gemma-web/src/main/webapp/scripts/lib/ext/extensions/Overrides.js
@@ -1,3 +1,18 @@
+/**
+ * This file contains various overrides for ext 3.4 elements
+ * 
+ * Includes overrides to:
+ * - textArea to allow control of word wrapping
+ * - Ext.data.Store to handle multiple independent filters
+ * - ColumnModel to allow adding and removing columns
+ * - vbox to handle collapsible elements
+ * - sliders to make them work in IE9
+ * 
+ * 
+ */
+
+
+
 
 // Override textArea to allow control of word wrapping
 // just adds a wordWrap config field to textArea
@@ -469,3 +484,32 @@ Ext.override(Ext.layout.VBoxLayout, {
     }
 });
 // end of vbox-collasible override
+
+
+/**
+ * override to make sliders work in IE9
+ * from http://www.sencha.com/forum/showthread.php?141254-Ext.Slider-not-working-properly-in-IE9
+ */
+Ext.override(Ext.dd.DragTracker, {
+    onMouseMove: function (e, target) {
+        var isIE9 = Ext.isIE && (/msie 9/.test(navigator.userAgent.toLowerCase())) && document.documentMode != 6;
+        if (this.active && Ext.isIE && !isIE9 && !e.browserEvent.button) {
+            e.preventDefault();
+            this.onMouseUp(e);
+            return;
+        }
+        e.preventDefault();
+        var xy = e.getXY(), s = this.startXY;
+        this.lastXY = xy;
+        if (!this.active) {
+            if (Math.abs(s[0] - xy[0]) > this.tolerance || Math.abs(s[1] - xy[1]) > this.tolerance) {
+                this.triggerStart(e);
+            } else {
+                return;
+            }
+        }
+        this.fireEvent('mousemove', this, e);
+        this.onDrag(e);
+        this.fireEvent('drag', this, e);
+    }
+});
