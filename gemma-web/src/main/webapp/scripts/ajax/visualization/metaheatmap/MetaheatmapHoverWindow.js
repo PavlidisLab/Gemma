301797d7b6184392504abf8015fc38830dcc5bb6
tvrossum <>
2011-11-18 17:59:03 +0000

    tiny spacing tweaks

--- gemma-web/src/main/webapp/scripts/ajax/visualization/metaheatmap/MetaheatmapHoverWindow.js
+++ gemma-web/src/main/webapp/scripts/ajax/visualization/metaheatmap/MetaheatmapHoverWindow.js
@@ -30,8 +30,8 @@ Gemma.Metaheatmap.HoverWindow = Ext.extend ( Ext.Window, {
 				'<tpl for=".">',
 				'<tpl if="type==\'condition\'">',   //condition
 				'<b>Experiment</b>: {datasetShortName}, {datasetName}<br><br>',
-				'<b>Factor</b>:{factorCategory} - {factorDescription}<br><br> ',
-				'<b>Contrast</b>:{contrastFactorValue} vs {baselineFactorValue}<br><br> ',				
+				'<b>Factor</b>: {factorCategory} - {factorDescription}<br><br> ',
+				'<b>Contrast</b>: {contrastFactorValue} vs {baselineFactorValue}<br><br> ',				
 				'<b>Specificity</b>: {specificityPercent}% of probes were differentially expressed under this condition ({numberDiffExpressedProbes} out of {numberOfProbesOnArray})<br><br> ',
 				'</tpl>',
 				'<tpl if="type==\'minipie\'">',     //minipie
@@ -44,15 +44,15 @@ Gemma.Metaheatmap.HoverWindow = Ext.extend ( Ext.Window, {
 //				'<tpl if="type==\'contrastCell\'">',  //contrast
 //				'<b>Gene</b>: {geneSymbol} {geneFullName}<br><br> ',
 //				'<b>Experiment</b>: {datasetShortName}, {datasetName}<br><br>',
-//				'<b>Factor</b>:{factorCategory} - {factorDescription}<br><br> ', 
+//				'<b>Factor</b>: {factorCategory} - {factorDescription}<br><br> ', 
 //				'<b>Log2 fold change</b>: {foldChange:sciNotation}<br>', 
 //				'<b>pValue</b>: {contrastPvalue:sciNotation} <br>',
 //				'</tpl>',
 				'<tpl if="type==\'cell\'">',		 //cell
 				'<b>Gene</b>: {geneSymbol} {geneFullName}<br><br> ',
 				'<b>Experiment</b>: {datasetShortName}, {datasetName}<br><br>',
-				'<b>Factor</b>:{factorCategory} - {factorDescription}<br><br> ',
-				'<b>Contrast</b>:{contrastFactorValue} vs {baselineFactorValue}<br><br> ',				
+				'<b>Factor</b>: {factorCategory} - {factorDescription}<br><br> ',
+				'<b>Contrast</b>: {contrastFactorValue} vs {baselineFactorValue}<br><br> ',				
 				'<b>p-value</b>: {pvalue:sciNotation}<br><br>',
 				'<b>log fold change</b>: {foldChange:sciNotation}',
 				'</tpl>', '</tpl></span>');
