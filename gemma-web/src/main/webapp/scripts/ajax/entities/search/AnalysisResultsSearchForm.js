d85654ad84cfeb6ae011d966f115994a57da7487
tvrossum <>
2011-12-12 17:19:26 +0000

    Accidentally committed link using test GO group. Back to the right one now.

--- gemma-web/src/main/webapp/scripts/ajax/entities/search/AnalysisResultsSearchForm.js
+++ gemma-web/src/main/webapp/scripts/ajax/entities/search/AnalysisResultsSearchForm.js
@@ -1217,8 +1217,8 @@ Gemma.AnalysisResultsSearchForm = Ext.extend(Ext.FormPanel, {
 							tooltip: Gemma.HelpText.WidgetDefaults.AnalysisResultsSearchForm.Examples.diffEx1TT,
 							listeners: {
 								click: function(){
-									//var goName = "GO_0021766";
-									var goName = "GO_0045208";
+									var goName = "GO_0021766";
+									//var goName = "GO_0045208";
 									var eeSetId = '6112';
 									var taxonId = '1';
 									this.runExampleQuery(eeSetId, goName, taxonId);
