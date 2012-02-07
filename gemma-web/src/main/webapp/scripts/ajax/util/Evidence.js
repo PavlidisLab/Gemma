64677f84743c5903ea3d3d712877777f49ab3932
frances <>
2012-02-07 19:48:12 +0000

    I cleaned up code.

--- gemma-web/src/main/webapp/scripts/ajax/util/Evidence.js
+++ gemma-web/src/main/webapp/scripts/ajax/util/Evidence.js
@@ -1,27 +1,21 @@
 Ext.namespace('Gemma');
 
-// This function returns display and tooltip text for the evidence the given evidence code represents.
+// It returns display and tooltip text for the evidence the given evidence code represents.
 Gemma.decodeEvidenceCode = function(evidenceCode) {
-	var displayText = "";
-	var tooltipText = "";
+	var EVIDENCE_TEXT = {
+		'EXP': {
+			displayText: Gemma.EvidenceCodes.expText,
+			tooltipText: Gemma.EvidenceCodes.expTT
+		},
+		'IC': {
+			displayText: Gemma.EvidenceCodes.icText,
+			tooltipText: Gemma.EvidenceCodes.icTT
+		},
+		'TAS': {
+			displayText: Gemma.EvidenceCodes.tasText,
+			tooltipText: Gemma.EvidenceCodes.tasTT
+		}
+	};
 	
-	switch (evidenceCode) {
-		case 'EXP' :
-        	displayText = Gemma.EvidenceCodes.expText;
-        	tooltipText = Gemma.EvidenceCodes.expTT;
-			break;
-		case 'IC' :
-        	displayText = Gemma.EvidenceCodes.icText;
-        	tooltipText = Gemma.EvidenceCodes.icTT;
-			break;
-		case 'TAS' :
-        	displayText = Gemma.EvidenceCodes.tasText;
-        	tooltipText = Gemma.EvidenceCodes.tasTT;
-			break;
-	}
-	
-	return {
-		displayText: displayText,
-		tooltipText: tooltipText
-	}
+	return EVIDENCE_TEXT[evidenceCode];
 };
