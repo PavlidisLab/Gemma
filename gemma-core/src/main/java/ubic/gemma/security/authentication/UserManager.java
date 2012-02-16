fc5ae7d88fda542d28bce0ac23d996d0ad9717ea
anton <anton@chibi.ubc.ca>
2012-02-16 18:27:46 +0000

    Making UserManager a service.

--- gemma-core/src/main/java/ubic/gemma/security/authentication/UserManager.java
+++ gemma-core/src/main/java/ubic/gemma/security/authentication/UserManager.java
@@ -22,6 +22,7 @@ import java.util.Collection;
 
 import org.springframework.security.provisioning.GroupManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.stereotype.Service;
 
 import ubic.gemma.model.common.auditAndSecurity.User;
 import ubic.gemma.model.common.auditAndSecurity.UserGroup;
@@ -30,6 +31,7 @@ import ubic.gemma.model.common.auditAndSecurity.UserGroup;
  * @author paul
  * @version $Id$
  */
+@Service
 public interface UserManager extends UserDetailsManager, GroupManager {
 
     /**
