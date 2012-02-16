84f8a7d720c0fdfd7b3ed0e824e4ffee77078ebd
anton <anton@chibi.ubc.ca>
2012-02-16 21:31:51 +0000

    Getting rid of Base classes.

--- gemma-mda/src/main/java/ubic/gemma/model/genome/TaxonDaoImpl.java
+++ gemma-mda/src/main/java/ubic/gemma/model/genome/TaxonDaoImpl.java
@@ -26,6 +26,7 @@ import org.hibernate.LockOptions;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 import org.springframework.stereotype.Repository;
 
 import ubic.gemma.model.common.protocol.ProtocolDaoImpl;
@@ -37,7 +38,7 @@ import ubic.gemma.util.BusinessKey;
  * @see ubic.gemma.model.genome.Taxon
  */
 @Repository
-public class TaxonDaoImpl extends ubic.gemma.model.genome.TaxonDaoBase {
+public class TaxonDaoImpl extends HibernateDaoSupport implements TaxonDao {
 
     private static Log log = LogFactory.getLog( ProtocolDaoImpl.class.getName() );
 
@@ -51,7 +52,6 @@ public class TaxonDaoImpl extends ubic.gemma.model.genome.TaxonDaoBase {
      * 
      * @see ubic.gemma.model.genome.TaxonDao#find(ubic.gemma.model.genome.Taxon)
      */
-    @Override
     public Taxon find( Taxon taxon ) {
 
         BusinessKey.checkValidKey( taxon );
@@ -74,7 +74,6 @@ public class TaxonDaoImpl extends ubic.gemma.model.genome.TaxonDaoBase {
 
     }
 
-    @Override
     public void handleThaw( final Taxon taxon ) throws Exception {
         if ( taxon == null ) {
             log.warn( "Attempt to thaw null" );
@@ -97,7 +96,6 @@ public class TaxonDaoImpl extends ubic.gemma.model.genome.TaxonDaoBase {
      * 
      * @see ubic.gemma.model.genome.TaxonDaoBase#findOrCreate(ubic.gemma.model.genome.Taxon)
      */
-    @Override
     public Taxon findOrCreate( Taxon taxon ) {
         Taxon existingTaxon = find( taxon );
         if ( existingTaxon != null ) {
@@ -118,7 +116,6 @@ public class TaxonDaoImpl extends ubic.gemma.model.genome.TaxonDaoBase {
     /**
      * @see ubic.gemma.model.genome.TaxonDao#findByAbbreviation(int, java.lang.String)
      */
-    @Override
     public Taxon handleFindByAbbreviation( final java.lang.String abbreviation ) {
         final String queryString = "from TaxonImpl t where t.abbreviation=:abbreviation";
         List<?> results = getHibernateTemplate().findByNamedParam( queryString, "abbreviation",
@@ -135,8 +132,239 @@ public class TaxonDaoImpl extends ubic.gemma.model.genome.TaxonDaoBase {
 
     }
 
-    @Override
     public Collection<? extends Taxon> load( Collection<Long> ids ) {
         return this.getHibernateTemplate().findByNamedParam( "from TaxonImpl t where t.id in (:ids)", "ids", ids );
     }
+    
+    
+    
+    
+    
+    /**
+     * @see ubic.gemma.model.genome.TaxonDao#create(int, java.util.Collection)
+     */
+    public java.util.Collection<? extends Taxon> create( final java.util.Collection<? extends Taxon> entities ) {
+        if ( entities == null ) {
+            throw new IllegalArgumentException( "Taxon.create - 'entities' can not be null" );
+        }
+        this.getHibernateTemplate().execute( new org.springframework.orm.hibernate3.HibernateCallback<Object>() {
+            public Object doInHibernate( org.hibernate.Session session ) throws org.hibernate.HibernateException {
+                for ( java.util.Iterator<? extends Taxon> entityIterator = entities.iterator(); entityIterator
+                        .hasNext(); ) {
+                    create( entityIterator.next() );
+                }
+                return null;
+            }
+        } );
+        return entities;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see ubic.gemma.persistence.BaseDao#create(java.lang.Object)
+     */
+    public Taxon create( final ubic.gemma.model.genome.Taxon taxon ) {
+        if ( taxon == null ) {
+            throw new IllegalArgumentException( "Taxon.create - 'taxon' can not be null" );
+        }
+
+        if ( StringUtils.isBlank( taxon.getCommonName() ) && StringUtils.isBlank( taxon.getScientificName() ) ) {
+            throw new IllegalArgumentException( "Cannot create a taxon without names: " + taxon );
+        }
+
+        this.getHibernateTemplate().save( taxon );
+        return taxon;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see ubic.gemma.model.genome.TaxonDao#findByAbbreviation(java.lang.String)
+     */
+    public Taxon findByAbbreviation( final java.lang.String abbreviation ) {
+        try {
+            return this.handleFindByAbbreviation( abbreviation );
+        } catch ( Throwable th ) {
+            throw new java.lang.RuntimeException(
+                    "Error performing 'ubic.gemma.model.genome.TaxonDaoBase.findByAbbreviation(java.lang.String abbreviation)' --> "
+                            + th, th );
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see ubic.gemma.model.genome.TaxonDao#findByCommonName(java.lang.String)
+     */
+    public Taxon findByCommonName( final java.lang.String commonName ) {
+        return this.findByCommonName( "from TaxonImpl t where t.commonName=:commonName", commonName );
+    }
+
+    /**
+     * @param queryString
+     * @param commonName
+     * @return
+     */
+    public Taxon findByCommonName( final java.lang.String queryString, final java.lang.String commonName ) {
+        java.util.List<String> argNames = new java.util.ArrayList<String>();
+        java.util.List<Object> args = new java.util.ArrayList<Object>();
+        args.add( commonName );
+        argNames.add( "commonName" );
+        java.util.Set results = new java.util.LinkedHashSet( this.getHibernateTemplate().findByNamedParam( queryString,
+                argNames.toArray( new String[argNames.size()] ), args.toArray() ) );
+        Object result = null;
+
+        if ( results.size() > 1 ) {
+            throw new org.springframework.dao.InvalidDataAccessResourceUsageException(
+                    "More than one instance of 'ubic.gemma.model.genome.Taxon"
+                            + "' was found when executing query --> '" + queryString + "'" );
+        } else if ( results.size() == 1 ) {
+            result = results.iterator().next();
+        }
+
+        return ( Taxon ) result;
+    }
+
+    /**
+     * @see ubic.gemma.model.genome.TaxonDao#findByScientificName(int, java.lang.String)
+     */
+    public Taxon findByScientificName( final java.lang.String scientificName ) {
+        return this.findByScientificName( "from TaxonImpl t where t.scientificName=:scientificName ", scientificName );
+    }
+
+    /**
+     * @see ubic.gemma.model.genome.TaxonDao#findByScientificName(int, java.lang.String, java.lang.String)
+     */
+
+    public Taxon findByScientificName( final java.lang.String queryString, final java.lang.String scientificName ) {
+        java.util.List<String> argNames = new java.util.ArrayList<String>();
+        java.util.List<Object> args = new java.util.ArrayList<Object>();
+        args.add( scientificName );
+        argNames.add( "scientificName" );
+        java.util.Set results = new java.util.LinkedHashSet( this.getHibernateTemplate().findByNamedParam( queryString,
+                argNames.toArray( new String[argNames.size()] ), args.toArray() ) );
+        Object result = null;
+        if ( results.size() > 1 ) {
+            throw new org.springframework.dao.InvalidDataAccessResourceUsageException(
+                    "More than one instance of 'ubic.gemma.model.genome.Taxon"
+                            + "' was found when executing query --> '" + queryString + "'" );
+        } else if ( results.size() == 1 ) {
+            result = results.iterator().next();
+        }
+
+        return ( Taxon ) result;
+    }
+
+    /**
+     * @see ubic.gemma.model.genome.TaxonDao#findChildTaxaByParent(ubic.gemma.model.genome.Taxon)
+     */
+
+    public Collection<Taxon> findChildTaxaByParent( Taxon parentTaxon ) {
+        String queryString = "from ubic.gemma.model.genome.TaxonImpl as taxon where taxon.parentTaxon = :parentTaxon";
+        Collection<Taxon> childTaxa = this.getHibernateTemplate().findByNamedParam( queryString, "parentTaxon",
+                parentTaxon );
+        return childTaxa;
+    }
+
+    /**
+     * @see ubic.gemma.model.genome.TaxonDao#load(int, java.lang.Long)
+     */
+    public Taxon load( final java.lang.Long id ) {
+        if ( id == null ) {
+            throw new IllegalArgumentException( "Taxon.load - 'id' can not be null" );
+        }
+        final Object entity = this.getHibernateTemplate().get( ubic.gemma.model.genome.TaxonImpl.class, id );
+        return ( ubic.gemma.model.genome.Taxon ) entity;
+    }
+
+    /**
+     * @see ubic.gemma.model.genome.TaxonDao#loadAll(int)
+     */
+
+    @SuppressWarnings("unchecked")
+    public java.util.Collection<Taxon> loadAll() {
+        final java.util.Collection<?> results = this.getHibernateTemplate().loadAll(
+                ubic.gemma.model.genome.TaxonImpl.class );
+
+        return ( Collection<Taxon> ) results;
+    }
+
+    /**
+     * @see ubic.gemma.model.genome.TaxonDao#remove(java.lang.Long)
+     */
+    public void remove( java.lang.Long id ) {
+        if ( id == null ) {
+            throw new IllegalArgumentException( "Taxon.remove - 'id' can not be null" );
+        }
+        ubic.gemma.model.genome.Taxon entity = this.load( id );
+        if ( entity != null ) {
+            this.remove( entity );
+        }
+    }
+
+    /**
+     * @see ubic.gemma.model.genome.TaxonDao#remove(java.util.Collection)
+     */
+    public void remove( java.util.Collection<? extends Taxon> entities ) {
+        if ( entities == null ) {
+            throw new IllegalArgumentException( "Taxon.remove - 'entities' can not be null" );
+        }
+        this.getHibernateTemplate().deleteAll( entities );
+    }
+
+    /**
+     * @see ubic.gemma.model.genome.TaxonDao#remove(ubic.gemma.model.genome.Taxon)
+     */
+    public void remove( ubic.gemma.model.genome.Taxon taxon ) {
+        if ( taxon == null ) {
+            throw new IllegalArgumentException( "Taxon.remove - 'taxon' can not be null" );
+        }
+        this.getHibernateTemplate().delete( taxon );
+    }
+
+    /**
+     * @see ubic.gemma.model.genome.TaxonDao.thaw#thaw(ubic.gemma.model.expression.bioAssay.BioAssay)
+     */
+    public void thaw( final ubic.gemma.model.genome.Taxon taxon ) {
+        try {
+            this.handleThaw( taxon );
+        } catch ( Throwable th ) {
+            throw new java.lang.RuntimeException(
+                    "Error performing 'ubic.gemma.model.genome.TaxonDao.thaw(ubic.gemma.model.genome.Taxon)' --> " + th,
+                    th );
+        }
+    }
+
+    /**
+     * @see ubic.gemma.model.genome.TaxonDao#update(java.util.Collection)
+     */
+    public void update( final java.util.Collection<? extends Taxon> entities ) {
+        if ( entities == null ) {
+            throw new IllegalArgumentException( "Taxon.update - 'entities' can not be null" );
+        }
+        this.getHibernateTemplate().execute( new org.springframework.orm.hibernate3.HibernateCallback<Object>() {
+            public Object doInHibernate( org.hibernate.Session session ) throws org.hibernate.HibernateException {
+                for ( java.util.Iterator<? extends Taxon> entityIterator = entities.iterator(); entityIterator
+                        .hasNext(); ) {
+                    update( entityIterator.next() );
+                }
+                return null;
+            }
+        } );
+    }
+
+    /**
+     * @see ubic.gemma.model.genome.TaxonDao#update(ubic.gemma.model.genome.Taxon)
+     */
+    public void update( ubic.gemma.model.genome.Taxon taxon ) {
+        if ( taxon == null ) {
+            throw new IllegalArgumentException( "Taxon.update - 'taxon' can not be null" );
+        }
+        this.getHibernateTemplate().update( taxon );
+    }
+
+    
+    
+    
 }
