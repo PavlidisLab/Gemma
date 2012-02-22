270fbfaca10047d056941a699c4da7e4683846e7
tvrossum <>
2012-02-22 19:18:02 +0000

    tests for GeneSetValueObject creation in GeneSetValueObjectHelper class

---
+++ gemma-core/src/test/java/ubic/gemma/model/genome/gene/GeneSetValueObjectHelperTest.java
@@ -0,0 +1,123 @@
+/*
+ * The Gemma project
+ * 
+ * Copyright (c) 2012 University of British Columbia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package ubic.gemma.model.genome.gene;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ubic.gemma.genome.gene.GeneSetValueObject;
+import ubic.gemma.genome.gene.GeneSetValueObjectHelper;
+import ubic.gemma.genome.gene.service.GeneSetService;
+import ubic.gemma.model.genome.Gene;
+import ubic.gemma.model.genome.Taxon;
+import ubic.gemma.testing.BaseSpringContextTest;
+
+
+/**
+ * TODO Document Me
+ * 
+ * @author tvrossum
+ * @version $Id$
+ */
+public class GeneSetValueObjectHelperTest extends BaseSpringContextTest {
+
+    private Gene g1 = null;
+    private Taxon tax1;
+    
+    @Autowired
+    GeneSetService geneSetService;
+
+    @Autowired
+    GeneSetValueObjectHelper geneSetValueObjectHelper;
+    
+    @Before
+    public void setUp() throws Exception {
+
+        g1 = this.getTestPeristentGene();
+        tax1 = this.getTaxon( "human" );
+        g1.setTaxon( tax1 );
+
+    }
+    
+    @Test
+    public void testConvertToValueObject() {
+
+        GeneSetMember gmember = GeneSetMember.Factory.newInstance();
+        gmember.setGene( g1 );
+        gmember.setScore( 0.22 );
+
+        GeneSet gset = GeneSet.Factory.newInstance();
+        gset.setName( "CreateTest" );
+        gset.getMembers().add( gmember );
+
+        gset = geneSetService.create( gset );
+
+        Long id = gset.getId();
+        assertNotNull( id );
+
+        geneSetService.remove( gset );
+
+        assertNull( geneSetService.load( id ) );
+        
+        GeneSetValueObject gsvo = geneSetValueObjectHelper.convertToValueObject( gset );
+                
+        assertEquals( gset.getId(), gsvo.getId());
+        assertEquals( gset.getMembers().size(), gsvo.getNumGenes() );
+        assertEquals( gset.getName(), gsvo.getName() );
+        assertEquals( gmember.getGene().getTaxon().getId(), gsvo.getTaxonId() );
+
+    }
+    
+    @Test
+    public void testConvertToLightValueObject() {
+
+        GeneSetMember gmember = GeneSetMember.Factory.newInstance();
+        gmember.setGene( g1 );
+        gmember.setScore( 0.22 );
+
+        GeneSet gset = GeneSet.Factory.newInstance();
+        gset.setName( "CreateTest" );
+        gset.getMembers().add( gmember );
+
+        gset = geneSetService.create( gset );
+
+        Long id = gset.getId();
+        assertNotNull( id );
+
+        geneSetService.remove( gset );
+
+        assertNull( geneSetService.load( id ) );
+        
+        GeneSetValueObject gsvo = geneSetValueObjectHelper.convertToValueObject( gset );
+        
+        assertNull( gsvo.getNumGenes() );
+        assertNull( gsvo.getGeneIds() );
+                
+        assertEquals( gset.getId(), gsvo.getId());
+        assertEquals( gset.getName(), gsvo.getName() );
+        assertEquals( gmember.getGene().getTaxon().getId(), gsvo.getTaxonId() );
+
+    }
+}
