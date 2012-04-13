c220c1691c0768860efd1ec070a40f6377f7e147
anton <anton@chibi.ubc.ca>
2012-04-13 12:26:24 +0000

    Created IndexController. We shouldn't call services from the front end.

---
+++ gemma-web/src/main/java/ubic/gemma/web/controller/IndexController.java
@@ -0,0 +1,18 @@
+package ubic.gemma.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import ubic.gemma.search.IndexService;
+import ubic.gemma.tasks.maintenance.IndexerTaskCommand;
+
+@Controller
+public class IndexController {
+
+    @Autowired
+    IndexService indexService;
+    
+    public String index (IndexerTaskCommand command) {        
+        return this.indexService.index( command );
+    }
+}
