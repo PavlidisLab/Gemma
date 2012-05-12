eeee1bde845b6b76a25ec054093c8a2e05c16d37
ppavlidis <paul@chibi.ubc.ca>
2012-05-12 02:08:39 +0000

    attempt to make initializeNewAnalysis transactional.

--- gemma-core/src/main/java/ubic/gemma/analysis/expression/coexpression/Gene2GenePopulationService.java
+++ gemma-core/src/main/java/ubic/gemma/analysis/expression/coexpression/Gene2GenePopulationService.java
@@ -16,8 +16,10 @@ package ubic.gemma.analysis.expression.coexpression;
 
 import java.util.Collection;
 
+import ubic.gemma.model.analysis.expression.coexpression.GeneCoexpressionAnalysis;
 import ubic.gemma.model.expression.experiment.BioAssaySet;
 import ubic.gemma.model.genome.Gene;
+import ubic.gemma.model.genome.Taxon;
 
 /**
  * @author paul
@@ -30,5 +32,11 @@ public interface Gene2GenePopulationService {
 
     public void analyze( Collection<BioAssaySet> expressionExperiments, Collection<Gene> toUseGenes, int stringency,
             String analysisName, boolean useDB );
+
+    /*
+     * Defined here so that we can make it transactional.
+     */
+    public GeneCoexpressionAnalysis intializeNewAnalysis( Collection<BioAssaySet> expressionExperiments, Taxon taxon,
+            Collection<Gene> toUseGenes, String analysisName, int stringency );
 
 }
