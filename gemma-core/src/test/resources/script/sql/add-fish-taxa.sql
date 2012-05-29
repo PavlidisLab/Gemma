d20453ac672c941add16ea6bb33d472f4679baa6
ppavlidis <paul@chibi.ubc.ca>
2012-05-29 21:33:41 +0000

    delete possibly conflicting records for test

--- gemma-core/src/test/resources/script/sql/add-fish-taxa.sql
+++ gemma-core/src/test/resources/script/sql/add-fish-taxa.sql
@@ -1,5 +1,9 @@
 -- Used by test: GeoConverterTest.
 -- $Id$
+delete from TAXON WHERE SCIENTIFIC_NAME = "Salmo salar";
+delete from TAXON WHERE SCIENTIFIC_NAME = "Oncorhynchus mykiss";
+delete from TAXON WHERE SCIENTIFIC_NAME = "Oncorhynchus nerka";
+delete from TAXON WHERE SCIENTIFIC_NAME = "Oncorhynchus tshawytscha";
 insert ignore into TAXON (SCIENTIFIC_NAME,COMMON_NAME,ABBREVIATION,NCBI_ID,IS_SPECIES,PARENT_TAXON_FK,IS_GENES_USABLE) values ("Salmo salar","atlantic salmon","ssal","8030",1,null,0);
 insert ignore into TAXON (SCIENTIFIC_NAME,COMMON_NAME,ABBREVIATION,NCBI_ID,IS_SPECIES,PARENT_TAXON_FK,IS_GENES_USABLE) values ("Oncorhynchus mykiss","rainbow troat","omyk","8022",1,null,0);
 insert ignore into TAXON (SCIENTIFIC_NAME,COMMON_NAME,ABBREVIATION,NCBI_ID,IS_SPECIES,PARENT_TAXON_FK,IS_GENES_USABLE) values ("Coregonus clupeaformis","whitefish","cclu","59861",1,null,0);
@@ -9,5 +13,4 @@ insert ignore into TAXON (SCIENTIFIC_NAME,COMMON_NAME,NCBI_ID,IS_SPECIES,PARENT_
 insert ignore into TAXON (SCIENTIFIC_NAME,COMMON_NAME,ABBREVIATION,NCBI_ID,IS_SPECIES,PARENT_TAXON_FK,IS_GENES_USABLE) values ("Oncorhynchus tshawytscha","chinook salmon","otsh","74940",1,null,0);
 insert ignore into TAXON (SCIENTIFIC_NAME,COMMON_NAME,NCBI_ID,IS_SPECIES,PARENT_TAXON_FK,IS_GENES_USABLE) values ("Oncorhynchus keta","chum salmon","8018",1,null,0);
 insert ignore into TAXON (SCIENTIFIC_NAME,COMMON_NAME,ABBREVIATION,NCBI_ID,IS_SPECIES,PARENT_TAXON_FK,IS_GENES_USABLE) values ("Osmerus mordax","rainbow smelt","omor","8014",1,null,0);
-
 insert ignore into TAXON (SCIENTIFIC_NAME,COMMON_NAME,NCBI_ID,IS_SPECIES,IS_GENES_USABLE) values ("Salmonidae","salmonid","8015",0,1);
