68969bf96a83b8595fd6539649e7b4fba18f91ce
nicolas <>
2012-06-01 21:27:39 +0000

    ExperimentalCharacteristic wasnt ordered in the evidence panel

--- gemma-mda/src/main/java/ubic/gemma/model/genome/gene/phenotype/valueObject/ExperimentalEvidenceValueObject.java
+++ gemma-mda/src/main/java/ubic/gemma/model/genome/gene/phenotype/valueObject/ExperimentalEvidenceValueObject.java
@@ -18,10 +18,10 @@
  */
 package ubic.gemma.model.genome.gene.phenotype.valueObject;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 import ubic.gemma.model.association.phenotype.ExperimentalEvidence;
 import ubic.gemma.model.common.description.BibliographicReferenceValueObject;
@@ -31,7 +31,7 @@ import ubic.gemma.model.common.description.VocabCharacteristicImpl;
 
 public class ExperimentalEvidenceValueObject extends EvidenceValueObject {
 
-    private Collection<CharacteristicValueObject> experimentCharacteristics = new ArrayList<CharacteristicValueObject>();
+    private Collection<CharacteristicValueObject> experimentCharacteristics = new TreeSet<CharacteristicValueObject>();
     private Set<CitationValueObject> relevantPublicationsCitationValueObjects = new HashSet<CitationValueObject>();
     private CitationValueObject primaryPublicationCitationValueObject = null;
 
