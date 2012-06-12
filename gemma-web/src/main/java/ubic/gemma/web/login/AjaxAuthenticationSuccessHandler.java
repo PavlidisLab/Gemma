681951dc954f961e9bc9468a9869f258c13bcc43
cmcdonald <>
2012-06-12 17:10:14 +0000

    bug 2976

--- gemma-web/src/main/java/ubic/gemma/web/login/AjaxAuthenticationSuccessHandler.java
+++ gemma-web/src/main/java/ubic/gemma/web/login/AjaxAuthenticationSuccessHandler.java
@@ -12,7 +12,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package ubic.gemma.security.authentication;
+package ubic.gemma.web.login;
 
 import java.io.IOException;
 
@@ -20,24 +20,30 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.RedirectStrategy;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
+import ubic.gemma.security.SecurityServiceImpl;
 import ubic.gemma.util.JSONUtil;
 
 /**
- * TODO Document Me
+ * Strategy used to handle a successful user authentication
+ * if it is a ajax style login (ajaxLoginTrue parameter = true) then no redirect happens and a some JSON is sent to the client
+ * 
+ * if the request is not ajax-style then the default redirection takes place
+ * 
+ * configuration in applicationContext-security.xml
  * 
  * @author cmcdonald
  * @version $Id$
  */
-public class AjaxAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+public class AjaxAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
     @Override
-    public void onAuthenticationFailure( HttpServletRequest request, HttpServletResponse response,
-            AuthenticationException exception ) throws ServletException, IOException {
+    public void onAuthenticationSuccess( HttpServletRequest request, HttpServletResponse response,
+            Authentication authentication ) throws ServletException, IOException {
 
         String ajaxLoginTrue = request.getParameter( "ajaxLoginTrue" );
 
@@ -55,19 +61,17 @@ public class AjaxAuthenticationFailureHandler extends SimpleUrlAuthenticationFai
                 }
             } );
 
-            super.onAuthenticationFailure( request, response, exception );
+            super.onAuthenticationSuccess( request, response, authentication );
+            authentication.getName();
 
-            jsonText = "{success:false}";
+            jsonText = "{success:true,user:\'" + authentication.getName() + "\',isAdmin:"
+                    + SecurityServiceImpl.isUserAdmin() + "}";
             jsonUtil.writeToResponse( jsonText );
-
-        }
-
-        else {
+        } else {
 
             this.setRedirectStrategy( new DefaultRedirectStrategy() );
 
-            super.onAuthenticationFailure( request, response, exception );
-
+            super.onAuthenticationSuccess( request, response, authentication );
         }
 
     }
