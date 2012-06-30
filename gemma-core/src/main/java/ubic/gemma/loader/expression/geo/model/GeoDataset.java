56b7c74dfac33396b24dc5631b9ad7ef57792edb
ppavlidis <paul@chibi.ubc.ca>
2012-06-30 20:18:30 +0000

    cleanup

--- gemma-core/src/main/java/ubic/gemma/loader/expression/geo/model/GeoDataset.java
+++ gemma-core/src/main/java/ubic/gemma/loader/expression/geo/model/GeoDataset.java
@@ -33,31 +33,189 @@ import org.apache.commons.logging.LogFactory;
  */
 public class GeoDataset extends GeoData {
 
+    public enum ExperimentType {
+        geneExpressionArraybased, geneExpressionSAGEbased, geneExpressionMPSSBased, geneExpressionRTPCRbased, proteinExpressionArraybased, proteinExpressionMSBased, arrayCGH, ChIPChip, SNP
+    }
+
+    public enum PlatformType {
+        dualChannel, dualChannelGenomic, SAGE, singleChannel, singleChannelGenomic, spottedDNAOrcDNA, spottedOligonucleotide, inSituOligonucleotide, oligonucleotideBeads, SAGENlaIII, SAGESau3A, SAGERsaI, SARST, RTPCR, MPSS, antibody, MS, other;
+    }
+
+    public enum SampleType {
+        RNA, genomic, protein, mixed, SAGE, MPSS, SARST
+    }
+
+    public enum ValueType {
+        count, logRatio, log2Ratio, log10ratio, logERatio, transformedCount
+    }
+
     private static final long serialVersionUID = 2659028881509672793L;
 
     private static Log log = LogFactory.getLog( GeoDataset.class.getName() );
 
+    /**
+     * gene expression array-based, gene expression SAGE-based, gene expression MPSS-based, gene expression
+     * RT-PCR-based, protein expression array-based, protein expression MS-based, array CGH, ChIP-chip, SNP
+     * 
+     * @param string
+     * @return
+     */
+    public static ExperimentType convertStringToExperimentType( String string ) {
+        if ( string.equals( "Expression profiling by array" ) ) {
+            return ExperimentType.geneExpressionArraybased;
+        } else if ( string.equals( "gene expression array-based" ) ) {
+            return ExperimentType.geneExpressionArraybased;
+        } else if ( string.equals( "gene expression SAGE-based" ) ) {
+            return ExperimentType.geneExpressionSAGEbased;
+        } else if ( string.equals( "gene expression MPSS-based" ) ) {
+            return ExperimentType.geneExpressionMPSSBased;
+        } else if ( string.equals( "gene expression RT-PCR-based" ) ) {
+            return ExperimentType.geneExpressionRTPCRbased;
+        } else if ( string.equals( "protein expression array-based" ) ) {
+            return ExperimentType.proteinExpressionArraybased;
+        } else if ( string.equals( "protein expression MS-based" ) ) {
+            return ExperimentType.proteinExpressionMSBased;
+        } else if ( string.equals( "array CGH" ) ) {
+            return ExperimentType.arrayCGH;
+        } else if ( string.equals( "ChIP-chip" ) ) {
+            return ExperimentType.ChIPChip;
+        } else if ( string.equals( "SNP" ) ) {
+            return ExperimentType.SNP;
+        } else if ( string.equals( "dual channel" ) ) { // legacy term.
+            log.warn( "Experiment Type '" + string + "' is a legacy term. Annotation may be inaccurate." );
+            return ExperimentType.geneExpressionArraybased;
+        } else if ( string.equals( "single channel" ) ) { // legacy term
+            log.warn( "Experiment Type '" + string + "' is a legacy term. Annotation may be inaccurate." );
+            return ExperimentType.geneExpressionArraybased;
+        } else {
+            throw new IllegalArgumentException( "Unknown experiment type " + string );
+        }
+    }
+
+    // spotted DNA/cDNA, spotted oligonucleotide, in situ oligonucleotide, oligonucleotide beads, SAGE NlaIII, SAGE
+    // Sau3A, SAGE RsaI, SARST, RT-PCR, MPSS, antibody, MS, other
+    public static PlatformType convertStringToPlatformType( String string ) {
+        if ( string.equals( "single channel" ) ) {
+            return PlatformType.singleChannel;
+        } else if ( string.equals( "dual channel" ) ) {
+            return PlatformType.dualChannel;
+        } else if ( string.equals( "single channel genomic" ) ) {
+            return PlatformType.singleChannelGenomic;
+        } else if ( string.equals( "dual channel genomic" ) ) {
+            return PlatformType.dualChannelGenomic;
+        } else if ( string.equals( "SAGE" ) ) {
+            return PlatformType.SAGE;
+        } else if ( string.equals( "MPSS" ) || string.equals( "high-throughput sequencing" ) ) {
+            return PlatformType.MPSS;
+        } else if ( string.equals( "spotted DNA/cDNA" ) ) {
+            return PlatformType.spottedDNAOrcDNA;
+        } else if ( string.equals( "spotted oligonucleotide" ) ) {
+            return PlatformType.spottedOligonucleotide;
+        } else if ( string.equals( "in situ oligonucleotide" ) ) {
+            return PlatformType.inSituOligonucleotide;
+        } else if ( string.equalsIgnoreCase( "oligonucleotide Beads" ) ) {
+            return PlatformType.oligonucleotideBeads;
+        } else if ( string.equals( "SAGE NlaIII" ) ) {
+            return PlatformType.SAGENlaIII;
+        } else if ( string.equals( "SAGE Sau3A" ) ) {
+            return PlatformType.SAGESau3A;
+        } else if ( string.equals( "SAGE RsaI" ) ) {
+            return PlatformType.SAGERsaI;
+        } else if ( string.equals( "SARST" ) ) {
+            return PlatformType.SARST;
+        } else if ( string.equals( "RT-PCR" ) ) {
+            return PlatformType.RTPCR;
+        } else if ( string.equals( "antibody" ) ) {
+            return PlatformType.antibody;
+        } else if ( string.equals( "MS" ) ) {
+            return PlatformType.MS;
+        } else if ( string.equals( "other" ) ) {
+            return PlatformType.other;
+        } else if ( string.equals( "nucleotide" ) ) { // legacy terminology
+            log.warn( "Platform Type '" + string + "' is a legacy term. Annotation may be inaccurate." );
+            return PlatformType.singleChannel;
+        } else {
+            throw new IllegalArgumentException( "Unknown platform technology type " + string );
+        }
+    }
+
+    /**
+     * @param string
+     * @return
+     */
+    public static SampleType convertStringToSampleType( String string ) {
+        if ( string.equals( "cDNA" ) ) {
+            return SampleType.RNA;
+        } else if ( string.equals( "RNA" ) ) {
+            return SampleType.RNA;
+        } else if ( string.equals( "genomic" ) ) {
+            return SampleType.genomic;
+        } else if ( string.equals( "protein" ) ) {
+            return SampleType.protein;
+        } else if ( string.equals( "mixed" ) ) {
+            return SampleType.mixed;
+        } else if ( string.equals( "SAGE" ) ) {
+            return SampleType.SAGE;
+        } else if ( string.equals( "MPSS" ) ) {
+            return SampleType.MPSS;
+        } else if ( string.equals( "SARST" ) ) {
+            return SampleType.SARST;
+        } else {
+            throw new IllegalArgumentException( "Unknown sample type " + string );
+        }
+    }
+
+    /**
+     * count, log ratio, log2 ratio, log10 ratio, logE ratio, log e ratio, transformed count
+     * 
+     * @param string
+     * @return
+     */
+    public static ValueType convertStringToValueType( String string ) {
+        if ( string.equals( "count" ) ) {
+            return ValueType.count;
+        } else if ( string.equals( "log ratio" ) ) {
+            return ValueType.logRatio;
+        } else if ( string.equals( "log2 ratio" ) ) {
+            return ValueType.log2Ratio;
+        } else if ( string.equals( "log10 ratio" ) ) {
+            return ValueType.log10ratio;
+        } else if ( string.equals( "logE ratio" ) ) {
+            return ValueType.logERatio;
+        } else if ( string.equals( "log e ratio" ) ) {
+            return ValueType.logERatio;
+        } else if ( string.equals( "transformed count" ) ) {
+            return ValueType.transformedCount;
+        } else {
+            throw new IllegalArgumentException( "Unknown value type " + string );
+        }
+    }
+
     public ExperimentType experimentType;
+
     private String completeness;
-
     private String datasetType;
-
     private String description;
-
     private String featureCount;
-
     private int numChannels;
     private int numSamples;
     private String order;
     private String organism;
 
     private GeoPlatform platform;
+
     private PlatformType platformType;
+
     private String pubmedId;
+
     private SampleType sampleType;
+
     private Collection<GeoSeries> series;
+
     private Collection<GeoSubset> subsets;
+
     private String updateDate;
+
     private ValueType valueType;
 
     public GeoDataset() {
@@ -76,6 +234,18 @@ public class GeoDataset extends GeoData {
 
     public void addSubset( GeoSubset subset ) {
         this.subsets.add( subset );
+    }
+
+    /**
+     * This is used when we break a series up into two, along organism lines.
+     * 
+     * @param s
+     */
+    public void dissociateFromSeries( GeoSeries s ) {
+        if ( !this.series.contains( s ) ) {
+            throw new IllegalArgumentException( this + " does not have a reference to " + s );
+        }
+        this.series.remove( s );
     }
 
     /**
@@ -316,172 +486,6 @@ public class GeoDataset extends GeoData {
      */
     public void setValueType( ValueType valueType ) {
         this.valueType = valueType;
-    }
-
-    // spotted DNA/cDNA, spotted oligonucleotide, in situ oligonucleotide, oligonucleotide beads, SAGE NlaIII, SAGE
-    // Sau3A, SAGE RsaI, SARST, RT-PCR, MPSS, antibody, MS, other
-    public static PlatformType convertStringToPlatformType( String string ) {
-        if ( string.equals( "single channel" ) ) {
-            return PlatformType.singleChannel;
-        } else if ( string.equals( "dual channel" ) ) {
-            return PlatformType.dualChannel;
-        } else if ( string.equals( "single channel genomic" ) ) {
-            return PlatformType.singleChannelGenomic;
-        } else if ( string.equals( "dual channel genomic" ) ) {
-            return PlatformType.dualChannelGenomic;
-        } else if ( string.equals( "SAGE" ) ) {
-            return PlatformType.SAGE;
-        } else if ( string.equals( "MPSS" ) || string.equals( "high-throughput sequencing" ) ) {
-            return PlatformType.MPSS;
-        } else if ( string.equals( "spotted DNA/cDNA" ) ) {
-            return PlatformType.spottedDNAOrcDNA;
-        } else if ( string.equals( "spotted oligonucleotide" ) ) {
-            return PlatformType.spottedOligonucleotide;
-        } else if ( string.equals( "in situ oligonucleotide" ) ) {
-            return PlatformType.inSituOligonucleotide;
-        } else if ( string.equalsIgnoreCase( "oligonucleotide Beads" ) ) {
-            return PlatformType.oligonucleotideBeads;
-        } else if ( string.equals( "SAGE NlaIII" ) ) {
-            return PlatformType.SAGENlaIII;
-        } else if ( string.equals( "SAGE Sau3A" ) ) {
-            return PlatformType.SAGESau3A;
-        } else if ( string.equals( "SAGE RsaI" ) ) {
-            return PlatformType.SAGERsaI;
-        } else if ( string.equals( "SARST" ) ) {
-            return PlatformType.SARST;
-        } else if ( string.equals( "RT-PCR" ) ) {
-            return PlatformType.RTPCR;
-        } else if ( string.equals( "antibody" ) ) {
-            return PlatformType.antibody;
-        } else if ( string.equals( "MS" ) ) {
-            return PlatformType.MS;
-        } else if ( string.equals( "other" ) ) {
-            return PlatformType.other;
-        } else if ( string.equals( "nucleotide" ) ) { // legacy terminology
-            log.warn( "Platform Type '" + string + "' is a legacy term. Annotation may be inaccurate." );
-            return PlatformType.singleChannel;
-        } else {
-            throw new IllegalArgumentException( "Unknown platform technology type " + string );
-        }
-    }
-
-    /**
-     * @param string
-     * @return
-     */
-    public static SampleType convertStringToSampleType( String string ) {
-        if ( string.equals( "cDNA" ) ) {
-            return SampleType.RNA;
-        } else if ( string.equals( "RNA" ) ) {
-            return SampleType.RNA;
-        } else if ( string.equals( "genomic" ) ) {
-            return SampleType.genomic;
-        } else if ( string.equals( "protein" ) ) {
-            return SampleType.protein;
-        } else if ( string.equals( "mixed" ) ) {
-            return SampleType.mixed;
-        } else if ( string.equals( "SAGE" ) ) {
-            return SampleType.SAGE;
-        } else if ( string.equals( "MPSS" ) ) {
-            return SampleType.MPSS;
-        } else if ( string.equals( "SARST" ) ) {
-            return SampleType.SARST;
-        } else {
-            throw new IllegalArgumentException( "Unknown sample type " + string );
-        }
-    }
-
-    /**
-     * count, log ratio, log2 ratio, log10 ratio, logE ratio, log e ratio, transformed count
-     * 
-     * @param string
-     * @return
-     */
-    public static ValueType convertStringToValueType( String string ) {
-        if ( string.equals( "count" ) ) {
-            return ValueType.count;
-        } else if ( string.equals( "log ratio" ) ) {
-            return ValueType.logRatio;
-        } else if ( string.equals( "log2 ratio" ) ) {
-            return ValueType.log2Ratio;
-        } else if ( string.equals( "log10 ratio" ) ) {
-            return ValueType.log10ratio;
-        } else if ( string.equals( "logE ratio" ) ) {
-            return ValueType.logERatio;
-        } else if ( string.equals( "log e ratio" ) ) {
-            return ValueType.logERatio;
-        } else if ( string.equals( "transformed count" ) ) {
-            return ValueType.transformedCount;
-        } else {
-            throw new IllegalArgumentException( "Unknown value type " + string );
-        }
-    }
-
-    /**
-     * gene expression array-based, gene expression SAGE-based, gene expression MPSS-based, gene expression
-     * RT-PCR-based, protein expression array-based, protein expression MS-based, array CGH, ChIP-chip, SNP
-     * 
-     * @param string
-     * @return
-     */
-    public static ExperimentType convertStringToExperimentType( String string ) {
-        if ( string.equals( "Expression profiling by array" ) ) {
-            return ExperimentType.geneExpressionArraybased;
-        } else if ( string.equals( "gene expression array-based" ) ) {
-            return ExperimentType.geneExpressionArraybased;
-        } else if ( string.equals( "gene expression SAGE-based" ) ) {
-            return ExperimentType.geneExpressionSAGEbased;
-        } else if ( string.equals( "gene expression MPSS-based" ) ) {
-            return ExperimentType.geneExpressionMPSSBased;
-        } else if ( string.equals( "gene expression RT-PCR-based" ) ) {
-            return ExperimentType.geneExpressionRTPCRbased;
-        } else if ( string.equals( "protein expression array-based" ) ) {
-            return ExperimentType.proteinExpressionArraybased;
-        } else if ( string.equals( "protein expression MS-based" ) ) {
-            return ExperimentType.proteinExpressionMSBased;
-        } else if ( string.equals( "array CGH" ) ) {
-            return ExperimentType.arrayCGH;
-        } else if ( string.equals( "ChIP-chip" ) ) {
-            return ExperimentType.ChIPChip;
-        } else if ( string.equals( "SNP" ) ) {
-            return ExperimentType.SNP;
-        } else if ( string.equals( "dual channel" ) ) { // legacy term.
-            log.warn( "Experiment Type '" + string + "' is a legacy term. Annotation may be inaccurate." );
-            return ExperimentType.geneExpressionArraybased;
-        } else if ( string.equals( "single channel" ) ) { // legacy term
-            log.warn( "Experiment Type '" + string + "' is a legacy term. Annotation may be inaccurate." );
-            return ExperimentType.geneExpressionArraybased;
-        } else {
-            throw new IllegalArgumentException( "Unknown experiment type " + string );
-        }
-    }
-
-    public enum ExperimentType {
-        geneExpressionArraybased, geneExpressionSAGEbased, geneExpressionMPSSBased, geneExpressionRTPCRbased, proteinExpressionArraybased, proteinExpressionMSBased, arrayCGH, ChIPChip, SNP
-    }
-
-    public enum PlatformType {
-        dualChannel, dualChannelGenomic, SAGE, singleChannel, singleChannelGenomic, spottedDNAOrcDNA, spottedOligonucleotide, inSituOligonucleotide, oligonucleotideBeads, SAGENlaIII, SAGESau3A, SAGERsaI, SARST, RTPCR, MPSS, antibody, MS, other
-    }
-
-    public enum ValueType {
-        count, logRatio, log2Ratio, log10ratio, logERatio, transformedCount
-    }
-
-    public enum SampleType {
-        RNA, genomic, protein, mixed, SAGE, MPSS, SARST
-    }
-
-    /**
-     * This is used when we break a series up into two, along organism lines.
-     * 
-     * @param s
-     */
-    public void dissociateFromSeries( GeoSeries s ) {
-        if ( !this.series.contains( s ) ) {
-            throw new IllegalArgumentException( this + " does not have a reference to " + s );
-        }
-        this.series.remove( s );
     }
 
 }
