320c32336230a756d8485da53bbfd3b815e4eb0a
frances <>
2012-07-25 22:40:41 +0000

    I modified code to use the new class Gemma.DataFilterCombo.

--- gemma-web/src/main/webapp/scripts/ajax/entities/experiment/ExpressionExperimentManage.js
+++ gemma-web/src/main/webapp/scripts/ajax/entities/experiment/ExpressionExperimentManage.js
@@ -913,30 +913,11 @@ Gemma.EEReportGridToolbar = Ext.extend(Ext.Toolbar, {
                     }
                 });
 
-        this.limitCombo = new Ext.form.ComboBox({
-                    typeAhead : true,
-                    width : 150,
-                    triggerAction : 'all',
-                    lazyRender : true,
-                    mode : 'local',
-                    defaultValue : '50',
-                    emptyText : "Number to display",
-                    store : new Ext.data.ArrayStore({
-                                id : 0,
-                                fields : ['count', 'displayText'],
-                                data : [[50, '50 recently updated'], [100, '100 recently updated'],
-                                        [200, '200 recently updated'], [300, '300 recently updated'],
-                                        [500, '500 recently updated'], [-50, '50 oldest updates'],
-                                        [-100, '100 oldest updates'], [-200, '200 oldest updates'],
-                                        [-300, '300 oldest updates'], [-500, '500 oldest updates']]
-                            }),
-                    valueField : 'count',
-                    displayField : 'displayText',
+        this.limitCombo = new Gemma.DataFilterCombo({
                     listeners : {
                         scope : this,
                         'select' : this.filterByLimit
                     }
-
                 });
 
         this.searchCombo = new Gemma.ExperimentAndExperimentGroupCombo({
