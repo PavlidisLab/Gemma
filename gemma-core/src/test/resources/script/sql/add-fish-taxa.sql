b9090ae943abbd907874818b6d8fabc3add04093
ppavlidis <paul@chibi.ubc.ca>
2012-10-13 00:24:16 +0000

    make sure updates include adding the parent_taxon.

--- gemma-core/src/test/resources/script/sql/add-fish-taxa.sql
+++ gemma-core/src/test/resources/script/sql/add-fish-taxa.sql
@@ -12,7 +12,7 @@ insert ignore into TAXON (SCIENTIFIC_NAME,COMMON_NAME,ABBREVIATION,NCBI_ID,IS_SP
 insert ignore into TAXON (SCIENTIFIC_NAME,COMMON_NAME,NCBI_ID,IS_SPECIES,PARENT_TAXON_FK,IS_GENES_USABLE) values ("Oncorhynchus keta","chum salmon","8018",1,@r,0);
 insert ignore into TAXON (SCIENTIFIC_NAME,COMMON_NAME,ABBREVIATION,NCBI_ID,IS_SPECIES,PARENT_TAXON_FK,IS_GENES_USABLE) values ("Osmerus mordax","rainbow smelt","omor","8014",1,@r,0);
 -- this gets loaded previously without this information during tests.
-update TAXON set ABBREVIATION="ssal", COMMON_NAME="atlantic salmon", NCBI_ID="8030", IS_GENES_USABLE=0, IS_SPECIES=1 WHERE SCIENTIFIC_NAME="Salmo salar";
-update TAXON set ABBREVIATION="omyk", COMMON_NAME="rainbow trout", NCBI_ID="8022", IS_GENES_USABLE=0, IS_SPECIES=1 WHERE SCIENTIFIC_NAME="Oncorhynchus mykiss";
-update TAXON set ABBREVIATION="cclu", COMMON_NAME="whitefish", NCBI_ID="59861", IS_GENES_USABLE=0, IS_SPECIES=1 WHERE SCIENTIFIC_NAME="Coregonus clupeaformis";
-update TAXON set ABBREVIATION="omor", COMMON_NAME="rainbow smelt", NCBI_ID="8014", IS_GENES_USABLE=0, IS_SPECIES=1 WHERE SCIENTIFIC_NAME="Osmerus mordax";
+update TAXON set ABBREVIATION="ssal", COMMON_NAME="atlantic salmon", NCBI_ID="8030", IS_GENES_USABLE=0, IS_SPECIES=1,PARENT_TAXON_FK=@r WHERE SCIENTIFIC_NAME="Salmo salar";
+update TAXON set ABBREVIATION="omyk", COMMON_NAME="rainbow trout", NCBI_ID="8022", IS_GENES_USABLE=0, IS_SPECIES=1,PARENT_TAXON_FK=@r WHERE SCIENTIFIC_NAME="Oncorhynchus mykiss";
+update TAXON set ABBREVIATION="cclu", COMMON_NAME="whitefish", NCBI_ID="59861", IS_GENES_USABLE=0, IS_SPECIES=1,PARENT_TAXON_FK=@r WHERE SCIENTIFIC_NAME="Coregonus clupeaformis";
+update TAXON set ABBREVIATION="omor", COMMON_NAME="rainbow smelt", NCBI_ID="8014", IS_GENES_USABLE=0, IS_SPECIES=1,PARENT_TAXON_FK=@r WHERE SCIENTIFIC_NAME="Osmerus mordax";
