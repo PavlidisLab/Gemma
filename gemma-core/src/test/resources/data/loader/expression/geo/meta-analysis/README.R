323b1875b409ef9c5b4b18da03cfea0afebd26b8
ppavlidis <paul@chibi.ubc.ca>
2012-11-28 14:46:51 +0000

    R code for test (assumes the differential expression results are correct)

---
+++ gemma-core/src/test/resources/data/loader/expression/geo/meta-analysis/README.R
@@ -0,0 +1,25 @@
+tdw<-read.delim("test-meta-analysis-input-down.txt", header=T, row.names=1)
+tup<-read.delim("test-meta-analysis-input.up.txt", header=T, row.names=1)
+
+
+length(which (p.adjust(apply(tup, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) ), method="BH") <= 0.1))
+length(which (p.adjust(apply(tdw, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) ), method="BH") <= 0.1))
+
+apply(tup, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )
+apply(tdw, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )
+
+sort(names(which (p.adjust(apply(tup, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) ), method="BH") < 0.1)))
+sort(names(which (p.adjust(apply(tdw, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) ), method="BH") < 0.1)))
+apply(tdw, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )["BCAP31"]
+
+apply(tup, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )["GUK1"]
+apply(tdw, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )["ABCF1"]
+apply(tup, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )["KXD1"]
+apply(tdw, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )["THRA"]
+apply(tdw, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )["PPM1G"]
+apply(tup, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )["SEPW1"]
+apply(tdw, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )["BCAP31"]
+apply(tup, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )["BCAP31"]
+apply(tup, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )["CAPRIN1"]
+
+1-pchisq(-2*sum(log(c(0.17976911531153372 , 0.6371425366325183	,0.0034514804690969347))), 6)
