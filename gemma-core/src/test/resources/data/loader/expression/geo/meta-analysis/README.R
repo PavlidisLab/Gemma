557ec44ce2fbe0da20c2e9cefd76140156fc0011
ppavlidis <paul@chibi.ubc.ca>
2012-12-03 18:31:12 +0000

    script for test

--- gemma-core/src/test/resources/data/loader/expression/geo/meta-analysis/README.R
+++ gemma-core/src/test/resources/data/loader/expression/geo/meta-analysis/README.R
@@ -8,10 +8,10 @@ length(which (p.adjust(apply(tdw, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*le
 apply(tup, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )
 apply(tdw, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )
 
-sort(names(which (p.adjust(apply(tup, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) ), method="BH") < 0.1)))
-sort(names(which (p.adjust(apply(tdw, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) ), method="BH") < 0.1)))
-apply(tdw, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )["BCAP31"]
+up<-sort(names(which (p.adjust(apply(tup, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) ), method="BH") < 0.1)))
+dw<-sort(names(which (p.adjust(apply(tdw, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) ), method="BH") < 0.1)))
 
+apply(tdw, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )["BCAP31"]
 apply(tup, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )["GUK1"]
 apply(tdw, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )["ABCF1"]
 apply(tup, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )["KXD1"]
@@ -22,4 +22,11 @@ apply(tdw, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )["BCAP31"]
 apply(tup, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )["BCAP31"]
 apply(tup, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )["CAPRIN1"]
 
+# trouble genes, that don't necessarily show up.
+apply(tup, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )["ACLY"]
+apply(tup, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )["ACTA2"]
+apply(tdw, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )["ABCF1"]
+apply(tdw, 1, function(x) 1 - pchisq(-2*sum(log(x)), 2*length(x)) )["ACO2" ]
+
 1-pchisq(-2*sum(log(c(0.17976911531153372 , 0.6371425366325183	,0.0034514804690969347))), 6)
+ 
