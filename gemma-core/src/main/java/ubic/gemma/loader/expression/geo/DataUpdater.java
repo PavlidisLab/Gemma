f67e9dab86014b595dea3d6d7c68e22575ec95a0
ppavlidis <paul@chibi.ubc.ca>
2013-02-25 20:21:43 +0000

    changes to help with loading of rna-seq data.

--- gemma-core/src/main/java/ubic/gemma/loader/expression/geo/DataUpdater.java
+++ gemma-core/src/main/java/ubic/gemma/loader/expression/geo/DataUpdater.java
@@ -16,14 +16,20 @@ package ubic.gemma.loader.expression.geo;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import ubic.basecode.dataStructure.matrix.DenseDoubleMatrix;
+import ubic.basecode.dataStructure.matrix.DoubleMatrix;
 import ubic.basecode.io.ByteArrayConverter;
 import ubic.gemma.analysis.preprocess.ProcessedExpressionDataVectorCreateService;
 import ubic.gemma.analysis.preprocess.SampleCoexpressionMatrixService;
@@ -39,13 +45,18 @@ import ubic.gemma.model.common.auditAndSecurity.eventType.DataAddedEvent;
 import ubic.gemma.model.common.auditAndSecurity.eventType.DataReplacedEvent;
 import ubic.gemma.model.common.auditAndSecurity.eventType.ExpressionExperimentPlatformSwitchEvent;
 import ubic.gemma.model.common.description.LocalFile;
+import ubic.gemma.model.common.quantitationtype.GeneralType;
+import ubic.gemma.model.common.quantitationtype.PrimitiveType;
 import ubic.gemma.model.common.quantitationtype.QuantitationType;
+import ubic.gemma.model.common.quantitationtype.ScaleType;
+import ubic.gemma.model.common.quantitationtype.StandardQuantitationType;
 import ubic.gemma.model.expression.arrayDesign.ArrayDesign;
 import ubic.gemma.model.expression.arrayDesign.ArrayDesignService;
 import ubic.gemma.model.expression.bioAssay.BioAssay;
 import ubic.gemma.model.expression.bioAssayData.BioAssayDimension;
 import ubic.gemma.model.expression.bioAssayData.BioAssayDimensionService;
 import ubic.gemma.model.expression.bioAssayData.RawExpressionDataVector;
+import ubic.gemma.model.expression.biomaterial.BioMaterial;
 import ubic.gemma.model.expression.designElement.CompositeSequence;
 import ubic.gemma.model.expression.experiment.ExpressionExperiment;
 import ubic.gemma.model.genome.Taxon;
@@ -190,15 +201,6 @@ public class DataUpdater {
     }
 
     /**
-     * @param ee
-     * @param qt
-     * @return
-     */
-    public int deleteData( ExpressionExperiment ee, QuantitationType qt ) {
-        return this.experimentService.removeData( ee, qt );
-    }
-
-    /**
      * Add an additional data (with associated quantitation type) to the selected experiment. Will do postprocessing if
      * the data quantitationtype is 'preferred', but if there is already a preferred quantitation type, an error will be
      * thrown.
@@ -268,6 +270,15 @@ public class DataUpdater {
 
         experimentService.update( ee );
         return ee;
+    }
+
+    /**
+     * @param ee
+     * @param qt
+     * @return
+     */
+    public int deleteData( ExpressionExperiment ee, QuantitationType qt ) {
+        return this.experimentService.removeData( ee, qt );
     }
 
     /**
@@ -351,6 +362,57 @@ public class DataUpdater {
     }
 
     /**
+     * Replaces data.
+     * 
+     * @param ee
+     * @param targetArrayDesign
+     * @param countMatrix Representing 'raw' counts (added after rpkm, if provided)
+     * @param rpkmMatrix Representing per-gene normalized data, which is treated as the 'preferred' data. If this is
+     *        provided, all the other data will be removed.
+     */
+    public void addCountDataMatricesToExperiment( ExpressionExperiment ee, ArrayDesign targetArrayDesign,
+            DoubleMatrix<String, String> countMatrix, DoubleMatrix<String, String> rpkmMatrix ) {
+        // make the proper matrices we need for loading.
+
+        if ( countMatrix == null && rpkmMatrix == null )
+            throw new IllegalArgumentException( "You must provide either counts or rpkm or both" );
+
+        if ( rpkmMatrix != null ) {
+            /*
+             * Treat this as the preferred data, so we have to do it first.
+             */
+
+            DoubleMatrix<CompositeSequence, BioMaterial> properRPKMMatrix = matchElementsToRowNames( targetArrayDesign,
+                    rpkmMatrix );
+            matchBioMaterialsToColNames( ee, rpkmMatrix, properRPKMMatrix );
+            QuantitationType rpkmqt = makeQt( true );
+            rpkmqt.setIsRatio( false );
+            rpkmqt.setName( "RPKM" );
+            rpkmqt.setDescription( "Reads (or fragments) per kb of gene model per million reads" );
+            rpkmqt.setIsBackgroundSubtracted( false );
+            rpkmqt.setIsNormalized( true );
+            ExpressionDataDoubleMatrix rpkmEEMatrix = new ExpressionDataDoubleMatrix( ee, rpkmqt, properRPKMMatrix );
+            // note call to replace
+            ee = replaceData( ee, targetArrayDesign, rpkmEEMatrix );
+        }
+
+        if ( countMatrix != null ) {
+
+            DoubleMatrix<CompositeSequence, BioMaterial> properCountMatrix = matchElementsToRowNames(
+                    targetArrayDesign, countMatrix );
+            matchBioMaterialsToColNames( ee, countMatrix, properCountMatrix );
+            QuantitationType countqt = makeQt( false );
+            countqt.setName( "Counts" );
+            countqt.setDescription( "Read counts" );
+            countqt.setIsBackgroundSubtracted( false );
+            countqt.setIsNormalized( false );
+            ExpressionDataDoubleMatrix countEEMatrix = new ExpressionDataDoubleMatrix( ee, countqt, properCountMatrix );
+            ee = addData( ee, targetArrayDesign, countEEMatrix );
+        }
+
+    }
+
+    /**
      * @param ee
      * @param note
      * @param replace if true, use a DataReplacedEvent; otherwise DataAddedEvent.
@@ -417,6 +479,131 @@ public class DataUpdater {
     }
 
     /**
+     * @param preferred
+     * @return
+     */
+    private QuantitationType makeQt( boolean preferred ) {
+        QuantitationType qt = QuantitationType.Factory.newInstance();
+        qt.setGeneralType( GeneralType.QUANTITATIVE );
+        qt.setScale( ScaleType.LINEAR );
+        qt.setIsBackground( false );
+        qt.setIsRatio( false );
+        qt.setIsBackgroundSubtracted( true );
+        qt.setIsNormalized( true );
+        qt.setIsMaskedPreferred( true );
+        qt.setIsPreferred( preferred );
+        qt.setIsBatchCorrected( false );
+        qt.setType( StandardQuantitationType.AMOUNT );
+        qt.setRepresentation( PrimitiveType.DOUBLE );
+        return qt;
+    }
+
+    /**
+     * @param ee
+     * @param rawMatrix
+     * @param finalMatrix
+     */
+    private void matchBioMaterialsToColNames( ExpressionExperiment ee, DoubleMatrix<String, String> rawMatrix,
+            DoubleMatrix<CompositeSequence, BioMaterial> finalMatrix ) {
+        // match column names to the samples. can have any order so be careful.
+        List<String> colNames = rawMatrix.getColNames();
+        Map<String, BioMaterial> bmMap = new HashMap<String, BioMaterial>();
+        Collection<BioAssay> bioAssays = ee.getBioAssays();
+        for ( BioAssay bioAssay : bioAssays ) {
+            Collection<BioMaterial> samplesUsed = bioAssay.getSamplesUsed();
+            assert samplesUsed.size() == 1;
+
+            BioMaterial bm = samplesUsed.iterator().next();
+            if ( bmMap.containsKey( bm.getName() ) ) {
+                // this might not actually be an error - but just in case...
+                throw new IllegalStateException( "Two biomaterials from the same experiment with the same name " );
+            }
+
+            bmMap.put( bm.getName(), bm );
+
+            if ( bioAssay.getAccession() != null ) {
+                // e.g. GSM123455
+                String accession = bioAssay.getAccession().getAccession();
+                if ( bmMap.containsKey( accession ) ) {
+                    throw new IllegalStateException( "Two bioassays with the same accession" );
+                }
+                bmMap.put( accession, bm );
+            }
+
+            // I think it will always be null, if it is from GEO anyway.
+            if ( bm.getExternalAccession() != null ) {
+                if ( bmMap.containsKey( bm.getExternalAccession().getAccession() ) ) {
+                    throw new IllegalStateException( "Two biomaterials with the same accession" );
+                }
+                bmMap.put( bm.getExternalAccession().getAccession(), bm );
+            }
+        }
+
+        List<BioMaterial> newColNames = new ArrayList<BioMaterial>();
+        for ( String colName : colNames ) {
+            BioMaterial bm = bmMap.get( colName );
+            if ( bm == null ) {
+                throw new IllegalStateException( "Could not match a column name to a biomaterial: " + colName );
+            }
+            newColNames.add( bm );
+        }
+        finalMatrix.setColumnNames( newColNames );
+    }
+
+    /**
+     * @param targetArrayDesign
+     * @param rawMatrix
+     * @return matrix with row names fixed up. ColumnNames still need to be done.
+     */
+    private DoubleMatrix<CompositeSequence, BioMaterial> matchElementsToRowNames( ArrayDesign targetArrayDesign,
+            DoubleMatrix<String, String> rawMatrix ) {
+
+        Map<String, CompositeSequence> pnmap = new HashMap<String, CompositeSequence>();
+
+        for ( CompositeSequence cs : targetArrayDesign.getCompositeSequences() ) {
+            pnmap.put( cs.getName(), cs );
+        }
+        int failedMatch = 0;
+        int timesWarned = 0;
+        List<CompositeSequence> newRowNames = new ArrayList<CompositeSequence>();
+        List<String> usableRowNames = new ArrayList<String>();
+        for ( String rowName : rawMatrix.getRowNames() ) {
+            CompositeSequence cs = pnmap.get( rowName );
+            if ( cs == null ) {
+                /*
+                 * This might be okay, but we not too much
+                 */
+                failedMatch++;
+                if ( timesWarned < 20 ) {
+                    log.warn( "No platform match to element named: " + rowName );
+                    timesWarned++;
+                }
+                if ( timesWarned == 20 ) {
+                    log.warn( "Further warnings suppressed" );
+                }
+            }
+            usableRowNames.add( rowName );
+            newRowNames.add( cs );
+        }
+
+        if ( usableRowNames.isEmpty() ) {
+            throw new IllegalArgumentException( "None of the rows matched the given platform elements" );
+        }
+        DoubleMatrix<CompositeSequence, BioMaterial> finalMatrix;
+        if ( failedMatch > 0 ) {
+            log.warn( failedMatch + "/" + rawMatrix.rows()
+                    + " elements could not be matched to the platform. Lines that did not match will be ignore." );
+            DoubleMatrix<String, String> useableData = rawMatrix.subsetRows( usableRowNames );
+            finalMatrix = new DenseDoubleMatrix<CompositeSequence, BioMaterial>( useableData.getRawMatrix() );
+
+        } else {
+            finalMatrix = new DenseDoubleMatrix<CompositeSequence, BioMaterial>( rawMatrix.getRawMatrix() );
+            finalMatrix.setRowNames( newRowNames );
+        }
+        return finalMatrix; // not completel final.
+    }
+
+    /**
      * determine the target array design. We use filtered versions of these platforms from GEO.
      * 
      * @param primaryTaxon
@@ -464,4 +651,5 @@ public class DataUpdater {
 
         return targetPlatform;
     }
+
 }
