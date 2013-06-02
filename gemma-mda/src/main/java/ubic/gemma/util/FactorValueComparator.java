17ae7bbacc169db43107bed108be95aa9b3c8c30
ppavlidis <paul@chibi.ubc.ca>
2013-06-02 00:56:17 +0000

    null check

--- gemma-mda/src/main/java/ubic/gemma/util/FactorValueComparator.java
+++ gemma-mda/src/main/java/ubic/gemma/util/FactorValueComparator.java
@@ -16,10 +16,18 @@ public class FactorValueComparator implements Comparator<FactorValue> {
 
     /*
      * (non-Javadoc)
+     * 
      * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
      */
     @Override
     public int compare( FactorValue v1, FactorValue v2 ) {
+        if ( v1 == null && v2 == null ) {
+            return 0;
+        } else if ( v1 == null ) {
+            return -1;
+        } else if ( v2 == null ) {
+            return 1;
+        }
         String s1 = v1.toString();
         String s2 = v2.toString();
         return s1.compareTo( s2 );
