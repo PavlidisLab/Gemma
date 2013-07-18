19c1a47ebc1bbffe67b19bd6cfaa073539170933
ptan <patrick.tan@ubc.ca>
2013-07-18 23:18:26 +0000

    Run GemmaCLI when there's no parameter provided.

--- gemma-core/etc/scripts/gemmaCli.sh
+++ gemma-core/etc/scripts/gemmaCli.sh
@@ -11,4 +11,12 @@ APPARGS=$@
 JAVACMD="${JAVA_HOME}/bin/java $JAVA_OPTS"
 
 CMD="$JAVACMD -classpath ${GEMMA_LIB}:${JARS} $APPARGS"
+CMD_DEFAULT="$JAVACMD -classpath ${GEMMA_LIB}:${JARS} ubic.gemma.apps.GemmaCLI"
+
+if [ -z "$1" ]
+    then
+        $CMD_DEFAULT
+        exit
+fi
+
 $CMD
