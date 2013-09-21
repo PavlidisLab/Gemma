44f17be35cc2d3b7e8ef5ae630a80da290e99f2b
ppavlidis <paul@chibi.ubc.ca>
2013-09-21 04:05:37 +0000

    another class that has to be outside of the test build path

--- gemma-core/src/main/java/ubic/gemma/testing/DummyMailSender.java
+++ gemma-core/src/main/java/ubic/gemma/testing/DummyMailSender.java
@@ -12,7 +12,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package ubic.gemma.util;
+package ubic.gemma.testing;
 
 import javax.mail.internet.MimeMessage;
 
