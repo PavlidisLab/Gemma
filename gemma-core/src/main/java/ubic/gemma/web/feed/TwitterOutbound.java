79e3820cc83d2e409306643f83fce63046f819df
cmcdonald <>
2014-01-17 20:28:26 +0000

    testing out some hardcoded keys to make sure I am not insane

--- gemma-core/src/main/java/ubic/gemma/web/feed/TwitterOutbound.java
+++ gemma-core/src/main/java/ubic/gemma/web/feed/TwitterOutbound.java
@@ -108,20 +108,32 @@ public class TwitterOutbound {
 
         if ( StringUtils.isNotBlank( feed ) ) {
             log.info( "Sending out tweet: '" + feed + "'" );
+            /*
             String consumerKey = Settings.getString( "gemma.twitter.consumer-key" );
             String consumerSecret = Settings.getString( "gemma.twitter.consumer-secret" );
             String accessToken = Settings.getString( "gemma.twitter.access-token" );
             String accessTokenSecret = Settings.getString( "gemma.twitter.access-token-secret" );
+            */
+            String consumerKey = "Ib5GJVyXIdKa3iECVurA";
+            String consumerSecret = "66roFaVk4HTmrivbOtx5YvtFRH8VMUdOK8vvuipQWs";
+            String accessToken = "322289197-qf41g0gqlepSBW1fYsaXHZ5Sspxauiw734e5TPu2";
+            String accessTokenSecret = "3o4HWskvacU4OCNiZRR97Y1REL8prAHh8pxmg5nVp7iPQ";
 
             Twitter twitter = new TwitterTemplate( consumerKey, consumerSecret, accessToken, accessTokenSecret );
             StatusDetails metadata = new StatusDetails();
             metadata.setWrapLinks( true );
+            
+            
             try{
-                Tweet tweet = twitter.timelineOperations().updateStatus( feed, metadata );
+                Tweet tweet = twitter.timelineOperations().updateStatus( feed, metadata);
                 log.info("tweet info:" + tweet.toString());
             }
             catch(Exception e){
+            	
               	log.info(e.toString());
+              	
+              	e.printStackTrace();
+              	
             }
         }
 
