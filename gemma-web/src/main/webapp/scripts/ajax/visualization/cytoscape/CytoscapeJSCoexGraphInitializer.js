c2fbb8415f32cce95be57805159572f1c1db65e4
ppavlidis <paul@chibi.ubc.ca>
2014-04-28 00:48:06 +0000

    zoom tool shows up correctly now

--- gemma-web/src/main/webapp/scripts/ajax/visualization/cytoscape/CytoscapeJSCoexGraphInitializer.js
+++ gemma-web/src/main/webapp/scripts/ajax/visualization/cytoscape/CytoscapeJSCoexGraphInitializer.js
@@ -148,31 +148,31 @@ Gemma.CytoscapeJSCoexGraphInitializer = function( visualization, graphData, read
       }
    } );
 
-   // this works but the fonts for the widget labels do not.
+    
    // http://plugins.jquery.com/cytoscape.js-panzoom/
    // the default values of each option are outlined below:
-//   visualization.cytoscapePanzoom( {
-//      zoomFactor : 0.1, // zoom factor per zoom tick
-//      zoomDelay : 45, // how many ms between zoom ticks
-//      minZoom : 0.1, // min zoom level
-//      maxZoom : 10, // max zoom level
-//      fitPadding : 50, // padding when fitting
-//      panSpeed : 10, // how many ms in between pan ticks
-//      panDistance : 10, // max pan distance per tick
-//      panDragAreaSize : 75, // the length of the pan drag box in which the vector for panning is calculated (bigger =
-//      // finer control of pan speed and direction)
-//      panMinPercentSpeed : 0.25, // the slowest speed we can pan by (as a percent of panSpeed)
-//      panInactiveArea : 8, // radius of inactive area in pan drag box
-//      panIndicatorMinOpacity : 0.5, // min opacity of pan indicator (the draggable nib); scales from this to 1.0
-//      autodisableForMobile : true, // disable the panzoom completely for mobile (since we don't really need it with
-//      // gestures like pinch to zoom)
-//
-//      // icon class names
-//      sliderHandleIcon : 'fa fa-minus',
-//      zoomInIcon : 'fa fa-plus',
-//      zoomOutIcon : 'fa fa-minus',
-//      resetIcon : 'fa fa-expand'
-//   } );
+   visualization.cytoscapePanzoom( {
+      zoomFactor : 0.1, // zoom factor per zoom tick
+      zoomDelay : 45, // how many ms between zoom ticks
+      minZoom : 0.1, // min zoom level
+      maxZoom : 10, // max zoom level
+      fitPadding : 50, // padding when fitting
+      panSpeed : 10, // how many ms in between pan ticks
+      panDistance : 10, // max pan distance per tick
+      panDragAreaSize : 75, // the length of the pan drag box in which the vector for panning is calculated (bigger =
+      // finer control of pan speed and direction)
+      panMinPercentSpeed : 0.25, // the slowest speed we can pan by (as a percent of panSpeed)
+      panInactiveArea : 8, // radius of inactive area in pan drag box
+      panIndicatorMinOpacity : 0.5, // min opacity of pan indicator (the draggable nib); scales from this to 1.0
+      autodisableForMobile : true, // disable the panzoom completely for mobile (since we don't really need it with
+      // gestures like pinch to zoom)
+
+      // icon class names
+      sliderHandleIcon : 'fa fa-minus',
+      zoomInIcon : 'fa fa-plus',
+      zoomOutIcon : 'fa fa-minus',
+      resetIcon : 'fa fa-expand'
+   } );
    
    // I did not get this to work
 //   visualization.cytoscapeNavigator({
