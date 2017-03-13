4b954a7e805a4bc6a8eadc5b8cd0c3482d015fa8
Stepan Tesar <tesar.stepann@gmail.com>
2017-03-13 13:40:23 -0700

    Work on git Issue #1 -  Code cleanup

--- gemma-model/src/main/java/ubic/gemma/model/genome/gene/phenotype/valueObject/EvidenceSourceValueObject.java
+++ gemma-model/src/main/java/ubic/gemma/model/genome/gene/phenotype/valueObject/EvidenceSourceValueObject.java
@@ -2,12 +2,10 @@ package ubic.gemma.model.genome.gene.phenotype.valueObject;
 
 import ubic.gemma.model.common.description.DatabaseEntry;
 import ubic.gemma.model.common.description.DatabaseEntryValueObject;
+import ubic.gemma.model.common.description.ExternalDatabaseValueObject;
 
 public class EvidenceSourceValueObject extends DatabaseEntryValueObject {
 
-    /**
-     * 
-     */
     private static final long serialVersionUID = 4085159943613845499L;
     // used by neurocarta to find the url of an evidence source
     private String externalUrl = "";
