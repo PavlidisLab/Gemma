3d1cdbaf30a313b42a12c4b83104e7aceb7b51f6
Stepan Tesar <tesar.stepann@gmail.com>
2017-03-22 11:55:22 -0700

    Work on git Issue #1 - Web - Finished basic status lines in curation tab

--- gemma-web/src/main/webapp/scripts/api/entities/CurationTools.js
+++ gemma-web/src/main/webapp/scripts/api/entities/CurationTools.js
@@ -1,4 +1,4 @@
-Ext.namespace( 'Gemma' );
+Ext.namespace('Gemma');
 
 /**
  *
@@ -8,33 +8,93 @@ Ext.namespace( 'Gemma' );
  * @class Gemma.CurationTools
  * @extends Ext.Panel
  */
-Gemma.CurationTools = Ext.extend( Ext.Panel, {
+Gemma.CurationTools = Ext.extend(Ext.Panel, {
     curatable: null,
 
-    border : false,
-    defaultType : 'box',
-    defaults : {
-        border : false
+    border: false,
+    defaultType: 'box',
+    defaults: {
+        border: false
     },
-    padding : 10,
+    padding: 10,
 
-    initComponent : function() {
-        Gemma.CurationTools.superclass.initComponent.call( this );
+    initComponent: function () {
+        Gemma.CurationTools.superclass.initComponent.call(this);
 
-        this.add( {
-            html :
-            '<div><h4>Curation status: </h4>' + this.getNeedsAttentionStatusHtml() + '</div>' +
-            '<div><h4>Troubled status: </h4>' + this.getTroubleStatusHtml() + '</div>'
-        } );
-    },
+        this.add({
+            html:
+                '<div class="v-padded"><span class="bold width110">Troubled status: </span>' + this.getTroubleStatusHtml() + '</div>' +
+                '<div class="v-padded"><span class="bold width110">Curation status: </span>' + this.getNeedsAttentionStatusHtml() + '</div>'
 
-
-    getNeedsAttentionStatusHtml : function(){
-        return this.curatable.needsAttention ? '<span class="red">Needs attention</span>' : '<span class="green">OK</span>';
+        });
     },
 
-    getTroubleStatusHtml : function(){
-        return this.curatable.troubled ? '<span class="red"><img src="/Gemma/images/icons/stop.png"/> Troubled</span>' : '<span class="green">Not Troubled</span>';
+
+    /**
+     * Composes the string for curation attention description
+     * @returns {string} string containing the status with appropriate icon, color, date and performer.
+     */
+    getNeedsAttentionStatusHtml: function () {
+        //Base status
+        var str = this.curatable.needsAttention
+            ?   '<span class="gold width130"><i class="fa fa-exclamation-circle fa-lg fa-fw"></i>Needs attention</span>'
+            :   '<span class="green width130"><i class="fa fa-check-circle-o fa-lg fa-fw"></i>OK</span>';
+
+        //Status description
+        if (this.curatable.lastNeedsAttentionEvent) {
+            str +=  '<span class="dark-gray">' +
+                        ' <i class="fa fa-calendar"></i> ' +
+                        'As of ' + this.curatable.lastNeedsAttentionEvent.date.toLocaleString() +
+                        ' <i class="fa fa-user"></i> ' +
+                        ' set by: ' + this.curatable.lastNeedsAttentionEvent.performer +
+                        ' <i class="fa fa-pencil"></i>' +
+                        ' Details: ' + this.getNeedsAttentionNonNullDescription(this.curatable.lastNeedsAttentionEvent) +
+                    '</span>';
+        }
+        return str;
+    },
+
+    /**
+     * Composes the string for trouble status description
+     * @returns {string} string containing the status with appropriate icon, color, date and performer.
+     */
+    getTroubleStatusHtml: function () {
+        //Base status
+        var str = this.curatable.troubled
+            ?   '<span class="red width130"><i class="fa fa-exclamation-triangle fa-lg fa-fw"></i>Troubled</span>'
+            :   '<span class="green width130"><i class="fa fa-check-circle fa-lg fa-fw"></i>Not Troubled</span>';
+
+        //Status details
+        if (this.curatable.lastTroubledEvent) {
+            str +=  '<span class="dark-gray">' +
+                        ' <i class="fa fa-calendar"></i> ' +
+                        'As of ' + this.curatable.lastTroubledEvent.date.toLocaleString() +
+                        ' <i class="fa fa-user"></i> ' +
+                        ' set by: ' + this.curatable.lastTroubledEvent.performer +
+                        ' <i class="fa fa-pencil"></i>' +
+                        ' Details: ' + this.curatable.lastTroubledEvent.detail +
+                    '</span>'
+        }
+
+        return str;
+    },
+
+    /**
+     * Iterates through the events detail and note, and creates an adequate description from the found strings.
+     * @param NeedsAttentionEvent the event to be described.
+     * @returns {string} description of the given event, or "Unspecified" if no suitable string is found.
+     */
+    getNeedsAttentionNonNullDescription: function(NeedsAttentionEvent){
+        var str = "Unspecified";
+
+        if(NeedsAttentionEvent.detail) str = NeedsAttentionEvent.detail;
+        if(NeedsAttentionEvent.note) str = NeedsAttentionEvent.note;
+
+        if(str.length > 100){
+            str = str.substring(0,99) + "...";
+        }
+
+        return str;
     }
 
 });
