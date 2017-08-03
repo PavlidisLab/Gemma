db864befcedf8a69156e35a1f5f4c9065c6bfe57
Stepan Tesar <tesar.stepann@gmail.com>
2017-08-03 14:00:26 -0700

    Add taxa API

---
+++ gemma-web/src/main/java/ubic/gemma/web/services/rest/TaxonWebService.java
@@ -0,0 +1,79 @@
+package ubic.gemma.web.services.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ubic.gemma.persistence.service.genome.taxon.TaxonService;
+import ubic.gemma.web.services.rest.util.Responder;
+import ubic.gemma.web.services.rest.util.ResponseDataObject;
+import ubic.gemma.web.services.rest.util.WebServiceWithFiltering;
+import ubic.gemma.web.services.rest.util.args.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import java.text.ParseException;
+
+/**
+ * RESTful interface for taxa.
+ *
+ * @author tesarst
+ */
+@Service
+@Path("/taxa")
+public class TaxonWebService extends WebServiceWithFiltering{
+
+    private TaxonService taxonService;
+
+    /**
+     * Required by spring
+     */
+    public TaxonWebService() {
+    }
+
+    /**
+     * Constructor for service autowiring
+     */
+    @Autowired
+    public TaxonWebService( TaxonService taxonService ) {
+        this.taxonService = taxonService;
+    }
+
+    /**
+     * Unlike most other web services, Taxa do not offer any advanced filtering or sorting functionality.
+     * The reason for this is that Taxa are a fairly small set of objects that rarely change.
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public ResponseDataObject all(
+            @Context final HttpServletResponse sr // The servlet response, needed for response code setting.
+    ) {
+        // Uses this.loadVOsPreFilter(...)
+        return super.all( null, null, null, null, sr );
+    }
+
+    /**
+     * Retrieves single taxon based on the given identifier.
+     *
+     * @param taxonArg can either be Taxon ID or one of its string identifiers:
+     *                 scientific name, common name, abbreviation. Using the ID is most efficient.
+     */
+    @GET
+    @Path("/{taxonArg: [a-zA-Z0-9\\.]+}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public ResponseDataObject taxon( // Params:
+            @PathParam("taxonArg") TaxonArg<Object> taxonArg, // Required
+            @Context final HttpServletResponse sr // The servlet response, needed for response code setting.
+    ) {
+        Object response = taxonArg.getValueObject( taxonService );
+        return this.autoCodeResponse( taxonArg, response, sr );
+    }
+
+    @Override
+    protected ResponseDataObject loadVOsPreFilter( FilterArg filter, IntArg offset, IntArg limit, SortArg sort,
+            HttpServletResponse sr ) throws ParseException {
+        return Responder.autoCode( taxonService.loadAllValueObjects(), sr );
+    }
+}
