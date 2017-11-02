948c2f5c03edcb735c389d2cc63208790ffbdac2
Stepan Tesar <tesar.stepann@gmail.com>
2017-11-02 11:12:02 -0700

    Fix basic FVVO not being properly serializable
    Signed-off-by: Stepan Tesar <tesar.stepann@gmail.com>

--- gemma-core/src/main/java/ubic/gemma/model/common/measurement/MeasurementValueObject.java
+++ gemma-core/src/main/java/ubic/gemma/model/common/measurement/MeasurementValueObject.java
@@ -6,16 +6,43 @@ import ubic.gemma.model.IdentifiableValueObject;
 public class MeasurementValueObject extends IdentifiableValueObject {
 
     private String value;
+    private String unit;
+    private Long unitId;
+    private String type;
+    private String representation;
 
     public MeasurementValueObject() {
     }
 
-    public MeasurementValueObject( Long id, Measurement measurement ) {
-        super( id );
+    public MeasurementValueObject( Measurement measurement ) {
+        super( measurement.getId() );
         this.value = measurement.getValue();
+        if ( measurement.getUnit() != null ) {
+            this.unit = measurement.getUnit().getUnitNameCV();
+            this.unitId = measurement.getUnit().getId();
+        }
+        this.type = measurement.getType() == null ? null : measurement.getType().getValue();
+        this.representation =
+                measurement.getRepresentation() == null ? null : measurement.getRepresentation().getValue();
     }
 
     public String getValue() {
         return value;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public Long getUnitId() {
+        return unitId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getRepresentation() {
+        return representation;
     }
 }
