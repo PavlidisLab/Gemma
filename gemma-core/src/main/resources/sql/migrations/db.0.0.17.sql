237e3a7726adf16ace0b4fa3765d1e679e9e0038
Stepan Tesar <tesar.stepann@gmail.com>
2018-01-23 13:33:20 -0800

    Database cleanup

--- gemma-core/src/main/resources/sql/migrations/db.0.0.17.sql
+++ gemma-core/src/main/resources/sql/migrations/db.0.0.17.sql
@@ -1,23 +1,27 @@
 delete from AUDIT_EVENT where AUDIT_TRAIL_FK in (
   select AUDIT_TRAIL_FK from PROTOCOL where ID in
   (
-    select PROTOCOL_FK from ANALYSIS where class='ProbeCoexpressionAnalysisImpl'
+    select PROTOCOL_FK from ANALYSIS where class='ProbeCoexpressionAnalysisImpl' or class='GeneCoexpressionAnalysisImpl'
   )
 );
 
 delete from AUDIT_TRAIL where ID in (
   select AUDIT_TRAIL_FK from PROTOCOL where ID in
   (
-    select PROTOCOL_FK from ANALYSIS where class='ProbeCoexpressionAnalysisImpl'
+    select PROTOCOL_FK from ANALYSIS where class='ProbeCoexpressionAnalysisImpl' or class='GeneCoexpressionAnalysisImpl'
   )
 );
 
 delete from PROTOCOL where ID in (
-  select PROTOCOL_FK from ANALYSIS where class='ProbeCoexpressionAnalysisImpl'
+  select PROTOCOL_FK from ANALYSIS where class='ProbeCoexpressionAnalysisImpl' or class='GeneCoexpressionAnalysisImpl'
 );
 
 -- Unused table
 DROP TABLE IF EXISTS COEXPRESSION_PROBE;
+DROP TABLE IF EXISTS HUMAN_GENE_CO_EXPRESSION;
+DROP TABLE IF EXISTS RAT_GENE_CO_EXPRESSION;
+DROP TABLE IF EXISTS MOUSE_GENE_CO_EXPRESSION;
+DROP TABLE IF EXISTS OTHER_GENE_CO_EXPRESSION;
 
 -- Non-existing instances
 DELETE FROM ANALYSIS WHERE class='ProbeCoexpressionAnalysisImpl';
