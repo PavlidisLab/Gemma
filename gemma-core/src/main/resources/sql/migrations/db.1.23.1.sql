69903f0070feaef54066fe2c506b03afe1882f58
Stepan Tesar <tesar.stepann@gmail.com>
2018-04-11 14:00:13 -0700

    Drop unused columns from the taxon table

--- gemma-core/src/main/resources/sql/migrations/db.1.23.1.sql
+++ gemma-core/src/main/resources/sql/migrations/db.1.23.1.sql
@@ -1,3 +1,4 @@
 ## start naming files to reference the release version and git tag at which point the code will require the changes in this file
 
 update CHROMOSOME_FEATURE set class = "GeneProduct" where class = "GeneProductImpl";
+alter table TAXON drop COLUMN `UNIGENE_PREFIX`, drop COLUMN `SWISS_PROT_SUFFIX`;
