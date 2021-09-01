df7e8c03b4f21dd75c0aa7a81a6643001fa74d4f
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2021-09-01 13:12:04 -0700

    Remove jetbrains annotations dependency

--- gemma-web/src/main/java/ubic/gemma/web/services/rest/util/args/AbstractArg.java
+++ gemma-web/src/main/java/ubic/gemma/web/services/rest/util/args/AbstractArg.java
@@ -1,14 +1,15 @@
 package ubic.gemma.web.services.rest.util.args;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import org.jetbrains.annotations.NotNull;
+import lombok.NonNull;
 import ubic.gemma.web.services.rest.util.MalformedArgException;
 
 /**
  * Base class for non Object-specific functionality argument types, that can be malformed on input (E.g an argument
  * representing a number was a non-numeric string in the request).
  *
- * The {@link Schema} annotation ensures that custom args are represented by a string in the OpenAPI specification.
+ * The {@link Schema} annotation used in sub-classes ensures that custom args are represented by a string in the OpenAPI
+ * specification.
  *
  * @author tesarst
  */
@@ -18,16 +19,27 @@ abstract class AbstractArg<T> implements Arg<T> {
 
     /* if this is malformed */
     private final boolean malformed;
-    private String errorMessage = "";
+    private String errorMessage;
     private Throwable exception;
 
-    AbstractArg( T value ) {
+    /**
+     * Constructor for well-formed value.
+     *
+     * Note that well-formed values can never be null. Although, the argument itself can be null to represent a value
+     * that is omitted by the request.
+     *
+     * @param value a well-formed value which cannot be null
+     */
+    AbstractArg( @NonNull T value ) {
         this.value = value;
         this.malformed = false;
     }
 
     /**
-     * Constructor used to inform that the received argument was not well-formed.
+     * Constructor used to inform that the received argument is malformed.
+     *
+     * If this is used, subsequent call to {@link #getValue()} will raise a {@link MalformedArgException} which will in
+     * turn converted to a proper response as it inherits {@link javax.ws.rs.WebApplicationException}.
      *
      * @param errorMessage the error message to be displayed to the client.
      * @param exception    the exception that the client should be informed about.
@@ -39,11 +51,20 @@ abstract class AbstractArg<T> implements Arg<T> {
         this.errorMessage = errorMessage;
     }
 
+    /**
+     * Test if this argument is malformed without triggering a {@link MalformedArgException}.
+     * @return
+     */
     public final boolean isMalformed() {
         return malformed;
     }
 
-    @NotNull
+    /**
+     * Obtain the value represented by this argument.
+     *
+     * @return the value represented by this argument, which can never be null
+     * @throws MalformedArgException if this arg is malformed
+     */
     @Override
     public final T getValue() throws MalformedArgException {
         if ( this.malformed ) {
