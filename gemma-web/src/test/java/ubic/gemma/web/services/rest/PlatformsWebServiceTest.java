eba666afa87b6dd80a44633890f110b6c520c15a
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2021-09-14 11:41:33 -0700

    Add missing fixtures for PlatformsWebServiceTest

--- gemma-web/src/test/java/ubic/gemma/web/services/rest/PlatformsWebServiceTest.java
+++ gemma-web/src/test/java/ubic/gemma/web/services/rest/PlatformsWebServiceTest.java
@@ -1,11 +1,17 @@
 package ubic.gemma.web.services.rest;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletResponse;
+import ubic.gemma.model.expression.arrayDesign.ArrayDesign;
 import ubic.gemma.model.expression.arrayDesign.ArrayDesignValueObject;
 import ubic.gemma.model.expression.designElement.CompositeSequenceValueObject;
+import ubic.gemma.model.expression.experiment.ExpressionExperiment;
 import ubic.gemma.model.expression.experiment.ExpressionExperimentValueObject;
+import ubic.gemma.persistence.service.expression.arrayDesign.ArrayDesignService;
+import ubic.gemma.persistence.service.expression.experiment.ExpressionExperimentService;
 import ubic.gemma.web.services.rest.util.PaginatedResponseDataObject;
 import ubic.gemma.web.services.rest.util.args.IntArg;
 import ubic.gemma.web.services.rest.util.args.PlatformArg;
@@ -19,6 +25,28 @@ public class PlatformsWebServiceTest extends BaseSpringWebTest {
 
     @Autowired
     private PlatformsWebService platformsWebService;
+
+    @Autowired
+    private ExpressionExperimentService eeService;
+
+    @Autowired
+    private ArrayDesignService arrayDesignService;
+
+    /* fixtures */
+    private ExpressionExperiment expressionExperiment;
+    private ArrayDesign arrayDesign;
+
+    @Before
+    public void setUp() {
+        expressionExperiment = getTestPersistentBasicExpressionExperiment();
+        arrayDesign = expressionExperiment.getBioAssays().iterator().next().getArrayDesignUsed();
+    }
+
+    @After
+    public void tearDown() {
+        eeService.remove( expressionExperiment );
+        arrayDesignService.remove( arrayDesign );
+    }
 
     @Test
     public void testAll() {
@@ -36,19 +64,22 @@ public class PlatformsWebServiceTest extends BaseSpringWebTest {
     @Test
     public void testPlatformDatasets() {
         PaginatedResponseDataObject<ExpressionExperimentValueObject> response = platformsWebService.platformDatasets(
-                PlatformArg.valueOf( "1" ),
+                PlatformArg.valueOf( this.arrayDesign.getId().toString() ),
                 IntArg.valueOf( "0" ),
                 IntArg.valueOf( "20" ),
                 new MockHttpServletResponse() );
         assertThat( response )
                 .hasFieldOrPropertyWithValue( "offset", 0 )
                 .hasFieldOrPropertyWithValue( "limit", 20 );
+        assertThat( response.getData() ).asList()
+                .hasSize( 1 )
+                .first().hasFieldOrPropertyWithValue( "id", expressionExperiment.getId() );
     }
 
     @Test
     public void testPlatformElements() {
         PaginatedResponseDataObject<CompositeSequenceValueObject> response = platformsWebService.platformElements(
-                PlatformArg.valueOf( "1" ),
+                PlatformArg.valueOf( this.arrayDesign.getId().toString() ),
                 IntArg.valueOf( "0" ),
                 IntArg.valueOf( "20" ),
                 new MockHttpServletResponse() );
