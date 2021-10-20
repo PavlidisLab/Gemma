8353e73e7be68c27f9c2994999fcaca02634bf77
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2021-10-20 12:51:25 -0700

    rest: Account for interactions in result set TSV output
    Interaction are represented unambiguously as 'contrast_{factorValueId}_{secondFactorValueId}'
    in the TSV header.
    
    Factor values are now identified by their id with metadata provided in a
    comment as to what they refer to.

--- gemma-core/src/main/java/ubic/gemma/core/analysis/service/ExpressionAnalysisResultSetFileServiceImpl.java
+++ gemma-core/src/main/java/ubic/gemma/core/analysis/service/ExpressionAnalysisResultSetFileServiceImpl.java
@@ -5,13 +5,14 @@ import org.springframework.stereotype.Service;
 import ubic.gemma.model.analysis.expression.diff.ContrastResult;
 import ubic.gemma.model.analysis.expression.diff.DifferentialExpressionAnalysisResult;
 import ubic.gemma.model.analysis.expression.diff.ExpressionAnalysisResultSet;
+import ubic.gemma.model.common.description.Characteristic;
+import ubic.gemma.model.common.measurement.Measurement;
 import ubic.gemma.model.expression.experiment.ExperimentalFactor;
 import ubic.gemma.model.expression.experiment.FactorValue;
 import ubic.gemma.model.genome.Gene;
 
 import java.io.IOException;
 import java.util.*;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,28 +20,37 @@ public class ExpressionAnalysisResultSetFileServiceImpl extends AbstractTsvFileS
 
     @Override
     public void writeTsvToAppendable( ExpressionAnalysisResultSet analysisResultSet, Map<DifferentialExpressionAnalysisResult, List<Gene>> result2Genes, Appendable appendable ) throws IOException {
-        Collection<ExperimentalFactor> experimentalFactors = analysisResultSet.getExperimentalFactors();
-
-        // TODO: have a structured format for header metadata
-        String experimentalFactorsMetadata = analysisResultSet.getExperimentalFactors().stream()
+        String experimentalFactorsMetadata = "[" + analysisResultSet.getExperimentalFactors().stream()
                 .map( this::formatExperimentalFactor )
-                .collect( Collectors.joining( ", " ) );
+                .collect( Collectors.joining( ", " ) ) + "]";
 
         List<String> header = new ArrayList<>();
 
         // add the basic columns
         header.addAll( Arrays.asList( "id", "probe_id", "probe_name", "gene_id", "gene_name", "gene_ncbi_id", "gene_official_symbol", "pvalue", "corrected_pvalue", "rank" ) );
 
-        // add all the contrasts present among the results
-        for ( ExperimentalFactor experimentalFactor : experimentalFactors ) {
-            // FIXME: some of the factor are singular, but seem to have multiple values
-            for ( FactorValue fv : experimentalFactor.getFactorValues() ) {
-                header.addAll( Arrays.asList(
-                        "contrast_" + experimentalFactor.getName() + "_" + fv.getValue() + "_log2fc",
-                        "contrast_" + experimentalFactor.getName() + "_" + fv.getValue() + "_tstat",
-                        "contrast_" + experimentalFactor.getName() + "_" + fv.getValue() + "_pvalue" ) );
-            }
-        }
+        // we need to peek in the contrast result to understand factor value interactions
+        // i.e. interaction between genotype and timepoint might result in a contrast_male_3h column, although we would
+        // use factor value IDs in the actual column name which might result in something like contrast_1292_2938
+        final Collection<ContrastResult> firstContrastResults = analysisResultSet.getResults().stream()
+                .findFirst()
+                .map( DifferentialExpressionAnalysisResult::getContrasts )
+                .orElse( Collections.emptyList() );
+
+        // this is the order the factor values are displayed
+        Comparator<ContrastResult> contrastResultComparator = Comparator
+                .comparing( ContrastResult::getFactorValue, Comparator.nullsLast( Comparator.comparing( FactorValue::getId ) ) )
+                .thenComparing( ContrastResult::getSecondFactorValue, Comparator.nullsLast( Comparator.comparing( FactorValue::getId ) ) );
+
+        firstContrastResults.stream().sorted( contrastResultComparator ).forEachOrdered( contrastResult -> {
+            String contrastResultPrefix = "contrast"
+                    + ( contrastResult.getFactorValue() != null ? "_" + contrastResult.getFactorValue().getId() : "" )
+                    + ( contrastResult.getSecondFactorValue() != null ? "_" + contrastResult.getSecondFactorValue().getId() : "" );
+            header.addAll( Arrays.asList(
+                    contrastResultPrefix + "_log2fc",
+                    contrastResultPrefix + "_tstat",
+                    contrastResultPrefix + "_pvalue" ) );
+        } );
 
         CSVPrinter printer = getTsvFormatBuilder( "Experimental factors: " + experimentalFactorsMetadata )
                 .setHeader( header.toArray( new String[header.size()] ) )
@@ -49,9 +59,6 @@ public class ExpressionAnalysisResultSetFileServiceImpl extends AbstractTsvFileS
         for ( DifferentialExpressionAnalysisResult analysisResult : analysisResultSet.getResults() ) {
             final List<Object> record = new ArrayList<>();
             final List<Gene> genes = result2Genes.getOrDefault( analysisResult, Collections.emptyList() );
-            final Map<FactorValue, ContrastResult> factorValue2Contrast = analysisResult.getContrasts()
-                    .stream()
-                    .collect( Collectors.toMap( ContrastResult::getFactorValue, Function.identity() ) );
             record.addAll( Arrays.asList( analysisResult.getId(),
                     analysisResult.getProbe().getId(),
                     analysisResult.getProbe().getName(),
@@ -62,33 +69,36 @@ public class ExpressionAnalysisResultSetFileServiceImpl extends AbstractTsvFileS
                     format( analysisResult.getPvalue() ),
                     format( analysisResult.getCorrectedPvalue() ),
                     format( analysisResult.getRank() ) ) );
-            // add contrasts columns
-            for ( ExperimentalFactor experimentalFactor : experimentalFactors ) {
-                for ( FactorValue fv : experimentalFactor.getFactorValues() ) {
-                    if ( factorValue2Contrast.containsKey( fv ) ) {
-                        ContrastResult contrastResult = factorValue2Contrast.get( fv );
-                        record.addAll( Arrays.asList( format( contrastResult.getLogFoldChange() ), format( contrastResult.getTstat() ), format( contrastResult.getPvalue() ) ) );
-                    } else {
-                        // the contrast might not be present if configured to
-                        record.addAll( Arrays.asList( null, null, null ) );
-                    }
-                }
-            }
+            analysisResult.getContrasts().stream().sorted( contrastResultComparator ).forEachOrdered( contrastResult -> {
+                record.addAll( Arrays.asList( format( contrastResult.getLogFoldChange() ), format( contrastResult.getTstat() ), format( contrastResult.getPvalue() ) ) );
+            } );
             printer.printRecord( record );
         }
 
     }
 
     private String formatExperimentalFactor( ExperimentalFactor experimentalFactor ) {
-        return experimentalFactor.getName() + " (" +
+        return "name: " + experimentalFactor.getName() + ", values: [" +
                 experimentalFactor.getFactorValues()
                         .stream()
                         .map( this::formatFactorValue )
-                        .collect( Collectors.joining( ", " ) ) + ")";
+                        .collect( Collectors.joining( ", " ) ) + "]";
     }
 
+
     private String formatFactorValue( FactorValue factorValue ) {
-        return factorValue.getValue() + ( factorValue.getIsBaseline() != null && factorValue.getIsBaseline() ? "*" : "" );
+        return "id: " + factorValue.getId()
+                + ( factorValue.getIsBaseline() != null && factorValue.getIsBaseline() ? "*" : "" )
+                + ( factorValue.getMeasurement() != null ? ", measurement: " + formatMeasurement( factorValue.getMeasurement() ) : "" )
+                + ", characteristics: [" + formatCharacteristics( factorValue.getCharacteristics() ) + "]";
+    }
+
+    private String formatMeasurement( Measurement measurement ) {
+        return measurement.getValue() + measurement.getUnit().getUnitNameCV();
+    }
+
+    private String formatCharacteristics( Collection<Characteristic> characteristics ) {
+        return characteristics.stream().map( Characteristic::getValue ).collect( Collectors.joining( ", " ) );
     }
 
     @Override
