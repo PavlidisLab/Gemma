2691ce5d23c501b8071b731a3d2b1a25fa473fa5
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2021-11-05 10:56:23 -0700

    Add a log4j appender to Slack
    Implement a basic log4j appender that logs error messages to a
    gemma-log-test Slack channel
    
    Use it instead of the mail appender by default.

---
+++ gemma-web/src/main/java/ubic/gemma/web/logging/SlackAppender.java
@@ -0,0 +1,63 @@
+package ubic.gemma.web.logging;
+
+import com.slack.api.Slack;
+import com.slack.api.methods.SlackApiException;
+import com.slack.api.methods.request.chat.ChatPostMessageRequest;
+import com.slack.api.model.Attachment;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.log4j.Appender;
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.spi.LoggingEvent;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class SlackAppender extends AppenderSkeleton implements Appender {
+
+    private String token;
+    private String channel;
+
+    public SlackAppender() {
+    }
+
+    @Override
+    protected void append( LoggingEvent loggingEvent ) {
+        try {
+            ChatPostMessageRequest.ChatPostMessageRequestBuilder request = ChatPostMessageRequest.builder()
+                    .channel( channel )
+                    .text( this.layout.format( loggingEvent ) );
+
+            // attach a stacktrace if available
+            if ( loggingEvent.getThrowableInformation() != null )
+                request.attachments( Arrays.asList( stacktraceAsAtatchment( loggingEvent ) ) );
+
+            Slack.getInstance().methods( token ).chatPostMessage( request.build() );
+        } catch ( IOException | SlackApiException e ) {
+            errorHandler.error( String.format( "Failed to send logging event to Slack channel %s.", channel ), e, 0, loggingEvent );
+        }
+    }
+
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public boolean requiresLayout() {
+        return true;
+    }
+
+    public void setToken( String token ) {
+        this.token = token;
+    }
+
+    public void setChannel( String channel ) {
+        this.channel = channel;
+    }
+
+    private static Attachment stacktraceAsAtatchment( LoggingEvent loggingEvent ) {
+        return Attachment.builder()
+                .title( ExceptionUtils.getMessage( loggingEvent.getThrowableInformation().getThrowable() ) )
+                .text( ExceptionUtils.getStackTrace( loggingEvent.getThrowableInformation().getThrowable() ) )
+                .build();
+    }
+}
