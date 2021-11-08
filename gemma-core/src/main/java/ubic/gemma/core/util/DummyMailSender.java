1e132822c8a066da99acbe6b22a64514a3d7b21b
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2021-11-08 13:30:24 -0800

    Fix DummyMailSender missing when passing the CLI -testing flag

--- gemma-core/src/main/java/ubic/gemma/core/util/DummyMailSender.java
+++ gemma-core/src/main/java/ubic/gemma/core/util/DummyMailSender.java
@@ -12,23 +12,20 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package ubic.gemma.core.util.test;
+package ubic.gemma.core.util;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.mail.MailException;
+import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.mail.javamail.MimeMessagePreparator;
-
-import javax.mail.internet.MimeMessage;
 
 /**
  * Mock mail sender for testing.
  *
  * @author Paul
  */
-public class DummyMailSender extends JavaMailSenderImpl {
+public class DummyMailSender implements MailSender {
 
     private static final Log log = LogFactory.getLog( DummyMailSender.class );
 
@@ -39,26 +36,6 @@ public class DummyMailSender extends JavaMailSenderImpl {
 
     @Override
     public void send( SimpleMailMessage[] simpleMessages ) throws MailException {
-        DummyMailSender.log.debug( "Sent" );
-    }
-
-    @Override
-    public void send( MimeMessage mimeMessage ) throws MailException {
-        DummyMailSender.log.debug( "Sent" );
-    }
-
-    @Override
-    public void send( MimeMessage[] mimeMessages ) throws MailException {
-        DummyMailSender.log.debug( "Sent" );
-    }
-
-    @Override
-    public void send( final MimeMessagePreparator mimeMessagePreparator ) throws MailException {
-        DummyMailSender.log.debug( "Sent" );
-    }
-
-    @Override
-    public void send( MimeMessagePreparator[] mimeMessagePreparators ) throws MailException {
         DummyMailSender.log.debug( "Sent" );
     }
 }
