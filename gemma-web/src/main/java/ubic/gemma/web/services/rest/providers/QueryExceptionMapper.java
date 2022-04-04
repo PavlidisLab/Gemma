63dc53710e163a2853ebd66af9f715e9b5df63c0
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2022-04-04 08:47:18 -0700

    rest: Add more details about the request when logging errors
    This was originally brought up in #324.

--- gemma-web/src/main/java/ubic/gemma/web/services/rest/providers/QueryExceptionMapper.java
+++ gemma-web/src/main/java/ubic/gemma/web/services/rest/providers/QueryExceptionMapper.java
@@ -5,10 +5,12 @@ import org.apache.commons.logging.LogFactory;
 import org.hibernate.QueryException;
 import ubic.gemma.web.services.rest.util.OpenApiUtils;
 import ubic.gemma.web.services.rest.util.ResponseErrorObject;
+import ubic.gemma.web.services.rest.util.ServletUtils;
 import ubic.gemma.web.services.rest.util.WellComposedErrorBody;
 import ubic.gemma.web.services.rest.util.args.FilterArg;
 
 import javax.servlet.ServletConfig;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
@@ -25,11 +27,14 @@ public class QueryExceptionMapper implements ExceptionMapper<QueryException> {
     private static Log log = LogFactory.getLog( QueryExceptionMapper.class.getName() );
 
     @Context
+    private HttpServletRequest request;
+
+    @Context
     private ServletConfig servletConfig;
 
     @Override
     public Response toResponse( QueryException e ) {
-        log.error( "Unknown query exception while serving request: ", e );
+        log.error( "Unknown query exception while serving request: " + ServletUtils.summarizeRequest( request ) + ".", e );
         WellComposedErrorBody error = new WellComposedErrorBody( Response.Status.BAD_REQUEST,
                 FilterArg.ERROR_MSG_MALFORMED_REQUEST );
         return Response.status( error.getStatus() ).entity( new ResponseErrorObject( error, OpenApiUtils.getOpenApi( servletConfig ) ) ).build();
