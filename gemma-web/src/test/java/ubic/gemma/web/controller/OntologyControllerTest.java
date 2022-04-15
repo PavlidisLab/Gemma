e46944c104cf4925308ab154c79e5ce3339a801f
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2022-04-15 13:45:24 -0700

    Add minimal unit test for OntologyController

---
+++ gemma-web/src/test/java/ubic/gemma/web/controller/OntologyControllerTest.java
@@ -0,0 +1,33 @@
+package ubic.gemma.web.controller;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.web.servlet.view.RedirectView;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ContextConfiguration
+public class OntologyControllerTest extends AbstractJUnit4SpringContextTests {
+
+    @Configuration
+    public static class OntologyControllerTestContextConfiguration {
+
+        @Bean
+        public OntologyController ontologyController() {
+            return new OntologyController();
+        }
+    }
+
+    @Autowired
+    private OntologyController ontologyController;
+
+    @Test
+    public void testGetObo() {
+        RedirectView rv = ontologyController.getObo();
+        assertThat( rv.getUrl() ).isEqualTo( "https://raw.githubusercontent.com/PavlidisLab/TGEMO/master/TGEMO.OWL" );
+    }
+}
