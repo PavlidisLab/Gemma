765cf0d282a43d845a6b05eb6b02cc741f015f39
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2022-08-02 18:29:40 -0700

    Add AllenBrainAtlasTest JUnit category

--- gemma-core/src/test/java/ubic/gemma/core/loader/expression/arrayDesign/ArrayDesignSequenceProcessorTest.java
+++ gemma-core/src/test/java/ubic/gemma/core/loader/expression/arrayDesign/ArrayDesignSequenceProcessorTest.java
@@ -22,6 +22,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import ubic.basecode.util.FileTools;
 import ubic.gemma.core.loader.expression.geo.AbstractGeoServiceTest;
---
+++ gemma-core/src/test/java/ubic/gemma/core/util/test/category/AllenBrainAtlasTest.java
@@ -0,0 +1,8 @@
+package ubic.gemma.core.util.test.category;
+
+/**
+ * Mark tests that require the Allen Brain Atlas RESTful API.
+ * @author poirigui
+ */
+public interface AllenBrainAtlasTest {
+}
