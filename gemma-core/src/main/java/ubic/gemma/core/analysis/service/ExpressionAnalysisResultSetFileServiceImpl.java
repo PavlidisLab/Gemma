1869263a09e9dc560aef2a7937dc6a15d54882e3
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2022-08-03 12:21:57 -0700

    Handle missing contrast result when rendering AnalysisResultSet to TSV (fix #365)

--- gemma-core/src/main/java/ubic/gemma/core/analysis/service/ExpressionAnalysisResultSetFileServiceImpl.java
+++ gemma-core/src/main/java/ubic/gemma/core/analysis/service/ExpressionAnalysisResultSetFileServiceImpl.java
@@ -1,6 +1,8 @@
 package ubic.gemma.core.analysis.service;
 
+import lombok.extern.apachecommons.CommonsLog;
 import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.stereotype.Service;
 import ubic.gemma.model.analysis.expression.diff.ContrastResult;
 import ubic.gemma.model.analysis.expression.diff.DifferentialExpressionAnalysisResult;
@@ -15,7 +17,10 @@ import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static java.util.function.Function.identity;
+
 @Service
+@CommonsLog
 public class ExpressionAnalysisResultSetFileServiceImpl extends AbstractTsvFileService<ExpressionAnalysisResultSet> implements ExpressionAnalysisResultSetFileService {
 
     @Override
@@ -27,20 +32,22 @@ public class ExpressionAnalysisResultSetFileServiceImpl extends AbstractTsvFileS
         // add the basic columns
         List<String> header = new ArrayList<>( Arrays.asList( "id", "probe_id", "probe_name", "gene_id", "gene_name", "gene_ncbi_id", "gene_official_symbol", "gene_official_name", "pvalue", "corrected_pvalue", "rank" ) );
 
-        // we need to peek in the contrast result to understand factor value interactions
-        // i.e. interaction between genotype and time point might result in a contrast_male_3h column, although we would
-        // use factor value IDs in the actual column name which might result in something like contrast_1292_2938
-        final Collection<ContrastResult> firstContrastResults = analysisResultSet.getResults().stream()
-                .findFirst()
-                .map( DifferentialExpressionAnalysisResult::getContrasts )
-                .orElse( Collections.emptyList() );
-
         // this is the order the factor values are displayed
         Comparator<ContrastResult> contrastResultComparator = Comparator
                 .comparing( ContrastResult::getFactorValue, Comparator.nullsLast( Comparator.comparing( FactorValue::getId ) ) )
                 .thenComparing( ContrastResult::getSecondFactorValue, Comparator.nullsLast( Comparator.comparing( FactorValue::getId ) ) );
 
-        firstContrastResults.stream().sorted( contrastResultComparator ).forEachOrdered( contrastResult -> {
+        // we need to peek in the contrast result to understand factor value interactions
+        // i.e. interaction between genotype and time point might result in a contrast_male_3h column, although we would
+        // use factor value IDs in the actual column name which might result in something like contrast_1292_2938
+        final List<ContrastResult> firstContrastResults = analysisResultSet.getResults().stream()
+                .findFirst()
+                .map( DifferentialExpressionAnalysisResult::getContrasts )
+                .orElse( Collections.emptyList() )
+                .stream().sorted( contrastResultComparator )
+                .collect( Collectors.toList() );
+
+        for ( ContrastResult contrastResult : firstContrastResults ) {
             String contrastResultPrefix = "contrast"
                     + ( contrastResult.getFactorValue() != null ? "_" + contrastResult.getFactorValue().getId() : "" )
                     + ( contrastResult.getSecondFactorValue() != null ? "_" + contrastResult.getSecondFactorValue().getId() : "" );
@@ -48,31 +55,49 @@ public class ExpressionAnalysisResultSetFileServiceImpl extends AbstractTsvFileS
                     contrastResultPrefix + "_log2fc",
                     contrastResultPrefix + "_tstat",
                     contrastResultPrefix + "_pvalue" ) );
-        } );
-
-        CSVPrinter printer = getTsvFormatBuilder( "Experimental factors: " + experimentalFactorsMetadata )
-                .setHeader( header.toArray( new String[0] ) )
-                .build()
-                .print( appendable );
-        for ( DifferentialExpressionAnalysisResult analysisResult : analysisResultSet.getResults() ) {
-            final List<Gene> genes = result2Genes.getOrDefault( analysisResult, Collections.emptyList() );
-            final List<Object> record = new ArrayList<>( Arrays.asList( analysisResult.getId(),
-                    analysisResult.getProbe().getId(),
-                    analysisResult.getProbe().getName(),
-                    genes.stream().map( Gene::getId ).map( String::valueOf ).collect( Collectors.joining( getSubDelimiter() ) ),
-                    genes.stream().map( Gene::getName ).collect( Collectors.joining( getSubDelimiter() ) ),
-                    genes.stream().map( Gene::getNcbiGeneId ).map( String::valueOf ).collect( Collectors.joining( getSubDelimiter() ) ),
-                    genes.stream().map( Gene::getOfficialSymbol ).collect( Collectors.joining( getSubDelimiter() ) ),
-                    genes.stream().map( Gene::getOfficialName ).collect( Collectors.joining( getSubDelimiter() ) ),
-                    format( analysisResult.getPvalue() ),
-                    format( analysisResult.getCorrectedPvalue() ),
-                    format( analysisResult.getRank() ) ) );
-            analysisResult.getContrasts().stream()
-                    .sorted( contrastResultComparator )
-                    .forEachOrdered( contrastResult -> record.addAll( Arrays.asList( format( contrastResult.getLogFoldChange() ), format( contrastResult.getTstat() ), format( contrastResult.getPvalue() ) ) ) );
-            printer.printRecord( record );
         }
 
+        List<Pair<FactorValue, FactorValue>> factors = firstContrastResults.stream().sorted( contrastResultComparator )
+                .map( cr -> Pair.of( cr.getFactorValue(), cr.getSecondFactorValue() ) )
+                .collect( Collectors.toList() );
+
+        try ( CSVPrinter printer = getTsvFormatBuilder( "Experimental factors: " + experimentalFactorsMetadata )
+                .setHeader( header.toArray( new String[0] ) )
+                .build()
+                .print( appendable ) ) {
+            for ( DifferentialExpressionAnalysisResult analysisResult : analysisResultSet.getResults() ) {
+                final List<Gene> genes = result2Genes.getOrDefault( analysisResult, Collections.emptyList() );
+                final List<Object> record = new ArrayList<>( Arrays.asList( analysisResult.getId(),
+                        analysisResult.getProbe().getId(),
+                        analysisResult.getProbe().getName(),
+                        genes.stream().map( Gene::getId ).map( String::valueOf ).collect( Collectors.joining( getSubDelimiter() ) ),
+                        genes.stream().map( Gene::getName ).collect( Collectors.joining( getSubDelimiter() ) ),
+                        genes.stream().map( Gene::getNcbiGeneId ).map( String::valueOf ).collect( Collectors.joining( getSubDelimiter() ) ),
+                        genes.stream().map( Gene::getOfficialSymbol ).collect( Collectors.joining( getSubDelimiter() ) ),
+                        genes.stream().map( Gene::getOfficialName ).collect( Collectors.joining( getSubDelimiter() ) ),
+                        format( analysisResult.getPvalue() ),
+                        format( analysisResult.getCorrectedPvalue() ),
+                        format( analysisResult.getRank() ) ) );
+                Map<Pair<FactorValue, FactorValue>, ContrastResult> contrastsByFirstAndSecondFactorValue = analysisResult.getContrasts().stream()
+                        .collect( Collectors.toMap( fv -> Pair.of( fv.getFactorValue(), fv.getSecondFactorValue() ), identity() ) );
+                // render contrast results in the same order than the first row and handle possibly missing columns
+                for ( ContrastResult contrastResult : firstContrastResults ) {
+                    ContrastResult cr = contrastsByFirstAndSecondFactorValue.get( Pair.of( contrastResult.getFactorValue(), contrastResult.getSecondFactorValue() ) );
+                    if ( cr != null ) {
+                        record.add( format( cr.getLogFoldChange() ) );
+                        record.add( format( cr.getTstat() ) );
+                        record.add( format( cr.getPvalue() ) );
+                    } else {
+                        log.warn( String.format( "%s is missing contrast result for [%s, %s]. The corresponding column in the TSV will be treated as NaN.",
+                                analysisResult, contrastResult.getFactorValue(), contrastResult.getSecondFactorValue() ) );
+                        record.add( "" );
+                        record.add( "" );
+                        record.add( "" );
+                    }
+                }
+                printer.printRecord( record );
+            }
+        }
     }
 
     private String formatExperimentalFactor( ExperimentalFactor experimentalFactor ) {
