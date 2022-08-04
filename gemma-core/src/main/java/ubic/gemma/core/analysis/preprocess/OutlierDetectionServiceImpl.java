b858f6f61c04846a6bc22cb819a79cf6b01374ae
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2022-08-03 22:44:25 -0700

    Move StaleStateException handling on the RESTful API
    Unfortunately, it appears that this error arises when there is high
    contention on this specific call.
    
    Inline removeForExperiment() to make sure that findByInvestigation() and
    remove() happens in a single transaction.

--- gemma-core/src/main/java/ubic/gemma/core/analysis/preprocess/OutlierDetectionServiceImpl.java
+++ gemma-core/src/main/java/ubic/gemma/core/analysis/preprocess/OutlierDetectionServiceImpl.java
@@ -19,6 +19,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.UnexpectedRollbackException;
 import ubic.basecode.dataStructure.matrix.DoubleMatrix;
 import ubic.gemma.core.analysis.preprocess.filter.FilteringException;
 import ubic.gemma.core.analysis.preprocess.filter.NoRowsLeftAfterFilteringException;
