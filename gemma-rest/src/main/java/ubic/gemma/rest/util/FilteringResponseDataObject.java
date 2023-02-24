c95470156fc30ac434354c7976c7dde14b66005c
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2023-02-24 13:34:42 -0800

    rest: Exclude null fields in FilteringResponseDataObject

--- gemma-rest/src/main/java/ubic/gemma/rest/util/FilteringResponseDataObject.java
+++ gemma-rest/src/main/java/ubic/gemma/rest/util/FilteringResponseDataObject.java
@@ -1,5 +1,6 @@
 package ubic.gemma.rest.util;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import ubic.gemma.persistence.util.Filters;
 import ubic.gemma.persistence.util.Sort;
 
@@ -13,15 +14,18 @@ import java.util.List;
  */
 public class FilteringResponseDataObject<T> extends ResponseDataObject<List<T>> {
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private final String filter;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private final String[] groupBy;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private final SortValueObject sort;
 
     /**
      * @param payload the data to be serialised and returned as the response payload.
      * @param groupBy the fields by which results are grouped
      */
-    public FilteringResponseDataObject( List<T> payload, @Nullable Filters filters, String[] groupBy, @Nullable Sort sort ) {
+    public FilteringResponseDataObject( List<T> payload, @Nullable Filters filters, @Nullable String[] groupBy, @Nullable Sort sort ) {
         super( payload );
         this.filter = filters != null ? filters.toOriginalString() : null;
         this.groupBy = groupBy;
