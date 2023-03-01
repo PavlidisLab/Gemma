PavlidisLab/Gemma

commit f6d7c8c645af5355ba338af5e5eec90b1aeb4a3d

Use a SingleExperimentAnalysis in AnalysisDao subclasses
The subclasses always expect a BioAssaySet via SingleExpressionAnalysis.getExperimentAnalyzed(),
so we enforce this by type.

Introduce a SingleExperimentAnalysisDao and SingleExperimentAnalysisDaoBase to regroup the code
that expects a BioAssaySet.

Rename a few findByInvestigation to findByExperiment for more clarity.

Use the Criteria API in SingleExperimentAnalysisDaoBase to safely inject
the class.

gemma-core/src/main/java/ubic/gemma/persistence/service/analysis/expression/diff/DifferentialExpressionAnalysisDaoImpl.java
@@ -23,7 +23,6 @@ import org.apache.commons.lang3.time.StopWatch;
 import org.hibernate.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import ubic.gemma.model.analysis.Investigation;
 import ubic.gemma.model.analysis.expression.diff.ExpressionAnalysisResultSet;
 import ubic.gemma.model.analysis.expression.diff.*;
 import ubic.gemma.model.expression.designElement.CompositeSequence;
@@ -31,19 +30,20 @@ import ubic.gemma.model.expression.experiment.*;
 import ubic.gemma.model.genome.Gene;
 import ubic.gemma.model.genome.Taxon;
 import ubic.gemma.persistence.service.AbstractDao;
-import ubic.gemma.persistence.service.analysis.AnalysisDaoBase;
+import ubic.gemma.persistence.service.analysis.SingleExperimentAnalysisDaoBase;
 import ubic.gemma.persistence.util.CommonQueries;
 import ubic.gemma.persistence.util.EntityUtils;
 
 import java.math.BigInteger;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author paul
  * @see    DifferentialExpressionAnalysis
  */
 @Repository
-class DifferentialExpressionAnalysisDaoImpl extends AnalysisDaoBase<DifferentialExpressionAnalysis>
+class DifferentialExpressionAnalysisDaoImpl extends SingleExperimentAnalysisDaoBase<DifferentialExpressionAnalysis>
         implements DifferentialExpressionAnalysisDao {
 
     @Autowired
@@ -140,8 +140,8 @@ class DifferentialExpressionAnalysisDaoImpl extends AnalysisDaoBase<Differential
     }
 
     @Override
-    public Map<Long, Collection<DifferentialExpressionAnalysis>> findByInvestigationIds(
-            Collection<Long> investigationIds ) {
+    public Map<Long, Collection<DifferentialExpressionAnalysis>> findByExperimentIds(
+            Collection<Long> experimentIds ) {
 
         Map<Long, Collection<DifferentialExpressionAnalysis>> results = new HashMap<>();
         //language=HQL
@@ -149,7 +149,7 @@ class DifferentialExpressionAnalysisDaoImpl extends AnalysisDaoBase<Differential
                 + "   inner join a.experimentAnalyzed e where e.id in (:eeIds)";
         List<?> qresult = this.getSessionFactory().getCurrentSession()
                 .createQuery( queryString )
-                .setParameterList( "eeIds", investigationIds )
+                .setParameterList( "eeIds", experimentIds )
                 .list();
         for ( Object o : qresult ) {
             Object[] oa = ( Object[] ) o;
@@ -500,14 +500,14 @@ class DifferentialExpressionAnalysisDaoImpl extends AnalysisDaoBase<Differential
     }
 
     @Override
-    public Collection<DifferentialExpressionAnalysis> findByInvestigation( Investigation investigation ) {
-        Long id = investigation.getId();
+    public Collection<DifferentialExpressionAnalysis> findByExperiment( BioAssaySet experiment ) {
         Collection<DifferentialExpressionAnalysis> results = new HashSet<>();
 
         //noinspection unchecked
         results.addAll( this.getSessionFactory().getCurrentSession().createQuery(
-                        "select distinct a from DifferentialExpressionAnalysis a where a.experimentAnalyzed.id=:eeid" )
-                .setParameter( "eeid", id ).list() );
+                        "select distinct a from DifferentialExpressionAnalysis a "
+                                + "where a.experimentAnalyzed = :ee" )
+                .setParameter( "ee", experiment ).list() );
 
         /*
          * Deal with the analyses of subsets of the investigation. User has to know this is possible.
@@ -516,10 +516,34 @@ class DifferentialExpressionAnalysisDaoImpl extends AnalysisDaoBase<Differential
         results.addAll( this.getSessionFactory().getCurrentSession().createQuery(
                         "select distinct a from ExpressionExperimentSubSet eess, DifferentialExpressionAnalysis a "
                                 + "join eess.sourceExperiment see "
-                                + "join a.experimentAnalyzed eeanalyzed where see.id=:eeid and eess=eeanalyzed" )
-                .setParameter( "eeid", id ).list() );
+                                + "join a.experimentAnalyzed eeanalyzed where see = :ee and eess = eeanalyzed" )
+                .setParameter( "ee", experiment ).list() );
 
         return results;
+    }
+
+    @Override
+    public Map<BioAssaySet, Collection<DifferentialExpressionAnalysis>> findByExperiments( Collection<? extends BioAssaySet> experiments ) {
+        Collection<DifferentialExpressionAnalysis> results = new HashSet<>();
+
+        //noinspection unchecked
+        results.addAll( this.getSessionFactory().getCurrentSession().createQuery(
+                        "select distinct a from DifferentialExpressionAnalysis a "
+                                + "where a.experimentAnalyzed in :ees" )
+                .setParameter( "ees", experiments ).list() );
+
+        /*
+         * Deal with the analyses of subsets of the investigation. User has to know this is possible.
+         */
+        //noinspection unchecked
+        results.addAll( this.getSessionFactory().getCurrentSession().createQuery(
+                        "select distinct a from ExpressionExperimentSubSet eess, DifferentialExpressionAnalysis a "
+                                + "join eess.sourceExperiment see "
+                                + "join a.experimentAnalyzed eeanalyzed where see in :ees and eess=eeanalyzed" )
+                .setParameter( "ees", experiments ).list() );
+
+        return results.stream()
+                .collect( Collectors.groupingBy( DifferentialExpressionAnalysis::getExperimentAnalyzed, Collectors.toCollection( ArrayList::new ) ) );
     }
 
     private void addFactorValues( Map<Long, Collection<FactorValue>> ee2fv, List<Object[]> fvs ) {

gemma-cli/src/main/java/ubic/gemma/core/apps/DifferentialExpressionAnalysisCli.java
@@ -505,7 +505,7 @@ public class DifferentialExpressionAnalysisCli extends ExpressionExperimentManip
      */
     private Collection<DifferentialExpressionAnalysis> tryToRedoBasedOnOldAnalysis( ExpressionExperiment ee ) {
         Collection<DifferentialExpressionAnalysis> oldAnalyses = differentialExpressionAnalysisService
-                .findByInvestigation( ee );
+                .findByExperiment( ee );
 
         if ( oldAnalyses.isEmpty() ) {
             throw new IllegalArgumentException( "There are no old analyses to redo" );

gemma-core/src/main/java/ubic/gemma/core/analysis/expression/AnalysisUtilServiceImpl.java
@@ -73,7 +73,7 @@ public class AnalysisUtilServiceImpl implements AnalysisUtilService {
         }
 
         for ( DifferentialExpressionAnalysis diff : differentialExpressionAnalysisService
-                .findByInvestigation( expExp ) ) {
+                .findByExperiment( expExp ) ) {
             try {
                 differentialExpressionAnalysisService.remove( diff );
             } catch ( Exception e ) {
@@ -81,7 +81,7 @@ public class AnalysisUtilServiceImpl implements AnalysisUtilService {
                 removedAll = false;
             }
         }
-        for ( CoexpressionAnalysis coex : coexpressionAnalysisService.findByInvestigation( expExp ) ) {
+        for ( CoexpressionAnalysis coex : coexpressionAnalysisService.findByExperiment( expExp ) ) {
             try {
                 coexpressionAnalysisService.remove( coex );
             } catch ( Exception e ) {

gemma-core/src/main/java/ubic/gemma/core/analysis/expression/coexpression/links/LinkAnalysisPersisterImpl.java
@@ -48,7 +48,7 @@ public class LinkAnalysisPersisterImpl implements LinkAnalysisPersister {
 
     @Override
     public boolean deleteAnalyses( BioAssaySet ee ) {
-        Collection<CoexpressionAnalysis> oldAnalyses = coexpressionAnalysisService.findByInvestigation( ee );
+        Collection<CoexpressionAnalysis> oldAnalyses = coexpressionAnalysisService.findByExperiment( ee );
 
         if ( oldAnalyses.isEmpty() )
             return false;

gemma-core/src/main/java/ubic/gemma/core/analysis/expression/diff/DifferentialExpressionAnalyzerServiceImpl.java
@@ -37,7 +37,6 @@ import ubic.gemma.model.common.auditAndSecurity.eventType.FailedDifferentialExpr
 import ubic.gemma.model.expression.designElement.CompositeSequence;
 import ubic.gemma.model.expression.experiment.*;
 import ubic.gemma.persistence.service.analysis.expression.diff.DifferentialExpressionAnalysisService;
-import ubic.gemma.persistence.service.analysis.expression.diff.DifferentialExpressionResultService;
 import ubic.gemma.persistence.service.analysis.expression.diff.ExpressionAnalysisResultSetService;
 import ubic.gemma.persistence.service.common.auditAndSecurity.AuditTrailService;
 import ubic.gemma.persistence.service.expression.experiment.ExpressionExperimentService;
@@ -76,7 +75,7 @@ public class DifferentialExpressionAnalyzerServiceImpl implements DifferentialEx
     @Override
     public int deleteAnalyses( ExpressionExperiment expressionExperiment ) {
         Collection<DifferentialExpressionAnalysis> diffAnalysis = differentialExpressionAnalysisService
-                .findByInvestigation( expressionExperiment );
+                .findByExperiment( expressionExperiment );
 
         int result = 0;
         if ( diffAnalysis == null || diffAnalysis.isEmpty() ) {
@@ -361,7 +360,7 @@ public class DifferentialExpressionAnalyzerServiceImpl implements DifferentialEx
     private void deleteOldAnalyses( ExpressionExperiment expressionExperiment,
             DifferentialExpressionAnalysis newAnalysis, Collection<ExperimentalFactor> factors ) {
         Collection<DifferentialExpressionAnalysis> diffAnalyses = differentialExpressionAnalysisService
-                .findByInvestigation( expressionExperiment );
+                .findByExperiment( expressionExperiment );
         int numDeleted = 0;
         if ( diffAnalyses == null || diffAnalyses.isEmpty() ) {
             DifferentialExpressionAnalyzerServiceImpl.log

gemma-core/src/main/java/ubic/gemma/core/analysis/preprocess/PreprocessorServiceImpl.java
@@ -20,18 +20,15 @@ import java.util.LinkedList;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import org.springframework.stereotype.Service;
 import ubic.gemma.core.analysis.expression.diff.DifferentialExpressionAnalyzerService;
 import ubic.gemma.core.analysis.preprocess.batcheffects.ExpressionExperimentBatchCorrectionService;
-import ubic.gemma.core.analysis.preprocess.filter.NoRowsLeftAfterFilteringException;
 import ubic.gemma.core.analysis.preprocess.svd.SVDServiceHelper;
 import ubic.gemma.core.analysis.report.ExpressionExperimentReportService;
 import ubic.gemma.core.analysis.service.ExpressionDataFileService;
 import ubic.gemma.core.datastructure.matrix.ExpressionDataDoubleMatrix;
 import ubic.gemma.model.analysis.expression.diff.DifferentialExpressionAnalysis;
-import ubic.gemma.model.common.auditAndSecurity.eventType.AuditEventType;
 import ubic.gemma.model.common.auditAndSecurity.eventType.BatchCorrectionEvent;
 import ubic.gemma.model.common.quantitationtype.QuantitationType;
 import ubic.gemma.model.expression.arrayDesign.ArrayDesign;
@@ -251,7 +248,7 @@ public class PreprocessorServiceImpl implements PreprocessorService {
          * Redo any old diff ex analyses
          */
         Collection<DifferentialExpressionAnalysis> oldAnalyses = differentialExpressionAnalysisService
-                .findByInvestigation( ee );
+                .findByExperiment( ee );
 
         if ( !oldAnalyses.isEmpty() ) {
 

gemma-core/src/main/java/ubic/gemma/persistence/service/analysis/AnalysisDao.java
@@ -18,9 +18,8 @@
  */
 package ubic.gemma.persistence.service.analysis;
 
-import ubic.gemma.model.analysis.Analysis;
-import ubic.gemma.model.analysis.Investigation;
-import ubic.gemma.model.expression.experiment.ExpressionExperiment;
+import ubic.gemma.model.analysis.SingleExperimentAnalysis;
+import ubic.gemma.model.expression.experiment.BioAssaySet;
 import ubic.gemma.model.genome.Taxon;
 import ubic.gemma.persistence.service.BaseDao;
 
@@ -30,19 +29,7 @@ import java.util.Map;
 /**
  * @see ubic.gemma.model.analysis.Analysis
  */
-public interface AnalysisDao<T extends Analysis> extends BaseDao<T> {
-
-    Collection<T> findByInvestigation( Investigation investigation );
-
-    /**
-     * Given a collection of investigations returns a Map of Analysis --&gt; collection of Investigations
-     * The collection of investigations returned by the map will include all the investigations for the analysis key iff
-     * one of the investigations for that analysis was in the given collection started with
-     *
-     * @param investigations investigations
-     * @return map to analyses
-     */
-    Map<Investigation, Collection<T>> findByInvestigations( Collection<Investigation> investigations );
+public interface AnalysisDao<T extends SingleExperimentAnalysis> extends BaseDao<T> {
 
     /**
      * @param name name
@@ -51,5 +38,4 @@ public interface AnalysisDao<T extends Analysis> extends BaseDao<T> {
     Collection<T> findByName( String name );
 
     Collection<T> findByTaxon( Taxon taxon );
-
 }

gemma-core/src/main/java/ubic/gemma/persistence/service/analysis/AnalysisDaoBase.java
@@ -1,84 +1,0 @@
-/*
- * The Gemma project.
- *
- * Copyright (c) 2006-2007 University of British Columbia
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-package ubic.gemma.persistence.service.analysis;
-
-import org.hibernate.SessionFactory;
-import ubic.gemma.model.analysis.Analysis;
-import ubic.gemma.model.analysis.Investigation;
-import ubic.gemma.model.expression.experiment.ExpressionExperiment;
-import ubic.gemma.model.genome.Taxon;
-import ubic.gemma.persistence.service.AbstractDao;
-
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * <p>
- * Base Spring DAO Class: is able to create, update, remove, load, and find objects of type
- * <code>ubic.gemma.model.analysis.Analysis</code>.
- * </p>
- *
- * @see ubic.gemma.model.analysis.Analysis
- */
-public abstract class AnalysisDaoBase<T extends Analysis> extends AbstractDao<T> implements AnalysisDao<T> {
-
-    private final Class<T> analysisClass;
-
-    protected AnalysisDaoBase( Class<T> elementClass, SessionFactory sessionFactory ) {
-        super( elementClass, sessionFactory );
-        this.analysisClass = elementClass;
-    }
-
-    @Override
-    public Collection<T> findByInvestigation( final Investigation investigation ) {
-        //noinspection unchecked
-        return this.getSessionFactory().getCurrentSession().createQuery(
-                "select an  from " + this.analysisClass.getSimpleName() + " an where an.experimentAnalyzed = :ee" )
-                .setParameter( "ee", investigation ).list();
-    }
-
-    @Override
-    public Map<Investigation, Collection<T>> findByInvestigations( final Collection<Investigation> investigations ) {
-        Map<Investigation, Collection<T>> results = new HashMap<>();
-
-        for ( Investigation ee : investigations ) {
-            results.put( ee, this.findByInvestigation( ee ) );
-        }
-
-        return results;
-    }
-
-    @Override
-    public Collection<T> findByName( String name ) {
-        return this.findByProperty( "name", name );
-    }
-
-    @Override
-    public Collection<T> findByTaxon( final Taxon taxon ) {
-        //language=HQL
-        final String queryString = "select distinct an from " + this.analysisClass.getSimpleName()
-                + " an inner join an.experimentAnalyzed ee inner join ee.bioAssays ba "
-                + "inner join ba.sampleUsed sample where sample.sourceTaxon = :taxon ";
-        //noinspection unchecked
-        return getSessionFactory().getCurrentSession().createQuery( queryString )
-                .setParameter( "taxon", taxon )
-                .list();
-    }
-}

commit e03c1e3d9ce1c8527de61bcb0f039c0a6b2ccf93

Fix incorrect setParameter with collection
This address an issue introduced in f6d7c8c645af5355ba338af5e5eec90b1aeb4a3d.

gemma-core/src/main/java/ubic/gemma/persistence/service/analysis/expression/diff/DifferentialExpressionAnalysisDaoImpl.java
@@ -530,7 +530,7 @@ class DifferentialExpressionAnalysisDaoImpl extends SingleExperimentAnalysisDaoB
         results.addAll( this.getSessionFactory().getCurrentSession().createQuery(
                         "select distinct a from DifferentialExpressionAnalysis a "
                                 + "where a.experimentAnalyzed in :ees" )
-                .setParameter( "ees", experiments ).list() );
+                .setParameterList( "ees", experiments ).list() );
 
         /*
          * Deal with the analyses of subsets of the investigation. User has to know this is possible.
@@ -540,7 +540,7 @@ class DifferentialExpressionAnalysisDaoImpl extends SingleExperimentAnalysisDaoB
                         "select distinct a from ExpressionExperimentSubSet eess, DifferentialExpressionAnalysis a "
                                 + "join eess.sourceExperiment see "
                                 + "join a.experimentAnalyzed eeanalyzed where see in :ees and eess=eeanalyzed" )
-                .setParameter( "ees", experiments ).list() );
+                .setParameterList( "ees", experiments ).list() );
 
         return results.stream()
                 .collect( Collectors.groupingBy( DifferentialExpressionAnalysis::getExperimentAnalyzed, Collectors.toCollection( ArrayList::new ) ) );