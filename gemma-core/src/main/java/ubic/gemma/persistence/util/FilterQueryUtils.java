PavlidisLab/Gemma

commit 3fb0368fa865a81c79640e70afe28a279a980d5a

Add filterable properties for GEEQ quality and suitability scores (fix #597)
Allow for expression to be used as property name alongside a null object
alias. This require modifying the way HQL parameter name is generated.

Add indices for the columns used for filtering in the GEEQ table.

gemma-core/src/main/java/ubic/gemma/persistence/util/FilterQueryUtils.java
@@ -174,7 +174,7 @@ public class FilterQueryUtils {
     }
 
     private static String formParamName( PropertyMapping mapping, int i ) {
-        return mapping.getProperty().replaceAll( "\\.", "_" ) + i;
+        return mapping.getProperty().replaceAll("\\W", "_");
     }
 
     private static String escapeLike( String s ) {

gemma-core/src/main/java/ubic/gemma/persistence/service/AbstractFilteringVoEnabledDao.java
@@ -305,8 +305,8 @@ public abstract class AbstractFilteringVoEnabledDao<O extends Identifiable, VO e
      */
     @With
     @Value
-    @AllArgsConstructor(access = AccessLevel.PRIVATE)
     protected static class FilterablePropertyMeta {
+        @Nullable
         String objectAlias;
         String propertyName;
         Class<?> propertyType;

gemma-core/src/main/java/ubic/gemma/persistence/service/expression/experiment/ExpressionExperimentDaoImpl.java
@@ -64,6 +64,7 @@ import ubic.gemma.persistence.util.*;
 
 import javax.annotation.Nullable;
 import java.util.*;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.groupingBy;
@@ -1658,6 +1659,11 @@ public class ExpressionExperimentDaoImpl
         configurer.unregisterProperty( "otherParts.size" );
         configurer.unregisterProperty( "otherRelevantPublications.size" );
 
+        // only expose selected fields for GEEQ
+        configurer.unregisterEntity( "geeq.", Geeq.class );
+        configurer.registerProperty( "geeq.publicQualityScore" );
+        configurer.registerProperty( "geeq.publicSuitabilityScore" );
+
         // the primary publication is not very useful, but its attached database entry is
         configurer.unregisterEntity( "primaryPublication.", BibliographicReference.class );
         configurer.registerEntity( "primaryPublication.pubAccession.", DatabaseEntry.class, 2 );
@@ -1694,6 +1700,14 @@ public class ExpressionExperimentDaoImpl
         if ( propertyName.equals( "bioAssayCount" ) ) {
             return getFilterablePropertyMeta( "bioAssays.size" )
                     .withDescription( "alias for bioAssays.size" );
+        }
+
+        if ( propertyName.equals( "geeq.publicQualityScore" ) ) {
+            return new FilterablePropertyMeta( null, "(case when geeq.manualQualityOverride = true then geeq.manualQualityScore else geeq.detectedQualityScore end)", Double.class, null, null );
+        }
+
+        if ( propertyName.equals( "geeq.publicSuitabilityScore" ) ) {
+            return new FilterablePropertyMeta( null, "(case when geeq.manualSuitabilityOverride = true then geeq.manualSuitabilityScore else geeq.detectedSuitabilityScore end)", Double.class, null, null );
         }
 
         return super.getFilterablePropertyMeta( propertyName );

gemma-core/src/main/resources/sql/init-indices.sql
@@ -108,3 +108,11 @@ ALTER TABLE MEASUREMENT
   ADD INDEX MEASUREMENT_REPRESENTATION (REPRESENTATION),
   ADD INDEX MEASUREMENT_TYPE(TYPE),
   ADD INDEX MEASUREMENT_VALUE (VALUE);
+
+ALTER TABLE GEEQ
+  ADD INDEX GEEQ_DETECTED_QUALITY_SCORE (DETECTED_QUALITY_SCORE),
+  ADD INDEX GEEQ_DETECTED_SUITABILITY_SCORE (DETECTED_SUITABILITY_SCORE),
+  ADD INDEX GEEQ_MANUAL_QUALITY_SCORE(MANUAL_QUALITY_SCORE),
+  ADD INDEX GEEQ_MANUAL_QUALITY_OVERRIDE(MANUAL_QUALITY_OVERRIDE),
+  ADD INDEX GEEQ_MANUAL_SUITABILITY_SCORE(MANUAL_SUITABILITY_SCORE),
+  ADD INDEX GEEQ_MANUAL_SUITABILITY_OVERRIDE(MANUAL_SUITABILITY_OVERRIDE);

gemma-core/src/test/java/ubic/gemma/model/expression/experiment/ExpressionExperimentServiceIntegrationTest.java
@@ -367,6 +367,18 @@ public class ExpressionExperimentServiceIntegrationTest extends BaseSpringContex
     }
 
     @Test
+    public void testFilterByGeeqScore() {
+        Filter f = expressionExperimentService.getFilter( "geeq.publicQualityScore", Filter.Operator.greaterOrEq, "0.9" );
+        assertThat( f )
+                .hasFieldOrPropertyWithValue( "objectAlias", null )
+                .hasFieldOrPropertyWithValue( "propertyName", "(case when geeq.manualQualityOverride = true then geeq.manualQualityScore else geeq.detectedQualityScore end)" )
+                .hasFieldOrPropertyWithValue( "requiredValue", 0.9 )
+                .hasFieldOrPropertyWithValue( "originalProperty", "geeq.publicQualityScore" );
+        assertThat( expressionExperimentService.load( Filters.by( f ), null ) )
+                .isEmpty();
+    }
+
+    @Test
     public void testCacheInvalidationWhenACharacteristicIsDeleted() {
         Characteristic c = new Characteristic();
         c.setCategory( "bar" );

commit 326d10ae3310e80323713ee680067fcc19158d1d

Fix bug introduced in 3fb0368fa865a81c79640e70afe28a279a980d5a when replacing non-word chars in param names

gemma-core/src/main/java/ubic/gemma/persistence/util/FilterQueryUtils.java
@@ -174,7 +174,7 @@ public class FilterQueryUtils {
     }
 
     private static String formParamName( PropertyMapping mapping, int i ) {
-        return mapping.getProperty().replaceAll("\\W", "_");
+        return mapping.getProperty().replaceAll( "\\W", "_" ) + i;
     }
 
     private static String escapeLike( String s ) {