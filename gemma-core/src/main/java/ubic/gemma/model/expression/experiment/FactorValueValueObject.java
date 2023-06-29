0757263cc6595f29beb1701a9e3ace9fd18c5fca
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2023-06-29 09:01:39 -0700

    Add a measurement VO to FactorValueValueObject (fix #761)
    Order characteristics by category then value if non-null.
    
    Hide measurement if null, this means the factor is categorical.
    
    Mark both value and summary as deprecated in FactorValueBasicValueObject.

--- gemma-core/src/main/java/ubic/gemma/model/expression/experiment/FactorValueValueObject.java
+++ gemma-core/src/main/java/ubic/gemma/model/expression/experiment/FactorValueValueObject.java
@@ -8,15 +8,18 @@
  */
 package ubic.gemma.model.expression.experiment;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import ubic.gemma.model.IdentifiableValueObject;
 import ubic.gemma.model.common.description.Characteristic;
+import ubic.gemma.model.common.measurement.MeasurementValueObject;
 import ubic.gemma.model.genome.gene.phenotype.valueObject.CharacteristicBasicValueObject;
 
 import javax.annotation.Nullable;
-import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 
 /**
@@ -47,9 +50,13 @@ public class FactorValueValueObject extends IdentifiableValueObject<FactorValue>
      */
     private Long charId;
     private Long factorId;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("measurement")
+    private MeasurementValueObject measurementObject;
     @JsonProperty("isMeasurement")
     private boolean measurement = false;
-    private Collection<CharacteristicBasicValueObject> characteristics;
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private List<CharacteristicBasicValueObject> characteristics;
 
     /**
      * Required when using the class as a spring bean.
@@ -79,6 +86,7 @@ public class FactorValueValueObject extends IdentifiableValueObject<FactorValue>
             this.setMeasurement( true );
             this.value = value.getMeasurement().getValue();
             this.charId = value.getMeasurement().getId();
+            this.measurementObject = new MeasurementValueObject( value.getMeasurement() );
         } else if ( c != null && c.getId() != null ) {
             this.charId = c.getId();
         } else {
@@ -103,6 +111,8 @@ public class FactorValueValueObject extends IdentifiableValueObject<FactorValue>
 
         this.characteristics = value.getCharacteristics().stream()
                 .map( CharacteristicBasicValueObject::new )
+                .sorted( Comparator.comparing( CharacteristicBasicValueObject::getCategory, Comparator.nullsLast( Comparator.naturalOrder() ) )
+                        .thenComparing( CharacteristicBasicValueObject::getValue, Comparator.nullsLast( Comparator.naturalOrder() ) ) )
                 .collect( Collectors.toList() );
     }
 
