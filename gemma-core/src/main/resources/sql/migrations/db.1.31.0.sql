5edafb956790d4cdcd9f9a1cec1f16e1d23b6dfc
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2023-08-28 10:36:25 -0700

    Add foreign keys for object and second object in separate statements

--- gemma-core/src/main/resources/sql/migrations/db.1.31.0.sql
+++ gemma-core/src/main/resources/sql/migrations/db.1.31.0.sql
@@ -3,10 +3,13 @@ alter table CHARACTERISTIC
     add column class                VARCHAR(255) after ID,
     add column PREDICATE            VARCHAR(255) after ORIGINAL_VALUE,
     add column PREDICATE_URI        VARCHAR(255) after PREDICATE,
-    add column OBJECT_FK            BIGINT references CHARACTERISTIC (ID) after PREDICATE_URI,
+    add column OBJECT_FK            BIGINT after PREDICATE_URI,
     add column SECOND_PREDICATE     VARCHAR(255) after OBJECT_FK,
     add column SECOND_PREDICATE_URI VARCHAR(255) after SECOND_PREDICATE,
-    add column SECOND_OBJECT_FK     BIGINT references CHARACTERISTIC (ID) after SECOND_PREDICATE_URI;
+    add column SECOND_OBJECT_FK     BIGINT after SECOND_PREDICATE_URI;
+alter table CHARACTERISTIC
+    add constraint CHARACTERISTIC_OBJECT_FKC foreign key (OBJECT_FK) references CHARACTERISTIC (ID),
+    add constraint CHARACTERISTIC_SECOND_OBJECT_FKC foreign key (SECOND_OBJECT_FK) references CHARACTERISTIC (ID)
 -- all characteristics in FV bags are statements
 update CHARACTERISTIC
 set class = 'Statement'
