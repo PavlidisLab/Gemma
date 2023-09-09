d1c67a09a8abae9c30255cbfbeb48bf59fd63f1e
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2023-09-09 12:38:37 -0700

    Add slow and fast test suites variants

---
+++ gemma-core/src/test/java/ubic/gemma/core/util/test/suite/SlowTests.java
@@ -0,0 +1,18 @@
+package ubic.gemma.core.util.test.suite;
+
+import org.junit.experimental.categories.Categories;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import ubic.gemma.core.util.test.category.SlowTest;
+
+/**
+ * Run all the slow tests.
+ * <p>
+ * If you run this and realize some tests are actually fast, remove their {@link SlowTest} annotation.
+ * @author poirigui
+ */
+@RunWith(Categories.class)
+@Categories.IncludeCategory(SlowTest.class)
+@Suite.SuiteClasses({ AllTests.class })
+public class SlowTests {
+}
