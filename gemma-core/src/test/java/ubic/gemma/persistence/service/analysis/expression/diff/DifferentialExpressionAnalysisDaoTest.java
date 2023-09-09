94a45bd1c2f0f238ad36322a1a44b268e87d4f0b
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2023-09-09 12:06:31 -0700

    Make analysis creation test more realistic
    Create 3 analysis result sets, don't reuse the analysis result.
    
    This setup actually reproduces the update after insert issue outlined in #826.

--- gemma-core/src/test/java/ubic/gemma/persistence/service/analysis/expression/diff/DifferentialExpressionAnalysisDaoTest.java
+++ gemma-core/src/test/java/ubic/gemma/persistence/service/analysis/expression/diff/DifferentialExpressionAnalysisDaoTest.java
@@ -2,11 +2,13 @@ package ubic.gemma.persistence.service.analysis.expression.diff;
 
 import org.hibernate.SessionFactory;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import ubic.gemma.core.util.test.BaseDatabaseTest;
+import ubic.gemma.core.util.test.category.SlowTest;
 import ubic.gemma.model.analysis.expression.diff.*;
 import ubic.gemma.model.expression.arrayDesign.ArrayDesign;
 import ubic.gemma.model.expression.designElement.CompositeSequence;
@@ -32,38 +34,44 @@ public class DifferentialExpressionAnalysisDaoTest extends BaseDatabaseTest {
     private DifferentialExpressionAnalysisDao differentialExpressionAnalysisDao;
 
     @Test
+    @Category(SlowTest.class)
     public void testCreateAnalysisWithResultSetAndPvalueDistribution() {
         DifferentialExpressionAnalysis analysis = new DifferentialExpressionAnalysis();
-        ExpressionAnalysisResultSet resultSet = new ExpressionAnalysisResultSet();
-        PvalueDistribution pvalueDist = new PvalueDistribution();
-        pvalueDist.setNumBins( 2 );
-        pvalueDist.setBinCounts( new byte[2] );
-        DifferentialExpressionAnalysisResult der = new DifferentialExpressionAnalysisResult();
         Taxon taxon = new Taxon();
         sessionFactory.getCurrentSession().persist( taxon );
         ArrayDesign ad = new ArrayDesign();
         ad.setPrimaryTaxon( taxon );
         sessionFactory.getCurrentSession().persist( ad );
-        for ( int i = 0; i < 1000; i++ ) {
-            CompositeSequence cs = new CompositeSequence();
-            cs.setArrayDesign( ad );
-            sessionFactory.getCurrentSession().persist( cs );
-            der.setProbe( cs );
-            der.setResultSet( resultSet );
-            der.getContrasts().add( new ContrastResult() );
-            der.getContrasts().add( new ContrastResult() );
-            resultSet.getResults().add( der );
+        for ( int j = 0; j < 3; j++ ) {
+            ExpressionAnalysisResultSet resultSet = new ExpressionAnalysisResultSet();
+            PvalueDistribution pvalueDist = new PvalueDistribution();
+            pvalueDist.setNumBins( 2 );
+            pvalueDist.setBinCounts( new byte[2] );
+            for ( int i = 0; i < 100; i++ ) {
+                DifferentialExpressionAnalysisResult der = new DifferentialExpressionAnalysisResult();
+                CompositeSequence cs = new CompositeSequence();
+                cs.setArrayDesign( ad );
+                sessionFactory.getCurrentSession().persist( cs );
+                der.setProbe( cs );
+                der.setResultSet( resultSet );
+                der.getContrasts().add( ContrastResult.Factory.newInstance() );
+                der.getContrasts().add( ContrastResult.Factory.newInstance() );
+                resultSet.getResults().add( der );
+            }
+            resultSet.setPvalueDistribution( pvalueDist );
+            analysis.getResultSets().add( resultSet );
         }
-        resultSet.setPvalueDistribution( pvalueDist );
-        analysis.getResultSets().add( resultSet );
         analysis = differentialExpressionAnalysisDao.create( analysis );
+        sessionFactory.getCurrentSession().flush();
         assertNotNull( analysis.getId() );
-        assertNotNull( resultSet.getId() );
-        assertNotNull( pvalueDist.getId() );
-        for ( DifferentialExpressionAnalysisResult result : resultSet.getResults() ) {
-            assertNotNull( result.getId() );
-            for ( ContrastResult contrast : result.getContrasts() ) {
-                assertNotNull( contrast.getId() );
+        for ( ExpressionAnalysisResultSet resultSet : analysis.getResultSets() ) {
+            assertNotNull( resultSet.getId() );
+            assertNotNull( resultSet.getPvalueDistribution().getId() );
+            for ( DifferentialExpressionAnalysisResult result : resultSet.getResults() ) {
+                assertNotNull( result.getId() );
+                for ( ContrastResult contrast : result.getContrasts() ) {
+                    assertNotNull( contrast.getId() );
+                }
             }
         }
     }
