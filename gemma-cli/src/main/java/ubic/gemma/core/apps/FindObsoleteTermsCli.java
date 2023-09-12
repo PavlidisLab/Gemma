1ff925987f957c81f69f33596db4c503fb7cbf0c
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2023-09-12 11:49:13 -0700

    Display which ontologies are still loading if less than 5 are remaining

--- gemma-cli/src/main/java/ubic/gemma/core/apps/FindObsoleteTermsCli.java
+++ gemma-cli/src/main/java/ubic/gemma/core/apps/FindObsoleteTermsCli.java
@@ -11,10 +11,13 @@ import ubic.gemma.core.ontology.OntologyService;
 import ubic.gemma.core.util.AbstractCLI;
 import ubic.gemma.model.genome.gene.phenotype.valueObject.CharacteristicValueObject;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletionService;
 import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 @Component
 public class FindObsoleteTermsCli extends AbstractCLI {
@@ -62,21 +65,28 @@ public class FindObsoleteTermsCli extends AbstractCLI {
             throw new IllegalArgumentException( "Auto-loading of ontologies is enabled, disable it by setting load.ontologies=false in Gemma.properties." );
         }
 
-        log.info( "Warming up ontologies ..." );
+        log.info( String.format( "Warming up %d ontologies ...", ontologies.size() ) );
         CompletionService<ubic.basecode.ontology.providers.OntologyService> completionService = new ExecutorCompletionService<>( ontologyTaskExecutor );
+        Map<ubic.basecode.ontology.providers.OntologyService, Future<ubic.basecode.ontology.providers.OntologyService>> futures = new LinkedHashMap<>();
         for ( ubic.basecode.ontology.providers.OntologyService ontology : ontologies ) {
-            completionService.submit( () -> {
+            futures.put( ontology, completionService.submit( () -> {
                 // we don't need all those features for detecting obsolete terms
                 ontology.setSearchEnabled( false );
                 ontology.setInferenceMode( ubic.basecode.ontology.providers.OntologyService.InferenceMode.NONE );
                 ontology.initialize( true, false );
                 return ontology;
-            } );
+            } ) );
         }
 
         for ( int i = 0; i < ontologies.size(); i++ ) {
             ubic.basecode.ontology.providers.OntologyService os = completionService.take().get();
             log.info( String.format( " === Ontology (%d/%d) warmed up: %s", i + 1, ontologies.size(), os ) );
+            if ( ontologies.size() - i < 4 ) {
+                log.info( "Still loading:\n\t" + futures.entrySet().stream().filter( e -> !e.getValue().isDone() )
+                        .map( Map.Entry::getKey )
+                        .map( ubic.basecode.ontology.providers.OntologyService::toString )
+                        .collect( Collectors.joining( "\n\t" ) ) );
+            }
         }
 
         log.info( "Ontologies warmed up, starting check..." );
