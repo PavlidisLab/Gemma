bd89cbc10a53335f4202c398fedea34cb7b1d70a
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2023-09-12 11:24:24 -0700

    Improve warming up of ontologies
    Disable search and inference that are unnecessary for finding obsolete
    terms.
    
    Use a CompletionService to print out warmed up ontologies as they are
    ready.

--- gemma-cli/src/main/java/ubic/gemma/core/apps/FindObsoleteTermsCli.java
+++ gemma-cli/src/main/java/ubic/gemma/core/apps/FindObsoleteTermsCli.java
@@ -1,24 +1,30 @@
 package ubic.gemma.core.apps;
 
 import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.stereotype.Component;
 import ubic.gemma.core.ontology.OntologyService;
-import ubic.gemma.core.ontology.providers.OntologyServiceFactory;
 import ubic.gemma.core.util.AbstractCLI;
 import ubic.gemma.core.util.AbstractCLIContextCLI;
 import ubic.gemma.model.genome.gene.phenotype.valueObject.CharacteristicValueObject;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.*;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutorCompletionService;
 
+@Component
 public class FindObsoleteTermsCli extends AbstractCLIContextCLI {
 
     @Autowired
     private OntologyService ontologyService;
+
+    @Autowired
+    @Qualifier("ontologyTaskExecutor")
+    private AsyncTaskExecutor ontologyTaskExecutor;
 
     @Autowired
     private List<ubic.basecode.ontology.providers.OntologyService> ontologies;
@@ -51,26 +57,21 @@ public class FindObsoleteTermsCli extends AbstractCLIContextCLI {
     @Override
     protected void doWork() throws Exception {
 
-
         log.info( "Warming up ontologies ..." );
-        ExecutorService executorService = Executors.newFixedThreadPool( 5 );
-        List<Future<ubic.basecode.ontology.providers.OntologyService>> futures = new ArrayList<>();
-
+        CompletionService<ubic.basecode.ontology.providers.OntologyService> completionService = new ExecutorCompletionService<>( ontologyTaskExecutor );
         for ( ubic.basecode.ontology.providers.OntologyService ontology : ontologies ) {
-            Future<ubic.basecode.ontology.providers.OntologyService> future = executorService.submit( () -> {
+            completionService.submit( () -> {
+                // we don't need all those features for detecting obsolete terms
+                ontology.setSearchEnabled( false );
+                ontology.setInferenceMode( ubic.basecode.ontology.providers.OntologyService.InferenceMode.NONE );
                 ontology.initialize( true, false );
                 return ontology;
             } );
-            futures.add( future );
         }
 
-        for ( Future<ubic.basecode.ontology.providers.OntologyService> future : futures ) {
-            try {
-                ubic.basecode.ontology.providers.OntologyService os = future.get();
-                log.info( " === Ontology warmed up: " + os );
-            } catch ( InterruptedException | ExecutionException e ) {
-                e.printStackTrace();
-            }
+        for ( int i = 0; i < ontologies.size(); i++ ) {
+            ubic.basecode.ontology.providers.OntologyService os = completionService.take().get();
+            log.info( String.format( " === Ontology (%d/%d) warmed up: %s", i + 1, ontologies.size(), os ) );
         }
 
         log.info( "Ontologies warmed up, starting check..." );
