9dd8c22f87aa41e61f7f2dd7b307646b42587b97
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2023-09-29 14:46:41 -0700

    Warn if Gemma.properties is readable by group or others (fix #877)

--- gemma-core/src/main/java/ubic/gemma/persistence/util/SettingsConfig.java
+++ gemma-core/src/main/java/ubic/gemma/persistence/util/SettingsConfig.java
@@ -12,9 +12,13 @@ import org.springframework.core.io.support.ResourcePropertySource;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFilePermission;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Beans declaration for making the settings available via the Spring Environment and placeholder substitution.
@@ -59,17 +63,21 @@ public class SettingsConfig {
         boolean userConfigLoaded = false;
         String catalinaBase = System.getenv( "CATALINA_BASE" );
         if ( catalinaBase != null ) {
-            File f = Paths.get( catalinaBase, "Gemma.properties" ).toFile();
+            Path p = Paths.get( catalinaBase, "Gemma.properties" );
+            File f = p.toFile();
             log.debug( "Loading configuration from " + f.getAbsolutePath() + " since $CATALINA_BASE is defined." );
             FileSystemResource r = new FileSystemResource( f );
             if ( !r.exists() ) {
                 throw new RuntimeException( f.getAbsolutePath() + " could not be loaded." );
             }
+            warnIfReadableByGroupOrOthers( p );
             result.add( new ResourcePropertySource( r ) );
             userConfigLoaded = true;
         }
-        FileSystemResource r = new FileSystemResource( Paths.get( System.getProperty( "user.home" ), "Gemma.properties" ).toFile() );
+        Path p = Paths.get( System.getProperty( "user.home" ), "Gemma.properties" );
+        FileSystemResource r = new FileSystemResource( p.toFile() );
         if ( r.exists() ) {
+            warnIfReadableByGroupOrOthers( p );
             result.add( new ResourcePropertySource( r ) );
         } else if ( !userConfigLoaded ) {
             throw new RuntimeException( "Gemma.properties could not be loaded and no other user configuration were supplied." );
@@ -79,5 +87,18 @@ public class SettingsConfig {
         // FIXME: local local.properties from ${gemma.appdata.home}
         result.add( new ResourcePropertySource( new ClassPathResource( "ubic/gemma/version.properties" ) ) );
         return result;
+    }
+
+    private static void warnIfReadableByGroupOrOthers( Path path ) throws IOException {
+        Set<PosixFilePermission> permissions;
+        try {
+            permissions = Files.getPosixFilePermissions( path );
+        } catch ( UnsupportedOperationException e ) {
+            return;
+        }
+        if ( permissions.contains( PosixFilePermission.GROUP_READ ) || permissions.contains( PosixFilePermission.OTHERS_READ ) ) {
+            log.warn( String.format( "%s may contain credentials and is not exclusively readable its owner. Adjust the permissions by running 'chmod go-r %s' to remove this warning.",
+                    path.getFileName(), path.toAbsolutePath() ) );
+        }
     }
 }
