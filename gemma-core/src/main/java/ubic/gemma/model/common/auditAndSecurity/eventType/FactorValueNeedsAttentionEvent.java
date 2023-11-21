f49a769008532384302825149897c9520ebbd6c5
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2023-11-21 12:05:47 -0800

    Add FactorValueNeedsAttentionEvent for compatibity with the 1.31 series

---
+++ gemma-core/src/main/java/ubic/gemma/model/common/auditAndSecurity/eventType/FactorValueNeedsAttentionEvent.java
@@ -0,0 +1,9 @@
+package ubic.gemma.model.common.auditAndSecurity.eventType;
+
+/**
+ * Indicates that a factor value needs attention.
+ * @author poirigui
+ */
+public class FactorValueNeedsAttentionEvent extends NeedsAttentionEvent {
+
+}
