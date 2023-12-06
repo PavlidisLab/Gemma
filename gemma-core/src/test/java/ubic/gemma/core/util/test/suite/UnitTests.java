cd513424b8151aacb569d223335e1fc0bc3bb12c
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2023-12-06 11:18:45 -0800

    Few cleanups for tests
    Simplify test suites declarations.
    
    Rename SpringContextTest category to IntegrationTest for more clarity

--- gemma-core/src/test/java/ubic/gemma/core/util/test/suite/UnitTests.java
+++ gemma-core/src/test/java/ubic/gemma/core/util/test/suite/UnitTests.java
@@ -3,10 +3,10 @@ package ubic.gemma.core.util.test.suite;
 import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import ubic.gemma.core.util.test.category.SpringContextTest;
+import ubic.gemma.core.util.test.category.IntegrationTest;
 
 @RunWith(Categories.class)
-@Categories.ExcludeCategory(SpringContextTest.class)
-@Suite.SuiteClasses({ AllTests.class })
+@Categories.ExcludeCategory(IntegrationTest.class)
+@Suite.SuiteClasses(AllTests.class)
 public class UnitTests {
 }
