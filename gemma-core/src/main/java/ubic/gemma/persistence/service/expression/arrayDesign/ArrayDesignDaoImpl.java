a153d0f6b529059ce3f5b88e2b03c439811453cc
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2024-03-27 23:39:58 -0700

    Limit the number of platforms when populating external references

--- gemma-core/src/main/java/ubic/gemma/persistence/service/expression/arrayDesign/ArrayDesignDaoImpl.java
+++ gemma-core/src/main/java/ubic/gemma/persistence/service/expression/arrayDesign/ArrayDesignDaoImpl.java
@@ -1001,7 +1001,8 @@ public class ArrayDesignDaoImpl extends AbstractCuratableDao<ArrayDesign, ArrayD
         }
         //noinspection unchecked
         List<Object[]> r = getSessionFactory().getCurrentSession()
-                .createQuery( "select ad.id, e from ArrayDesign ad join ad.externalReferences e" )
+                .createQuery( "select ad.id, e from ArrayDesign ad join ad.externalReferences e where ad.id in :ids" )
+                .setParameterList( "ids", optimizeParameterList( EntityUtils.getIds( results ) ) )
                 .setCacheable( true )
                 .list();
         Map<Long, Set<DatabaseEntry>> dbi = r.stream()
