96d354cdd595dd8ecf6c5ee2586a3bf41239efc6
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2024-05-31 16:09:01 -0700

    Update completion scripts
    Add a helper script for generating completions now that it can be done
    without network access.

---
+++ update-completion-scripts.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+project_dir=$(dirname "$0")
+gemma_cli_bin="$project_dir"/gemma-cli/target/appassembler/bin/gemma-cli
+
+if [ ! -f "$gemma_cli_bin"  ]; then
+  echo "The gemma-cli executable does not exist. Run 'mvn package' first."
+  exit 1
+fi
+
+"$gemma_cli_bin" --completion --completion-executable=gemma-cli --completion-shell=bash > "$project_dir"/gemma-cli/src/main/config/bash_completion.d/gemma-cli &
+"$gemma_cli_bin" --completion --completion-executable=gemma-cli --completion-shell=fish > "$project_dir"/gemma-cli/src/main/config/fish/completions/gemma-cli.fish &
+"$gemma_cli_bin" --completion --completion-executable=gemma-cli-staging --completion-shell=fish > "$project_dir"/gemma-cli/src/main/config/fish/completions/gemma-cli-staging.fish &
+wait
