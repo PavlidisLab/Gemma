13e34fda27fb559ac7e188e11468bb46e9941dec
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2024-06-10 10:58:26 -0700

    Fix timezone handling when parsing Maven datetime from buildinfo
    The timezone is represented in UTC.

--- gemma-rest/src/test/java/ubic/gemma/rest/providers/WebApplicationExceptionMapperTest.java
+++ gemma-rest/src/test/java/ubic/gemma/rest/providers/WebApplicationExceptionMapperTest.java
@@ -116,7 +116,7 @@ public class WebApplicationExceptionMapperTest extends JerseyTest {
                 .extracting( "response" )
                 .extracting( "entity" )
                 .asInstanceOf( InstanceOfAssertFactories.INPUT_STREAM )
-                .hasContent( String.format( "{\"error\":{\"code\":400,\"message\":\"test\"},\"apiVersion\":\"%s\",\"buildInfo\":{\"version\":\"1.0.0\",\"timestamp\":\"2024-05-20T11:41:58.000+00:00\",\"gitHash\":\"1234\"}}",
+                .hasContent( String.format( "{\"error\":{\"code\":400,\"message\":\"test\"},\"apiVersion\":\"%s\",\"buildInfo\":{\"version\":\"1.0.0\",\"timestamp\":\"2024-05-20T04:41:58.000+00:00\",\"gitHash\":\"1234\"}}",
                         version ) );
     }
 }
