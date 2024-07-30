ff95d9ebb7ef04a938abd7de89f794936427cfb0
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2024-07-30 10:34:34 -0400

    Unify XML and Java configurations for error handling

--- gemma-web/src/test/java/ubic/gemma/web/util/BaseWebTest.java
+++ gemma-web/src/test/java/ubic/gemma/web/util/BaseWebTest.java
@@ -2,7 +2,6 @@ package ubic.gemma.web.util;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -12,14 +11,8 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.HandlerExceptionResolver;
-import org.springframework.web.servlet.handler.HandlerExceptionResolverComposite;
-import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
-import org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver;
 import ubic.gemma.core.context.EnvironmentProfiles;
 import ubic.gemma.core.util.MailEngine;
-
-import java.util.Arrays;
-import java.util.Properties;
 
 import static org.mockito.Mockito.mock;
 
@@ -35,52 +28,11 @@ public abstract class BaseWebTest extends AbstractJUnit4SpringContextTests {
 
     public abstract static class BaseWebTestContextConfiguration {
 
-        private static final String HANDLED_EXCEPTION_LOGGER_NAME = "ubic.gemma.web.loggers.HandledException";
-
         @Bean
         public HandlerExceptionResolver exceptionResolver() {
-            HandlerExceptionResolverComposite compositeResolver = new HandlerExceptionResolverComposite();
-            compositeResolver.setExceptionResolvers( Arrays.asList( springExceptionResolver(), simpleMappingExceptionResolver(), clientAbortExceptionResolver(), unhandledExceptionResolver() ) );
-            return compositeResolver;
+            return new DefaultHandlerExceptionResolver();
         }
 
-        private DefaultHandlerExceptionResolver springExceptionResolver() {
-            DefaultHandlerExceptionResolver resolver = new DefaultHandlerExceptionResolver();
-            resolver.setWarnLogCategory( HANDLED_EXCEPTION_LOGGER_NAME );
-            return resolver;
-        }
-
-        private SimpleMappingExceptionResolver simpleMappingExceptionResolver() {
-            SimpleMappingExceptionResolver resolver = new SimpleMappingExceptionResolver();
-            resolver.setDefaultErrorView( "error/500" );
-            resolver.addStatusCode( "error/400", 400 );
-            resolver.addStatusCode( "error/403", 403 );
-            resolver.addStatusCode( "error/404", 404 );
-            resolver.addStatusCode( "error/500", 500 );
-            resolver.addStatusCode( "error/503", 503 );
-            Properties mappings = new Properties();
-            mappings.setProperty( AccessDeniedException.class.getName(), "error/403" );
-            mappings.setProperty( EntityNotFoundException.class.getName(), "error/404" );
-            mappings.setProperty( ServiceUnavailableException.class.getName(), "error/503" );
-            mappings.setProperty( IllegalArgumentException.class.getName(), "error/400" );
-            resolver.setExceptionMappings( mappings );
-            resolver.setWarnLogCategory( HANDLED_EXCEPTION_LOGGER_NAME );
-            return resolver;
-        }
-
-        private ClientAbortExceptionResolver clientAbortExceptionResolver() {
-            ClientAbortExceptionResolver resolver = new ClientAbortExceptionResolver();
-            resolver.setWarnLogCategory( HANDLED_EXCEPTION_LOGGER_NAME );
-            return resolver;
-        }
-
-        private UnhandledExceptionResolver unhandledExceptionResolver() {
-            UnhandledExceptionResolver resolver = new UnhandledExceptionResolver();
-            resolver.setErrorCategory( "ubic.gemma.web.loggers.UnhandledException" );
-            resolver.setStatusCode( 500 );
-            resolver.setErrorView( "error/500" );
-            return resolver;
-        }
 
         @Bean
         public MessageUtil messageUtil() {
