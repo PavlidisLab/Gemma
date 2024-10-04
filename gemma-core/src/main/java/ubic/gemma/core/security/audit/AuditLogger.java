1b0b3f8feed4e1f37613d2af3639c7001a297aec
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2024-10-03 17:26:21 -0700

    Use the date from the audit event for logging

--- gemma-core/src/main/java/ubic/gemma/core/security/audit/AuditLogger.java
+++ gemma-core/src/main/java/ubic/gemma/core/security/audit/AuditLogger.java
@@ -2,8 +2,11 @@ package ubic.gemma.core.security.audit;
 
 import lombok.extern.apachecommons.CommonsLog;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.helpers.ISO8601DateFormat;
 import ubic.gemma.model.common.auditAndSecurity.AuditEvent;
 import ubic.gemma.model.common.auditAndSecurity.Auditable;
+
+import java.text.DateFormat;
 
 /**
  * Logger for created {@link AuditEvent}.
@@ -13,17 +16,22 @@ import ubic.gemma.model.common.auditAndSecurity.Auditable;
 public class AuditLogger {
 
     /**
+     * Date format consistent with the logging.
+     */
+    private static final DateFormat dateFormat = new ISO8601DateFormat();
+
+    /**
      * Log a given audit event.
      */
     public void log( Auditable auditable, AuditEvent event ) {
         if ( !log.isInfoEnabled() ) {
             return;
         }
-        log.info( String.format( "%s event%s on entity %s:%d on %s by %s%s%s",
+        log.info( String.format( "%s | %s event%s on entity %s:%d by %s%s%s",
+                dateFormat.format( event.getDate() ),
                 event.getAction(),
                 event.getEventType() != null ? " of type " + event.getEventType().getClass().getName() : "",
                 auditable.getClass(), auditable.getId(),
-                event.getDate(),
                 event.getPerformer() != null ? event.getPerformer().getUserName() : "[anonymous]",
                 StringUtils.isNotBlank( event.getNote() ) ? ": " + event.getNote() : "",
                 StringUtils.isNotBlank( event.getDetail() ) ? "\n" + event.getDetail() : "" ) );
