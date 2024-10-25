20d8912a3e51375b04b4d2e5c7fbeeaeb07c53a6
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2024-10-25 12:51:32 -0700

    rest: Update generated examples

--- gemma-rest/generate-examples.sh
+++ gemma-rest/generate-examples.sh
@@ -2,11 +2,18 @@
 
 project_dir=$(dirname "$0")
 
-http get https://gemma.msl.ubc.ca/rest/v2/datasets/1/data | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-data.tsv &
-http get https://gemma.msl.ubc.ca/rest/v2/datasets/1/data/processed | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-processed-data.tsv &
-http get https://gemma.msl.ubc.ca/rest/v2/datasets/1/data/raw | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-processed-data.tsv &
-# TODO: http get https://gemma.msl.ubc.ca/rest/v2/datasets/GSE199762/data/singleCell | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-single-cell-data.tsv &
-http get https://gemma.msl.ubc.ca/rest/v2/platforms/2/annotations | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/platform-annotations.tsv &
-http get https://gemma.msl.ubc.ca/rest/v2/resultSets/425093 Accept:text/tab-separated-values | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/result-set.tsv &
+# hostname to use for generating files
+gemma_host=gemma.msl.ubc.ca
+# credentials if force-regenerating the files
+gemma_credentials=
+# set to true to force file regeneration, require admin credentials
+force=false
+
+curl -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/datasets/1/data?force=${force}" | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-data.tsv &
+curl -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/datasets/1/data/processed?force=${force}" | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-processed-data.tsv &
+curl -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/datasets/1/data/raw?force=${force} | head" -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-processed-data.tsv &
+curl -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/datasets/GSE199762/data/singleCell?force=${force}" | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-single-cell-data.tsv &
+curl -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/platforms/2/annotations?force=${force}" | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/platform-annotations.tsv &
+curl -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/resultSets/425093" -H Accept:text/tab-separated-values | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/result-set.tsv &
 
 wait
