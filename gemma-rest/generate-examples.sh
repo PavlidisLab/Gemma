96abedc134dbbea3af644782330516983f997e4b
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2024-10-29 10:56:11 -0700

    Improve script for updating REST API docs examples

--- gemma-rest/generate-examples.sh
+++ gemma-rest/generate-examples.sh
@@ -1,13 +1,30 @@
 #!/bin/sh
 
+set -e
+
 project_dir=$(dirname "$0")
 
 # hostname to use for generating files
-gemma_host=gemma.msl.ubc.ca
+gemma_host=${GEMMA_HOST-gemma.msl.ubc.ca}
+
 # credentials if force-regenerating the files
-gemma_credentials=
+if [ -n "$GEMMA_USERNAME" ]; then
+  if [ -n "$GEMMA_PASSWORD" ]; then
+    gemma_credentials="$GEMMA_USERNAME:$GEMMA_PASSWORD"
+  elif [ -n "$GEMMA_PASSWORD_CMD" ]; then
+    gemma_credentials="$GEMMA_USERNAME:$($GEMMA_PASSWORD_CMD)"
+  else
+    # curl will prompt for the password
+    gemma_credentials="$GEMMA_USERNAME"
+  fi
+else
+  echo "GEMMA_USERNAME is not set."
+  exit 1
+fi
 # set to true to force file regeneration, require admin credentials
 force=false
+
+echo "Updating REST API docs examples from $gemma_host..."
 
 curl -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/datasets/1/data?force=${force}" | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-data.tsv &
 curl -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/datasets/1/data/processed?force=${force}" | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-processed-data.tsv &
