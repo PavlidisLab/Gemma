ca1b3135224fd9992a1aa8cd5d2b52e56e90dec2
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2024-10-29 21:49:14 -0700

    rest: Update REST docs examples

--- gemma-rest/generate-examples.sh
+++ gemma-rest/generate-examples.sh
@@ -12,7 +12,7 @@ if [ -n "$GEMMA_USERNAME" ]; then
   if [ -n "$GEMMA_PASSWORD" ]; then
     gemma_credentials="$GEMMA_USERNAME:$GEMMA_PASSWORD"
   elif [ -n "$GEMMA_PASSWORD_CMD" ]; then
-    gemma_credentials="$GEMMA_USERNAME:$($GEMMA_PASSWORD_CMD)"
+    gemma_credentials="$GEMMA_USERNAME:$(eval $GEMMA_PASSWORD_CMD)"
   else
     # curl will prompt for the password
     gemma_credentials="$GEMMA_USERNAME"
@@ -26,12 +26,12 @@ force=false
 
 echo "Updating REST API docs examples from $gemma_host..."
 
-curl -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/datasets/1/data?force=${force}" | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-data.tsv &
-curl -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/datasets/1/data/processed?force=${force}" | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-processed-data.tsv &
-curl -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/datasets/1/data/raw?force=${force} | head" -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-processed-data.tsv &
-curl -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/datasets/1/design?force=${force}" | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-design.tsv &
-curl -u "${gemma_credentials}" --compressed -H Accept:text/tab-separated-values "https://${gemma_host}/rest/v2/datasets/GSE199762/data/singleCell?force=${force}" | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-single-cell-data.tsv &
-curl -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/platforms/2/annotations?force=${force}" | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/platform-annotations.tsv &
-curl -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/resultSets/425093" -H Accept:text/tab-separated-values | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/result-set.tsv &
+curl --show-error --silent -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/datasets/1/data?force=${force}" | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-data.tsv &
+curl --show-error --silent -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/datasets/1/data/processed?force=${force}" | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-processed-data.tsv &
+curl --show-error --silent -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/datasets/1/data/raw?force=${force}" | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-processed-data.tsv &
+curl --show-error --silent -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/datasets/1/design?force=${force}" | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-design.tsv &
+curl --show-error --silent -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/datasets/GSE199762/data/singleCell?force=${force}" -H Accept:text/tab-separated-values | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-single-cell-data.tsv &
+curl --show-error --silent -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/platforms/2/annotations?force=${force}" | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/platform-annotations.tsv &
+curl --show-error --silent -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/resultSets/425093" -H Accept:text/tab-separated-values | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/result-set.tsv &
 
 wait
