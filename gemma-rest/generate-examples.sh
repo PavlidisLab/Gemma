81353cfb3794a0ec2121f16714d5cb0bef541c2f
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2024-11-27 08:50:05 -0800

    Few improvements for scripts
    Use the same requests session for all DWR requests.
    
    Use the GEMMA_HOST environment variable for building URLs.

--- gemma-rest/generate-examples.sh
+++ gemma-rest/generate-examples.sh
@@ -5,14 +5,15 @@ set -e
 project_dir=$(dirname "$0")
 
 # hostname to use for generating files
-gemma_host=${GEMMA_HOST-gemma.msl.ubc.ca}
+gemma_host=${GEMMA_HOST-https://gemma.msl.ubc.ca}
+examples_dir="$project_dir"/src/main/resources/restapidocs/examples
 
 # credentials if force-regenerating the files
 if [ -n "$GEMMA_USERNAME" ]; then
   if [ -n "$GEMMA_PASSWORD" ]; then
     gemma_credentials="$GEMMA_USERNAME:$GEMMA_PASSWORD"
   elif [ -n "$GEMMA_PASSWORD_CMD" ]; then
-    gemma_credentials="$GEMMA_USERNAME:$(eval $GEMMA_PASSWORD_CMD)"
+    gemma_credentials="$GEMMA_USERNAME:$(eval "$GEMMA_PASSWORD_CMD")"
   else
     # curl will prompt for the password
     gemma_credentials="$GEMMA_USERNAME"
@@ -26,12 +27,10 @@ force=false
 
 echo "Updating REST API docs examples from $gemma_host..."
 
-curl --show-error --silent -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/datasets/1/data?force=${force}" | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-data.tsv &
-curl --show-error --silent -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/datasets/1/data/processed?force=${force}" | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-processed-data.tsv &
-curl --show-error --silent -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/datasets/1/data/raw?force=${force}" | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-processed-data.tsv &
-curl --show-error --silent -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/datasets/1/design?force=${force}" | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-design.tsv &
-curl --show-error --silent -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/datasets/GSE199762/data/singleCell?force=${force}" -H Accept:text/tab-separated-values | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/dataset-single-cell-data.tsv &
-curl --show-error --silent -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/platforms/2/annotations?force=${force}" | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/platform-annotations.tsv &
-curl --show-error --silent -u "${gemma_credentials}" --compressed "https://${gemma_host}/rest/v2/resultSets/425093" -H Accept:text/tab-separated-values | head -n 20 > "$project_dir"/src/main/resources/restapidocs/examples/result-set.tsv &
-
-wait
+curl --show-error --silent -u "${gemma_credentials}" --compressed "${gemma_host}/rest/v2/datasets/1/data?force=${force}" | head -n 20 > "$examples_dir"/dataset-data.tsv
+curl --show-error --silent -u "${gemma_credentials}" --compressed "${gemma_host}/rest/v2/datasets/1/data/processed?force=${force}" | head -n 20 > "$examples_dir"/dataset-processed-data.tsv
+curl --show-error --silent -u "${gemma_credentials}" --compressed "${gemma_host}/rest/v2/datasets/1/data/raw?force=${force}" | head -n 20 > "$examples_dir"/dataset-processed-data.tsv
+curl --show-error --silent -u "${gemma_credentials}" --compressed "${gemma_host}/rest/v2/datasets/1/design?force=${force}" | head -n 20 > "$examples_dir"/dataset-design.tsv
+curl --show-error --silent -u "${gemma_credentials}" --compressed "${gemma_host}/rest/v2/datasets/GSE199762/data/singleCell?force=${force}" -H Accept:text/tab-separated-values | head -n 20 > "$examples_dir"/dataset-single-cell-data.tsv
+curl --show-error --silent -u "${gemma_credentials}" --compressed "${gemma_host}/rest/v2/platforms/2/annotations?force=${force}" | head -n 20 > "$examples_dir"//platform-annotations.tsv
+curl --show-error --silent -u "${gemma_credentials}" --compressed "${gemma_host}/rest/v2/resultSets/425093" -H Accept:text/tab-separated-values | head -n 20 > "$examples_dir"/result-set.tsv
--- gemma-web/generate-dwr-client.py
+++ gemma-web/generate-dwr-client.py
@@ -4,85 +4,88 @@
 # This script generates the DWR client so that it can be bundled with the remaining static assets
 #
 
+import os
 import re
-from getpass import getpass
-
 import requests
 from bs4 import BeautifulSoup
+from getpass import getpass
+from os.path import dirname, join
 
-gemma_host_url = 'http://localhost:8080'
+gemma_host_url = os.getenv('GEMMA_HOST') or 'https://gemma.msl.ubc.ca'
 header = "/* this code is generated, see generate-dwr-client.py for details */"
-dwr_script_dir = "src/main/webapp/scripts/api/dwr"
+dwr_script_dir = join(dirname(__file__), "src/main/webapp/scripts/api/dwr")
+session_id = getpass('Supply your JSESSIONID for ' + gemma_host_url + ': ')
 
-res = requests.get('http://localhost:8080/dwr/index.html',
-                   cookies={'JSESSIONID': getpass('Supply your JSESSIONID: ')})
-res.raise_for_status()
-s = BeautifulSoup(res.text, features='html.parser')
-controllers = []
-for l in s.find_all('a'):
-    if l.get('href').startswith('/dwr/test/'):
-        controllers.append(l.get('href')[len('/dwr/test/'):])
-controllers.sort()
+with requests.Session() as session:
+    session.cookies['JSESSIONID'] = session_id
+    res = session.get(gemma_host_url + '/dwr/index.html')
+    res.raise_for_status()
+    s = BeautifulSoup(res.text, features='html.parser')
+    controllers = []
+    for l in s.find_all('a'):
+        if l.get('href').startswith('/dwr/test/'):
+            controllers.append(l.get('href')[len('/dwr/test/'):])
+    controllers.sort()
 
-print(f'Found {len(controllers)} DWR controllers')
+    print(f'Found {len(controllers)} DWR controllers')
 
-with open(dwr_script_dir + '/index.js', 'w') as index:
-    index.write(header + '\n')
+    with open(dwr_script_dir + '/index.js', 'w') as index:
+        index.write(header + '\n')
 
-    with open(dwr_script_dir + '/engine.js', 'w') as f:
-        res = requests.get(gemma_host_url + '/dwr/engine.js')
-        res.raise_for_status()
-        e = res.text
-        e = e.replace("if (dwr == null) ", "")
-        e = e.replace("if (dwr.engine == null) ", "")
-        e = e.replace("if (DWREngine == null) ", "")
-        f.write(header + '\n')
-        f.write(e)
-        f.write('window.dwr = dwr;\n')
-        f.write('window.DWREngine = DWREngine;\n')
-        f.write('export default dwr;\n')
-    index.write("import dwr from './engine'\n")
-    print('Wrote engine.js')
-
-    with open(dwr_script_dir + '/util.js', 'w') as f:
-        res = requests.get(gemma_host_url + '/dwr/util.js')
-        res.raise_for_status()
-        u = res.text
-        u = u.replace("if (dwr == null) var dwr = {};", "")
-        u = u.replace("if (dwr.util == null) ", "")
-        u = u.replace("if (DWRUtil == null) ", "")
-        f.write(header + '\n')
-        f.write(u)
-        f.write("window.DWRUtil = DWRUtil;\n")
-        f.write('export default dwr.util;\n')
-    index.write("import './util'\n")
-    print('Wrote util.js')
-
-    wrote_models = False
-    for controller in controllers:
-        res = requests.get(gemma_host_url + '/dwr/interface/' + controller + '.js')
-        res.raise_for_status()
-        with open(dwr_script_dir + '/interface/' + controller + '.js', 'w') as f:
-            m, c = res.text.split("// Provide a default path to dwr.engine")
-            c = c.split("if (" + controller + " == null) ")[1]
-            if not wrote_models:
-                with open(dwr_script_dir + '/models.js', 'w') as mf:
-                    mf.write(header + "\n")
-                    mf.write(m)
-                    matches = sorted(re.findall(r'function (.+)\(\) \{', m))
-                    for match in matches:
-                        mf.write("window." + match + " = " + match + "\n")
-                    mf.write("module.exports = {" + ', '.join(matches) + "};\n")
-                    print('Wrote models.js')
-                    wrote_models = True
-                index.write("import './models'\n")
+        with open(dwr_script_dir + '/engine.js', 'w') as f:
+            res = session.get(gemma_host_url + '/dwr/engine.js')
+            res.raise_for_status()
+            e = res.text
+            e = e.replace("if (dwr == null) ", "")
+            e = e.replace("if (dwr.engine == null) ", "")
+            e = e.replace("if (DWREngine == null) ", "")
             f.write(header + '\n')
-            f.write(c)
-            f.write("window." + controller + " = " + controller + ";\n")
-            f.write("export default " + controller + ";\n")
-            print('Wrote interface/' + controller + '.js')
-        index.write("import './interface/" + controller + "'\n")
+            f.write(e)
+            f.write('window.dwr = dwr;\n')
+            f.write('window.DWREngine = DWREngine;\n')
+            f.write('export default dwr;\n')
+        index.write("import dwr from './engine'\n")
+        print('Wrote engine.js')
 
-    index.write("import './overrides'\n")
-    index.write("export default dwr;")
-    print('Wrote index.js')
+        with open(dwr_script_dir + '/util.js', 'w') as f:
+            res = session.get(gemma_host_url + '/dwr/util.js')
+            res.raise_for_status()
+            u = res.text
+            u = u.replace("if (dwr == null) var dwr = {};", "")
+            u = u.replace("if (dwr.util == null) ", "")
+            u = u.replace("if (DWRUtil == null) ", "")
+            f.write(header + '\n')
+            f.write(u)
+            f.write("window.DWRUtil = DWRUtil;\n")
+            f.write('export default dwr.util;\n')
+        index.write("import './util'\n")
+        print('Wrote util.js')
+
+        wrote_models = False
+        for controller in controllers:
+            res = session.get(gemma_host_url + '/dwr/interface/' + controller + '.js')
+            res.raise_for_status()
+            with open(dwr_script_dir + '/interface/' + controller + '.js', 'w') as f:
+                m, c = res.text.split("// Provide a default path to dwr.engine")
+                c = c.split("if (" + controller + " == null) ")[1]
+                if not wrote_models:
+                    with open(dwr_script_dir + '/models.js', 'w') as mf:
+                        mf.write(header + "\n")
+                        mf.write(m)
+                        matches = sorted(re.findall(r'function (.+)\(\) \{', m))
+                        for match in matches:
+                            mf.write("window." + match + " = " + match + "\n")
+                        mf.write("module.exports = {" + ', '.join(matches) + "};\n")
+                        print('Wrote models.js')
+                        wrote_models = True
+                    index.write("import './models'\n")
+                f.write(header + '\n')
+                f.write(c)
+                f.write("window." + controller + " = " + controller + ";\n")
+                f.write("export default " + controller + ";\n")
+                print('Wrote interface/' + controller + '.js')
+            index.write("import './interface/" + controller + "'\n")
+
+        index.write("import './overrides'\n")
+        index.write("export default dwr;")
+        print('Wrote index.js')
