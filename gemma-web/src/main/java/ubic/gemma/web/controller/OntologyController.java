830ff722c66c0520b0aa6ededeb4e4e8b105a5ce
OganM <ogan.mancarcii@gmail.com>
2025-01-30 16:02:31 -0800

    rebase

--- gemma-web/src/main/java/ubic/gemma/web/controller/OntologyController.java
+++ gemma-web/src/main/java/ubic/gemma/web/controller/OntologyController.java
@@ -16,12 +16,14 @@ import ubic.gemma.core.ontology.FactorValueOntologyService;
 import ubic.gemma.core.ontology.providers.GemmaOntologyService;
 import ubic.gemma.web.util.EntityNotFoundException;
 import ubic.gemma.web.util.ServiceUnavailableException;
+import ubic.basecode.ontology.model.AnnotationProperty;
 
 import java.io.StringWriter;
 import java.util.List;
 import java.util.Optional;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
+import java.util.Arrays;
 
 import static org.apache.commons.text.StringEscapeUtils.escapeHtml4;
 
@@ -49,7 +51,11 @@ public class OntologyController {
     @Autowired
     private FactorValueOntologyService factorValueOntologyService;
 
-    @RequestMapping(value = "/TGEMO.OWL", method = { RequestMethod.GET, RequestMethod.HEAD })
+
+    @Autowired
+    private ServletContext servletContext;
+
+    @RequestMapping(value = "/{tgemo:TGEMO|TGEMO.OWL}", method = { RequestMethod.GET, RequestMethod.HEAD })
     public RedirectView getOntology() {
         String gemmaOntologyUrl = gemmaOntologyService.getOntologyUrl();
         RedirectView redirectView = new RedirectView( gemmaOntologyUrl );
@@ -57,8 +63,9 @@ public class OntologyController {
         return redirectView;
     }
 
-    @RequestMapping(value = "/{termId}", method = { RequestMethod.GET, RequestMethod.HEAD })
-    public RedirectView getTerm( @PathVariable("termId") String termId ) {
+    @ResponseBody
+    @RequestMapping(value = "/{termId:TGEMO_.*}", method = { RequestMethod.GET, RequestMethod.HEAD }, produces = { MediaType.TEXT_HTML_VALUE, "application/rdf+xml" })
+    public String getTerm( @PathVariable("termId") String termId ) {
         if ( !gemmaOntologyService.isOntologyLoaded() ) {
             throw new ServiceUnavailableException( "TGEMO is not loaded." );
         }
@@ -67,14 +74,36 @@ public class OntologyController {
         if ( term == null ) {
             throw new EntityNotFoundException( String.format( "No term with IRI %s in TGEMO.", iri ) );
         }
-        String gemmaOntologyUrl = gemmaOntologyService.getOntologyUrl();
-        String urlWithFragment = UriComponentsBuilder.fromHttpUrl( gemmaOntologyUrl )
-                .fragment( iri )
-                .build()
-                .toUriString();
-        RedirectView redirectView = new RedirectView( urlWithFragment );
-        redirectView.setStatusCode( HttpStatus.FOUND );
-        return redirectView;
+
+        StringBuilder s = new StringBuilder( String.format( "<title>%s</title>", escapeHtml4( term.getLabel() ) ) +
+                "<div class=\"padded\"><ul style=\"padding-bottom:0.5em\">" +
+                String.format( "<h2 style=\"padding-bottom:0.5em\">%s: %s</h2>",
+                        escapeHtml4( term.getLocalName() ),
+                        escapeHtml4( term.getLabel() ) ));
+                if ( term.getUri() != null) {
+                    s.append(String.format( "<li><a href=%s>%s</a></li>",
+                            escapeHtml4(term.getUri()
+                                    .replaceFirst( "^" + Pattern.quote( TGEMO_URI_PREFIX ), servletContext.getContextPath() + "/ont/" ) ) ,
+                            escapeHtml4( TGEMO_URI_PREFIX + term.getLocalName() ) ) );
+                }
+
+
+        for (AnnotationProperty annot: term.getAnnotations()) {
+            if (Arrays.asList("hasDefinition","definition").contains(annot.getProperty()) ){
+                s.append( "<li>" )
+                        .append( annot.getContents() )
+                        .append( "</li>" );
+            }
+        }
+
+
+        s.append( "</ul>" );
+        s.append( String.format("<p>Ontology IRI: <a href=%s>%s</a></p>",
+                servletContext.getContextPath() + "/ont/TGEMO",
+                TGEMO_URI_PREFIX + "TGEMO" ) );
+        s.append( "</div>" );
+        return s.toString();
+
     }
 
     @ResponseBody
