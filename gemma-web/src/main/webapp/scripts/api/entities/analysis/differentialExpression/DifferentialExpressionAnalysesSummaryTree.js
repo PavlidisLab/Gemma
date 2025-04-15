27a4066bea5281af6299152ccf4432fe3466feaf
OganM <ogan.mancarcii@gmail.com>
2025-04-15 15:50:44 -0700

    shorten the differential expression label, link paper

--- gemma-web/src/main/webapp/scripts/api/entities/analysis/differentialExpression/DifferentialExpressionAnalysesSummaryTree.js
+++ gemma-web/src/main/webapp/scripts/api/entities/analysis/differentialExpression/DifferentialExpressionAnalysesSummaryTree.js
@@ -250,12 +250,11 @@ Gemma.DifferentialExpressionAnalysesSummaryTree = Ext
                         }
                     }
 
-                    var analysisDesc = this.getANOVAtypeText(numberOfFactors);
-                    if (numberOfFactors === 1 || interaction <= 0) {
-                        analysisDesc += ' on ';
-                    } else {
-                        analysisDesc += ' with interactions on ';
+                    var analysisDesc = "";
+                    if( numberOfFactors !== 1 && interaction >0 ){
+                        analysisDesc += " Interactions on "
                     }
+
 
                     var deleteText = '';
                     var redoText = '';
@@ -294,8 +293,8 @@ Gemma.DifferentialExpressionAnalysesSummaryTree = Ext
                     subsetText = '<span ext:qtip="Analysis was run by subsetting the data on the factor '
                         + subsetFactor.category + " (" + subsetFactor.description
                         + ") and selecting samples where the value was \'" + subsetFactorValue.factorValue + '\'">'
-                        + " using a subset of the data (" + subsetFactor.category + " = " + analysis.subsetFactorValue.factorValue
-                        + ')</span>';
+                        + " on subset: <b>" + subsetFactor.category + " = " + analysis.subsetFactorValue.factorValue
+                        + '</b></span>';
                 }
                 return subsetText;
             },
@@ -307,34 +306,6 @@ Gemma.DifferentialExpressionAnalysesSummaryTree = Ext
                         + " onClick='Gemma.ExpressionExperimentDataFetch.fetchDiffExpressionData({0})' > " +
                         "<i class='gray-blue fa fa-cloud-download fa-lg' ext:qtip='Download all differential expression data for this analysis in a tab-delimited format'></i></span>",
                         analysis.id);
-            },
-            getANOVAtypeText: function (numberOfFactors) {
-
-                var analysisDesc = '';
-                // just being overly safe here
-                switch (numberOfFactors) {
-                    case 1:
-                        analysisDesc = 'One-way ANOVA';
-                        break;
-                    case 2:
-                        analysisDesc = 'Two-way ANOVA';
-                        break;
-                    case 3:
-                        analysisDesc = 'Three-way ANOVA';
-                        break;
-                    case 4:
-                        analysisDesc = 'Four-way ANOVA';
-                        break;
-                    case 5:
-                        analysisDesc = 'Five-way ANOVA';
-                        break;
-                    case 6:
-                        analysisDesc = 'Six-way ANOVA';
-                        break;
-                    default:
-                        analysisDesc = 'n-way ANOVA';
-                }
-                return analysisDesc;
             },
             /**
              * get the number of probes that are differentially expressed and the number of 'up' and 'down' probes
@@ -520,13 +491,13 @@ Gemma.DifferentialExpressionAnalysesSummaryTree = Ext
                         var ef = resultSet.experimentalFactors[k];
 
                         if (ef.type === 'continuous') {
-                            factorText = factorText + ef.name + ': ' + ef.description;
+                            factorText = factorText + '<b>' + ef.name  + "</b>" + ': ' + ef.description;
                         } else {
                             if ( k > 0 && k < resultSet.experimentalFactors.length ) {
                                 factorText = factorText + "&nbsp;X&nbsp;";
                                 interaction = interaction + 1;
                             }
-                            factorText = factorText + ef.name;
+                            factorText = factorText + '<b>' + ef.name + "</b>";
                         }
                     }
                 }
