ebb961fcc6d736710642cc5f33303102d5652fc7
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2025-06-09 10:14:12 -0700

    Fix rank calculation when repeated values are present
    Also, apply rank-based filtering for any quantile-normalize or
    batch-corrected data.

---
+++ gemma-core/src/main/java/ubic/gemma/core/util/ArrayUtils.java
@@ -0,0 +1,41 @@
+package ubic.gemma.core.util;
+
+/**
+ * Utilities and algorithms for arrays.
+ * @author poirigui
+ */
+public class ArrayUtils {
+
+    /**
+     * A binary search that returns the index of the first occurrence of the value in the array.
+     * @see java.util.Arrays#binarySearch(double[], double)
+     */
+    public static int binarySearchFirst( double[] array, double value ) {
+        int low = 0;
+        int high = array.length;
+        while ( true ) {
+            // special case for insertion point at the end of the array
+            if ( low == array.length ) {
+                return -array.length - 1;
+            }
+            int mid = ( low + high ) / 2;
+            double midVal = array[mid];
+            if ( low == high ) {
+                if ( midVal == value ) {
+                    return mid;
+                } else {
+                    return -mid - 1;
+                }
+            } else if ( midVal < value ) {
+                low = mid + 1; // Search in the right half
+                assert low <= high;
+            } else if ( midVal > value ) {
+                high = mid - 1; // Search in the left half
+                assert high >= low;
+            } else {
+                high = mid;
+                assert high >= low;
+            }
+        }
+    }
+}
