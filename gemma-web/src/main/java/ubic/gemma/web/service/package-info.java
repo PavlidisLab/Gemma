e137624462a598da64fc51774e6afac0148515a0
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2025-07-30 17:23:06 -0700

    Avoid multiple transactions when retrieving EE details
    Relocate all the logic for fetching EE details in a helper service such
    that everything can be retrieved in a single transaction (and thus
    database connection).

---
+++ gemma-web/src/main/java/ubic/gemma/web/service/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains services specific to the Gemma Web application.
+ * @author poirigui
+ */
+@ParametersAreNonnullByDefault
+package ubic.gemma.web.service;
+
+import javax.annotation.ParametersAreNonnullByDefault;
