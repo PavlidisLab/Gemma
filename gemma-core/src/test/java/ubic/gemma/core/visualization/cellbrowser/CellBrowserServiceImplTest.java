e8471be965decda547726da4dbd6267469cee2c5
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2025-07-31 13:55:34 -0700

    Fix CellBrowserServiceImplTest

--- gemma-core/src/test/java/ubic/gemma/core/visualization/cellbrowser/CellBrowserServiceImplTest.java
+++ gemma-core/src/test/java/ubic/gemma/core/visualization/cellbrowser/CellBrowserServiceImplTest.java
@@ -5,13 +5,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.test.context.ContextConfiguration;
 import ubic.gemma.core.context.TestComponent;
 import ubic.gemma.core.util.test.BaseTest;
 import ubic.gemma.core.util.test.TestPropertyPlaceholderConfigurer;
 import ubic.gemma.model.expression.experiment.ExpressionExperiment;
+import ubic.gemma.persistence.service.expression.experiment.SingleCellExpressionExperimentService;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 
 @ContextConfiguration
 public class CellBrowserServiceImplTest extends BaseTest {
@@ -22,12 +29,24 @@ public class CellBrowserServiceImplTest extends BaseTest {
 
         @Bean
         public static PropertyPlaceholderConfigurer placeholderConfigurer() {
-            return new TestPropertyPlaceholderConfigurer( "gemma.cellBrowser.baseUrl=http://localhost:8080/cellbrowser" );
+            return new TestPropertyPlaceholderConfigurer( "gemma.cellBrowser.baseUrl=http://localhost:8080/cellbrowser", "gemma.cellBrowser.dir=/tmp/cellBrowser" );
+        }
+
+        @Bean
+        public ConversionService conversionService() {
+            DefaultFormattingConversionService service = new DefaultFormattingConversionService();
+            service.addConverter( String.class, Path.class, source -> Paths.get( ( String ) source ) );
+            return service;
         }
 
         @Bean
         public CellBrowserService cellBrowserService() {
             return new CellBrowserServiceImpl();
+        }
+
+        @Bean
+        public SingleCellExpressionExperimentService singleCellExpressionExperimentService() {
+            return mock();
         }
     }
 
