048cee2bea6236a3516d95e801eee62a4ee7886b
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2025-08-20 14:17:56 -0700

    Fix Converter signature with latest release of commons-cli

--- gemma-cli/src/main/java/ubic/gemma/cli/util/EnumeratedByCommandConverter.java
+++ gemma-cli/src/main/java/ubic/gemma/cli/util/EnumeratedByCommandConverter.java
@@ -6,7 +6,7 @@ import org.apache.commons.cli.Converter;
  * This interface is implemented by {@link Converter} that can use an external command to get possible values.
  * @author poirigui
  */
-public interface EnumeratedByCommandConverter<T, E extends Throwable> extends Converter<T, E> {
+public interface EnumeratedByCommandConverter<T, E extends Exception> extends Converter<T, E> {
 
     /**
      * Command that can be used to get possible values.
