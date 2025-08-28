caff304426661e3c1556ff80cf98cf77b207bc46
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2025-08-28 12:47:12 -0700

    Add missing StandardArgNames

---
+++ gemma-cli/src/main/java/ubic/gemma/cli/util/StandardArgNames.java
@@ -0,0 +1,9 @@
+package ubic.gemma.cli.util;
+
+public class StandardArgNames {
+
+    public static final String
+            FILE = "file",
+            DIR = "dir",
+            DATE = "date";
+}
