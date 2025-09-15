87dd0602559658e6f01eeccfd3b576aad67994bc
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2025-09-14 18:42:20 -0700

    Fix migration query for backfilling batch and cell type factors

--- gemma-core/src/main/resources/sql/migrations/db.1.33.0.sql
+++ gemma-core/src/main/resources/sql/migrations/db.1.33.0.sql
@@ -1,6 +1,7 @@
 alter table EXPERIMENTAL_FACTOR
     add column IS_AUTO_GENERATED tinyint not null default false;
 update EXPERIMENTAL_FACTOR
+    join CHARACTERISTIC C on C.ID = EXPERIMENTAL_FACTOR.CATEGORY_FK = C.ID
 set IS_AUTO_GENERATED = true
-where CATEGORY = 'batch'
-   or CATEGORY = 'cell type';
+where C.CATEGORY = 'batch'
+   or C.CATEGORY = 'cell type';
