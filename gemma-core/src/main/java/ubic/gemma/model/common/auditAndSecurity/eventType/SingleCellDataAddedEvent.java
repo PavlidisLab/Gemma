9a4a876887ef887e587a658fd695778a20ab15de
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2025-10-09 13:46:10 -0700

    Proactively add event types from #1529

---
+++ gemma-core/src/main/java/ubic/gemma/model/common/auditAndSecurity/eventType/SingleCellDataAddedEvent.java
@@ -0,0 +1,9 @@
+package ubic.gemma.model.common.auditAndSecurity.eventType;
+
+/**
+ * Special case of {@link DataAddedEvent} for single-cell data.
+ *
+ * @author poirigui
+ */
+public class SingleCellDataAddedEvent extends DataAddedEvent {
+}
