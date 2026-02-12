05f85dbba6edc8a1b7bb60885b74b2686bcd6d5d
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2026-02-11 16:03:39 -0800

    Add an option to deploy Wiki with sync instead of copy

--- gemma-cli/deploy-wiki.sh
+++ gemma-cli/deploy-wiki.sh
@@ -10,6 +10,8 @@
 # Environment variables:
 # GEMMA_CLI_WIKI_PAGE_SUFFIX:  Suffix to append to each generated wiki page (defaults to " (generated)").
 # GEMMA_CLI_WIKI_DEPLOY_TOKEN: Token to use for deploying
+# GEMMA_CLI_WIKI_DEPLOY_SYNC:  If set, the deployment will be done using rclone sync instead of copy, which will delete
+#                              any files that are not present in the source directory. Use with caution.
 #
 
 set -e
@@ -32,6 +34,12 @@ if [ -n "$GEMMA_CLI_WIKI_DEPLOY_TOKEN" ]; then
   echo "Using provided deployment token."
   export RCLONE_WEBDAV_BEARER_TOKEN="$GEMMA_CLI_WIKI_DEPLOY_TOKEN"
 fi
+if [ -n "$GEMMA_CLI_WIKI_DEPLOY_SYNC" ]; then
+  echo "Using rclone sync for deployment. This will delete any files in the destination that are not present in the source directory."
+  rclone_command="sync"
+else
+  rclone_command="copy"
+fi
 wiki_dest="$1"
 
 if [ -n "$(git -C "$project_dir" status --porcelain --untracked-files=no)" ]; then
@@ -48,6 +56,6 @@ echo "Generating Gemma CLI Wiki pages under $gemma_cli_wiki_dir..."
 ./gemma-cli/target/appassembler/bin/gemma-cli --completion --completion-wiki --completion-wiki-output-dir "$gemma_cli_wiki_dir" --completion-wiki-page-suffix "$gemma_cli_wiki_page_suffix"
 
 echo "Deploying Gemma CLI Wiki to $wiki_dest..."
-rclone copy "$gemma_cli_wiki_dir/List of Gemma CLI Tools$gemma_cli_wiki_page_suffix/" "$wiki_dest" --exclude '@*/**' --exclude '*.url'
+rclone "$rclone_command" "$gemma_cli_wiki_dir/List of Gemma CLI Tools$gemma_cli_wiki_page_suffix/" "$wiki_dest" --exclude '@*/**' --exclude '*.url'
 
 echo "Deployment completed!"
